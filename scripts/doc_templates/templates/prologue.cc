d9990522197a86d673c0cdb249078ceaed6ce5c7
Earl Schwab <eschwab.esmf@gmail.com>
2002-07-25 17:04:38 +0000

    new file for C++ headers processed by protex

---
+++ scripts/doc_templates/templates/prologue.cc
@@ -0,0 +1,114 @@
+% $Id: prologue.cc,v 1.1 2002/07/25 17:04:38 eschwab Exp $
+
+//
+// ProTeX sample C++ source: Style 1
+//
+// Last update: 10Jun02 Earl Schwab
+
+//
+// Use this for your C++ class definition
+//
+
+//-------------------------------------------------------------------------
+//         NASA/NCAR, Scientific Computing Division CSS
+//-------------------------------------------------------------------------
+//BOP
+//
+// !CLASS: 
+// 
+// !SUPERCLASSES: 
+// 
+// !AGGREGATE CLASSES: 
+// 
+// !ASSOCIATE CLASSES: 
+// 
+// !FRIEND CLASSES: 
+// 
+// !PUBLIC DATA MEMBERS:
+//
+// !PUBLIC MEMBER FUNCTIONS:
+//
+// !DESCRIPTION: 
+//
+// !BUGS:  
+//
+// !SEE ALSO: 
+//
+// !REVISION HISTORY: 
+//
+//  27Jun97   Jim Becile  Initial code.
+//
+//EOP
+//-------------------------------------------------------------------------
+
+// 
+// Use this for your C++ class methods
+// 
+
+//-------------------------------------------------------------------------
+//         NASA/NCAR, Scientific Computing Division CS
+//-------------------------------------------------------------------------
+//BOP
+//
+// !METHOD:  
+// 
+// !INTERFACE:
+//
+// !INPUT PARAMETERS: 
+//
+// !INPUT/OUTPUT PARAMETERS: 
+//
+// !OUTPUT PARAMETERS:
+//
+// !DESCRIPTION: 
+//
+// !SYSTEM ROUTINES: 
+//
+// !FILES USED:  
+//
+// !BUGS:  
+//
+// !SEE ALSO: 
+//
+// !REVISION HISTORY: 
+//
+//  27Jun97   Jim Becile  Initial code.
+//
+//EOP
+//-------------------------------------------------------------------------
+
+//
+// Use !FUNCTION for your non-OO subroutines/functions (i.e. not methods)
+//
+
+//-------------------------------------------------------------------------
+//         NASA/NCAR, Scientific Computing Division CSS
+//-------------------------------------------------------------------------
+//BOP
+//
+// !FUNCTION:  
+// 
+// !INTERFACE:
+//
+// !INPUT PARAMETERS: 
+//
+// !INPUT/OUTPUT PARAMETERS: 
+//
+// !OUTPUT PARAMETERS:
+//
+// !DESCRIPTION: 
+//
+// !SYSTEM ROUTINES: 
+//
+// !FILES USED:  
+//
+// !BUGS:  
+//
+// !SEE ALSO: 
+//
+// !REVISION HISTORY: 
+//
+//  27Jun97   Jim Becile  Initial code.
+// 
+//EOP
+//-------------------------------------------------------------------------
