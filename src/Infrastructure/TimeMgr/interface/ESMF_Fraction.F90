412fe5f68f55a596f881c1fdd5335948439766ab
Silverio Vasquez <silverio.vasquez@noaa.gov>
2002-10-28 16:54:38 +0000

    Added header to file

--- src/Infrastructure/TimeMgr/interface/ESMF_Fraction.F90
+++ src/Infrastructure/TimeMgr/interface/ESMF_Fraction.F90
@@ -1,12 +1,46 @@
-! $Id: ESMF_Fraction.F90,v 1.1 2002/10/07 20:23:35 eschwab Exp $
+! $Id: ESMF_Fraction.F90,v 1.2 2002/10/28 16:54:38 svasquez Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+! ESMF Fraction Module
+!
+! (all lines below between the !BOP and !EOP markers will be included in
+!  the automated document processing.)
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+! put any constants or macros which apply to the whole component in this
+!  include file.  anything public or esmf-wide should be up higher at
+!  the top level include files.
+
+#include <ESMF_TimeMgr.h>
+
+!------------------------------------------------------------------------------
+! module definition
+
       module ESMF_FractionMod
 !===============================================================================
 !BOP
 !
 ! !MODULE: ESMF_FractionMod
 !
+! !DESCRIPTION:
+!
+!
+!
+!
+!
+!
 ! !USES:
 		use ESMF_TypesMod
+!------------------------------------------------------------------------------
+
 !
 ! !PUBLIC TYPES:
         implicit none
@@ -30,7 +64,11 @@
 !  09Aug02   Earl Schwab  Initial code.
 !
 
+!------------------------------------------------------------------------------
+
 	!contains
+
+!------------------------------------------------------------------------------
 
 	! wrappers to C++ fraction routines
 
