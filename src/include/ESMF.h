85191116e2ce67b3c529dfd975cda4461091f43e
Nancy Collins <nancy@ucar.edu>
2002-10-28 23:56:46 +0000

    main fortran header file

---
+++ src/include/ESMF.h
@@ -0,0 +1,16 @@
+#if 0
+// $Id: ESMF.h,v 1.1 2002/10/28 23:56:46 nscollins Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+//
+// main include file which includes all others
+#endif
+
+#include "ESMF_Arch.inc"
+#include "ESMF_Macros.inc"
