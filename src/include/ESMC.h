5ba18cb4efcce01ef956bf1149f7e586326dd36f
Nancy Collins <nancy@ucar.edu>
2002-10-28 23:56:30 +0000

    include arch.inc instead of arch.h

--- src/include/ESMC.h
+++ src/include/ESMC.h
@@ -1,4 +1,4 @@
-// $Id: ESMC.h,v 1.1 2002/10/28 23:32:44 nscollins Exp $
+// $Id: ESMC.h,v 1.2 2002/10/28 23:56:30 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -10,6 +10,6 @@
 //
 // main include file which includes all others
 
-#include "ESMC_Arch.h"
+#include "ESMC_Arch.inc"
 #include "ESMF_Macros.inc"
 #include "ESMC_Types.h"
