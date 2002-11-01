272af38b45a6e780f52ecaaa7aae7f633b9c3484
John Wolfe <jwolfe@ucar.edu>
2002-11-01 17:53:40 +0000

    Generated Grid comp with templates.

---
+++ src/Infrastructure/Grid/include/ESMC_Grid.h
@@ -0,0 +1,56 @@
+// $Id: ESMC_Grid.h,v 1.1 2002/11/01 17:53:40 jwolfe Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+
+// ESMC Grid include file for C++
+
+// these lines prevent this file from being read more than once if it
+// ends up being included multiple times
+
+#ifndef ESMC_Grid_H
+#define ESMC_Grid_H
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+// The code in this file implements constants and macros for the Grid...
+//
+// < insert a paragraph or two explaining what you'll find in this file >
+//
+// (all lines below between the !BOP and !EOP markers will be included in 
+//  the automated document processing.)
+//
+//-----------------------------------------------------------------------------
+//
+
+
+// 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section contains ...
+//
+//
+
+#define ESMC_GridMACRO1(arg1, arg2)  foo(arg1, arg2)
+
+
+// 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section contains ...
+//
+//
+
+#define ESMC_GridCONSTANT_FRED   23
+
+
+#endif  // ESMC_Grid_H
---
+++ src/Infrastructure/Grid/include/ESMF_Grid.h
@@ -0,0 +1,56 @@
+! $Id: ESMF_Grid.h,v 1.1 2002/11/01 17:54:06 jwolfe Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+! ESMF Grid include file for F90
+
+! these lines prevent this file from being read more than once if it
+! ends up being included multiple times
+
+#ifndef ESMF_Grid_H
+#define ESMF_Grid_H
+
+!------------------------------------------------------------------------------
+!
+! !DESCRIPTION:
+!
+! The code in this file implements constants and macros for the Grid...
+!
+! < insert a paragraph or two explaining what you'll find in this file >
+!
+! (all lines below between the !BOP and !EOP markers will be included in 
+!  the automated document processing.)
+!
+!------------------------------------------------------------------------------
+!
+
+
+! 
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!
+! This section contains ...
+!
+!
+
+#define ESMF_GridMACRO1(arg1, arg2)  foo(arg1, arg2)
+
+
+! 
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!
+! This section contains ...
+!
+!
+
+#define ESMF_GridCONSTANT_FRED   23
+
+
+#endif  // ESMF_Grid_H
