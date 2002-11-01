05fe36c52a10c94d7d05c93a8469e5b3acfd667c
John Wolfe <jwolfe@ucar.edu>
2002-11-01 18:23:10 +0000

    Committing files generated automatically by component scripts.

---
+++ src/Infrastructure/DistGrid/include/ESMC_DistGrid.h
@@ -0,0 +1,56 @@
+// $Id: ESMC_DistGrid.h,v 1.1 2002/11/01 18:26:55 jwolfe Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+
+// ESMC DistGrid include file for C++
+
+// these lines prevent this file from being read more than once if it
+// ends up being included multiple times
+
+#ifndef ESMC_DistGrid_H
+#define ESMC_DistGrid_H
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+// The code in this file implements constants and macros for the DistGrid...
+//
+// < insert a paragraph or two explaining what you'll find in this file >
+//
+// (all lines below between the !BOP and !EOP markers will be included in 
+//  the automated document processing.)
+//
+//-----------------------------------------------------------------------------
+//
+
+
+// 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section contains ...
+//
+//
+
+#define ESMC_DistGridMACRO1(arg1, arg2)  foo(arg1, arg2)
+
+
+// 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section contains ...
+//
+//
+
+#define ESMC_DistGridCONSTANT_FRED   23
+
+
+#endif  // ESMC_DistGrid_H
