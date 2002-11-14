d184d2fc4b894489b50e26cf300617b81d6af5f5
John Wolfe <jwolfe@ucar.edu>
2002-11-14 23:43:03 +0000

    v

---
+++ src/Infrastructure/TimeMgmt/ESMF_TimeMgmtMod.f
@@ -0,0 +1,36 @@
+! $Id: ESMF_TimeMgmtMod.f,v 1.1 2002/11/14 23:44:23 jwolfe Exp $
+      module ESMF_TimeMgmtMod
+!===============================================================================
+!BOP
+! !MODULE: ESMF_TimeMgmtMod
+!
+! !USES:
+      use ESMF_BasicUtilMod
+      use ESMF_ErrorMod
+      use ESMF_TimeMod
+      use ESMF_DateMod
+      use ESMF_TimeMgrMod
+      use ESMF_AlarmMod
+!
+! !DESCRIPTION:
+! Contains all of the time Management utilities.
+!EOP
+!===============================================================================
+
+      implicit none
+
+
+      end module ESMF_TimeMgmtMod
+
+
+
+
+
+
+
+
+
+
+
+
+
