c77e9992df4a435b322a0e38e568130321fdec78
John Wolfe <jwolfe@ucar.edu>
2002-12-13 18:52:27 +0000

    Adding some coding to start internally creating a simple 2-D grid.

--- src/Infrastructure/PhysGrid/src/ESMF_PhysGrid.F90
+++ src/Infrastructure/PhysGrid/src/ESMF_PhysGrid.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_PhysGrid.F90,v 1.9 2002/12/09 16:57:02 jwolfe Exp $
+! $Id: ESMF_PhysGrid.F90,v 1.10 2002/12/13 18:52:27 jwolfe Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -70,9 +70,13 @@
         integer :: num_region_ids        ! a counter for the number of
                                          ! region identifiers for this
                                          ! subgrid
-        real :: global_min_coord1        ! coordinate extents in first coord
+        integer :: global_max_i          ! cell count extents in 1st coord
+                                         ! direction
+        integer :: global_max_j          ! cell count extents in 2nd coord
+                                         ! direction
+        real :: global_min_coord1        ! coordinate extents in 1st coord
         real :: global_max_coord1        !    direction
-        real :: global_min_coord2        ! coordinate extents in second coord
+        real :: global_min_coord2        ! coordinate extents in 2nd coord
         real :: global_max_coord2        !    direction
         type (ESMF_Array) :: center_coord1   ! coord of center of
                                              ! each cell in 1st direction
@@ -111,7 +115,7 @@
 
 !------------------------------------------------------------------------------
 ! !PUBLIC TYPES:
-   public ESMF_PhysGrid
+   public ESMF_PhysGrid, ESMF_PhysGridType
 !------------------------------------------------------------------------------
 !
 ! !PUBLIC MEMBER FUNCTIONS:
@@ -119,7 +123,7 @@
    public ESMF_PhysGridCreate
    public ESMF_PhysGridDestroy
 !   public ESMF_PhysGridGetCoord
-!   public ESMF_PhysGridSetCoord
+   public ESMF_PhysGridSetCoord
 !   public ESMF_PhysGridGetMetric
 !   public ESMF_PhysGridSetMetric
 !   public ESMF_PhysGridGetLMask
@@ -157,7 +161,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter, private :: version = &
-      '$Id: ESMF_PhysGrid.F90,v 1.9 2002/12/09 16:57:02 jwolfe Exp $'
+      '$Id: ESMF_PhysGrid.F90,v 1.10 2002/12/13 18:52:27 jwolfe Exp $'
 
 !==============================================================================
 !
@@ -171,7 +175,7 @@
 ! !PRIVATE MEMBER FUNCTIONS:
          module procedure ESMF_PhysGridCreateNew
 !        module procedure ESMF_PhysGridCreateEmpty
-!        module procedure ESMF_PhysGridCreateInternal
+         module procedure ESMF_PhysGridCreateInternal
 !        module procedure ESMF_PhysGridCreateStagger
 !        module procedure ESMF_PhysGridCreateRead
 !        module procedure ESMF_PhysGridCreateCopy
@@ -193,7 +197,7 @@
 
 ! !PRIVATE MEMBER FUNCTIONS:
 !        module procedure ESMF_PhysGridSetCoordFromArray
-!        module procedure ESMF_PhysGridSetCoordInternal
+         module procedure ESMF_PhysGridSetCoordInternal
 !        module procedure ESMF_PhysGridSetCoordStagger
 !        module procedure ESMF_PhysGridSetCoordRead
 
@@ -260,9 +264,6 @@
       character (len = *), intent(in), optional :: name  
       integer, intent(out), optional :: rc               
 
-!     integer, intent(in) :: arg1                        
-!     integer, intent(in) :: arg2                        
-!
 ! !DESCRIPTION:
 !     Allocates memory for a new {\tt PhysGrid} object and constructs its
 !     internals.  Returns a pointer to a new {\tt PhysGrid}.
@@ -316,6 +317,102 @@
       if(rcpresent) rc = ESMF_SUCCESS
 
       end function ESMF_PhysGridCreateNew
+
+!------------------------------------------------------------------------------
+!BOP
+! !IROUTINE: 
+!     ESMF_PhysGridCreateInternal - Create a new PhysGrid internally
+
+! !INTERFACE:
+      function ESMF_PhysGridCreateInternal(name, global_max_i, global_max_j, &
+                                           global_min_coord1, &
+                                           global_max_coord1, &
+                                           global_min_coord2, &
+                                           global_max_coord2, rc)
+!
+! !RETURN VALUE:
+      type(ESMF_PhysGrid) :: ESMF_PhysGridCreateInternal
+!
+! !ARGUMENTS:
+      character (len = *), intent(in), optional :: name  
+      integer, intent(in) :: global_max_i
+      integer, intent(in) :: global_max_j
+      real, intent(in) :: global_min_coord1
+      real, intent(in) :: global_max_coord1
+      real, intent(in) :: global_min_coord2
+      real, intent(in) :: global_max_coord2
+      integer, intent(out), optional :: rc               
+!
+! !DESCRIPTION:
+!     Allocates memory for a new {\tt PhysGrid} object, constructs its
+!     internals, and internally generates the {\tt PhysGrid}.  Returns a
+!     pointer to the new {\tt PhysGrid}.
+!
+!     The arguments are:
+!     \begin{description}
+!     \item[[name]]
+!          {\tt PhysGrid} name.
+!     \item[[global\_max\_i]]
+!          Number of grid increments in the 1st coordinate direction.
+!     \item[[global\_max\_j]]
+!          Number of grid increments in the 2nd coordinate direction.
+!     \item[[global\_min\_coord1]]
+!          Minimum physical coordinate in the 1st coordinate direction.
+!     \item[[global\_max\_coord1]]
+!          Maximum physical coordinate in the 1st coordinate direction.
+!     \item[[global\_min\_coord2]]
+!          Minimum physical coordinate in the 2nd coordinate direction.
+!     \item[[global\_max\_coord2]]
+!          Maximum physical coordinate in the 2nd coordinate direction.
+!     \item[[rc]] 
+!          Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+!   \end{description}
+!
+! !REQUIREMENTS:  TODO
+!EOP
+
+      type(ESMF_PhysGridType), pointer :: physgrid   ! Pointer to new physgrid
+      integer :: status=ESMF_FAILURE                 ! Error status
+      logical :: rcpresent=.FALSE.                   ! Return code present
+
+!     Initialize pointers
+      nullify(physgrid)
+      nullify(ESMF_PhysGridCreateInternal%ptr)
+
+!     Initialize return code
+      if(present(rc)) then
+        rcpresent=.TRUE.
+        rc = ESMF_FAILURE
+      endif
+
+      allocate(physgrid, stat=status)
+!     If error write message and return.
+!     Formal error handling will be added asap.
+      if(status .NE. ESMF_SUCCESS) then
+        print *, "ERROR in ESMF_PhysGridCreateInternal: Allocate"
+        return
+      endif
+
+!     Call construction method to allocate and initialize grid internals.
+      call ESMF_PhysGridConstructNew(physgrid, name, status)
+      if(status .NE. ESMF_SUCCESS) then
+        print *, "ERROR in ESMF_PhysGridCreateInternal: PhysGrid construct"
+        return
+      endif
+
+!     Set physgrid attributes from function arguments
+      physgrid%global_max_i = global_max_i
+      physgrid%global_max_j = global_max_j
+      physgrid%global_min_coord1 = global_min_coord1
+      physgrid%global_max_coord1 = global_max_coord1
+      physgrid%global_min_coord2 = global_min_coord2
+      physgrid%global_max_coord2 = global_max_coord2
+
+!     Set return values.
+      ESMF_PhysGridCreateInternal%ptr => physgrid
+      if(rcpresent) rc = ESMF_SUCCESS
+
+      end function ESMF_PhysGridCreateInternal
 
 !------------------------------------------------------------------------------
 !BOP
@@ -581,6 +678,67 @@
 !  code goes here
 !
       end subroutine ESMF_PhysGridSetValue
+
+!------------------------------------------------------------------------------
+!BOP
+! !IROUTINE: 
+!     ESMF_PhysGridSetCoordInternal - Set Coords for a PhysGrid internally
+
+! !INTERFACE:
+      subroutine ESMF_PhysGridSetCoordInternal(physgrid, coord_id, rc)
+!
+! !ARGUMENTS:
+      type(ESMF_PhysGrid) :: physgrid
+      integer, intent(in) :: coord_id
+      integer, intent(out), optional :: rc            
+
+!
+! !DESCRIPTION:
+!     Compute a PhysGrid's coordinates from a given gridtype and set of
+!     physical parameters.
+!
+!     The arguments are:
+!     \begin{description}
+!     \item[physgrid]
+!          Class to be modified.
+!     \item[[coord_id]]
+!          Identifier for which set of coordinates are being set:
+!             1  center\_x
+!             2  center\_y
+!             3  corner\_x
+!             4  corner\_y
+!             5  face\_x
+!             6  face\_y
+!     \item[[rc]]
+!          Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+!     \end{description}
+!
+!EOP
+! !REQUIREMENTS: 
+
+      integer :: status=ESMF_SUCCESS              ! Error status
+      logical :: rcpresent=.FALSE.                ! Return code present
+
+!     Initialize return code
+      if(present(rc)) then
+        rcpresent = .TRUE.
+        rc = ESMF_FAILURE
+      endif
+
+!     For now, assume just an x-y grid on one processor.  TODO: decide
+!     whether each gridtype requires a separate subroutine or put it all here
+!     call LayoutGetDEPosition(PE_id)
+!     PE_id = 1
+
+
+      if(status .NE. ESMF_SUCCESS) then
+        print *, "ERROR in ESMF_PhysGridConstructNew: PhysGrid construct"
+        return
+      endif
+
+      if(rcpresent) rc = ESMF_SUCCESS
+
+      end subroutine ESMF_PhysGridSetCoordInternal
 
 !------------------------------------------------------------------------------
 !BOP
