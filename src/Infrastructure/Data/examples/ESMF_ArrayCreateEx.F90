5c92c702eb9cffeab6130c5fbc78cd31ba427c9a
Nancy Collins <nancy@ucar.edu>
2002-12-16 22:47:26 +0000

    add more code to example

--- src/Infrastructure/Data/examples/ESMF_ArrayCreateEx.F90
+++ src/Infrastructure/Data/examples/ESMF_ArrayCreateEx.F90
@@ -1,9 +1,9 @@
-! $Id: ESMF_ArrayCreateEx.F90,v 1.3 2002/12/10 22:45:09 nscollins Exp $
+! $Id: ESMF_ArrayCreateEx.F90,v 1.4 2002/12/16 22:47:26 nscollins Exp $
 !
 ! Example/test code which creates a new field.
 
-!-------------------------------------------------------------------------
-!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 
 !BOP
 !
@@ -23,14 +23,17 @@
     implicit none
 
 !   ! Local variables
-    integer :: nx, ny, rc       
+    integer :: nx, ny, arank, rc       
     type(ESMF_ArraySpec) :: arrayspec
     type(ESMF_Array) :: array1, array2, array3
     real (selected_real_kind(6,45)), dimension(:,:), pointer :: realptr
 
     
+!-------------------------------------------------------------------------------
 !   ! Example 1:
-!   !
+!   !  Create based on an existing F90 pointer.  The pointer is unassociated,
+!   !   so the sizes must be specified.  This routine allocates space for the
+!   !   array, but leaves the data uninitialized.
  
     nx = 10
     ny = 20    
@@ -44,10 +47,15 @@
     print *, "array 1 destroy returned"
 
 
+!-------------------------------------------------------------------------------
 !   ! Example 2:
-!   !
+!   !  Create based on an array specification.
 
-!   !   arrayspec = ESMF_ArraySpecCreate()
+    arank = 2
+!   !   arrayspec = ESMF_ArraySpecCreate(arank, ESMF_DATA_REAL, ESMF_KIND_4, &
+    !                                         nx, ny, rc)
+
+!   !   array2 = ESMF_ArrayCreate(arrayspec, ESMF_NO_DATA, rc)
 
      end program ESMF_ArrayCreateEx
     
