bef69cb23834aae3919b992864b95882c1e2e6e3
Nancy Collins <nancy@ucar.edu>
2003-01-09 22:26:08 +0000

    fix level of indirection code

--- src/Superstructure/Component/interface/ESMC_Component_F.C
+++ src/Superstructure/Component/interface/ESMC_Component_F.C
@@ -1,4 +1,4 @@
-// $Id: ESMC_Component_F.C,v 1.2 2003/01/08 23:36:46 nscollins Exp $
+// $Id: ESMC_Component_F.C,v 1.3 2003/01/09 22:26:08 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -35,34 +35,34 @@
 // the interface subroutine names MUST be in lower case
 extern "C" {
 
-     void FTN(c_esmc_componentcreate)(ESMC_Component *ptr, char *name,
+     void FTN(c_esmc_componentcreate)(ESMC_Component **ptr, char *name,
                      ESMC_Layout *layout, ESMC_CompType *ctype,
                      ESMC_ModelType *mtype, char *filepath, int *status) {
-         ptr = ESMC_ComponentCreate(name, layout, *ctype, *mtype, filepath, status);
+         *ptr = ESMC_ComponentCreate(name, layout, *ctype, *mtype, filepath, status);
      }
 
-     void FTN(c_esmc_componentdestroy)(ESMC_Component *ptr, int *status) {
-         *status = ESMC_ComponentDestroy(ptr);
+     void FTN(c_esmc_componentdestroy)(ESMC_Component **ptr, int *status) {
+         *status = ESMC_ComponentDestroy(*ptr);
      }
 
-     void FTN(c_esmc_componentinit)(ESMC_Component *ptr, int *status) {
-         *status = ptr->ESMC_ComponentInit();
+     void FTN(c_esmc_componentinit)(ESMC_Component **ptr, int *status) {
+         *status = (*ptr)->ESMC_ComponentInit();
      }
 
-     void FTN(c_esmc_componentrun)(ESMC_Component *ptr, int *timesteps, int *status) {
-         *status = ptr->ESMC_ComponentRun(*timesteps);
+     void FTN(c_esmc_componentrun)(ESMC_Component **ptr, int *timesteps, int *status) {
+         *status = (*ptr)->ESMC_ComponentRun(*timesteps);
      }
 
-     void FTN(c_esmc_componentfinalize)(ESMC_Component *ptr, int *status) {
-         *status = ptr->ESMC_ComponentFinalize();
+     void FTN(c_esmc_componentfinalize)(ESMC_Component **ptr, int *status) {
+         *status = (*ptr)->ESMC_ComponentFinalize();
      }
 
-     void FTN(c_esmc_componentvalidate)(ESMC_Component *ptr, char *opts, int *status) {
-         *status = ptr->ESMC_ComponentValidate(opts);
+     void FTN(c_esmc_componentvalidate)(ESMC_Component **ptr, char *opts, int *status) {
+         *status = (*ptr)->ESMC_ComponentValidate(opts);
      }
 
-     void FTN(c_esmc_componentprint)(ESMC_Component *ptr, char *opts, int *status) {
-         *status = ptr->ESMC_ComponentPrint(opts);
+     void FTN(c_esmc_componentprint)(ESMC_Component **ptr, char *opts, int *status) {
+         *status = (*ptr)->ESMC_ComponentPrint(opts);
      }
 
 };
