c53475d73037927583b5896d0fea1467d86a84f6
Nancy Collins <nancy@ucar.edu>
2003-01-29 00:07:58 +0000

    start of tests

---
+++ src/Superstructure/Component/tests/ESMF_CompCreate.F90
@@ -0,0 +1,80 @@
+! $Id: ESMF_CompCreate.F90,v 1.1 2003/01/29 00:07:58 nscollins Exp $
+!
+! Test code which creates a new Component.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+! Tests, cursory and exahustive, for Component Create code.
+!
+!
+!\begin{verbatim}
+
+    program ESMF_CompCreateTest
+    
+!   ! Some common definitions.  This requires the C preprocessor.
+#include "ESMF.h"
+
+!   ! Other ESMF modules which are needed by Comps
+    use ESMF_IOMod
+    use ESMF_LayoutMod
+    use ESMF_CompMod
+    
+    implicit none
+    
+!   ! Local variables
+    integer :: x, y, rc
+    integer :: timestep
+    integer, dimension(2) :: delist
+    character(ESMF_MAXSTR) :: cname
+    type(ESMF_Layout) :: layout
+    type(ESMF_Comp) :: comp1, comp2, comp3, comp4
+        
+!-------------------------------------------------------------------------
+!   ! Test 1:
+!   !
+!   !  Quick Test - Create, Init, Run, Finalize, Destroy a Component.
+ 
+    print *, "Component Test 1:"
+
+    delist = (/ 0, 1 /)
+    layout = ESMF_LayoutCreate(2, 1, delist, ESMF_XFAST, rc)
+
+    cname = "Atmosphere"
+    comp1 = ESMF_CompCreate(cname, layout, ESMF_GRIDCOMP, &
+                                       ESMF_ATM, "/usr/local", rc=rc)  
+
+    print *, "Comp Create returned, name = ", trim(cname)
+
+    call ESMF_CompInit(comp1, rc)
+    print *, "Comp Init returned"
+
+
+    !call ESMF_CompPrint(comp1, rc)
+    !print *, "Comp Print returned"
+
+    timestep = 1
+    call ESMF_CompRun(comp1, timestep, rc)
+    print *, "Comp Run returned"
+
+
+    call ESMF_CompFinalize(comp1, rc)
+    print *, "Comp Finalize returned"
+
+
+    call ESMF_CompDestroy(comp1, rc)
+    print *, "Comp Destroy returned"
+
+    call ESMF_LayoutDestroy(layout, rc);
+    print *, "Layout Destroy returned"
+
+    print *, "Component Test 1 finished"
+
+
+    end program ESMF_CompCreateTest
+    
+!\end{verbatim}
+    
---
+++ src/Superstructure/Component/tests/ESMF_StateCreate.F90
@@ -0,0 +1,162 @@
+! $Id: ESMF_StateCreate.F90,v 1.1 2003/01/29 00:07:58 nscollins Exp $
+!
+! Test code which creates a new State.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+! Tests, cursory and exahustive, for State Create code.
+!
+!
+!\begin{verbatim}
+
+    program ESMF_StateCreateTest
+    
+!   ! Some common definitions.  This requires the C preprocessor.
+#include "ESMF.h"
+
+!   ! Other ESMF modules which are needed by States
+    use ESMF_IOMod
+    use ESMF_ArrayMod
+    use ESMF_FieldMod
+    use ESMF_BundleMod
+    use ESMF_StateMod
+    
+    implicit none
+    
+!   ! Local variables
+    integer :: x, y, rc
+    integer :: timestep
+    integer, dimension(2) :: delist
+    logical :: isneeded
+    character(ESMF_MAXSTR) :: cname, sname, bname, fname
+    type(ESMF_Array) :: array1
+    type(ESMF_Field) :: field1
+    type(ESMF_Bundle) :: bundle1, bundle2
+    type(ESMF_State) :: state1, state2, state3, state4
+        
+!-------------------------------------------------------------------------
+!   ! Test 1:
+!   !
+!   !  Quick Test - Create/Destroy an Empty State.
+ 
+    print *, "State Test 1: Empty State"
+
+    cname = "Atmosphere"
+    state1 = ESMF_StateCreate(cname, ESMF_STATEIMPORT, rc)  
+    print *, "State Create returned, name = ", trim(cname)
+
+    call ESMF_StatePrint(state1, rc)
+    print *, "State Print returned", rc
+
+    call ESMF_StateDestroy(state1, rc)
+    print *, "State Destroy returned", rc
+
+    print *, "State Test 1 finished"
+
+
+!-------------------------------------------------------------------------
+!   ! Test 2:
+!   !
+!   !  Quick Test - Create, Add Data, Query, then Destroy a State.
+ 
+    print *, "State Test 2: Export State"
+
+    cname = "Ocean"
+    state2 = ESMF_StateCreate(cname, ESMF_STATEEXPORT, rc)  
+    print *, "State Create returned, name = ", trim(cname)
+
+    bundle1 = ESMF_BundleCreate(rc=rc)
+    print *, "Bundle Create returned", rc
+
+    call ESMF_StateAddData(state2, bundle1, rc)
+    print *, "StateAddData returned", rc
+    
+    call ESMF_StatePrint(state2, rc)
+    print *, "State Print returned", rc
+
+    call ESMF_StateDestroy(state2, rc)
+    print *, "State Destroy returned", rc
+
+    call ESMF_BundleDestroy(bundle1, rc)
+    print *, "Bundle Destroy returned", rc
+
+    print *, "State Test 2 finished"
+
+
+!-------------------------------------------------------------------------
+!   ! Test 3:
+!   !
+!   !  Quick Test - Create, Add Placeholder, Query, then Destroy a State.
+ 
+    print *, "State Test 3: Export State with Placeholder"
+
+    cname = "Ocean"
+    state3 = ESMF_StateCreate(cname, ESMF_STATEEXPORT, rc)  
+    print *, "State Create returned, name = ", trim(cname)
+
+    sname = "Downward wind"
+    call ESMF_StateAddNameOnly(state3, sname, rc)
+    print *, "StateAddNameOnly returned", rc
+    
+    call ESMF_StatePrint(state3, rc)
+    print *, "State Print returned", rc
+
+    ! save for next test, do not destroy yet
+
+    print *, "State Test 3 finished"
+
+
+!-------------------------------------------------------------------------
+!   ! Test 4:
+!   !
+!   !  Quick Test - Mark and query Needed flags
+ 
+    print *, "State Test 4: Get/Set Needed flags in Export State"
+
+    ! inherit state3 from test above
+
+    sname = "Downward wind"
+    call ESMF_StateSetNeeded(state3, sname, rc)
+    print *, "StateSetNeeded returned", rc
+    
+    call ESMF_StatePrint(state3, rc)
+    print *, "State Print returned", rc
+
+    call ESMF_StateGetNeeded(state3, sname, isneeded, rc)
+    print *, "StateGetNeeded returned", rc
+
+    if (isneeded) then
+      print *, "Data marked as needed", trim(sname)
+
+      bname = sname
+      bundle2 = ESMF_BundleCreate(bname, rc=rc)
+      print *, "Bundle Create returned", rc, "name =", trim(bname)
+      
+      fname = "Downward wind field"
+      field1 = ESMF_FieldCreate(fname, rc=rc)
+      print *, "Field Create returned", rc, "name =", trim(fname)
+
+      call ESMF_BundleAddField(bundle2, field1, rc) 
+      print *, "Bundle AddField returned", rc
+
+      call ESMF_StateAddData(state3, bundle1, rc)
+      print *, "StateAddData returned", rc
+    else
+      print *, "Data marked as not needed", trim(sname)
+    endif
+    
+    call ESMF_StateDestroy(state3, rc)
+    print *, "State Destroy returned", rc
+
+    print *, "State Test 4 finished"
+
+
+
+    end program ESMF_StateCreateTest
+    
+!\end{verbatim}
+    
