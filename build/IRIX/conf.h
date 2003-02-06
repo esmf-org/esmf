010469e0cc8a97cd232d8876e008a41e52abfc7d
John Wolfe <jwolfe@ucar.edu>
2003-02-06 23:40:18 +0000

    Getting ESMF running using IRIX architecture, small changes to make current with build system.

--- build/IRIX/conf.h
+++ build/IRIX/conf.h
@@ -1,5 +1,5 @@
 #ifdef ESMC_RCS_HEADER
-"$Id: conf.h,v 1.3 2003/02/03 06:09:03 eschwab Exp $"
+"$Id: conf.h,v 1.4 2003/02/06 23:40:45 jwolfe Exp $"
 "Defines the configuration for this machine"
 #endif
 
@@ -64,5 +64,6 @@
 #define ESMC_USE_P_FOR_DEBUGGER
 
 #define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_SIZE 128
 
 #endif
