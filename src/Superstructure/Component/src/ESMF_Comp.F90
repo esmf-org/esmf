67d616b787ebdf5fe13e197a4690865cfdf3e3f0
Nancy Collins <nancy@ucar.edu>
2003-02-18 22:06:19 +0000

    add layout check

--- src/Superstructure/Component/src/ESMF_Comp.F90
+++ src/Superstructure/Component/src/ESMF_Comp.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_Comp.F90,v 1.18 2003/02/18 22:01:43 nscollins Exp $
+! $Id: ESMF_Comp.F90,v 1.19 2003/02/18 22:06:19 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -36,7 +36,6 @@
       use ESMF_BaseMod
       use ESMF_IOMod
       use ESMF_LayoutMod
-      !use ESMF_TimeMod
       use ESMF_ClockMod
       use ESMF_StateMod
       implicit none
@@ -164,7 +163,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter, private :: version = &
-      '$Id: ESMF_Comp.F90,v 1.18 2003/02/18 22:01:43 nscollins Exp $'
+      '$Id: ESMF_Comp.F90,v 1.19 2003/02/18 22:06:19 nscollins Exp $'
 
 !==============================================================================
 ! 
@@ -534,8 +533,10 @@ end interface
 !    Component to call Initialization routine for.
 !
 !   \item[{[clock]}]  Start time, total model time, etc.
-!   \item[{[layout]}]  Number of processors for this model.
-!   \item[{[filepath]}]  Where to find model-specific files.
+!
+!   \item[{[layout]}]  Number of processors for this component.
+!
+!   \item[{[filepath]}]  Where to find component-specific files.
 !
 !   \item[{[rc]}]
 !    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
@@ -562,8 +563,11 @@ end interface
         ! See if this is currently running on a DE which is part of the
         ! proper Layout.
 	call ESMF_LayoutGetDEId(component%compp%layout, de_id, status)
-
-        ! TODO: decide whether to return here or continue.
+        if (status .ne. ESMF_SUCCESS) then
+          ! this is not our DE
+          if (rcpresent) rc = ESMF_SUCCESS
+          return
+        endif
 
         ! TODO: handle optional args, do framework setup for this comp.
         ! Call user-supplied init routine.
@@ -635,8 +639,11 @@ end interface
         ! See if this is currently running on a DE which is part of the
         ! proper Layout.
 	call ESMF_LayoutGetDEId(component%compp%layout, de_id, status)
-
-        ! TODO: decide whether to return here or continue.
+        if (status .ne. ESMF_SUCCESS) then
+          ! this is not our DE
+          if (rcpresent) rc = ESMF_SUCCESS
+          return
+        endif
 
         ! TODO: handle optional args, do framework setup for this comp.
         ! Call user-supplied init routine.
@@ -701,8 +708,11 @@ end interface
         ! See if this is currently running on a DE which is part of the
         ! proper Layout.
 	call ESMF_LayoutGetDEId(component%compp%layout, de_id, status)
-
-        ! TODO: decide whether to return here or continue.
+        if (status .ne. ESMF_SUCCESS) then
+          ! this is not our DE
+          if (rcpresent) rc = ESMF_SUCCESS
+          return
+        endif
 
         ! TODO: handle optional args, do framework setup for this comp.
         ! Call user-supplied init routine.
