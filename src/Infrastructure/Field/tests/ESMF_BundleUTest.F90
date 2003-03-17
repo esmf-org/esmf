28b3d364bf933540f9214042c3359fd624519f24
Silverio Vasquez <silverio.vasquez@noaa.gov>
2003-03-17 18:07:24 +0000

    Changed File names.

---
+++ src/Infrastructure/Field/tests/ESMF_BundleUTest.F90
@@ -0,0 +1,240 @@
+! $Id: ESMF_BundleUTest.F90,v 1.3 2003/03/17 18:07:24 svasquez Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_BundleTest
+
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF.h>
+!
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_BundleTest - Bundle Unit Tests
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 Bundle unit tests.
+! The companion file ESMF\_Bundle.F90 contains the definitions for the
+! Bundle methods.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod     ! test methods
+      use ESMF_BundleMod  ! the class to test
+      use ESMF_IOMod
+      use ESMF_ArrayMod
+      use ESMF_DataMapMod
+      use ESMF_GridMod
+      use ESMF_FieldMod
+
+      implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_BundleUTest.F90,v 1.3 2003/03/17 18:07:24 svasquez Exp $'
+!------------------------------------------------------------------------------
+
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+!     ! Local variables
+      integer :: i, x, y, rc, mycell, fieldcount
+      type(ESMF_Grid) :: grid
+      type(ESMF_ArraySpec) :: arrayspec
+      type(ESMF_Array) :: arraya, arrayb
+      type(ESMF_DataMap) :: datamap
+      type(ESMF_RelLoc) :: relativelocation
+      character (len = ESMF_MAXSTR) :: bname1, bname2, fname1, fname2, fname3
+      type(ESMF_IOspec) :: iospec
+      type(ESMF_Field) :: field(10), returnedfield1, returnedfield2, returnedfield3, simplefield
+      type(ESMF_Bundle) :: bundle1, bundle2, bundle3, bundle4
+      real (selected_real_kind(6,45)), dimension(:,:), pointer :: f90ptr1, f90ptr2
+
+
+
+      ! individual test result code
+
+      ! individual test name
+      character(ESMF_MAXSTR) :: named
+
+
+      ! individual test failure message
+      character(ESMF_MAXSTR) :: failMsg
+      character(ESMF_MAXSTR) :: name
+
+
+      ! instantiate a Bundle 
+      type(ESMF_Bundle) :: bundle
+
+      print *, "******************Bundles UNIT TESTS****************************"
+      print *
+
+!    ! Verify that getting the name  of an uninitialized Bundle is handled properly.
+      call ESMF_BundleGetName(bundle1, bname1, rc)
+     write(failMsg, *) "Subroutine should have returned ESMF_FAILURE"
+     write(name, *) "Getting name of uninitalized Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+     
+     ! The following code is commented out because it crashes the program
+     ! It will be uncommented when the bug is fixed
+     !  Verify that the Field count query from an uninitialized Bundle is handled properly
+     !call ESMF_BundleGetFieldCount(bundle1, fieldcount, rc);
+     !write(failMsg, *) ""
+     !write(name, *) "Getting Field count from an uninitialized Bundle Test"
+     !call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+!    !  Create several empty Fields and add them to a new Bundle.
+     field(1) = ESMF_FieldCreateNoData(name="pressure", rc=rc)
+     field(2) = ESMF_FieldCreateNoData(name="temperature", rc=rc)
+     field(3) = ESMF_FieldCreateNoData(name="heat flux", rc=rc)
+
+!    ! Verify that a Bundle can created with 3 Fields
+     bundle1 = ESMF_BundleCreate(3, field, name="atmosphere data", rc=rc)
+     write(failMsg, *) ""
+     write(name, *) "Creating Bundle with 3 No Data Fields Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!    ! Verify that adding a Field to an uninitialized Bundle is handled correctly
+!    ! This code is commented out because it crashes the program.
+!    !It will be uncommented when the bug is fixed
+     !call ESMF_BundleAddFields(bundle2, simplefield, rc=rc);
+     !write(failMsg, *) "Add Field to uncreated Bundle failed"
+     !write(name, *) "Adding a Field to an uncreated Bundle Test"
+     !call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+     !  Verify that a Field can be added to a Bundle
+     simplefield = ESMF_FieldCreate(grid, arrayspec, ESMF_CELL_CENTER, &
+                                    name="rh", rc=rc)
+
+     !  Verify that an empty Bundle can be created
+     bundle2 = ESMF_BundleCreate(name="time step 1", rc=rc);
+     write(failMsg, *) ""
+     write(name, *) "Creating Empty Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+     !  Verify that recreating a created Bundle is handled properly
+     bundle2 = ESMF_BundleCreate(name="time step 1", rc=rc);
+     write(failMsg, *) ""
+     write(name, *) "Creating a Bundle that has already been created Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+     !  Verify that the Field count query from an empty Bundle is handled properly
+     call ESMF_BundleGetFieldCount(bundle2, fieldcount, rc);
+     write(failMsg, *) ""
+     write(name, *) "Getting Field count from an empty Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fieldcount.eq.0), name, failMsg, result, ESMF_SRCLINE)
+     print *, "Field count of empty Bundle = ", fieldcount
+
+     !  Verify that a Field can be added to an empty Bundle
+     call ESMF_BundleAddFields(bundle2, simplefield, rc=rc);
+     write(failMsg, *) ""
+     write(name, *) "Adding a Field to an Empty Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+     !  Verify that the Field count can be queried from a Bundle
+     call ESMF_BundleGetFieldCount(bundle2, fieldcount, rc);
+     write(failMsg, *) ""
+     write(name, *) "Getting Field count from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fieldcount.eq.1), name, failMsg, result, ESMF_SRCLINE)
+
+!   !  Create an empty Bundle and then add multiple fields to it.
+     bundle3 = ESMF_BundleCreate(name="southern hemisphere", rc=rc);
+     bundle3 = ESMF_BundleCreate(name="northern hemisphere", rc=rc);
+   
+     !  Verify that multiple Fields can be added to a Bundle 
+     call ESMF_BundleAddFields(bundle3, 3, field, rc);
+     write(failMsg, *) ""
+     write(name, *) "Adding multiple Fields to a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+     !  Verify that Fields count can be queried from a Bundle 
+     call ESMF_BundleGetFieldCount(bundle3, fieldcount, rc);
+     write(failMsg, *) ""
+     write(name, *) "Getting Field count from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fieldcount.eq.3), name, failMsg, result, ESMF_SRCLINE)
+
+     ! Verify that the first Field names can be queried fron a Bundle
+     call ESMF_BundleGetFields(bundle1, "pressure", returnedfield1, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting first Field by name from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+     call ESMF_FieldGetName(returnedfield1, fname1, rc)
+     write(failMsg, *) "Subroutine returned ESMF_FAILURE or incorrect name returned"
+     write(name, *) "Getting first Field from a Bundle Test continued"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fname1.eq."pressure"), name, failMsg, result, ESMF_SRCLINE)
+
+     ! Verify that the second Field names can be queried fron a Bundle
+     call ESMF_BundleGetFields(bundle1, 2, returnedfield2, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting a second Field by index from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+     call ESMF_FieldGetName(returnedfield2, fname2, rc)
+     write(failMsg, *) "Subroutine returned ESMF_FAILURE or incorrect name returned"
+     write(name, *) "Getting a second Field from a Bundle Test continued"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fname2.eq."temperature"), name, failMsg, result, ESMF_SRCLINE)
+
+
+     ! Verify that the third Field names can be queried fron a Bundle
+     call ESMF_BundleGetFields(bundle1, 3, returnedfield3, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting a third Field by index from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+     call ESMF_FieldGetName(returnedfield3, fname3, rc)
+     write(failMsg, *) "Subroutine returned ESMF_FAILURE or incorrect name returned"
+     write(name, *) "Getting a third Field from a Bundle Test continued"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(fname3.eq."heat flux"), name, failMsg, result, ESMF_SRCLINE)
+     print *, "Bundle returned, field names = ", &
+                    trim(fname1), ", ", trim(fname2), ", ", trim(fname3)
+
+     ! Verify that the fourth Field names cannot be queried fron a Bundle
+     ! because there are ony three Fields in the Bundle
+     call ESMF_BundleGetFields(bundle1, 4, returnedfield3, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting a non-existent Field from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+     ! Verify that the zeroth Field names cannot be queried fron a Bundle
+     call ESMF_BundleGetFields(bundle1, 0, returnedfield3, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting a zeroth non-existent Field from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+     ! Verify that the querying Field with wrong name from a Bundle returns FAILURE
+     call ESMF_BundleGetFields(bundle1, "nressure", returnedfield1, rc)
+     write(failMsg, *) ""
+     write(name, *) "Getting wrong Field name from a Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+     ! Verify that the Bundle name can be queried 
+     call ESMF_BundleGetName(bundle1, bname1, rc)
+     write(failMsg, *) ""
+     write(name, *) "Query Bundle Name Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(bname1.eq."atmosphere data"), &
+				name, failMsg, result, ESMF_SRCLINE)
+     print *, "Bundle name = ", trim(bname1)
+
+      ! Verify that a Bundle can be destroyed
+     call ESMF_BundleDestroy(bundle1, rc=rc)
+     write(failMsg, *) ""
+     write(name, *) "Bundle Destroy Test"
+     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+     print *, "rc = ", (rc)
+
+      ! Verify that destroying a destroyed Bundle is handled correctly
+     call ESMF_BundleDestroy(bundle1, rc=rc)
+     write(failMsg, *) ""
+     write(name, *) "Destroying a Destroyed Bundle Test"
+     call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+
+     print *, "rc = ", (rc)
+
+      end program ESMF_BundleTest
---
+++ src/Infrastructure/Field/tests/ESMF_FieldUTest.F90
@@ -0,0 +1,295 @@
+! $Id: ESMF_FieldUTest.F90,v 1.3 2003/03/17 18:07:25 svasquez Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_FieldTest
+
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF.h>
+!
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_FieldTest - One line general statement about this test
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 Field unit tests.
+! The companion file ESMF\_Field.F90 contains the definitions for the
+! Field methods.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod     ! test methods
+      use ESMF_BaseMod
+      use ESMF_IOMod
+      use ESMF_ArrayMod
+      use ESMF_DataMapMod
+      use ESMF_GridMod
+      use ESMF_FieldMod
+
+      implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_FieldUTest.F90,v 1.3 2003/03/17 18:07:25 svasquez Exp $'
+!------------------------------------------------------------------------------
+
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+
+      ! individual test result code
+      integer :: rc = 1
+
+      ! individual test failure message
+      character(ESMF_MAXSTR) :: failMsg
+      character(ESMF_MAXSTR) :: name
+
+!     !LOCAL VARIABLES:
+      integer :: x, y
+      type(ESMF_Grid) :: grid, grid2, grid3, grid4
+      type(ESMF_Array) :: arr, arr2
+      real, dimension(:,:), pointer :: f90ptr1
+      type(ESMF_DataMap) :: dm
+      type(ESMF_RelLoc) :: rl
+      character (len = 20) :: fname, fname1, fname2
+      type(ESMF_IOSpec) :: ios
+      type(ESMF_Field) :: f1, f2, f3, f4, f5
+
+      print *, "******************FIELDS UNIT TESTS****************************"
+      print *
+
+      !------------------------------------------------------------------------
+
+      ! Test Requirement FLD1.1.3
+      ! Fields may be created as in FLD1.1.1 without allocating data or specifying 
+      ! an associated data array. In this case specifying the grid parameters and 
+      ! data array dimensions may be deferred until data is attached.
+      f1 = ESMF_FieldCreateNoData(rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating a Field with no data Test Req. FLD1.1.3"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that an initialized Field can be printed
+      call ESMF_FieldPrint(f1, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing an initialized Field with no data Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that the Field name can be queried from a no data Field
+      call ESMF_FieldGetName(f1, fname, rc=rc)
+      write(failMsg, *) "returned name not 'default_name'"
+      write(name, *) "Getting name of Field with no data Test"
+      call ESMF_Test((fname.eq."default_name"), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Test Requirement FLD1.4
+      ! Fields may be deleted.
+      call ESMF_FieldDestroy(f1, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Destroying initialized Field Test Req. FLD1.4"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+     ! Verifing that printing an uninitialized Field is handled properly.
+      call ESMF_FieldPrint(f2, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing an uninitialized Field Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+       ! verify that querying the name of a destroyed Field is handled properly.
+!      The following code is commented out because it crashes the program.
+!      It will be uncommented when the bug is fixed.
+!      call ESMF_FieldGetName(f1, fname, rc=rc)
+!      write(failMsg, *) ""
+!      write(name, *) "Getting name of a destroyed Field Test"
+!      call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that printing a destroyed Field is handled properly.
+      call ESMF_FieldPrint(f1, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing destroyed Field Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Test requirement FLD1.5.1.
+      ! The only default attribute of a field will be a name. A unique name will 
+      ! be generated if not supplied by the user.
+      ! Create Field f1 and f2 with no data
+      f1 = ESMF_FieldCreateNoData(rc=rc)
+      f2 = ESMF_FieldCreateNoData(rc=rc)
+      Call ESMF_FieldGetName(f1, fname1, rc=rc)
+      Call ESMF_FieldGetName(f2, fname2, rc=rc)
+      write(failMsg, *) "Field names not unique"
+      write(name, *) "Unique default Field names Test, FLD1.5.1"
+      call ESMF_Test((fname1.ne.fname2), name, failMsg, result, ESMF_SRCLINE)
+      print *, "Field (f1) name = ", trim(fname1)
+      print *, "Field (f2) name = ", trim(fname2)
+      call ESMF_FieldPrint(f1)
+      call ESMF_FieldPrint(f2)
+      call ESMF_FieldDestroy(f1)
+      call ESMF_FieldDestroy(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field can be created with a name
+      f2 = ESMF_FieldCreateNoData("pressure", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating Field with name Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that the Field name can be queried.
+      Call ESMF_FieldGetName(f2, fname, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Getting a Field name Test"
+      call ESMF_Test((fname.eq."pressure"), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that recreating a Field is allowed.
+      f2 = ESMF_FieldCreateNoData("temperature", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Recreate a created Field Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field can be created after it has been destroyed
+      call ESMF_FieldDestroy(f2)
+      f2 = ESMF_FieldCreateNoData("precipitation", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Recreate a destroyed Field Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that an uninitialized Grid can be printed
+      call ESMF_GridPrint(grid, "", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing an uninitialized Grid Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Grid can be created
+      grid =  ESMF_GridCreate("atmgrid", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating a Grid Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+     
+      ! Verifing that a Grid can be printed
+      call ESMF_GridPrint(grid, "", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing a Grid Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that recreating a created Grid is allowed.
+      grid =  ESMF_GridCreate("landgrid", rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Recreating a created Grid Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that an Array can be created
+      allocate(f90ptr1(10,20))
+      arr = ESMF_ArrayCreate(f90ptr1, ESMF_NO_COPY, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating an Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that an Array can be printed
+      call ESMF_ArrayPrint(arr, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Printing an Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that recreating a created Array is allowed
+      allocate(f90ptr1(10,20))
+      arr = ESMF_ArrayCreate(f90ptr1, ESMF_NO_COPY, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Recreating a created Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field can be created with a Grid and Array
+      f3 = ESMF_FieldCreate(grid, arr, ESMF_NO_COPY, ESMF_CELL_CENTER, &
+                                   dm, "Field 0", ios, rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating a Field with a Grid and Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f3)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field with a Grid and Array can be destroyed
+      call ESMF_FieldDestroy(f3, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Destroying a Field with a Grid and Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f3)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field can be created with an uninitialized Grid and Array
+      f3 = ESMF_FieldCreate(grid2, arr2, ESMF_NO_COPY, ESMF_CELL_CENTER, &
+                                   dm, "Field 0", ios, rc)
+      write(failMsg, *) ""
+      write(name, *) "Creating a Field with an uninitialized Grid and Array Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f3)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a Field with no data can be destroyed
+      call ESMF_FieldDestroy(f2, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Destroying a Field with no data Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verifing that a destroying a destroyed  Field is handled properly.
+      call ESMF_FieldDestroy(f2, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Destroying a destroyed Field Test"
+      call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldPrint(f2)
+      !------------------------------------------------------------------------
+
+      ! Verify that a Grid cannot be gotten from a Field created with no data
+      f5 = ESMF_FieldCreateNoData(rc=rc)
+      call ESMF_FieldGetGrid(f5, grid3, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Getting a Grid from a Field created with no data Test"
+      call ESMF_Test((rc.eq.ESMF_FAILURE), name, failMsg, result, ESMF_SRCLINE)
+      call ESMF_FieldDestroy(f5, rc=rc)
+      !------------------------------------------------------------------------
+
+      ! Req. 1.6.2
+      ! A field shall be able to return a reference to its grid.
+      grid =  ESMF_GridCreate("oceangrid", rc=rc)
+      arr = ESMF_ArrayCreate(f90ptr1, ESMF_NO_COPY, rc=rc)
+      f3 = ESMF_FieldCreate(grid, arr, ESMF_NO_COPY, ESMF_CELL_CENTER, &
+                                   dm, "Field 0", ios, rc)
+      call ESMF_FieldGetGrid(f3, grid3, rc=rc)
+      write(failMsg, *) ""
+      write(name, *) "Getting a Grid from a Field created with no data Test"
+      call ESMF_GridPrint(grid, "", rc=rc)
+      call ESMF_GridPrint(grid3, "", rc=rc)
+      !------------------------------------------------------------------------
+
+      end program ESMF_FieldTest
