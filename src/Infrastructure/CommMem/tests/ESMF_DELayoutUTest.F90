87209b88f14f76d28736c80b6fb657605b5aaf40
Cecelia DeLuca <cecelia.deluca@noaa.gov>
2003-03-17 17:32:13 +0000

    standardize name

--- src/Infrastructure/CommMem/tests/ESMF_DELayoutUTest.F90
+++ src/Infrastructure/CommMem/tests/ESMF_DELayoutUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_DELayoutCreateUTest.F90,v 1.1 2003/03/15 01:01:05 eschwab Exp $
+! $Id: ESMF_DELayoutUTest.F90,v 1.1 2003/03/17 17:32:14 cdeluca Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_DELayoutCreateUTest.F90,v 1.1 2003/03/15 01:01:05 eschwab Exp $'
+      '$Id: ESMF_DELayoutUTest.F90,v 1.1 2003/03/17 17:32:14 cdeluca Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
