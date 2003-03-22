b2ce72733a23f0a754055f6688cf9916cc52ceb3
Earl Schwab <eschwab.esmf@gmail.com>
2003-03-22 00:03:03 +0000

    make Validate and Print routines invoke C++ inherited and overridden versions

--- scripts/doc_templates/templates/inter_ESMC_class_F.C
+++ scripts/doc_templates/templates/inter_ESMC_class_F.C
@@ -1,4 +1,4 @@
-// $Id: inter_ESMC_class_F.C,v 1.3 2003/03/21 22:49:35 eschwab Exp $
+// $Id: inter_ESMC_class_F.C,v 1.4 2003/03/22 00:03:03 eschwab Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -73,11 +73,11 @@ extern "C" {
        }
 
        void FTN(c_esmc_<class>validate)(ESMC_<Class> **ptr, char *opts, int *status) {
-           *status = (*ptr)->ESMC_<Class>Validate(opts);
+           *status = (*ptr)->ESMC_BaseValidate(opts);
        }
 
        void FTN(c_esmc_<class>print)(ESMC_<Class> **ptr, char *opts, int *status) {
-           *status = (*ptr)->ESMC_<Class>Print(opts);
+           *status = (*ptr)->ESMC_BasePrint(opts);
        }
 
 };
