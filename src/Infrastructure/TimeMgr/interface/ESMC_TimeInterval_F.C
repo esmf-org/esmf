cd061d95eb115cb01158be71fddcb8545c220e66
Earl Schwab <eschwab.esmf@gmail.com>
2003-03-27 01:45:58 +0000

    added Init method

--- src/Infrastructure/TimeMgr/interface/ESMC_TimeInterval_F.C
+++ src/Infrastructure/TimeMgr/interface/ESMC_TimeInterval_F.C
@@ -1,4 +1,4 @@
-// $Id: ESMC_TimeInterval_F.C,v 1.3 2003/03/24 17:41:38 eschwab Exp $
+// $Id: ESMC_TimeInterval_F.C,v 1.4 2003/03/27 01:45:58 eschwab Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -32,13 +32,20 @@
 // the interface subroutine names MUST be in lower case
 extern "C" {
 
-#if 0
-       // keep this for shallow classes, get rid of create/destroy above
-       void FTN(c_esmc_timeintervalinit)(ESMC_TimeInterval **ptr, int *arg1, int *arg2,
-                                                   int *arg3, int *status) {
-           *status = (*ptr)->ESMC_TimeIntervalInit(*arg1, *arg2, *arg3);
+       void FTN(c_esmc_timeintervalinit)(ESMC_TimeInterval **ptr,
+                                 int *YY, int *MO, int *D, int *H, int *M,
+                                 ESMF_IKIND_I8 *S, int *MS, int *US, int *NS,
+                                 double *d_, double *h_, double *m_,
+                                 double *s_, double *ms_, double *us_,
+                                 double *ns_, int *Sn, int *Sd,
+                                 ESMC_Calendar **cal, int *status) {
+
+           *status = (*ptr)->ESMC_TimeIntervalInit(YY, MO, D, H, M, S, MS,
+                                           US, NS, d_, h_, m_, s_, ms_,
+                                           us_, ns_, Sn, Sd, *cal);
        }
 
+#if 0
        void FTN(c_esmc_timeintervalget)(ESMC_TimeInterval **ptr, 
                                          <value> *value, int *status} {
            *status = (*ptr)->ESMC_TimeIntervalGet(&value);
