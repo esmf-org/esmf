4b0ca5871063f0e9153fdf830ced36d0e759ef06
Nancy Collins <nancy@ucar.edu>
2003-04-01 23:56:37 +0000

    add tests for arrays of derived types

--- src/Infrastructure/Data/tests/ESMF_ArrayF90Ptr.F90
+++ src/Infrastructure/Data/tests/ESMF_ArrayF90Ptr.F90
@@ -57,6 +57,32 @@
     end type 
     
 
+    type PtrSWrap1  
+    sequence
+      type(PtrWrap1),dimension(:),pointer :: dummy
+    end type 
+    
+    type PtrSWrap2  
+    sequence
+      type(PtrWrap1),dimension(:,:),pointer :: dummy
+    end type 
+    
+    type PtrSWrap3  
+    sequence
+      type(PtrWrap1),dimension(:,:,:),pointer :: dummy
+    end type 
+    
+    type PtrSWrap4  
+    sequence
+      type(PtrWrap1),dimension(:,:,:,:),pointer :: dummy
+    end type 
+    
+    type PtrSWrap5  
+    sequence
+      type(PtrWrap1),dimension(:,:,:,:,:),pointer :: dummy
+    end type 
+    
+
     
     type(PtrWrap1) :: sizetest1(2)
     type(PtrWrap2) :: sizetest2(2)
@@ -70,6 +96,12 @@
     type(PtrRWrap4) :: sizetest4R(2)
     type(PtrRWrap5) :: sizetest5R(2)
     
+    type(PtrSWrap1) :: sizetest1S(2)
+    type(PtrSWrap2) :: sizetest2S(2)
+    type(PtrSWrap3) :: sizetest3S(2)
+    type(PtrSWrap4) :: sizetest4S(2)
+    type(PtrSWrap5) :: sizetest5S(2)
+    
 
 
     call c_ESMF_SizePrint(sizetest1(1), sizetest1(2), 1)
@@ -78,11 +110,17 @@
     call c_ESMF_SizePrint(sizetest4(1), sizetest4(2), 4)
     call c_ESMF_SizePrint(sizetest5(1), sizetest5(2), 5)
 
-    call c_ESMF_SizePrint(sizetest1(1), sizetest1(2), 1)
-    call c_ESMF_SizePrint(sizetest2(1), sizetest2(2), 2)
-    call c_ESMF_SizePrint(sizetest3(1), sizetest3(2), 3)
-    call c_ESMF_SizePrint(sizetest4(1), sizetest4(2), 4)
-    call c_ESMF_SizePrint(sizetest5(1), sizetest5(2), 5)
+    call c_ESMF_SizePrint(sizetest1R(1), sizetest1R(2), 1)
+    call c_ESMF_SizePrint(sizetest2R(1), sizetest2R(2), 2)
+    call c_ESMF_SizePrint(sizetest3R(1), sizetest3R(2), 3)
+    call c_ESMF_SizePrint(sizetest4R(1), sizetest4R(2), 4)
+    call c_ESMF_SizePrint(sizetest5R(1), sizetest5R(2), 5)
+
+    call c_ESMF_SizePrint(sizetest1S(1), sizetest1S(2), 1)
+    call c_ESMF_SizePrint(sizetest2S(1), sizetest2S(2), 2)
+    call c_ESMF_SizePrint(sizetest3S(1), sizetest3S(2), 3)
+    call c_ESMF_SizePrint(sizetest4S(1), sizetest4S(2), 4)
+    call c_ESMF_SizePrint(sizetest5S(1), sizetest5S(2), 5)
 
 
     end program test
