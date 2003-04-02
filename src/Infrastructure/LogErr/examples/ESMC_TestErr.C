959a58ceda9433b20dd481f6ea3b05439555848f
Shep Smithline <Shep.Smithline@noaa.gov>
2003-04-02 17:30:53 +0000

    C/C++ test cases

---
+++ src/Infrastructure/LogErr/examples/ESMC_TestErr.C
@@ -0,0 +1,13 @@
+#include "../include/ESMF_LogMacros.inc"
+#include "../include/ESMC_LogErr.h"
+#include "../include/ESMF_LogConstants.inc"
+#include "../include/ESMF_ErrConstants.inc"
+
+int main() {
+
+ESMC_Log aLog;
+int int_num=88;
+aLog.ESMC_LogInit();
+aLog.ESMC_LogInfo("Hi there agent %d",int_num);
+}
+
