5ca87f3a48c66dee78ac33626d18215fa2332b26
Nancy Collins <nancy@ucar.edu>
2003-04-08 22:45:38 +0000

    move include files from src/include to here

---
+++ src/Infrastructure/Base/include/ESMC.h
@@ -0,0 +1,19 @@
+// $Id: ESMC.h,v 1.1 2003/04/08 22:45:38 nscollins Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+//
+// main include file which includes all others
+
+// system dependent #defines from build process
+#include "conf.h"
+
+#include "ESMC_Macros.h"
+
+// TODO: this file needs any external include files added here
+
