2060b8bd69e0f6defc734e4864f34dc0cf9da775
Nancy Collins <nancy@ucar.edu>
2003-04-11 00:09:02 +0000

    fix the fix

--- src/Infrastructure/Field/tests/ESMF_BundleUTest.F90
+++ src/Infrastructure/Field/tests/ESMF_BundleUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_BundleUTest.F90,v 1.11 2003/04/11 00:00:40 nscollins Exp $
+! $Id: ESMF_BundleUTest.F90,v 1.12 2003/04/11 00:09:02 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_BundleUTest.F90,v 1.11 2003/04/11 00:00:40 nscollins Exp $'
+      '$Id: ESMF_BundleUTest.F90,v 1.12 2003/04/11 00:09:02 nscollins Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -504,5 +504,4 @@
 #endif
 
 
-#endif
       end program ESMF_BundleUTest
