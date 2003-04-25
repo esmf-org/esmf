19400f7f1465620c28f92532263021999489267c
Earl Schwab <eschwab.esmf@gmail.com>
2003-04-25 09:00:05 +0000

    standardized decorations

--- src/Infrastructure/TimeMgr/include/ESMC_Fraction.h
+++ src/Infrastructure/TimeMgr/include/ESMC_Fraction.h
@@ -1,49 +1,77 @@
-// $Id: ESMC_Fraction.h,v 1.2 2003/02/11 19:03:32 eschwab Exp $
+// $Id: ESMC_Fraction.h,v 1.3 2003/04/25 09:00:05 eschwab Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+//
+// ESMF Fraction C++ definition include file
+//
+// (all lines below between the !BOP and !EOP markers will be included in
+//  the automated document processing.)
+//-------------------------------------------------------------------------
+//
+ // these lines prevent this file from being read more than once if it
+ // ends up being included multiple times
+
 #ifndef ESMC_FRACTION_H
 #define ESMC_FRACTION_H
 
-#include <ESMC_Base.h>
+//-------------------------------------------------------------------------
 
-class ESMC_Fraction
-{
+ // Put any constants or macros which apply to the whole component in this file.
+ // Anything public or esmf-wide should be up higher at the top level
+ // include files.
+
 //-------------------------------------------------------------------------
 //BOP
 //
-// !CLASS: ESMC_Fraction
-//
-// !SUPERCLASSES:
-//
-// !AGGREGATE CLASSES:
-//
-// !ASSOCIATE CLASSES:
-//
-// !FRIEND CLASSES:
-//
-// !PUBLIC DATA MEMBERS:
-//
-// !PUBLIC MEMBER FUNCTIONS:
-  public:
-
-    ESMC_Fraction(void);
-    ~ESMC_Fraction(void);
+// !CLASS: ESMC_Fraction - represent and manipulate rational fractions
 //
 // !DESCRIPTION:
-//      ESMF C++ {\tt Fraction} class
+//      ESMF C++ {\tt ESMC_Fraction} class
 //
-// !BUGS:
-//
-// !SEE ALSO:
-//
-// !REVISION HISTORY:
-//
-//  10Jun02   Earl Schwab  Initial code.
-//
-//EOP
 //-------------------------------------------------------------------------
+//
+// !USES:
+#include <ESMC_Base.h>  // all classes inherit from the ESMC Base class.
+
+// !PUBLIC TYPES:
+ class ESMC_Fraction;
+
+// !PRIVATE TYPES:
+ // class configuration type:  not needed for ESMC_Fraction
+
+ // class definition type
+class ESMC_Fraction
+{
   protected:
 
     int n;  // Integer fraction (exact) n/d; numerator
     int d;  // Integer fraction (exact) n/d; denominator
-};
+
+// !PUBLIC MEMBER FUNCTIONS:
+
+  public:
+
+    // native C++ constructor/destructor
+    ESMC_Fraction(void);
+    ~ESMC_Fraction(void);
+
+ // < declare the rest of the public interface methods here >
+
+// !PRIVATE MEMBER FUNCTIONS:
+//
+  private:
+//
+ // < declare private interface methods here >
+//
+//EOP
+//-------------------------------------------------------------------------
+
+};  // end class ESMC_Fraction
 
 #endif // ESMC_FRACTION_H
