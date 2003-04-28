83bb2f31a7565e070811e71c9a990a26d82eaff5
Nancy Collins <nancy@ucar.edu>
2003-04-28 17:31:15 +0000

    use correct comment

--- src/Infrastructure/Route/include/ESMF_Route.h
+++ src/Infrastructure/Route/include/ESMF_Route.h
@@ -1,4 +1,4 @@
-! $Id: ESMF_Route.h,v 1.3 2003/03/11 14:19:50 nscollins Exp $
+! $Id: ESMF_Route.h,v 1.4 2003/04/28 17:31:15 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -44,4 +44,4 @@
 
 
 
-#endif  // ESMF_Route_H
+#endif  ! ESMF_Route_H
