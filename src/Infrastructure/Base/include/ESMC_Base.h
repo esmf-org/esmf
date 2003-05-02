6497281a758def23417e3d13beef547931ccc24b
Nancy Collins <nancy@ucar.edu>
2003-05-02 15:34:14 +0000

    fix typo in function name

--- src/Infrastructure/Base/include/ESMC_Base.h
+++ src/Infrastructure/Base/include/ESMC_Base.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Base.h,v 1.16 2003/05/02 14:11:03 nscollins Exp $
+// $Id: ESMC_Base.h,v 1.17 2003/05/02 15:34:14 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -231,6 +231,6 @@ int ESMC_AxisIndexInit(ESMC_AxisIndex *ai, int l, int r, int max,
                            int decomp, int gstart);
 int ESMC_AxisIndexGet(ESMC_AxisIndex *ai, int *l, int *r, int *max, 
                            int *decomp, int *gstart);
-ESMC_Logical ESMC_AxisIndexGet(ESMC_AxisIndex *ai1, ESMC_AxisIndex *ai2);
+ESMC_Logical ESMC_AxisIndexEqual(ESMC_AxisIndex *ai1, ESMC_AxisIndex *ai2);
 
 #endif  // ESMC_BASE_H
