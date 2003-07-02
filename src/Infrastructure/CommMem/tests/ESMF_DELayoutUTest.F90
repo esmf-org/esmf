422fd57bc7de4c8d2b2ea6c08a9b4199c1e51c1a
Nancy Collins <nancy@ucar.edu>
2003-07-02 17:20:01 +0000

    add missing trailing single quote to version string

--- src/Infrastructure/CommMem/tests/ESMF_DELayoutUTest.F90
+++ src/Infrastructure/CommMem/tests/ESMF_DELayoutUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_DELayoutUTest.F90,v 1.6 2003/07/01 22:37:45 nscollins Exp $
+! $Id: ESMF_DELayoutUTest.F90,v 1.7 2003/07/02 17:20:01 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_DELayoutUTest.F90,v 1.6 2003/07/01 22:37:45 nscollins Exp $
+      '$Id: ESMF_DELayoutUTest.F90,v 1.7 2003/07/02 17:20:01 nscollins Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
