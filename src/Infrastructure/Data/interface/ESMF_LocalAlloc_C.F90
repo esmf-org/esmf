6bd580a39fca97b0960c939d2187f1740990a352
John Wolfe <jwolfe@ucar.edu>
2003-07-15 21:04:33 +0000

    Need this file here for allocation routines.

---
+++ src/Infrastructure/Data/interface/ESMF_LocalAlloc_C.F90
@@ -0,0 +1,55 @@
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+! F77 interface files for C++ layer calling into F90 implementation layer.
+!  This cannot use any F90 syntax, including modules, or allocatable 
+!   arrays, or ...
+!
+!==============================================================================
+!
+!------------------------------------------------------------------------------
+! INCLUDES
+#include "ESMF.h"
+!==============================================================================
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+!      character(*), parameter, private :: version = &
+!      '$Id: ESMF_LocalAlloc_C.F90,v 1.1 2003/07/15 21:04:33 jwolfe Exp $'
+!==============================================================================
+   subroutine f_esmf_localarrayf90allocate(array, rank, type, kind, counts, rc)
+       use ESMF_BaseMod    ! ESMF base class
+       use ESMF_LocalArrayMod
+     type(ESMF_LocalArray) :: array
+     integer :: rank
+     type(ESMF_DataType) :: type
+     type(ESMF_DataKind) :: kind
+     integer :: counts(rank)
+     integer, intent(out), optional :: rc     
+
+     ! Beware - these args are not in the same order
+     call ESMF_LocalArrConstrF90Ptr(array, counts, rank, type, kind, rc)
+    
+   end subroutine f_esmf_localarrayf90allocate
+
+   subroutine f_esmf_localarrayf90deallocate(array, rank, type, kind, rc)
+       use ESMF_BaseMod    ! ESMF base class
+       use ESMF_LocalArrayMod
+     type(ESMF_LocalArray) :: array
+     integer :: rank
+     type(ESMF_DataType) :: type
+     type(ESMF_DataKind) :: kind
+     integer, intent(out), optional :: rc     
+
+     call ESMF_LocalArrayF90Deallocate(array, rank, type, kind, rc)
+    
+   end subroutine f_esmf_localarrayf90deallocate
+
+
