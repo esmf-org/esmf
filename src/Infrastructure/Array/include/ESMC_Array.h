9e5538e868b63649da5ba39a5c5c0ab04fd999b2
John Wolfe <jwolfe@ucar.edu>
2003-07-23 16:58:23 +0000

    Changed API for ESMC_ArrayGetAllAxisIndices.

--- src/Infrastructure/Array/include/ESMC_Array.h
+++ src/Infrastructure/Array/include/ESMC_Array.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Array.h,v 1.6 2003/07/22 19:36:49 nscollins Exp $
+// $Id: ESMC_Array.h,v 1.7 2003/07/23 16:58:23 jwolfe Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -96,8 +96,9 @@ class ESMC_Array : public ESMC_LocalArray {  // inherits from LocalArray class
  // optional index values for subsetting and handling arrays standalone
     int ESMC_ArrayGetAxisIndex(ESMC_DomainType dt, struct ESMC_AxisIndex *index) const;
     int ESMC_ArraySetAxisIndex(ESMC_DomainType dt, struct ESMC_AxisIndex *index);
-    int ESMC_ArrayGetAllAxisIndices(ESMC_Grid *grid, struct ESMC_AxisIndex *total,
-                 struct ESMC_AxisIndex *comp, struct ESMC_AxisIndex *excl) const; 
+    int ESMC_ArrayGetAllAxisIndices(struct ESMC_AxisIndex *global, int nDEs,
+                 struct ESMC_AxisIndex *total, struct ESMC_AxisIndex *comp,
+                 struct ESMC_AxisIndex *excl) const; 
 
     
  // accessor methods for class members
