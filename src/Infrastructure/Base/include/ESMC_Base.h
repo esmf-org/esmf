7f6680d99acb29a9c393651493aa17422bf90787
John Wolfe <jwolfe@ucar.edu>
2003-08-26 22:40:52 +0000

    Working on the DomainList structure.

--- src/Infrastructure/Base/include/ESMC_Base.h
+++ src/Infrastructure/Base/include/ESMC_Base.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Base.h,v 1.20 2003/08/22 21:49:19 jwolfe Exp $
+// $Id: ESMC_Base.h,v 1.21 2003/08/26 22:40:52 jwolfe Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -139,7 +139,7 @@ struct ESMC_Domain {
 struct ESMC_DomainList {
     int num_domains;
     int current_size;
-    struct ESMC_Domain domains[ESMF_MAXDIM];  // not right, just temp
+    struct ESMC_Domain *domains;
 };
 
 // !PRIVATE TYPES:
