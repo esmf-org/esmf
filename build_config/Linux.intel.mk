f609a0654a2224abffffc55c265caf3d797f6964
Nancy Collins <nancy@ucar.edu>
2003-09-09 15:17:43 +0000

    these are NOT error checked and are based on the makefiles as of march - but they are here just as placeholders until dennis puts in the real versions

---
+++ build_config/Linux.intel.mk
@@ -0,0 +1,130 @@
+# $Id: Linux.intel.mk,v 1.1 2003/09/09 15:17:43 nscollins Exp $ 
+
+# Location of MPI (Message Passing Interface) software
+
+#ESMC_MPIRUN      = 
+MPI_HOME       = ${ESMF_DIR}/src/Infrastructure/mpiuni
+MPI_LIB        = -lmpiuni
+MPI_INCLUDE    = -I${MPI_HOME}
+MPIRUN         =  ${MPI_HOME}/mpirun
+
+# MP_LIB is for openMP
+MP_LIB          = -L/usr/lib/mpi/lib -lmpi 
+# For pthreads (or omp)
+THREAD_LIB      = -lpthread
+
+#
+AR		   = ar
+AR_FLAGS	   = cr
+RM		   = rm -f
+OMAKE		   = ${MAKE}
+RANLIB		   = ranlib
+SHELL		   = /bin/sh
+SED		   = /bin/sed
+SH_LD		   = ecc 
+# ######################### C and Fortran compiler ########################
+#
+C_CC		   = ecc 
+C_FC		   = efc
+C_FC_MOD           = -I
+C_CLINKER_SLFLAG   = -Wl,-rpath,
+C_FLINKER_SLFLAG   = -Wl,-rpath,
+C_CLINKER	   = ecc
+C_FLINKER	   = efc
+C_CCV		   = ${C_CC} -V -c -w -x c
+C_FCV              = efc -V -c -w
+C_SYS_LIB	   = -ldl -lc -lg2c -lm
+#C_SYS_LIB	   = -ldl -lc -lf2c -lm
+#C_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm  #Use /usr/lib/libf2c.a if that's what your f77 uses.
+# ---------------------------- BOPT - g options ----------------------------
+G_COPTFLAGS	   = -g 
+G_FOPTFLAGS	   = -g
+# ----------------------------- BOPT - O options -----------------------------
+O_COPTFLAGS	   = -O 
+O_FOPTFLAGS	   = -O
+# ########################## C++ compiler ##################################
+#
+CXX_CC		   = ecc 
+#CXX_CC		   = ecc -mp 
+CXX_FC		   = efc -mp 
+CXX_CLINKER_SLFLAG = -Wl,-rpath,
+CXX_FLINKER_SLFLAG = -Wl,-rpath,
+CXX_CLINKER	   = ecc
+CXX_FLINKER	   = ecc
+CXX_CCV		   = ${CXX_CC} -V -c -w -x c++
+#CXX_SYS_LIB	   = -ldl -lc -lf2c -lm
+CXX_SYS_LIB	   = -ldl -lc -lg2c -lm
+#CXX_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm
+C_F90CXXLD         = efc -mp
+C_F90CXXLIBS       = -lcprts
+C_CXXF90LD         = ecc 
+C_CXXF90LIBS       = -lCEPCF90 -lIEPCF90 -lF90 -lintrins
+C_CXXSO            = ecc -shared
+# ------------------------- BOPT - g_c++ options ------------------------------
+GCXX_COPTFLAGS	   = -g 
+GCXX_FOPTFLAGS	   = -g
+# ------------------------- BOPT - O_c++ options ------------------------------
+OCXX_COPTFLAGS	   = -O 
+OCXX_FOPTFLAGS	   = -O
+# -------------------------- BOPT - g_complex options ------------------------
+GCOMP_COPTFLAGS	   = -g
+GCOMP_FOPTFLAGS	   = -g
+# --------------------------- BOPT - O_complex options -------------------------
+OCOMP_COPTFLAGS	   = -O
+OCOMP_FOPTFLAGS	   = -O
+##################################################################################
+
+PARCH		   = linux_intel
+
+#########
+
+.F90.o:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -cpp -FR ${FCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.F.o:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -cpp0 -FR ${ESMC_INCLUDE} $<
+
+.f90.o:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -cpp -FI ${FCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.f.o:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -cpp0 -FI ${ESMC_INCLUDE} $<
+
+.c.o:
+	${CC} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.C.o:
+	${CXX} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.F90.a:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -cpp -FR ${FCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.F.a:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -cpp0 -FR ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.f90.a:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -cpp -FI ${FCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.f.a:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -cpp0 -FI ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.c.a:
+	${CC} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.C.a:
+	${CXX} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+#############
+
---
+++ build_config/Linux.pgi.mk
@@ -0,0 +1,136 @@
+# $Id: Linux.pgi.mk,v 1.1 2003/09/09 15:17:43 nscollins Exp $ 
+#
+ESMF_PREC 32
+
+#     See the file build/base_variables.defs for a complete explanation of all these fields
+#
+AR		   = ar
+AR_FLAGS	   = cr
+RM		   = rm -f
+OMAKE		   = ${MAKE}
+RANLIB		   = ranlib
+SHELL		   = /bin/sh
+SED		   = /bin/sed
+SH_LD		   = pgcc 
+# ######################### C and Fortran compiler ########################
+#
+C_CC		   = pgcc -mp
+C_FC		   = pgf90 -mp
+C_FC_MOD           = -I
+C_CLINKER_SLFLAG   = -Wl,-rpath,
+C_FLINKER_SLFLAG   = -Wl,-rpath,
+C_CLINKER	   = pgcc
+C_FLINKER	   = pgf90
+C_CCV		   = ${C_CC} --version
+C_FCV              = pgf90 -mp
+C_SYS_LIB	   = -ldl -lc -lg2c -lm
+#C_SYS_LIB	   = -ldl -lc -lf2c -lm
+#C_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm  #Use /usr/lib/libf2c.a if that's what your f77 uses.
+# ---------------------------- BOPT - g options ----------------------------
+G_COPTFLAGS	   = -g 
+G_FOPTFLAGS	   = -g
+# ----------------------------- BOPT - O options -----------------------------
+O_COPTFLAGS	   = -O 
+O_FOPTFLAGS	   = -O
+# ########################## C++ compiler ##################################
+#
+CXX_CC		   = pgCC -tlocal
+# TODO:  upgrade PGI suite?  "pgCC -mp" disables C++ exception handling !
+#        should be ok for now since we're not using OpenMP in the 1st ESMF ver.
+#CXX_CC		   = pgCC -mp -tlocal
+CXX_FC		   = pgf90 -mp -tlocal
+CXX_CLINKER_SLFLAG = -Wl,-rpath,
+CXX_FLINKER_SLFLAG = -Wl,-rpath,
+CXX_CLINKER	   = pgCC
+CXX_FLINKER	   = pgCC
+CXX_CCV		   = ${CXX_CC} --version
+#CXX_SYS_LIB	   = -ldl -lc -lf2c -lm
+CXX_SYS_LIB	   = -ldl -lc -lg2c -lm
+#CXX_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm
+C_F90CXXLD         = pgf90 -mp
+C_F90CXXLIBS       = -lpgc -lstd -lC
+C_CXXF90LD         = pgCC 
+C_CXXF90LIBS       = -lpgf90 -lpgf90_rpm1 -lpgf902 -lpgf90rtl -lpgftnrtl
+C_CXXSO            = pgCC -shared
+# ------------------------- BOPT - g_c++ options ------------------------------
+GCXX_COPTFLAGS	   = -g 
+GCXX_FOPTFLAGS	   = -g
+# ------------------------- BOPT - O_c++ options ------------------------------
+OCXX_COPTFLAGS	   = -O 
+OCXX_FOPTFLAGS	   = -O
+# -------------------------- BOPT - g_complex options ------------------------
+GCOMP_COPTFLAGS	   = -g
+GCOMP_FOPTFLAGS	   = -g
+# --------------------------- BOPT - O_complex options -------------------------
+OCOMP_COPTFLAGS	   = -O
+OCOMP_FOPTFLAGS	   = -O
+##################################################################################
+
+PARCH		   = linux
+
+
+#########
+
+.F90.o:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} ${FCPPFLAGS} -Mpreprocess -Mfreeform ${ESMC_INCLUDE} $<
+
+.F.o:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -Mfreeform ${ESMC_INCLUDE} $<
+
+.f90.o:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} ${FCPPFLAGS} -Mpreprocess -Mnofreeform ${ESMC_INCLUDE} $<
+
+.f.o:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -Mnofreeform ${ESMC_INCLUDE} $<
+
+.c.o:
+	${CC} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.C.o:
+	${CXX} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+
+.F90.a:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} ${FCPPFLAGS} -Mpreprocess -Mfreeform ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.F.a:
+	${FC} -c ${C_FC_MOD}${ESMC_MODDIR} ${FOPTFLAGS} ${FFLAGS} -Mfreeform ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.f90.a:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} ${FCPPFLAGS} -Mpreprocess -Mnofreeform ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.f.a:
+	${FC} -c ${FOPTFLAGS} ${FFLAGS} -Mnofreeform ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.c.a:
+	${CC} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+.C.a:
+	${CXX} -c ${COPTFLAGS} ${CFLAGS} ${CCPPFLAGS} ${ESMC_INCLUDE} $<
+	${AR} ${AR_FLAGS} ${LIBNAME} $*.o
+	${RM} $*.o
+
+#############
+
+
+# Location of MPI (Message Passing Interface) software
+
+#ESMC_MPIRUN      = 
+MPI_HOME       = ${ESMF_DIR}/src/Infrastructure/mpiuni
+MPI_LIB        = -lmpiuni
+MPI_INCLUDE    = -I${MPI_HOME}
+MPIRUN         =  ${MPI_HOME}/mpirun
+
+# MP_LIB is for openMP
+MP_LIB          = -lpgmp
+# For pthreads (or omp)
+THREAD_LIB      = -lpthread
