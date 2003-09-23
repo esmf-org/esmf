1013d088fb54ee950266b467accd9122ba3e49ab
Nancy Collins <nancy@ucar.edu>
2003-09-23 15:19:26 +0000

    C++ entry points and inteface code

---
+++ src/Superstructure/Component/interface/ESMC_CplComp.C
@@ -0,0 +1,375 @@
+// $Id: ESMC_CplComp.C,v 1.1 2003/09/23 15:19:26 nscollins Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+
+// ESMC Component method implementation (body) file
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+// The code in this file implements the C++ {\tt Component} methods declared
+// in the companion file {\tt ESMC\_CplComp.h}.  They are wrappers for the
+// actual code which is implemented in F90.
+//
+//-----------------------------------------------------------------------------
+//
+ // insert any higher level, 3rd party or system includes here
+#include <string.h>
+#include <stdio.h>
+#include "ESMC.h"
+
+//-----------------------------------------------------------------------------
+//BOP
+// !CLASS: Component object
+//
+// !DESCRIPTION:
+//  Component class which provides interfaces to the Fortran implementation
+//    of Components.
+//EOP
+//-----------------------------------------------------------------------------
+
+ // associated class definition file
+#include "ESMC_Comp.h"
+#include "ESMC_CplComp.h"
+
+ // return min value 
+#define min(a,b)  (((a)<(b))?(a):(b))
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+ static const char *const version = 
+           "$Id: ESMC_CplComp.C,v 1.1 2003/09/23 15:19:26 nscollins Exp $";
+//-----------------------------------------------------------------------------
+
+//
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section includes all the Component routines
+//
+//
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompCreate - Create a new Component
+//
+// !INTERFACE:
+      ESMC_CplComp *ESMC_CplCompCreate(
+//
+// !RETURN VALUE:
+//     pointer to newly allocated ESMC_CplComp
+//
+// !ARGUMENTS:
+      char *name,
+      ESMC_DELayout *layout,
+      enum ESMC_ModelType mtype,
+      char *configfile,
+      int *rc) {           // out - return code
+//
+// !DESCRIPTION:
+//      C++ interface for creating a new {\tt Component}.  Calls into F90 
+//      code to allocate the actual object.
+//
+//      Note: this is a class helper function, not a class method
+//      (see declaration in {\tt ESMC\_CplComp.h})
+//
+//EOP
+// !REQUIREMENTS:  AAAn.n.n
+
+    ESMC_CplComp *comp;
+
+    comp = new ESMC_CplComp;
+    FTN(f_esmf_cplcompcreate)(comp, name, layout, NULL, configfile, 
+                             rc, strlen(name), strlen(configfile));
+
+    return comp;
+
+ } // end ESMC_CplCompCreate
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompDestroy - free a Component created with Create
+//
+// !INTERFACE:
+      int ESMC_CplCompDestroy(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_CplComp *component) {    // in - component object to destroy
+//
+// !DESCRIPTION:
+//      ESMF routine which destroys a {\tt Component} object previously 
+//      allocated via an {\tt ESMC\_CplCompCreate} routine. 
+//
+//      Note: this is a class helper function, not a class method
+//      (see declaration in {\tt ESMC\_CplComp.h})
+//
+//EOP
+// !REQUIREMENTS:  
+
+    int rc;
+
+    FTN(f_esmf_cplcompdestroy)(component, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompDestroy
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompInitialize - call the Component init Routine
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompInitialize(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of init to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Initialize routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_cplcompinitialize)(this, importstate, exportstate, clock, 
+                                   &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompInitialize
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompRun - call the Component run Routine
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompRun(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of run to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Run routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_cplcomprun)(this, importstate, exportstate, clock, &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompRun
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompFinalize - call the Component finalize Routine
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompFinalize(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of finalize to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Finalize routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_cplcompfinalize)(this, importstate, exportstate, clock, 
+                                 &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompFinalize
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompGet - get info from a Component
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompGet(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      char *name) const {     // out -  etc - all add queries
+//
+// !DESCRIPTION:
+//    Returns information about the Component object.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_cplcompget)(this, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompGet
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompSet - set info for a Component
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompSet(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *name) {     // in - what exactly is reasonable here?
+//
+// !DESCRIPTION:
+//    Sets information associated with the Component object.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_cplcompset)(this, &rc);
+
+    return rc;
+
+ } // end ESMC_CplCompSet
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompValidate - internal consistency check for a Component
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompValidate(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *options) const {    // in - validate options
+//
+// !DESCRIPTION:
+//      Validates that a Component is internally consistent.
+//      Returns error code if problems are found.  ESMC\_Base class method.
+//
+//EOP
+
+    int rc;
+    const ESMC_CplComp *comp;
+
+    comp = this;
+    FTN(f_esmf_cplcompvalidate)(comp, options, &rc, strlen(options));
+
+    return rc;
+
+ } // end ESMC_CplCompValidate
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplCompPrint - print contents of a Component
+//
+// !INTERFACE:
+      int ESMC_CplComp::ESMC_CplCompPrint(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *options) const {     //  in - print options
+//
+// !DESCRIPTION:
+//      Print information about a Component.  The options control the
+//      type of information and level of detail.  ESMC\_Base class method.
+//
+//EOP
+
+    int rc;
+    const ESMC_CplComp *comp;
+
+    comp = this;
+    FTN(f_esmf_cplcompprint)(comp, options, &rc, strlen(options));
+
+    return rc;
+
+ } // end ESMC_CplCompPrint
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_CplComp - native C++ constructor
+//
+// !INTERFACE:
+      ESMC_CplComp::ESMC_CplComp(
+//
+// !RETURN VALUE:
+//    none
+//
+// !ARGUMENTS:
+      void) {
+//
+// !DESCRIPTION:
+//
+//EOP
+// !REQUIREMENTS:  SSSn.n, GGGn.n
+
+//
+//  TODO: what code goes here?
+//
+
+ } // end ESMC_CplComp
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ~ESMC_CplComp - native C++ destructor
+//
+// !INTERFACE:
+      ESMC_CplComp::~ESMC_CplComp(void) {
+//
+// !RETURN VALUE:
+//    none
+//
+// !ARGUMENTS:
+//    none
+//
+// !DESCRIPTION:
+//
+//EOP
+
+//
+//  TODO: what code goes here?
+//
+
+ } // end ~ESMC_CplComp
---
+++ src/Superstructure/Component/interface/ESMC_GridComp.C
@@ -0,0 +1,372 @@
+// $Id: ESMC_GridComp.C,v 1.1 2003/09/23 15:19:26 nscollins Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+
+// ESMC Component method implementation (body) file
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+// The code in this file implements the C++ {\tt Component} methods declared
+// in the companion file {\tt ESMC\_GridComp.h}.  They are wrappers for the
+// actual code which is implemented in F90.
+//
+//-----------------------------------------------------------------------------
+//
+ // insert any higher level, 3rd party or system includes here
+#include <string.h>
+#include <stdio.h>
+#include "ESMC.h"
+
+//-----------------------------------------------------------------------------
+//BOP
+// !CLASS: Component object
+//
+// !DESCRIPTION:
+//  Component class which provides interfaces to the Fortran implementation
+//    of Components.
+//EOP
+//-----------------------------------------------------------------------------
+
+ // associated class definition file
+#include "ESMC_Comp.h"
+#include "ESMC_GridComp.h"
+
+ // return min value 
+#define min(a,b)  (((a)<(b))?(a):(b))
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+ static const char *const version = 
+           "$Id: ESMC_GridComp.C,v 1.1 2003/09/23 15:19:26 nscollins Exp $";
+//-----------------------------------------------------------------------------
+
+//
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section includes all the Component routines
+//
+//
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompCreate - Create a new Component
+//
+// !INTERFACE:
+      ESMC_GridComp *ESMC_GridCompCreate(
+//
+// !RETURN VALUE:
+//     pointer to newly allocated ESMC_GridComp
+//
+// !ARGUMENTS:
+      char *name,
+      ESMC_DELayout *layout,
+      enum ESMC_ModelType mtype,
+      ESMC_Grid *grid,
+      char *configfile,
+      int *rc) {           // out - return code
+//
+// !DESCRIPTION:
+//      C++ interface for creating a new {\tt Component}.  Calls into F90 
+//      code to allocate the actual object.
+//
+//      Note: this is a class helper function, not a class method
+//      (see declaration in {\tt ESMC\_GridComp.h})
+//
+//EOP
+// !REQUIREMENTS:  AAAn.n.n
+
+    ESMC_GridComp *comp;
+
+    comp = new ESMC_GridComp;
+    FTN(f_esmf_gridcompcreate)(comp, name, layout, &mtype, grid, NULL, 
+                         configfile, rc, strlen(name), strlen(configfile));
+
+    return comp;
+
+ } // end ESMC_GridCompCreate
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompDestroy - free a Component created with Create
+//
+// !INTERFACE:
+      int ESMC_GridCompDestroy(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_GridComp *component) {    // in - component object to destroy
+//
+// !DESCRIPTION:
+//      ESMF routine which destroys a {\tt Component} object previously 
+//      allocated via an {\tt ESMC\_GridCompCreate} routine. 
+//
+//      Note: this is a class helper function, not a class method
+//      (see declaration in {\tt ESMC\_GridComp.h})
+//
+//EOP
+// !REQUIREMENTS:  
+
+    int rc;
+
+    FTN(f_esmf_gridcompdestroy)(component, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompDestroy
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompInitialize - call the Component init Routine
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompInitialize(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of init to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Initialize routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompinitialize)(this, importstate, exportstate, clock, 
+                                   &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompInitialize
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompRun - call the Component run Routine
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompRun(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of run to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Run routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcomprun)(this, importstate, exportstate, clock, &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompRun
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompFinalize - call the Component finalize Routine
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompFinalize(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      ESMC_State *importstate,   // in/out: required data
+      ESMC_State *exportstate,   // in/out: produced data
+      ESMC_Clock *clock,         // in: model clock
+      int phase)  {              // in: if > 0, which phase of finalize to invoke
+//
+// !DESCRIPTION:
+//    Invokes the Finalize routine for an {\tt ESMC\_Component}.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompfinalize)(this, importstate, exportstate, clock, 
+                                 &phase, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompFinalize
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompGet - get info from a Component
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompGet(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      char *name) const {     // out -  etc - all add queries
+//
+// !DESCRIPTION:
+//    Returns information about the Component object.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompget)(this, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompGet
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompSet - set info for a Component
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompSet(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *name) {     // in - what exactly is reasonable here?
+//
+// !DESCRIPTION:
+//    Sets information associated with the Component object.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompset)(this, &rc);
+
+    return rc;
+
+ } // end ESMC_GridCompSet
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompValidate - internal consistency check for a Component
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompValidate(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *options) const {    // in - validate options
+//
+// !DESCRIPTION:
+//      Validates that a Component is internally consistent.
+//      Returns error code if problems are found.  ESMC\_Base class method.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompvalidate)(this, options, &rc, strlen(options));
+
+    return rc;
+
+ } // end ESMC_GridCompValidate
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridCompPrint - print contents of a Component
+//
+// !INTERFACE:
+      int ESMC_GridComp::ESMC_GridCompPrint(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+      const char *options) const {     //  in - print options
+//
+// !DESCRIPTION:
+//      Print information about a Component.  The options control the
+//      type of information and level of detail.  ESMC\_Base class method.
+//
+//EOP
+
+    int rc;
+
+    FTN(f_esmf_gridcompprint)(this, options, &rc, strlen(options));
+
+    return rc;
+
+ } // end ESMC_GridCompPrint
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_GridComp - native C++ constructor
+//
+// !INTERFACE:
+      ESMC_GridComp::ESMC_GridComp(
+//
+// !RETURN VALUE:
+//    none
+//
+// !ARGUMENTS:
+      void) {
+//
+// !DESCRIPTION:
+//
+//EOP
+// !REQUIREMENTS:  SSSn.n, GGGn.n
+
+//
+//  TODO: what code goes here?
+//
+
+ } // end ESMC_GridComp
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ~ESMC_GridComp - native C++ destructor
+//
+// !INTERFACE:
+      ESMC_GridComp::~ESMC_GridComp(void) {
+//
+// !RETURN VALUE:
+//    none
+//
+// !ARGUMENTS:
+//    none
+//
+// !DESCRIPTION:
+//
+//EOP
+
+//
+//  TODO: what code goes here?
+//
+
+ } // end ~ESMC_GridComp
