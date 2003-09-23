7508e41751ca38ec33fff5d6a68429815c222c54
Nancy Collins <nancy@ucar.edu>
2003-09-23 15:22:55 +0000

    test of C++ interfaces to components

---
+++ src/Superstructure/Component/tests/ESMC_CompCreate.C
@@ -0,0 +1,62 @@
+// $Id: ESMC_CompCreate.C,v 1.1 2003/09/23 15:22:55 nscollins Exp $
+//
+// Test code which creates a new Component.
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+//BOP
+//
+// !DESCRIPTION:
+// Tests, cursory and exahustive, for Component Create code.
+//
+//
+//\begin{verbatim}
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "ESMC.h"
+#include "ESMC_Comp.h"
+#include "ESMC_GridComp.h"
+
+
+int main(int argc, char **argv)
+{
+    
+//  Local variables
+    int rc;
+    char *cname;
+    ESMC_DELayout *delayout;
+    ESMC_GridComp *comp1;
+        
+//-------------------------------------------------------------------------
+//   Test 1:
+//   
+//   Quick Test - Create, Init, Run, Finalize, Destroy a Component.
+
+    rc = ESMC_FrameworkInitialize();
+ 
+    printf("Component Test 1:\n");
+
+    // TODO: query framework for default layout here
+
+    cname = "Atmosphere";
+    delayout = NULL;
+    comp1 = ESMC_GridCompCreate(cname, delayout, ESMF_ATM, NULL, "grid.rc", &rc);
+
+    printf("Grid Comp Create returned, name = '%s'\n", cname);
+
+    rc = comp1->ESMC_GridCompPrint("");
+    printf("Comp Print returned\n");
+
+    rc = ESMC_GridCompDestroy(comp1);
+    printf("Comp Run returned\n");
+
+    printf("Component Test 1 finished\n");
+
+    rc = ESMC_FrameworkFinalize();
+
+    exit (0);
+}
+//\end{verbatim}
+    
