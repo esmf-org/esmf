28f0d471c399c8de2ae3ac4da54514dda902397d
Nancy Collins <nancy@ucar.edu>
2003-10-16 20:38:53 +0000

    working on jazz

--- build_config/Linux.pgi.default.mk
+++ build_config/Linux.pgi.default.mk
@@ -1,10 +1,10 @@
-# $Id: Linux.pgi.default.mk,v 1.1 2003/09/25 21:21:53 flanigan Exp $
+# $Id: Linux.pgi.default.mk,v 1.2 2003/10/16 20:38:53 nscollins Exp $
 #
 #  Linux.pgi.default.mk
 #
 
 #
-#  Make sure precession is 32.
+#  Make sure precision is 32.
 #
 ESMF_PREC = 32
 
@@ -21,11 +21,21 @@ endif
 
 # Location of MPI (Message Passing Interface) software
 
-#ESMC_MPIRUN      = 
+ifeq ($(ESMF_COMM),mpiuni)
+# this section is set up to bypass all MPI
 MPI_HOME       = ${ESMF_DIR}/src/Infrastructure/mpiuni
 MPI_LIB        = -lmpiuni
 MPI_INCLUDE    = -I${MPI_HOME}
 MPIRUN         =  ${MPI_HOME}/mpirun
+endif
+
+ifeq ($(ESMF_COMM),mpich)
+# set up to use MPICH
+MPI_HOME       = 
+MPI_LIB        = -lmpich
+MPI_INCLUDE    = -DESMF_MPICH=1
+MPIRUN         = mpirun
+endif
 
 # MP_LIB is for openMP
 MP_LIB          = -lpgmp
@@ -44,15 +54,27 @@ SED		   = /bin/sed
 SH_LD		   = pgcc 
 # ######################### C and Fortran compiler ########################
 #
+ifneq ($(ESMF_COMM),mpich)
 C_CC		   = pgcc -mp
 C_FC		   = pgf90 -mp
+CXX_CC		   = pgCC -tlocal
+CXX_FC		   = pgf90 -mp -tlocal
+endif
+
+ifeq ($(ESMF_COMM),mpich)
+C_CC		   = mpicc -mp
+C_FC		   = mpif90 -mp
+CXX_CC		   = mpiCC -tlocal
+CXX_FC		   = mpif90 -mp -tlocal
+endif
+
 C_FC_MOD           = -I
 C_CLINKER_SLFLAG   = -Wl,-rpath,
 C_FLINKER_SLFLAG   = -Wl,-rpath,
-C_CLINKER	   = pgcc
-C_FLINKER	   = pgf90
+C_CLINKER	   = ${C_CC}
+C_FLINKER	   = ${C_FC}
 C_CCV		   = ${C_CC} --version
-C_FCV              = pgf90 -mp
+C_FCV              = ${C_FC}
 C_SYS_LIB	   = -ldl -lc -lg2c -lm
 #C_SYS_LIB	   = -ldl -lc -lf2c -lm
 #C_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm  #Use /usr/lib/libf2c.a if that's what your f77 uses.
@@ -64,24 +86,19 @@ O_COPTFLAGS	   = -O
 O_FOPTFLAGS	   = -O
 # ########################## C++ compiler ##################################
 #
-CXX_CC		   = pgCC -tlocal
-# TODO:  upgrade PGI suite?  "pgCC -mp" disables C++ exception handling !
-#        should be ok for now since we're not using OpenMP in the 1st ESMF ver.
-#CXX_CC		   = pgCC -mp -tlocal
-CXX_FC		   = pgf90 -mp -tlocal
 CXX_CLINKER_SLFLAG = -Wl,-rpath,
 CXX_FLINKER_SLFLAG = -Wl,-rpath,
-CXX_CLINKER	   = pgCC
-CXX_FLINKER	   = pgCC
+CXX_CLINKER	   = ${CXX_CC}
+CXX_FLINKER	   = ${CXX_CC}
 CXX_CCV		   = ${CXX_CC} --version
 #CXX_SYS_LIB	   = -ldl -lc -lf2c -lm
 CXX_SYS_LIB	   = -ldl -lc -lg2c -lm
 #CXX_SYS_LIB	   = -ldl -lc /usr/lib/libf2c.a -lm
-C_F90CXXLD         = pgf90 -mp
+C_F90CXXLD         = ${CXX_FC} -mp
 C_F90CXXLIBS       = -lpgc -lstd -lC
-C_CXXF90LD         = pgCC 
+C_CXXF90LD         = ${CXX_CC} 
 C_CXXF90LIBS       = -lpgf90 -lpgf90_rpm1 -lpgf902 -lpgf90rtl -lpgftnrtl
-C_CXXSO            = pgCC -shared
+C_CXXSO            = ${CXX_CC} -shared
 # ------------------------- BOPT - g_c++ options ------------------------------
 GCXX_COPTFLAGS	   = -g 
 GCXX_FOPTFLAGS	   = -g
