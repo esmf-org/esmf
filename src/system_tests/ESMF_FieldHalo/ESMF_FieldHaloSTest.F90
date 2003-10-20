811dea5a40107331b1c8b872bf1b3b5b9dd971a2
Nancy Collins <nancy@ucar.edu>
2003-10-20 21:51:31 +0000

    test for 4 DEs before creating a 4-way DE list

--- src/system_tests/ESMF_FieldHalo/ESMF_FieldHaloSTest.F90
+++ src/system_tests/ESMF_FieldHalo/ESMF_FieldHaloSTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldHaloSTest.F90,v 1.3 2003/10/20 20:13:58 cdeluca Exp $
+! $Id: ESMF_FieldHaloSTest.F90,v 1.4 2003/10/20 21:51:31 nscollins Exp $
 !
 ! System test FieldHalo
 !  Description on Sourceforge under System Test #70385
@@ -71,6 +71,10 @@
     !    print *, "This system test needs to run 12-way, current np = ", ndes
     !    goto 10
     !endif
+    if (ndes .ne. 4) then
+        print *, "This system test needs to run 4-way, current np = ", ndes
+        goto 10
+    endif
 
     call ESMF_DELayoutGetDEId(deflayout, de_id, rc) 
     if (rc .ne. ESMF_SUCCESS) goto 10
