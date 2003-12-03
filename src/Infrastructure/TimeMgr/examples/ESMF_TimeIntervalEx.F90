651b17864c001f9f756dec7075464e41320fe868
Silverio Vasquez <silverio.vasquez@noaa.gov>
2003-12-03 17:29:37 +0000

    Added intermediate rc's checking and setting of final rc.

--- src/Infrastructure/TimeMgr/examples/ESMF_TimeIntervalEx.F90
+++ src/Infrastructure/TimeMgr/examples/ESMF_TimeIntervalEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_TimeIntervalEx.F90,v 1.13 2003/10/22 02:33:09 eschwab Exp $
+! $Id: ESMF_TimeIntervalEx.F90,v 1.14 2003/12/03 17:29:37 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -30,7 +30,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_TimeIntervalEx.F90,v 1.13 2003/10/22 02:33:09 eschwab Exp $'
+      '$Id: ESMF_TimeIntervalEx.F90,v 1.14 2003/12/03 17:29:37 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! instantiate some time intervals
@@ -43,35 +43,80 @@
       type(ESMF_TimeInterval) :: remainder
 
       ! result code
-      integer :: rc
+      integer :: rc, finalrc
+      FINALrc = ESMF_SUCCESS
 
       ! initialize time interval1 to 1 day, 1800 seconds (0.5 hour)
       call ESMF_TimeIntervalSet(timeInterval1, d=1, s=1800, rc=rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! initialize time interval2 to 4 days, 5400 seconds (1.5 hours)
       call ESMF_TimeIntervalSet(timeInterval2, d=4, s=5400, rc=rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       call ESMF_TimeIntervalGet(timeInterval1, d=D, h=H, m=M, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Time Interval1 = ", D, " days, ", H, " hours, ", M, " minutes."
       call ESMF_TimeIntervalPrint(timeInterval1, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       call ESMF_TimeIntervalGet(timeInterval2, d=D, h=H, m=M, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Time Interval2 = ", D, " days, ", H, " hours, ", M, " minutes."
       call ESMF_TimeIntervalPrint(timeInterval2, "string", rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
 
       ! difference
       timeInterval3 = timeInterval2 - timeInterval1
       call ESMF_TimeIntervalGet(timeInterval3, d=D, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Difference between TimeInterval2 and TimeInterval1 = ", &
                D, " days, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! sum
       timeInterval3 = timeInterval2 + timeInterval1
       call ESMF_TimeIntervalGet(timeInterval3, d=D, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Sum of TimeInterval1 and TimeInterval2 = ", D, " days, ", &
                S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
 
       ! divide
       quotient = timeInterval2 / timeInterval1
@@ -82,63 +127,139 @@
       print *, "TimeInterval2 modulo TimeInterval1 = " 
       call ESMF_TimeIntervalPrint(remainder, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! divide by integer
       timeInterval3 = timeInterval2 / 2
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "TimeInterval2 divided by 2 = ", D, " days, ", H, " hours, ", &
                M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
 
       ! divide by double precision real
       divisor = 1.5
       timeInterval3 = timeInterval2 / divisor
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "TimeInterval2 divided by 1.5 = ", D, " days, ", H, " hours, ", &
                M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! multiply by integer
       timeInterval3 = timeInterval1 * 3
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "TimeInterval1 multiplied by 3 = ", D, " days, ", H, &
                " hours, ", M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
 
       ! multiply by double precision real
       multiplier = 2.25
       timeInterval3 = timeInterval1 * multiplier
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "TimeInterval1 multiplied by 2.25 = ", D, " days, ", H, &
                " hours, ", M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! change time interval 1 to negative value
       timeInterval1 = timeInterval1 * (-1)
       call ESMF_TimeIntervalGet(timeInterval1, d=D, h=H, m=M, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Time Interval1 changed to ", D, " days, ", H, " hours, ", &
                M, " minutes."
       call ESMF_TimeIntervalPrint(timeInterval1, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! absolute value
       timeInterval3 = ESMF_TimeIntervalAbsValue(timeInterval1)
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Absolute value of TimeInterval1 = ", D, " days, ", H, &
                " hours, ", M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! negative absolute value of time interval 1
       timeInterval3 = ESMF_TimeIntervalNegAbsValue(timeInterval1)
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Negative absolute value of TimeInterval1 = ", D, " days, ", H, &
                " hours, ", M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
 
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       ! negative absolute value of time interval 2
       timeInterval3 = ESMF_TimeIntervalNegAbsValue(timeInterval2)
       call ESMF_TimeIntervalGet(timeInterval3, d=D, h=H, m=M, s=S, rc=rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
+
       print *, "Negative absolute value of TimeInterval2 = ", D, " days, ", H, &
                " hours, ", M, " minutes, ", S, " seconds."
       call ESMF_TimeIntervalPrint(timeInterval3, "string", rc)
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+      end if
 
       ! comparison
       if (timeInterval2 > timeInterval1) then
@@ -148,5 +269,12 @@
       else if (timeInterval2 == timeInterval1) then
         print *, "TimeInterval2 is equal to TimeInterval1"
       end if
+
+     if (finalrc.EQ.ESMF_SUCCESS) then
+        print *, "PASS: ESMF_TimeIntervalEx.F90"
+     else
+        print *, "FAIL: ESMF_TimeIntervalEx.F90"
+     end if
+
 
       end program ESMF_TimeIntervalEx
