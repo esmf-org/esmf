dd7f9c5e5620ed1b0de018218f8614e9e248c8d0
Silverio Vasquez <silverio.vasquez@noaa.gov>
2003-12-18 17:08:21 +0000

    Add Latex commands to eliminate rc checks from documents.

--- src/Infrastructure/TimeMgr/examples/ESMF_ClockEx.F90
+++ src/Infrastructure/TimeMgr/examples/ESMF_ClockEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ClockEx.F90,v 1.27 2003/12/02 23:50:06 svasquez Exp $
+! $Id: ESMF_ClockEx.F90,v 1.28 2003/12/18 17:08:21 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -13,25 +13,23 @@
       program ESMF_ClockEx
 
 !------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_ClockEx.F90,v 1.28 2003/12/18 17:08:21 svasquez Exp $'
+!------------------------------------------------------------------------------
 !
 !==============================================================================
 !BOP
+!\begin{verbatim}
 ! !PROGRAM: ESMF_ClockEx - Clock initialization and time-stepping
 !
 ! !DESCRIPTION:
 !
 ! This program shows an example of how to set-up, run, and examine a basic clock
-!EOP
 !-----------------------------------------------------------------------------
-! !USES:
       use ESMF_Mod
       implicit none
 
-!------------------------------------------------------------------------------
-! The following line turns the CVS identifier string into a printable variable.
-      character(*), parameter :: version = &
-      '$Id: ESMF_ClockEx.F90,v 1.27 2003/12/02 23:50:06 svasquez Exp $'
-!------------------------------------------------------------------------------
 
       ! instantiate a clock 
       type(ESMF_Clock) :: clock
@@ -50,159 +48,233 @@
       type(ESMF_TimeInterval) :: time_diff
       integer(ESMF_KIND_I8) :: advanceCount
       real(ESMF_KIND_R8) :: days_r8
+      integer :: rc
+!\end{verbatim}
+!EOP
 
       ! result code
-      integer :: rc, finalrc
+      integer :: finalrc
+      finalrc = ESMF_SUCCESS
 
+!BOP
+!\begin{verbatim}
       !
       ! initialization
       !
-      finalrc = ESMF_SUCCESS
 
       ! initialize calendar to be Gregorian type
       call ESMF_CalendarSet(gregorianCalendar, ESMF_CAL_GREGORIAN, rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! initialize time interval to 2 days, 4 hours (6 timesteps in 13 days)
       call ESMF_TimeIntervalSet(timeStep, d=2, h=4, rc=rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! initialize start time to 4/1/2003 2:24:00 ( 1/10 of a day )
       call ESMF_TimeSet(startTime, yr=2003, mm=4, dd=1, h=2, m=24, &
                         calendar=gregorianCalendar, rc=rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! initialize stop time to 4/14/2003 2:24:00 ( 1/10 of a day )
       call ESMF_TimeSet(stopTime, yr=2003, mm=4, dd=14, h=2, m=24, &
                         calendar=gregorianCalendar, rc=rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! initialize the clock with the above values
       clock = ESMF_ClockCreate("Clock 1", timeStep, startTime, stopTime, rc=rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! print starting time (initial current time)
       call ESMF_ClockPrint(clock, "currtime string", rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       !
       ! time step clock from start time to stop time
       !
 
       do while (.not.ESMF_ClockIsStopTime(clock, rc))
+!\end{verbatim}
+!EOP
 
         if (rc.NE.ESMF_SUCCESS) then
             finalrc = ESMF_FAILURE
         end if
 
+!BOP
+!\begin{verbatim}
         call ESMF_ClockAdvance(clock, rc=rc)
+!\end{verbatim}
+!EOP
 
         if (rc.NE.ESMF_SUCCESS) then
             finalrc = ESMF_FAILURE
         end if
 
+!BOP
+!\begin{verbatim}
         call ESMF_ClockPrint(clock, "currtime string", rc)
+!\end{verbatim}
+!EOP
 
         if (rc.NE.ESMF_SUCCESS) then
             finalrc = ESMF_FAILURE
         end if
 
+!BOP
+!\begin{verbatim}
       end do
-
       !
       ! examine clock
       !
 
       ! get and print clock's time_step
       call ESMF_ClockGet(clock, timeStep=time_step, rc=rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       call ESMF_ClockPrint(clock, "timestep string", rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
+!BOP
+!\begin{verbatim}
       ! get time step in integer days and seconds
       call ESMF_TimeIntervalGet(time_step, d=days, s=sec, rc=rc)
 
+      print *, "Clock's timestep = ", days, " integer days, ", &
+                sec, " integer seconds."
+!\end{verbatim}
+!EOP
+
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-      print *, "Clock's timestep = ", days, " integer days, ", &
-                sec, " integer seconds."
-
+!BOP
+!\begin{verbatim}
       ! get time step in floating point days
       call ESMF_TimeIntervalGet(time_step, d_r8=days_r8, rc=rc)
 
+      print *, "Clock's timestep = ", days_r8, " floating point days."
+!\end{verbatim}
+!EOP
+
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-      print *, "Clock's timestep = ", days_r8, " floating point days."
-
+!BOP
+!\begin{verbatim}
       ! get start time's floating point day of the year
       call ESMF_TimeGet(startTime, dayOfYear_r8=days_r8, rc=rc)
 
+      print *, "Start time's floating point day of the year = ", days_r8
+
+!\end{verbatim}
+!EOP
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-      print *, "Start time's floating point day of the year = ", days_r8
-
+!BOP
+!\begin{verbatim}
       ! get stop time's integer day of the year
       call ESMF_TimeGet(stopTime, dayOfYear=yD, rc=rc)
 
+      print *, "Stop time's integer day of the year = ", yD
+!\end{verbatim}
+!EOP
+
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-      print *, "Stop time's integer day of the year = ", yD
-
+!BOP
+!\begin{verbatim}
       ! get the number of times the clock was advanced
       call ESMF_ClockGet(clock, advanceCount=advanceCount, rc=rc)
 
+      print *, "The clock was advanced ", advanceCount, " times."
+!\end{verbatim}
+!EOP
+
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
-
-      print *, "The clock was advanced ", advanceCount, " times."
   
+!BOP
+!\begin{verbatim}
       ! calculate the difference between the start and stop times
       time_diff = stopTime - startTime
       call ESMF_TimeIntervalGet(time_diff, d=days, s=sec, rc=rc)
 
+      print *, "Difference between start and stop times = ", days, " days, ", &
+                sec, " seconds."
+!\end{verbatim}
+!EOP
+
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-      print *, "Difference between start and stop times = ", days, " days, ", &
-                sec, " seconds."
-
+!BOP
+!\begin{verbatim}
       ! destroy clock
       call ESMF_ClockDestroy(clock, rc)
+!\end{verbatim}
+!EOP
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
@@ -214,5 +286,8 @@
          print *, "FAIL: ESMF_ClockEx.F90"
       end if
 
-
+!BOP
+!\begin{verbatim}
       end program ESMF_ClockEx
+!\end{verbatim}
+!EOP
