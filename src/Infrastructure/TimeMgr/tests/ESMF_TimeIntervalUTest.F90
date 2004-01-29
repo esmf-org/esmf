942d3cb26df62575b79e50b3dad6b8feb6d04266
Earl Schwab <eschwab.esmf@gmail.com>
2004-01-29 04:45:46 +0000

    cleanup

--- src/Infrastructure/TimeMgr/tests/ESMF_TimeIntervalUTest.F90
+++ src/Infrastructure/TimeMgr/tests/ESMF_TimeIntervalUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_TimeIntervalUTest.F90,v 1.4 2004/01/26 21:29:38 eschwab Exp $
+! $Id: ESMF_TimeIntervalUTest.F90,v 1.5 2004/01/29 04:45:46 eschwab Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -25,7 +25,7 @@
 !
 ! The code in this file drives F90 Time Interval unit tests.
 ! The companion file ESMF\_TimeInterval.F90 contains the definitions for the
-! Clock methods.
+! TimeInterval methods.
 !
 !EOP
 !-----------------------------------------------------------------------------
@@ -37,7 +37,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_TimeIntervalUTest.F90,v 1.4 2004/01/26 21:29:38 eschwab Exp $'
+      '$Id: ESMF_TimeIntervalUTest.F90,v 1.5 2004/01/29 04:45:46 eschwab Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -52,19 +52,6 @@
       ! individual test failure message
       character(ESMF_MAXSTR) :: failMsg
 
-      logical :: bool
-      ! instantiate a clock 
-      type(ESMF_Clock) :: clock, clock1, clock_gregorian, clock_julian, clock_no_leap, clock_360day
-
-      ! Random number
-      real :: ranNum
-      integer :: seed(32)
-      integer :: timevals(8)
-
-      ! instantiate a calendar
-      type(ESMF_Calendar) :: gregorianCalendar, julianCalendar, no_leapCalendar, esmf_360dayCalendar
-      type(ESMF_CalendarType) :: cal_type
-
       ! instantiate timestep, start and stop times
       type(ESMF_TimeInterval) :: timeStep, timeStep2, timeStep3
       type(ESMF_Time) :: startTime, stopTime, startTime2, stopTime2
@@ -75,14 +62,14 @@
       integer(ESMF_KIND_I4) :: day, hour
 
 
-!--------------------------------------------------------------------------------
-!     The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
-!     always run. When the environment variable, EXHAUSTIVE, is set to ON then
-!     the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
-!     to OFF, then only the sanity unit tests.
-!     Special strings (Non-exhaustive and exhaustive) have been
-!     added to allow a script to count the number and types of unit tests.
-!--------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!    The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!    always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!    the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!    to OFF, then only the sanity unit tests.
+!    Special strings (Non-exhaustive and exhaustive) have been
+!    added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
 
      ! initialize ESMF framework
       call ESMF_Initialize(rc)
