f987c5e0cd0dfa8a8327313934cf106011ca4495
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-02-12 22:31:02 +0000

    Changed BOP/EOP to BOC/EOC and deleted "verbatims".

--- src/Infrastructure/DELayout/examples/ESMF_DELayout_F1Ex.F90
+++ src/Infrastructure/DELayout/examples/ESMF_DELayout_F1Ex.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_DELayout_F1Ex.F90,v 1.7 2004/01/07 21:09:51 svasquez Exp $
+! $Id: ESMF_DELayout_F1Ex.F90,v 1.8 2004/02/12 22:31:02 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -26,7 +26,8 @@
 
 !See the following code fragments for examples of how to create DELayouts and use them in the communications routines.
 !Also see the Programming Model section of this document.  
-!\begin{verbatim}
+!EOP
+!BOC
 
 program ESMF_DELayout_F1Ex
 
@@ -43,14 +44,12 @@ program ESMF_DELayout_F1Ex
   integer :: nx, ny, x, y, id, rc
   integer, dimension(20) :: array1, array2
   integer :: i, result, len
-!\end{verbatim}
-!EOP
+!EOC
 
   integer :: finalrc
   finalrc = ESMF_SUCCESS
 
-!BOP
-!\begin{verbatim}
+!BOC
   ! initialize framework
   call ESMF_Initialize(rc)
 
@@ -65,44 +64,38 @@ program ESMF_DELayout_F1Ex
 
   ! verify size of layout
   call ESMF_DELayoutGetSize(layout, nx, ny, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
   if (rc.NE.ESMF_SUCCESS) then
       finalrc = ESMF_FAILURE
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   ! initialize framework
   print *, "ESMF_DELayoutGetSize(nx, ny) = ", nx, ny
 
   ! get our DE's position within the layout
   call ESMF_DELayoutGetDEPosition(layout, x, y, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
   if (rc.NE.ESMF_SUCCESS) then
       finalrc = ESMF_FAILURE
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   print *, "ESMF_DELayoutGetDEPosition(x, y) = ", x, y
 
   ! get our DE id
   call ESMF_DELayoutGetDEid(layout, id, rc)
 
   print *, "ESMF_DELayoutGetDEid(id) = ", id
-!\end{verbatim}
-!EOP
+!EOC
 
   if (rc.NE.ESMF_SUCCESS) then
       finalrc = ESMF_FAILURE
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
 
   ! populate DE 0 array with first half of row 5
   do i=1,len
@@ -122,52 +115,43 @@ program ESMF_DELayout_F1Ex
   ! perform allreduce with our DE's array
   if (id .eq. 0) then
     call ESMF_DELayoutAllReduce(layout, array1, result, len, ESMF_SUM, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
     if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
     end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   else
     call ESMF_DELayoutAllReduce(layout, array2, result, len, ESMF_SUM, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
     if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
     end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   endif
 
   ! ... and the answer is ...
   print *, "ESMF_DELayoutAllReduce(sum) = ", result
-!\end{verbatim}
-!EOP
+!EOC
 
   if (result.NE.11620) then
      finalrc = ESMF_FAILURE
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   call ESMF_DELayoutDestroy(layout, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
   if (rc.NE.ESMF_SUCCESS) then
       finalrc = ESMF_FAILURE
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
   call ESMF_Finalize(rc)
-!\end{verbatim}
-!EOP
+!EOC
 
   if (rc.NE.ESMF_SUCCESS) then
       finalrc = ESMF_FAILURE
@@ -179,8 +163,6 @@ program ESMF_DELayout_F1Ex
      print *, "FAIL: ESMF_DELayout_F1Ex.F90"
   end if
 
-!BOP
-!\begin{verbatim}
+!BOC
 end program ESMF_DELayout_F1Ex
-!\end{verbatim}
-!EOP
+!EOC
