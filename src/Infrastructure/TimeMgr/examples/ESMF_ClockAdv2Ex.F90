3d4af541f5f02d854ed47fe8f69a57812e9de437
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-02-13 18:11:41 +0000

    Changed BOP/EOP to BOC/EOC and deleted "verbatims".

--- src/Infrastructure/TimeMgr/examples/ESMF_ClockAdv2Ex.F90
+++ src/Infrastructure/TimeMgr/examples/ESMF_ClockAdv2Ex.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ClockAdv2Ex.F90,v 1.1 2004/01/30 01:29:49 eschwab Exp $
+! $Id: ESMF_ClockAdv2Ex.F90,v 1.2 2004/02/13 18:11:41 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -15,8 +15,7 @@
 !------------------------------------------------------------------------------
 !EXAMPLE        String used by test script to count examples.
 !==============================================================================
-!BOP
-!\begin{verbatim}
+!BOC
 ! !PROGRAM: ESMF_ClockAdv2Ex - Clock initialization and time-stepping
 !
 ! !DESCRIPTION:
@@ -46,15 +45,13 @@
       real(ESMF_KIND_R8) :: time_step_count
       integer :: rc
 
-!\end{verbatim}
-!EOP
+!EOC
 
       ! result code
       integer :: finalrc
       finalrc = ESMF_SUCCESS
 
-!BOP
-!\begin{verbatim}
+!BOC
       !
       ! initialization
       !
@@ -62,157 +59,130 @@
       ! initialize calendar to be Gregorian type
       gregorianCalendar = ESMF_CalendarCreate("Gregorian", &
                                               ESMF_CAL_GREGORIAN, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! initialize time step to 1 hour
       call ESMF_TimeIntervalSet(timeStep, h=1, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! initialize start time to 5/15/2003 12:00:00 noon
       call ESMF_TimeSet(startTime, yy=2003, mm=5, dd=15, h=12, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! initialize stop time to 5/15/2003 08:00:00 PM
       call ESMF_TimeSet(stopTime, yy=2003, mm=5, dd=15, h=20, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! initialize stop time to 6 hours later
       call ESMF_TimeIntervalSet(runDuration, h=6, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! initialize the clock with the above values
       clock = ESMF_ClockCreate("Clock A", timeStep, startTime, &
                                runDuration=runDuration, rc=rc)
 !                               runTimeStepCount=4, rc=rc)
 !                               stopTime=stopTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_ClockPrint(clock, "starttime string", rc) ! string format
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_ClockPrint(clock, "stoptime string", rc) ! string format
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_ClockPrint(clock, "timestep string", rc) ! string format
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_ClockGet(clock, runDuration=run_duration, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Clock's run duration = " 
       call ESMF_TimeIntervalPrint(run_duration, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_ClockGet(clock, runTimeStepCount=time_step_count, rc=rc)
       print *, "Clock's run duration as a number of timesteps = ", &
                time_step_count 
       print *
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
 
       !
       ! time step clock from start time to stop time
       !
 
       do while (.not.ESMF_ClockIsStopTime(clock, rc))
-!\end{verbatim}
-!EOP
+!EOC
 
         if (rc.NE.ESMF_SUCCESS) then
             finalrc = ESMF_FAILURE
         end if
 
-!BOP
-!\begin{verbatim}
+!BOC
         call ESMF_ClockAdvance(clock, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
         if (rc.NE.ESMF_SUCCESS) then
             finalrc = ESMF_FAILURE
@@ -226,41 +196,34 @@
 
       ! print clock current time
       call ESMF_ClockPrint(clock, "currtime string", rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! get the number of times the clock was advanced
       call ESMF_ClockGet(clock, advanceCount=advanceCount, rc=rc)
       print *, "The clock was advanced ", advanceCount, " times."
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! destroy clock
       call ESMF_ClockDestroy(clock, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
      call ESMF_CalendarDestroy(gregorianCalendar, rc)
-!\end{verbatim}    
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
@@ -272,8 +235,6 @@
         print *, "FAIL: ESMF_ClockAdv2Ex.F90"
      end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       end program ESMF_ClockAdv2Ex
-!\end{verbatim}
-!EOP
+!EOC
