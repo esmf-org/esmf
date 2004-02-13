4c87f6e446b97ab79876bbf254a4e8320501a890
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-02-13 18:05:56 +0000

    Changed BOP/EOP to BOC/EOC and deleted "verbatims".

--- src/Infrastructure/TimeMgr/examples/ESMF_CalendarEx.F90
+++ src/Infrastructure/TimeMgr/examples/ESMF_CalendarEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_CalendarEx.F90,v 1.23 2004/02/11 06:51:54 eschwab Exp $
+! $Id: ESMF_CalendarEx.F90,v 1.24 2004/02/13 18:05:56 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -15,8 +15,7 @@
 !------------------------------------------------------------------------------
 !EXAMPLE        String used by test script to count examples.
 !==============================================================================
-!BOP
-!\begin{verbatim}
+!BOC
 ! !PROGRAM: ESMF_CalendarEx - Calendar examples
 !
 ! !DESCRIPTION:
@@ -39,55 +38,47 @@
       type(ESMF_Time) :: checkTime
       type(ESMF_Time) :: yearOne
       type(ESMF_CalendarType) :: calType
-!\end{verbatim}
-!EOP
+!EOC
 
       ! result code
       integer ::  finalrc
       finalrc = ESMF_SUCCESS
 
-!BOP
-!\begin{verbatim}
+!BOC
       !
       ! Julian Day Calendar
       !
       ! initialize calendar to be Julian type
       julianDayCalendar = ESMF_CalendarCreate("JulianDay", &
                                               ESMF_CAL_JULIANDAY, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       !
       ! Gregorian Calendar
       !
       ! initialize calendar to be Gregorian type
       gregorianCalendar = ESMF_CalendarCreate("Gregorian", &
                                               ESMF_CAL_GREGORIAN, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_CalendarGet(gregorianCalendar, calendarType=calType, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! demonstrate ESMF_CalendarType (==) overloaded operator
       if (calType == ESMF_CAL_GREGORIAN) then
           print *, "gregorianCalendar is of type ESMF_CAL_GREGORIAN."
@@ -107,83 +98,69 @@
       !
       call ESMF_TimeSet(timeZero, s=0, sN=0, sD=1, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(timeZero, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero Gregorian = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
       yy = -4713
       call ESMF_TimeSet(timeZero, yy=yy, mm=11, dd=24, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero Gregorian -4713/11/24 = " 
       call ESMF_TimePrint(timeZero, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       yy = 2003
       call ESMF_TimeSet(checkTime, yy=yy, mm=4, dd=17, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time1 Gregorian = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -191,31 +168,27 @@
       yy = -4800
       call ESMF_TimeSet(checkTime, yy=yy, mm=3, dd=1, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!\end{verbatim}
-!EOP
+!EOC
       call ESMF_TimeGet(checkTime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time2 Gregorian = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -223,35 +196,29 @@
       yy = -4800 
       call ESMF_TimeSet(checkTime, yy=yy, mm=2, dd=29, &
                         calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_FAILURE) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time3 Gregorian = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -261,47 +228,39 @@
       dl = dl * 100000  !   since F90 constants
       dl = dl + 67300   !     are 32-bit
       call ESMF_TimeSet(checkTime, d_i8=dl, calendar=julianDayCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! switch calendar to perform conversion
       call ESMF_TimeSet(checkTime, calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy_i8=yyl, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time4 Gregorian = ", &
                yyl, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -310,46 +269,38 @@
       dl = dl * 100000  !   since F90 constants
       dl = dl + 67301   !     are 32-bit
       call ESMF_TimeSet(checkTime, d_i8=dl, calendar=julianDayCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       ! switch calendar to perform conversion
       call ESMF_TimeSet(checkTime, calendar=gregorianCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy_i8=yyl, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time5 Gregorian = ", &
                yyl, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -357,95 +308,79 @@
       ! No Leap Calendar examples
       !
       noLeapCalendar = ESMF_CalendarCreate("NoLeap", ESMF_CAL_NOLEAP, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeSet(timeZero, s=0, sN=0, sD=1, &
                         calendar=noLeapCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(timeZero, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero No Leap = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
       yy = -4713
       call ESMF_TimeSet(timeZero, yy=yy, mm=11, dd=24, &
                         calendar=noLeapCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero No Leap -4713/11/24 = " 
       call ESMF_TimePrint(timeZero, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
 
-!BOP
-!\begin{verbatim}
+!BOC
       yy = 2004
       call ESMF_TimeSet(checkTime, yy=yy, mm=5, dd=18, &
                         calendar=noLeapCalendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Check Time1 No Leap = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
@@ -453,130 +388,107 @@
       ! 360 Day Calendar examples
       !
       day360Calendar = ESMF_CalendarCreate("360Day", ESMF_CAL_360DAY, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeSet(timeZero, s=0, sN=0, sD=1, &
                         calendar=day360Calendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(timeZero, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero 360 Day = ", &
                 yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
 
       yy = -4713
       call ESMF_TimeSet(timeZero, yy=yy, mm=11, dd=24, &
                         calendar=day360Calendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       print *, "Time Zero 360 Day -4713/11/24 = " 
       call ESMF_TimePrint(timeZero, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       yy = 2005
       call ESMF_TimeSet(checkTime, yy=yy, mm=6, dd=19, &
                         calendar=day360Calendar, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimePrint(checkTime, rc=rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       call ESMF_TimeGet(checkTime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, rc=rc)
 
       print *, "Check Time1 360 Day = ", &
                yy, "/", mm, "/", dd, " ", h, ":", m, ":", s
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
      call ESMF_CalendarDestroy(julianDayCalendar, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
      call ESMF_CalendarDestroy(gregorianCalendar, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
      call ESMF_CalendarDestroy(noLeapCalendar, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
       end if
 
-!BOP
-!\begin{verbatim}
+!BOC
      call ESMF_CalendarDestroy(day360Calendar, rc)
-!\end{verbatim}
-!EOP
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
           finalrc = ESMF_FAILURE
@@ -588,8 +500,6 @@
         print *, "FAIL: ESMF_CalendarEx.F90"
      end if
 
-!BOP
-!\begin{verbatim}
+!BOC
       end program ESMF_CalendarEx
-!\end{verbatim}
-!EOP
+!EOC
