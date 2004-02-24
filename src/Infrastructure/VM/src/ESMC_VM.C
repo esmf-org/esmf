62bb8012cae115d0c5019c94fb041d88b148527c
Gerhard Theurich <theurich@sourcespring.net>
2004-02-24 20:36:29 +0000

    Initial source file implementing ESMC_VM and ESMC_VMPlan methods

---
+++ src/Infrastructure/VM/src/ESMC_VM.C
@@ -0,0 +1,221 @@
+// $Id: ESMC_VM.C,v 1.1 2004/02/24 20:36:29 theurich Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2003, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the GPL.
+
+// ESMC VM method implementation (body) file
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+// The code in this file implements the methods of two C++ classes 
+// {\tt ESMC_VM} and {\tt ESMC_VMPlan} which are defined in the companion 
+// file {\tt ESMC\_VM.C}.
+//
+// Currently class {\tt ESMC_VM} is derived from base class {\tt vmachine}
+// and class {\tt ESMC_VMPlan} is derived from base class {\tt vmplan}. There
+// are only very few new features that the derived classes add to their base
+// classes, thus most of the implementing code is located in {vmachine.C}.
+//
+//-----------------------------------------------------------------------------
+
+// insert any higher level, 3rd party or system includes here
+#include <iostream.h>  // cout
+#include <ESMC_Base.h>  
+#include <ESMC.h>
+
+// associated class definition file
+#include <ESMC_VM.h>
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+ static const char *const version = "$Id: ESMC_VM.C,v 1.1 2004/02/24 20:36:29 theurich Exp $";
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// Module variable pointing to the global default VM
+// The global VM will be initialized in call ESMC_VMInitialize() and wrapped up
+// calling ESMC_VMFinalize(). 
+ESMC_VM *GlobalVM = NULL;  
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//
+// This section includes all the VM routines
+//
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_VMInitialize
+//
+// !INTERFACE:
+ESMC_VM *ESMC_VMInitialize(
+//
+// !RETURN VALUE:
+//    ESMC_VM* to GlobalVM
+//
+// !ARGUMENTS:
+//
+  int *rc){   // return code
+//
+// !DESCRIPTION:
+//    Initialize the global virtual machine as an all MPI VM and return a 
+//    handle to the local instance of the GlobalVM.
+//
+//EOP
+//-----------------------------------------------------------------------------
+  GlobalVM = new ESMC_VM;
+  GlobalVM->vmachine_init();             // set up default vmachine (all MPI)
+  *rc = ESMF_SUCCESS;
+  return GlobalVM;
+}
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_VMFinalize
+//
+// !INTERFACE:
+void ESMC_VMFinalize(
+//
+// !RETURN VALUE:
+//    void
+//
+// !ARGUMENTS:
+//
+  int *rc){   // return code
+//
+// !DESCRIPTION:
+//    Finalize the global virtual machine referenced by GlobalVM
+//
+//EOP
+//-----------------------------------------------------------------------------
+  GlobalVM->vmachine_finalize();
+  *rc = ESMF_SUCCESS;
+}
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_VMGet
+//
+// !INTERFACE:
+int ESMC_VM::ESMC_VMGet(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+//
+  ESMF_KIND_I4 *mypet,          // out - petid
+  ESMF_KIND_I4 *npets,          // out - number of PETs
+  ESMF_KIND_I4 *npes,           // out - number of PEs
+  int          *mpic){          // out - MPI Intracommunicator for VM
+//
+// !DESCRIPTION:
+//    Get information about a VM object
+//
+//EOP
+//-----------------------------------------------------------------------------
+  if (mypet != ESMC_NULL_POINTER)
+    *mypet = this->vmachine_mypet();
+  if (npets != ESMC_NULL_POINTER)
+    *npets = this->vmachine_npets();
+  if (npes != ESMC_NULL_POINTER){
+    int npets = this->vmachine_npets();
+    *npes = 0; // reset
+    for (int i=0; i<npets; i++)
+      *npes += this->vmachine_ncpet(i);
+  }
+  if (mpic != ESMC_NULL_POINTER)
+    *mpic = this->vmachine_mpi_comm();
+  return ESMF_SUCCESS;
+}
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_VMGetPET
+//
+// !INTERFACE:
+int ESMC_VM::ESMC_VMGetPET(
+//
+// !RETURN VALUE:
+//    int error return code
+//
+// !ARGUMENTS:
+//
+  ESMF_KIND_I4 *petid,          // in  - petid for this PET
+  ESMF_KIND_I4 *npes,           // out - number of PEs for this PET
+  ESMF_KIND_I4 *ssiid,          // out - ssiid for this PET
+  ESMF_KIND_I4 *mthpet,         // out - thread master pet for this PET
+  ESMF_KIND_I4 *nthpet,         // out - number of treads in group with PET
+  ESMF_KIND_I4 *tidpet){        // out - thread id for this PET
+//
+// !DESCRIPTION:
+//    Get PET specific information about a VM object
+//
+//EOP
+//-----------------------------------------------------------------------------
+  if (npes != ESMC_NULL_POINTER)
+    *npes = this->vmachine_ncpet(*petid);
+  if (ssiid != ESMC_NULL_POINTER)
+    *ssiid = this->vmachine_ssiid(*petid);
+  if (mthpet != ESMC_NULL_POINTER)
+    *mthpet = this->vmachine_mthpet(*petid);
+  if (nthpet != ESMC_NULL_POINTER)
+    *nthpet = this->vmachine_nthpet(*petid);
+  if (tidpet != ESMC_NULL_POINTER)
+    *tidpet = this->vmachine_tidpet(*petid);
+  return ESMF_SUCCESS;
+}
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE:  ESMC_VMFillFromVmachine
+//
+// !INTERFACE:
+void ESMC_VM::ESMC_VMFillFromVmachine(
+//
+//
+// !ARGUMENTS:
+//
+  vmachine &vmach){         // in - vmachine to use
+//
+// !DESCRIPTION:
+//    Fill the VM members according to an existing vmachine object
+//
+//EOP
+//-----------------------------------------------------------------------------
+  mypet = vmach.mypet;
+  npets = vmach.npets;
+  lpid = vmach.lpid;
+  pid = vmach.pid;
+  tid = vmach.tid;
+  ncpet = vmach.ncpet;
+  cid = vmach.cid;
+  mpionly = vmach.mpionly;
+  mpi_g = vmach.mpi_g;
+  mpi_c = vmach.mpi_c;
+  pth_mutex2 = vmach.pth_mutex2;
+  pth_mutex = vmach.pth_mutex;
+  pth_finish_count = vmach.pth_finish_count;
+  commarray = vmach.commarray;
+}
+//-----------------------------------------------------------------------------
