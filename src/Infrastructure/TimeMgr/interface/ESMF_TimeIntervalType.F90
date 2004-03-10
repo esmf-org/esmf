cadf26f8f6e98d6ca8ee986f79bb95ffaa4c7e4e
Earl Schwab <eschwab.esmf@gmail.com>
2004-03-10 03:02:19 +0000

    split Time and TimeInterval type definitions to resolve mutual dependency

---
+++ src/Infrastructure/TimeMgr/interface/ESMF_TimeIntervalType.F90
@@ -0,0 +1,99 @@
+! $Id: ESMF_TimeIntervalType.F90,v 1.1 2004/03/10 03:02:19 eschwab Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+!     ESMF TimeIntervalType Module
+      module ESMF_TimeIntervalTypeMod
+!
+!==============================================================================
+!
+! This file contains the TimeInterval class definition.  The TimeInterval class
+! methods are defined in ESMF_TimeInterval.F90.  This split is to resolve
+! mutual dependency with ESMF_Time.
+!
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF_TimeMgr.inc>
+
+!===============================================================================
+!BOPI
+!
+! !MODULE: ESMF_TimeIntervalTypeMod
+!
+! !DESCRIPTION:
+! Part of Time Manager F90 API wrapper of C++ implemenation.
+!
+! Defines F90 types for corresponding C++ class {\tt ESMC\_TimeInterval}.
+!
+! See {\tt ../include/ESMC\_TimeInterval.h} for complete description.
+!
+!------------------------------------------------------------------------------
+! !USES:
+      ! inherit from ESMF base class
+      use ESMF_BaseMod
+
+      ! inherit from base time class
+      use ESMF_BaseTimeMod
+
+      ! associated derived types
+      use ESMF_TimeTypeMod
+      use ESMF_CalendarMod
+
+      implicit none
+
+!------------------------------------------------------------------------------
+! !PRIVATE TYPES:
+!     None: all types defined in this file are public and propagated up
+!     via ESMF_TimeIntervalMod in ESMF_TimeInterval.F90
+
+!------------------------------------------------------------------------------
+!     ! ESMF_TimeInterval
+!
+!     ! F90 class type to match C++ TimeInterval class in size only;
+!     !  all dereferencing within class is performed by C++ implementation
+
+!     ! Equivalent sequence and kind to C++:
+
+      type ESMF_TimeInterval
+      sequence                             ! match C++ storage order
+      private                              !   (members opaque on F90 side)
+        type(ESMF_BaseTime)   :: baseTime  ! inherit base class
+        type(ESMF_Time)       :: startTime ! start time for absolute calendar
+!                                              intervals
+        type(ESMF_Time)       :: endTime   ! end time for absolute calendar
+!                                              intervals
+#if !defined(ESMF_NO_INITIALIZERS) && !defined(ESMF_AIX_8_INITBUG)
+        type(ESMF_Calendar), pointer :: calendar => NULL() ! associated calendar
+        integer(ESMF_KIND_I8) :: yy = 0    ! calendar interval number of years
+        integer(ESMF_KIND_I8) :: mm = 0    ! calendar interval number of months
+        integer(ESMF_KIND_I8) :: d  = 0    ! calendar interval number of days
+#else
+        type(ESMF_Calendar), pointer :: calendar  ! associated calendar
+        integer(ESMF_KIND_I8) :: yy        ! calendar interval number of years
+        integer(ESMF_KIND_I8) :: mm        ! calendar interval number of months
+        integer(ESMF_KIND_I8) :: d         ! calendar interval number of days
+#endif
+      end type
+
+!------------------------------------------------------------------------------
+! !PUBLIC TYPES:
+!     The types defined in this file are public and propagated up via 
+!     ESMF_TimeIntervalMod in ESMF_TimeInterval.F90      
+
+!EOPI
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter, private :: version = &
+      '$Id: ESMF_TimeIntervalType.F90,v 1.1 2004/03/10 03:02:19 eschwab Exp $'
+!------------------------------------------------------------------------------
+
+      end module ESMF_TimeIntervalTypeMod
---
+++ src/Infrastructure/TimeMgr/interface/ESMF_TimeType.F90
@@ -0,0 +1,89 @@
+! $Id: ESMF_TimeType.F90,v 1.1 2004/03/10 03:02:19 eschwab Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+!     ESMF TimeType Module
+      module ESMF_TimeTypeMod
+!
+!==============================================================================
+!
+! This file contains the Time class definition.  The Time class methods
+! are defined in ESMF_Time.F90.  This split is to resolve mutual 
+! dependency with ESMF_TimeInterval.
+!
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF_TimeMgr.inc>
+
+!===============================================================================
+!BOPI
+!
+! !MODULE: ESMF_TimeTypeMod
+!
+! !DESCRIPTION:
+! Part of Time Manager F90 API wrapper of C++ implemenation.
+!
+! Defines F90 types for corresponding C++ class {\tt ESMC\_Time}.
+!
+! See {\tt ../include/ESMC\_Time.h} for complete description.
+!
+!------------------------------------------------------------------------------
+! !USES:
+      ! inherit from base time class
+      use ESMF_BaseTimeMod
+
+      ! associated derived types
+      use ESMF_CalendarMod
+
+      implicit none
+
+!------------------------------------------------------------------------------
+! !PRIVATE TYPES:
+!     None: all types defined in this file are public and propagated up
+!     via ESMF_TimeMod in ESMF_Time.F90
+
+!------------------------------------------------------------------------------
+!     ! ESMF_Time
+!
+!     ! F90 class type to match C++ Time class in size only;
+!     !  all dereferencing within class is performed by C++ implementation
+
+!     ! Equivalent sequence and kind to C++:
+
+      type ESMF_Time
+      sequence                                  ! match C++ storage order
+      private                                   !  (members opaque on F90 side)
+      type(ESMF_BaseTime)          :: baseTime  ! inherit base class
+#if !defined(ESMF_NO_INITIALIZERS) && !defined(ESMF_AIX_8_INITBUG)
+      type(ESMF_Calendar), pointer :: calendar => NULL() ! associated calendar
+      integer                      :: timeZone = 0 ! local timezone
+      integer                      :: pad      = 0 ! to satisfy halem compiler
+#else
+      type(ESMF_Calendar), pointer :: calendar  ! associated calendar
+      integer                      :: timeZone  ! local timezone
+      integer                      :: pad       ! to satisfy halem compiler
+#endif
+     end type
+
+!------------------------------------------------------------------------------
+! !PUBLIC TYPES:
+!     The types defined in this file are public and propagated up via 
+!     ESMF_TimeMod in ESMF_Time.F90      
+
+!EOPI
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter, private :: version = &
+      '$Id: ESMF_TimeType.F90,v 1.1 2004/03/10 03:02:19 eschwab Exp $'
+!------------------------------------------------------------------------------
+
+      end module ESMF_TimeTypeMod
