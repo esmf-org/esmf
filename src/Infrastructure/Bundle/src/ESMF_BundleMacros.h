8fe0d746b5cdf292ddf8ac57d0961c6174e89815
Nancy Collins <nancy@ucar.edu>
2004-03-16 18:13:33 +0000

    generate .F90 file automatically from .cpp file, use standard macros from base class

--- src/Infrastructure/Bundle/src/ESMF_BundleMacros.h
+++ src/Infrastructure/Bundle/src/ESMF_BundleMacros.h
@@ -1,5 +1,5 @@
 #if 0
-! $Id: ESMF_BundleMacros.h,v 1.3 2004/03/09 21:30:11 svasquez Exp $
+! $Id: ESMF_BundleMacros.h,v 1.4 2004/03/16 18:13:33 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -15,107 +15,37 @@
 #if 0
 !------------------------------------------------------------------------------
 ! Macros for the Bundle class.
-! these are defined because they contain a variable number of commas.
-! they are used as parms in macro calls, and are rescanned by the
-! preprocessor after expansion.   7 is the maximum rank supported by Fortran.
 !------------------------------------------------------------------------------
 #endif
 
-#define COL1 :
-#define COL2 :,:
-#define COL3 :,:,:
-#define COL4 :,:,:,:
-#define COL5 :,:,:,:,:
-#define COL6 :,:,:,:,:,:
-#define COL7 :,:,:,:,:,:,:
-
-#define LEN1 counts(1)
-#define LEN2 counts(1),counts(2)
-#define LEN3 counts(1),counts(2),counts(3)
-#define LEN4 counts(1),counts(2),counts(3),counts(4)
-#define LEN5 counts(1),counts(2),counts(3),counts(4),counts(5)
-#define LEN6 counts(1),counts(2),counts(3),counts(4),counts(5),counts(6)
-#define LEN7 counts(1),counts(2),counts(3),counts(4),counts(5),counts(6),counts(7)
-
-#define RNG1 lb(1):ub(1)
-#define RNG2 lb(1):ub(1),lb(2):ub(2)
-#define RNG3 lb(1):ub(1),lb(2):ub(2),lb(3):ub(3)
-#define RNG4 lb(1):ub(1),lb(2):ub(2),lb(3):ub(3),lb(4):ub(4)
-#define RNG5 lb(1):ub(1),lb(2):ub(2),lb(3):ub(3),lb(4):ub(4),lb(5):ub(5)
-#define RNG6 lb(1):ub(1),lb(2):ub(2),lb(3):ub(3),lb(4):ub(4),lb(5):ub(5),lb(6):ub(6)
-#define RNG7 lb(1):ub(1),lb(2):ub(2),lb(3):ub(3),lb(4):ub(4),lb(5):ub(5),lb(6):ub(6),lb(7):ub(7)
-
-#define LOC1 1
-#define LOC2 1,1
-#define LOC3 1,1,1
-#define LOC4 1,1,1,1
-#define LOC5 1,1,1,1,1
-#define LOC6 1,1,1,1,1,1
-#define LOC7 1,1,1,1,1,1,1
-
+#include "ESMF_StdCppMacros.h"
 
 #if 0
 !------------------------------------------------------------------------------
-! Expand a string into each of the T/K/R procedure interface blocks
+! Documentation for the general BundleGetDataPointer macro
 !------------------------------------------------------------------------------
 #endif
 
-#define BundleInterfaceMacro(funcname) \
-!------------------------------------------------------------------------------ @\
-! <This section created by macro - do not edit directly> @\
-    module procedure ESMF_##funcname##I21D @\
-    module procedure ESMF_##funcname##I41D @\
-    module procedure ESMF_##funcname##I81D @\
-    module procedure ESMF_##funcname##I22D @\
-    module procedure ESMF_##funcname##I42D @\
-    module procedure ESMF_##funcname##I82D @\
-    module procedure ESMF_##funcname##I23D @\
-    module procedure ESMF_##funcname##I43D @\
-    module procedure ESMF_##funcname##I83D @\
-    module procedure ESMF_##funcname##I24D @\
-    module procedure ESMF_##funcname##I44D @\
-    module procedure ESMF_##funcname##I84D @\
-    module procedure ESMF_##funcname##I25D @\
-    module procedure ESMF_##funcname##I45D @\
-    module procedure ESMF_##funcname##I85D @\
-    module procedure ESMF_##funcname##R41D @\
-    module procedure ESMF_##funcname##R81D @\
-    module procedure ESMF_##funcname##R42D @\
-    module procedure ESMF_##funcname##R82D @\
-    module procedure ESMF_##funcname##R43D @\
-    module procedure ESMF_##funcname##R83D @\
-    module procedure ESMF_##funcname##R44D @\
-    module procedure ESMF_##funcname##R84D @\
-    module procedure ESMF_##funcname##R45D @\
-    module procedure ESMF_##funcname##R85D @\
-! < end macro - do not edit directly >  @\
-!------------------------------------------------------------------------------ @\
-
-#if 0
-!------------------------------------------------------------------------------
-! Create a new array based on an unallocated F90 array and a list of counts.
-!------------------------------------------------------------------------------
-#endif
-
-#define BundleGetDataPointerMacro(mname, mtypekind, mrank, mdim, mlen, mrng, mloc) \
+#define BundleGetDataPointerDoc() \
 !------------------------------------------------------------------------------ @\
 ! <Created by macro - do not edit directly > @\
 !BOP @\
-! !IROUTINE: ESMF_BundleGetDataPointer##mtypekind##mrank##D - Retrieve F90 pointer directly from a Bundle @\
+! !IROUTINE: ESMF_BundleGetDataPointer - Retrieve Fortran pointer directly from a Bundle @\
  @\
 ! !INTERFACE: @\
-      subroutine ESMF_BundleGetDataPointer##mtypekind##mrank##D(bundle, fieldname, f90ptr, copyflag, rc) @\
+!     ! Private name; call using ESMF_BundleGetDataPointer()
+!      subroutine ESMF_BundleGetDataPointer<rank><type><kind>(bundle, fieldname, fptr, copyflag, rc) @\
 ! @\
 ! !ARGUMENTS: @\
-      type(ESMF_Bundle), intent(in) :: bundle @\
-      character(len=*), intent(in) :: fieldname @\
-      mname (ESMF_KIND_##mtypekind), dimension(mdim), pointer :: f90ptr @\
-      type(ESMF_CopyFlag), intent(in), optional :: copyflag @\
-      integer, intent(out), optional :: rc   @\
+!      type(ESMF_Bundle), intent(in) :: bundle @\
+!      character(len=*), intent(in) :: fieldname @\
+!      <type> (ESMF_KIND_<kind>), dimension(<rank>), pointer :: fptr @\
+!      type(ESMF_CopyFlag), intent(in), optional :: copyflag @\
+!      integer, intent(out), optional :: rc   @\
 ! @\
 ! !DESCRIPTION: @\
-! Retrieves data from a bundle, returning a direct F90 pointer to the start @\
-!  of the actual data array.  @\
+! Retrieves data from a bundle, returning a direct Fortran pointer to @\
+!   the data.  @\
 ! @\
 ! The arguments are: @\
 !  \begin{description} @\
@@ -124,9 +54,9 @@
 !  \item[fieldname] @\
 !   The name of the {\tt ESMF\_Field} inside the {\tt ESMF\_Bundle} @\
 !   to return.  The {\tt ESMF\_Bundle} cannot have packed data. @\
-!  \item[f90ptr] @\
-!   An unassociated Fortrn 90 pointer of the proper Type, Kind, and Rank as the data @\
-!   in the Bundle.  When this call returns successfully, the pointer will now reference @\
+!  \item[fptr] @\
+!   An unassociated Fortran pointer of the proper Type, Kind, and Rank as the data @\
+!   in the Bundle.  When this call returns successfully, the pointer will now point to @\
 !   the data in the Bundle.  This is either a reference or a copy, depending on the @\
 !   setting of the following argument.  The default is to return a reference. @\
 !  \item[{[copyflag]}] @\
@@ -136,10 +66,25 @@
 !    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors. @\
 !  \end{description} @\
 ! @\
- @\
-! @\
 !EOP @\
-! !REQUIREMENTS: @\
+ @\
+
+#if 0
+!------------------------------------------------------------------------------
+! Get a Fortran pointer back to Bundle Data macro
+!------------------------------------------------------------------------------
+#endif
+
+#define BundleGetDataPointerMacro(mname, mtypekind, mrank, mdim, mlen, mrng, mloc) \
+!------------------------------------------------------------------------------ @\
+! <Created by macro - do not edit directly > @\
+      subroutine ESMF_BundleGetDataPointer##mrank##D##mtypekind(bundle, fieldname, fptr, copyflag, rc) @\
+ @\
+      type(ESMF_Bundle), intent(in) :: bundle @\
+      character(len=*), intent(in) :: fieldname @\
+      mname (ESMF_KIND_##mtypekind), dimension(mdim), pointer :: fptr @\
+      type(ESMF_CopyFlag), intent(in), optional :: copyflag @\
+      integer, intent(out), optional :: rc   @\
  @\
         ! Local variables @\
         type (ESMF_Field) :: field          ! field object @\
@@ -160,10 +105,10 @@
         endif @\
  @\
         ! Test to see if array already allocated, and fail if so. @\
-        !if (allocated(f90arr)) then @\
-        !  print *, "Error: Data Pointer cannot already be allocated" @\
-        !  return @\
-        !endif @\
+        if (associated(fptr)) then @\
+          print *, "Error: Data Pointer cannot already be associated" @\
+          return @\
+        endif @\
  @\
         call ESMF_BundleGetField(bundle, fieldname, field, status) @\
         if (status .ne. ESMF_SUCCESS) then @\
@@ -177,7 +122,7 @@
           return @\
         endif @\
  @\
-        call ESMF_ArrayGetData(array, f90ptr, copyflag, rc=status) @\
+        call ESMF_ArrayGetData(array, fptr, copyflag, rc=status) @\
         if (status .ne. ESMF_SUCCESS) then @\
           print *, "Error: ArrayGetData failed" @\
           return @\
@@ -185,7 +130,7 @@
  @\
         if (rcpresent) rc = status @\
  @\
-        end subroutine ESMF_BundleGetDataPointer##mtypekind##mrank##D   @\
+        end subroutine ESMF_BundleGetDataPointer##mrank##D##mtypekind   @\
  @\
 ! < end macro - do not edit directly >  @\
 !------------------------------------------------------------------------------ @\
