b60dbda5335abb4ef24826368bb47a9dc4d115cf
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-04-08 21:48:17 +0000

    Added unit tests.

--- src/Infrastructure/Array/tests/ESMF_ArrayUTest.F90
+++ src/Infrastructure/Array/tests/ESMF_ArrayUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ArrayUTest.F90,v 1.1 2004/04/08 19:20:22 svasquez Exp $
+! $Id: ESMF_ArrayUTest.F90,v 1.2 2004/04/08 21:48:17 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -15,7 +15,7 @@
 !------------------------------------------------------------------------------
 !
 
-#include <ESMF.h>
+#include <ESMF_Macros.inc>
 
 !==============================================================================
 !BOP
@@ -35,36 +35,33 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_ArrayUTest.F90,v 1.1 2004/04/08 19:20:22 svasquez Exp $'
+      '$Id: ESMF_ArrayUTest.F90,v 1.2 2004/04/08 21:48:17 svasquez Exp $'
 !------------------------------------------------------------------------------
 
-     ! cumulative result: count failures; no failures equals "all pass"
-     integer :: result = 0
-
-
 !   ! Local variables
-    integer :: nx, ny, arank, brank, rc 
-    integer :: i, j, k, l, m, ni, nj, nk, nl, nm
-    type(ESMF_ArraySpec) :: arrayspec, spec
-    type(ESMF_DataType) :: atype
-    type(ESMF_DataKind) :: akind
-    integer :: counts(ESMF_MAXDIM), lb(1), ub(1), rlb(1), rub(1)
-    type(ESMF_Array) :: array1, array2, array3, array4
-    real(ESMF_KIND_R8), dimension(:,:,:), pointer :: real3dptr, real3d2ptr
-    real(ESMF_KIND_R8), dimension(:,:), pointer :: realptr, realptr2
-    integer(ESMF_KIND_I4), dimension(:), pointer :: intptr, intptr2
-    integer(ESMF_KIND_I4), dimension(:,:), pointer :: int2Dptr
+    type(ESMF_Array) :: array1
     character(ESMF_MAXSTR) :: filename
     real, dimension(:,:), pointer :: f90ptr1
 
 
     ! individual test failure message
     character(ESMF_MAXSTR) :: failMsg
-    character(ESMF_MAXSTR) :: name
-    integer :: result = 0
+    character(ESMF_MAXSTR) :: name, array_name
+    integer :: rc, result = 0
 
     
     call ESMF_Initialize()
+
+!-------------------------------------------------------------------------------
+!   !  Create an Array Test
+    ! THe following code core dumps it will be uncommented
+    ! when bug 932018 is fixed.
+    !allocate(f90ptr1(10,20))
+    !write(failMsg, *) "Did not return ESMF_SUCCESS" 
+    !write(name, *) "Create Array Test"
+    !!array1 =  ESMF_ArrayCreate(f90ptr1, ESMF_DATA_COPY, rc=rc)
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+    !print *, "array 1 create returned"
 
 !-------------------------------------------------------------------------------
 !   !  Create an Array Test
@@ -73,9 +70,29 @@
     allocate(f90ptr1(10,20))
     write(failMsg, *) "Did not return ESMF_SUCCESS" 
     write(name, *) "Create Array Test"
-    array1 =  ESMF_ArrayCreate( f90ptr1, ESMF_DATA_COPY, rc=rc)
+    array1 =  ESMF_ArrayCreate(f90ptr1, ESMF_DATA_REF, rc=rc)
     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
     print *, "array 1 create returned"
+
+!-------------------------------------------------------------------------------
+!   !  Set Array Name Test
+ 
+    !NEX_UTest
+    write(failMsg, *) "Did not return ESMF_SUCCESS" 
+    write(name, *) "Set Array Name Test"
+    call ESMF_ArraySet(array1, name="SAM", rc=rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+    print *, "array 1 create returned"
+
+!-------------------------------------------------------------------------------
+!   !  Get Array Name Test
+ 
+    !NEX_UTest
+    write(failMsg, *) "Did not return ESMF_SUCCESS or returned wrong name" 
+    write(name, *) "Get Array Name Test"
+    call ESMF_ArrayGet(array1, name=array_name, rc=rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS.and.array_name.eq."SAM"), name, failMsg, result, ESMF_SRCLINE)
+    print *, "array 1 is called ", array_name
 
 !-------------------------------------------------------------------------------
 !   !  Destroy an Array Test
