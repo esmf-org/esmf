05ef9b5ade76e42b249ed9fbd5d4d6b5f0d48199
Nancy Collins <nancy@ucar.edu>
2004-04-29 21:12:46 +0000

    include macro for doing right test of glue arguments

--- src/Infrastructure/Base/include/ESMC_Start.h
+++ src/Infrastructure/Base/include/ESMC_Start.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Start.h,v 1.1 2004/04/23 17:31:05 nscollins Exp $
+// $Id: ESMC_Start.h,v 1.2 2004/04/29 21:12:46 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -13,6 +13,7 @@
 
 // common macros and constants
 #include "ESMC_Macros.h"
+#include "ESMC_F90Interface.h"
 
 // shared macros between fortran and C++.  this must come first before
 // the system dependent file below.
