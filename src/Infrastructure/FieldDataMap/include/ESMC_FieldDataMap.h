f81f9ee432973800f019310d420fe05a8ed6534c
Nancy Collins <nancy@ucar.edu>
2004-05-10 15:38:14 +0000

    for the C interfaces to datamaps

---
+++ src/Infrastructure/FieldDataMap/include/ESMC_FieldDataMap.h
@@ -0,0 +1,27 @@
+// $Id: ESMC_FieldDataMap.h,v 1.1 2004/05/10 15:38:14 nscollins Exp $
+//
+//-------------------------------------------------------------------------
+//BOP
+//
+// !CLASS: ESMC_FieldDataMap
+
+// TODO: make the Field DataMap class based on the Array DataMap,
+// and just add the additional information needed for the field level.
+
+// !PUBLIC MEMBER FUNCTIONS:
+
+// !INTERFACE:
+//  void ESMC_FieldDataMapValidate(ESMC_FieldDataMap *datamap, int *rc);
+//   // Routine to validate the internal state of a Field datamap object.
+//   // !REQUIREMENTS:  FLD4.1
+//   //
+//   //
+// !INTERFACE:
+//  void ESMC_FieldDataMapPrint(ESMC_FieldDataMap *datamap, char *options, int *rc);
+//   // Routine to print information about a Field datamap object.
+//   // !REQUIREMENTS:
+//   //
+//   //
+//EOP
+//
+
