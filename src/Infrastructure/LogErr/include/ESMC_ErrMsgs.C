a05de86e0a6b54c70e224064aab8a958d7dbd3e4
Earl Schwab <eschwab.esmf@gmail.com>
2004-05-17 19:24:13 +0000

    added C++ messages (501-1000)

--- src/Infrastructure/LogErr/include/ESMC_ErrMsgs.C
+++ src/Infrastructure/LogErr/include/ESMC_ErrMsgs.C
@@ -1,4 +1,4 @@
-//$Id: ESMC_ErrMsgs.C,v 1.1 2004/05/14 23:33:36 eschwab Exp $
+//$Id: ESMC_ErrMsgs.C,v 1.2 2004/05/17 19:24:13 eschwab Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -14,9 +14,10 @@
 // prepending or appending with user strings.
 
 static const char *errMsg[] = {
-// Error message                                        Return Code
-// -----------------------------------------------      ----------------------
-  "Invalid object ",                                  // ESMF_RC_OBJ_OBJ
+
+// F90 Error message                                     Return Code (1-500)
+// -----------------------------------------------       ----------------------
+  "Invalid object ",                                  // ESMF_RC_OBJ_BAD
   "Object SetDefault method not called ",             // ESMF_RC_OBJ_INIT
   "Object Create method not called ",                 // ESMF_RC_OBJ_CREATE
   "Corrupted ESMF object detected ",                  // ESMF_RC_OBJ_COR
@@ -68,5 +69,104 @@ static const char *errMsg[] = {
 
   "System call error ",                               // ESMF_RC_SYS
   "Resource is busy ",                                // ESMF_RC_BUSY
-  "Error in library called by ESMF "                  // ESMF_RC_LIB
+  "Error in library called by ESMF ",                 // ESMF_RC_LIB
+
+// 47-500 reserved for future F90 return code definitions
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","",
+
+// C++ Error message                                     Return Code (501-1000)
+// -----------------------------------------------       -----------------------
+  "Invalid object ",                                  // ESMC_RC_OBJ_BAD
+  "Object SetDefault method not called ",             // ESMC_RC_OBJ_INIT
+  "Object Create method not called ",                 // ESMC_RC_OBJ_CREATE
+  "Corrupted ESMF object detected ",                  // ESMC_RC_OBJ_COR
+  "Object is in wrong state ",                        // ESMC_RC_OBJ_WRONGSTATE
+
+  "Invalid argument ",                                // ESMC_RC_ARG_BAD
+  "Argument rank is not required size ",              // ESMC_RC_ARG_RANK
+  "Argument sizes do not match ",                     // ESMC_RC_ARG_SIZE
+  "Value unrecognized or out of range ",              // ESMC_RC_ARG_VALUE
+  "Two arguments not allowed to be the same ",        // ESMC_RC_ARG_DUP
+  "Two arguments must be same object type ",          // ESMC_RC_ARG_SAMETYPE
+  "Two arguments must have the same communicators ",  // ESMC_RC_ARG_SAMECOMM
+  "Arguments are incompatible ",                      // ESMC_RC_ARG_INCOMP
+  "Argument contains invalid ESMF object ",           // ESMC_RC_ARG_CORRUPT
+  "Wrong argument specified ",                        // ESMC_RC_ARG_WRONG
+  "Input argument out of range ",                     // ESMC_RC_ARG_OUTOFRANGE
+  "Unrecognized option string ",                      // ESMC_RC_ARG_OPT
+
+  "Operation not yet supported ",                     // ESMC_RC_NOT_IMPL
+
+  "Unable to open file ",                             // ESMC_RC_FILE_OPEN
+  "Unable to create file ",                           // ESMC_RC_FILE_CREATE
+  "Unable to read from file ",                        // ESMC_RC_FILE_READ
+  "Unable to write to file ",                         // ESMC_RC_FILE_WRITE
+  "Unexpected data in file ",                         // ESMC_RC_FILE_UNEXPECTED
+  "Unable to close file ",                            // ESMC_RC_FILE_CLOSE
+  "Instrumented region is still active ",             // ESMC_RC_FILE_ACTIVE
+
+  "Value cannot be a NULL pointer ",                  // ESMC_RC_PTR_NULL
+  "Invalid pointer ",                                 // ESMC_RC_PTR_BAD
+  "Pointer must already be allocated ",               // ESMC_RC_PTR_NOTALLOC
+  "Pointer must not already be allocated ",           // ESMC_RC_PTR_ISALLOC
+  "Unable to allocate requested memory ",             // ESMC_RC_MEM
+  "Memory corrupted ",                                // ESMC_RC_MEMC
+
+  "Name already exists ",                             // ESMC_RC_DUP_NAME
+  "Name too long, must be less than ESMF_MAXSTR ",    // ESMC_RC_LONG_NAME
+  "String too long, must be less than ESMF_MAXSTR ",  // ESMC_RC_LONG_STR
+  "Cannot copy non-existent object ",                 // ESMC_RC_COPY_FAIL
+  "Cannot divide by zero ",                           // ESMC_RC_DIV_ZERO
+  "Cannot get value ",                                // ESMC_RC_CANNOT_GET
+  "Cannot set value ",                                // ESMC_RC_CANNOT_SET
+  "Not found ",                                       // ESMC_RC_NOT_FOUND
+  "Not valid " ,                                      // ESMC_RC_NOT_VALID
+
+  "Internal error: List overflow ",                   // ESMC_RC_INTNRL_LIST
+  "Internal error: Inconsistent data ",               // ESMC_RC_INTNRL_INCONS
+  "Internal error: Unknown error ",                   // ESMC_RC_INTNRL_BAD
+
+  "System call error ",                               // ESMC_RC_SYS
+  "Resource is busy ",                                // ESMC_RC_BUSY
+  "Error in library called by ESMF ",                 // ESMC_RC_LIB
+
+// 547-1000 reserved for future C++ return code definitions
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","","","","","","","","","","","","","","","","","","","","","","","",
+  "","","",""
+
 };
