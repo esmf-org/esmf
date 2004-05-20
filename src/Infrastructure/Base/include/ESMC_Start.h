343a05a8c909cee6739ed28913f10e8a5f2075f7
Nancy Collins <nancy@ucar.edu>
2004-05-20 11:41:03 +0000

    add stubs where we might decide to include macros automatically

--- src/Infrastructure/Base/include/ESMC_Start.h
+++ src/Infrastructure/Base/include/ESMC_Start.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Start.h,v 1.4 2004/05/19 12:41:48 nscollins Exp $
+// $Id: ESMC_Start.h,v 1.5 2004/05/20 11:41:03 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -19,6 +19,11 @@
 // the system dependent file below.  also shared error messages.
 #include "ESMF_Macros.inc"
 #include "ESMF_ErrReturnCodes.inc"
+
+// if we decide users should get the logerr macros by default, include
+// this here.  otherwise users can include it or not as they choose.
+// #include "ESMF_LogConstants.inc"
+// #include "ESMF_LogMacros.inc"
 
 // system dependent #defines from build process
 #include "ESMC_Conf.h"
