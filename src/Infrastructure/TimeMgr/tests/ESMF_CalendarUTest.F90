2fc8244e00eb975244346a0c73207cc1f7c049e3
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-05-24 20:18:38 +0000

    Remove Clock Destroy calls, where clock is being used in loop.

--- src/Infrastructure/TimeMgr/tests/ESMF_CalendarUTest.F90
+++ src/Infrastructure/TimeMgr/tests/ESMF_CalendarUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_CalendarUTest.F90,v 1.24 2004/05/24 17:07:18 svasquez Exp $
+! $Id: ESMF_CalendarUTest.F90,v 1.25 2004/05/24 20:18:38 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -37,7 +37,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_CalendarUTest.F90,v 1.24 2004/05/24 17:07:18 svasquez Exp $'
+      '$Id: ESMF_CalendarUTest.F90,v 1.25 2004/05/24 20:18:38 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -820,7 +820,6 @@
       write(name, *) "Clock initialization with above settings Test"
       call ESMF_Test((rc.eq.ESMF_SUCCESS), &
                       name, failMsg, result, ESMF_SRCLINE)
-      call ESMF_ClockDestroy(clock_no_leap, rc)
 
       ! ----------------------------------------------------------------------------
 
@@ -872,7 +871,6 @@
       write(name, *) "Clock initialization with above settings Test"
       call ESMF_Test((rc.eq.ESMF_SUCCESS), &
                       name, failMsg, result, ESMF_SRCLINE)
-      call ESMF_ClockDestroy(clock_360day, rc)
 
       ! ----------------------------------------------------------------------------
 
