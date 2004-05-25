f76e2e30af1fcfbad15d5c589f20752cedf909c4
Nancy Collins <nancy@ucar.edu>
2004-05-25 12:11:54 +0000

    update protex to matchlatest doc revisions

--- src/Superstructure/Component/interface/ESMF_CplCompSetServ.F90
+++ src/Superstructure/Component/interface/ESMF_CplCompSetServ.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_CplCompSetServ.F90,v 1.1 2004/03/19 20:01:42 cdeluca Exp $
+! $Id: ESMF_CplCompSetServ.F90,v 1.2 2004/05/25 12:11:54 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -30,27 +30,28 @@
 !      integer, intent(out), optional :: rc
 !
 ! !DESCRIPTION:
-!  Available to be called by a cplcomp at any time after 
-!  {\tt SetInternalState} has been called.
-!  Since Init, Run, and Finalize must be separate subroutines, data that
+!  Available to be called by an {\tt ESMF\_CplComp} at any time after 
+!  {\tt ESMF\_CplCompSetInternalState} has been called.
+!  Since init, run, and finalize must be separate subroutines, data that
 !  they need to share in common can either be module global data, or can
 !  be allocated in a private data block, and the address of that block
 !  can be registered with the framework and retrieved by this call.
-!  When running multiple instantiations of a cplcomp, for example during
-!  ensemble runs, it may be simpler to maintain private data specific to 
-!  each run with private data blocks.  A corresponding {\tt SetInternalState}
-!  call sets the data pointer to this block, and this call retrieves the 
-!  data pointer.
+!  When running multiple instantiations of an {\tt ESMF\_CplComp}, 
+!  for example during ensemble runs, 
+!  it may be simpler to maintain private data specific to 
+!  each run with private data blocks.  A corresponding 
+!  {\tt ESMF\_CplCompSetInternalState} call sets the data pointer to 
+!  this block, and this call retrieves the data pointer.
 !    
 !  The arguments are:
 !  \begin{description}
 !   \item[cplcomp]
-!    CplComp object returned from a {\tt ESMF\_CplCompCreate} call.
+!    An {\tt ESMF\_CplComp} object.
 !   \item[dataPointer]
 !    A derived type, containing only a pointer to the private data block.
 !    The framework will fill in the block and when this call returns the
 !    pointer is set to the same address set during 
-!    {\tt ESMF\_CplSetInternalState}.
+!    {\tt ESMF\_CplCompSetInternalState}.
 !    This level of indirection is needed to reliably set and retrieve 
 !    the data block no matter which architecture or compiler is used.  
 !   \item[{[rc]}] 
@@ -74,33 +75,38 @@
 !      integer, intent(out), optional :: rc
 !
 ! !DESCRIPTION:
-!  Intended to be called by a cplcomp during the Registration process.
-!  A cplcomp calls SetEntryPoint for each of the predefined Init,
-!  Run, and Finalize routines, to assocate the internal subroutine to be
-!  called for each function.  If multiple phases for Init, Run, or Finalize
+!  Intended to be called by an {\tt ESMF\_CplComp} during the 
+!  registration process.
+!  An {\tt ESMF\_CplComp} calls {\tt ESMF\_SetEntryPoint} for each of 
+!  the predefined init, run, and finalize routines, 
+!  to assocate the internal subroutine to be
+!  called for each function.  If multiple phases for init, run, or finalize
 !  are needed, this can be called with phase numbers.
 !
 !  After this subroutine returns, the framework now knows how to call
-!  the Initialize, Run, and Finalize routines for the subcplcomps.
+!  the initialize, run, and finalize routines for this child 
+!  {\tt ESMF\_CplComp}.
 !    
 !  The arguments are:
 !  \begin{description}
 !   \item[cplcomp]
-!    CplComp object returned from a {\tt ESMF\_CplCompCreate} call.
+!    An {\tt ESMF\_CplComp} object.
 !   \item[subroutineType]
 !    One of a set of predefined subroutine types - e.g. {\tt ESMF\_SETINIT}, 
 !    {\tt ESMF\_SETRUN}, {\tt ESMF\_SETFINAL}.
 !   \item[subroutineName]
-!    The name of the CplComp's subroutine to be associated with the
-!    subroutineType (e.g. Initialize).   This subroutine does not have to be
+!    The name of the {\tt cplcomp} subroutine to be associated with the
+!    {\tt subroutineType}.  This subroutine does not have to be
 !    public to the module.
 !   \item[{[phase]}] 
-!    For CplComps which need to initialize or run or finalize in mutiple
-!    phases, the phase number which corresponds to this subroutine name.
+!    For {\tt ESMF\_CplComp}s which need to initialize, run, or finalize 
+!    with mutiple phases, the phase number which 
+!    corresponds to this subroutine name.
 !    For single phase subroutines, either omit this argument, or use the
-!    parameter {\tt ESMF\_SINGLEPHASE}.   The CplComp writer must document
-!    the requirements of the CplComp for how and when the multiple phases
-!    are expected to be called.
+!    parameter {\tt ESMF\_SINGLEPHASE}.   The {\tt ESMF\_CplComp} writer 
+!    must document
+!    the requirements of the {\tt ESMF\_CplComp} for how and when 
+!    the multiple phases are expected to be called.
 !   \item[{[rc]}] 
 !    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
 !   \end{description}
@@ -118,21 +124,23 @@
 !      integer, intent(out), optional :: rc
 !
 ! !DESCRIPTION:
-!  Available to be called by a cplcomp at any time, but expected to be
-!  most useful when called during the Registration process, or Initialization.
-!  Since Init, Run, and Finalize must be separate subroutines, data that
+!  Available to be called by an {\tt ESMF\_CplComp} at any time, but 
+!  expected to be
+!  most useful when called during the registration process, or initialization.
+!  Since init, run, and finalize must be separate subroutines, data that
 !  they need to share in common can either be module global data, or can
 !  be allocated in a private data block, and the address of that block
 !  can be registered with the framework and retrieved by subsequent calls.
-!  When running multiple instantiations of a cplcomp, for example during
+!  When running multiple instantiations of an {\tt ESMF\_CplComp} , 
+!  for example during
 !  ensemble runs, it may be simpler to maintain private data specific to 
-!  each run with private data blocks.  A corresponding {\tt GetInternalState}
-!  call retrieves the data pointer.
+!  each run with private data blocks.  A corresponding 
+!  {\tt ESMF\_CplCompGetInternalState} call retrieves the data pointer.
 !    
 !  The arguments are:
 !  \begin{description}
-!   \item[cplcomp]
-!    CplComp object returned from a {\tt ESMF\_CplCompCreate} call.
+!   \item[cplcomp] 
+!    An {\tt ESMF\_CplComp} object.
 !   \item[dataPointer]
 !    A pointer to the private data block, wrapped in a derived type which
 !    contains only a pointer to the block.  This level of indirection is
@@ -157,23 +165,24 @@
 !      integer, intent(out), optional :: rc
 !
 ! !DESCRIPTION:
-!  Call a Coupler {\tt ESMF\_CplComp}'s SetServices registration routine.  
-!  The parent CplComp must first create a subcplcomp and then
-!  call this routine, giving it the cplcomp derived type
-!  returned from the create, plus the public, well-known, subroutine name
-!  that is the registration routine for that cplcomp.  This name must be
-!  documented by the subcplcomp provider.
+!  Call an {\tt ESMF\_CplComp}'s setservices registration routine.  
+!  The parent component must first create an {\tt ESMF\_CplComp}, then
+!  call this routine. The arguments are the object returned from the create
+!  call, plus the public, well-known, subroutine name
+!  that is the registration routine for this {\tt ESMF\_CplComp}.  
+!  This name must be documented by the {\tt ESMF\_CplComp} provider.
 !
-!  After this subroutine returns, the framework now knows how to call
-!  the Initialize, Run, and Finalize routines for the subcplcomps.
+!  After this subroutine returns the framework now knows how to call
+!  the initialize, run, and finalize routines for the {\tt ESMF\_CplComp}.
 !    
 !  The arguments are:
 !  \begin{description}
 !   \item[cplcomp]
-!    CplComp object returned from a {\tt ESMF\_CplCompCreate} call.
+!    An {\tt ESMF\_CplComp} object.
 !   \item[subroutineName]
-!    The public name of the CplComp's {\tt SetServices} call.  
-!    A cplcomp writer must provide this information.
+!    The public name of the CplComp's {\tt ESMF\_CplCompSetServices} call.  
+!    An {\tt ESMF\_CplComp} writer must provide this information.
+!    Note this is the actual subroutine, not a character string.
 !   \item[{[rc]}] 
 !    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
 !   \end{description}
