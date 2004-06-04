534f5054ab8c80233aceae9a72515370844d5d9a
Nancy Collins <nancy@ucar.edu>
2004-06-04 08:24:38 +0000

    still needs work, but update for vm changes

--- src/system_tests/ESMF_CplOnExclDEs/ESMF_CplOnExclDEsSTest.F90
+++ src/system_tests/ESMF_CplOnExclDEs/ESMF_CplOnExclDEsSTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_CplOnExclDEsSTest.F90,v 1.12 2004/05/27 22:47:20 jwolfe Exp $
+! $Id: ESMF_CplOnExclDEsSTest.F90,v 1.13 2004/06/04 08:24:38 nscollins Exp $
 !
 ! System test code CouplingOnExclDEs
 !  Description on Sourceforge under System Test #62503
@@ -31,10 +31,9 @@
     implicit none
     
     ! Local variables
-    integer :: i, de_id, ndes, mid, rc, delist(64), pid, cid
+    integer :: i, pe_id, ndes, mid, rc, delist(64), pid, cid
     character(len=ESMF_MAXSTR) :: cname1, cname2, cplname
-    type(ESMF_DELayout) :: layout1, layout2, layout3
-    type(ESMF_VM) :: vm
+    type(ESMF_VM) :: vm, childvm1, childvm2
     type(ESMF_State) :: c1exp, c2imp
     type(ESMF_GridComp) :: comp1, comp2
     type(ESMF_CplComp) :: cpl
@@ -71,46 +70,47 @@
 !-------------------------------------------------------------------------
 !
     ! Initialize the ESMF Framework
-    call ESMF_Initialize(rc=rc)
+    call ESMF_Initialize(globalvm=vm, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-    call ESMF_VMGetGlobal(vm, rc=rc)
-    ! Query default layout
-    layout1 = ESMF_DELayoutCreate(vm, rc=rc)
+    call ESMF_VMGet(vm, petCount=npets, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-    call ESMF_DELayoutGetNumDEs(layout1, ndes, rc=rc)
-    if (ndes .lt. 8) then
-        print *, "This system test needs to run at least 8-way, current np = ", ndes
+    if (npets .lt. 8) then
+        print *, "This system test needs to run at least 8-way, current np = ", npets
         goto 10
     endif
 
 
-    mid = ndes / 2
-
+    mid = npets / 2
 
     ! Create the 2 model components and coupler
     cname1 = "user model 1"
-    layout2 = ESMF_DELayoutCreate(layout1, (/ 2, ndes/4 /), rc=rc)
-    !delist = (/ (i, i=0, mid-1) /)
-    !layout2 = ESMF_DELayoutCreate(layout1, 2, (/ 2, ndes/4 /), (/ 0, 0 /), &
-    !                                                  de_indices=delist, rc=rc)
-    comp1 = ESMF_GridCompCreate(cname1, layout=layout2, rc=rc)
+
+    ! TODO: create a child vm with half the pets here
+    childvm1 = vm
+
+    comp1 = ESMF_GridCompCreate(childvm1, cname1, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+
     print *, "Created component ", trim(cname1), "rc =", rc
     call ESMF_GridCompPrint(comp1, "", rc)
 
 
     cname2 = "user model 2"
-    layout3 = ESMF_DELayoutCreate(layout1, (/ 1, ndes/2 /), rc=rc)
-    !delist = (/ (i, i=mid, ndes-1) /)
-    !layout3 = ESMF_DELayoutCreate(layout1, 2, (/ 1, ndes/2 /), (/ 0, 0 /), &
-    !                                                  de_indices=delist, rc=rc)
 
+    ! TODO: create a child vm with other half the pets here
+    childvm1 = vm
 
-    comp2 = ESMF_GridCompCreate(cname2, layout=layout3, rc=rc)
+    comp2 = ESMF_GridCompCreate(childvm2, cname2, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+
     print *, "Created component ", trim(cname2), "rc =", rc
     call ESMF_GridCompPrint(comp2, "", rc)
 
     cplname = "user one-way coupler"
-    cpl = ESMF_CplCompCreate(cplname, layout=layout1, rc=rc)
+    cpl = ESMF_CplCompCreate(vm, cplname, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
     print *, "Created component ", trim(cplname), ", rc =", rc
     call ESMF_CplCompPrint(cpl, "", rc)
 
@@ -123,38 +123,45 @@
 !  Register section
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
-      call ESMF_GridCompSetServices(comp1, userm1_register, rc)
-      print *, "Comp SetServices finished, rc= ", rc
+    call ESMF_GridCompSetServices(comp1, userm1_register, rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp SetServices finished, rc= ", rc
 
-      call ESMF_GridCompSetServices(comp2, userm2_register, rc)
-      print *, "Comp SetServices finished, rc= ", rc
+    call ESMF_GridCompSetServices(comp2, userm2_register, rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp SetServices finished, rc= ", rc
 
-      call ESMF_CplCompSetServices(cpl, usercpl_register, rc)
-      print *, "Comp SetServices finished, rc= ", rc
+    call ESMF_CplCompSetServices(cpl, usercpl_register, rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp SetServices finished, rc= ", rc
 
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
 !  Create and initialize a clock.
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
-      ! initialize calendar to be Gregorian type
-      gregorianCalendar = ESMF_CalendarCreate("Gregorian", &
-                                              ESMF_CAL_GREGORIAN, rc)
+    ! initialize calendar to be Gregorian type
+    gregorianCalendar = ESMF_CalendarCreate("Gregorian", &
+                                            ESMF_CAL_GREGORIAN, rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-      ! initialize time interval to 6 hours
-      call ESMF_TimeIntervalSet(timeStep, h=6, rc=rc)
+    ! initialize time interval to 6 hours
+    call ESMF_TimeIntervalSet(timeStep, h=6, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-      ! initialize start time to 3/28/2003
-      call ESMF_TimeSet(startTime, yy=2003, mm=5, dd=1, &
-                        calendar=gregorianCalendar, rc=rc)
+    ! initialize start time to 3/28/2003
+    call ESMF_TimeSet(startTime, yy=2003, mm=5, dd=1, &
+                      calendar=gregorianCalendar, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-      ! initialize stop time to 3/29/2003
-      call ESMF_TimeSet(stopTime, yy=2003, mm=5, dd=2, &
-                        calendar=gregorianCalendar, rc=rc)
+    ! initialize stop time to 3/29/2003
+    call ESMF_TimeSet(stopTime, yy=2003, mm=5, dd=2, &
+                      calendar=gregorianCalendar, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
-      ! initialize the clock with the above values
-      clock = ESMF_ClockCreate("Clock 1", timeStep, startTime, stopTime, rc=rc)
-
+    ! initialize the clock with the above values
+    clock = ESMF_ClockCreate("Clock 1", timeStep, startTime, stopTime, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
 
  
 !-------------------------------------------------------------------------
@@ -163,16 +170,19 @@
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
  
-      c1exp = ESMF_StateCreate("comp1 export", ESMF_STATEEXPORT, cname1)
-      call ESMF_GridCompInitialize(comp1, exportState=c1exp, clock=clock, rc=rc)
-      print *, "Comp 1 Initialize finished, rc =", rc
+    c1exp = ESMF_StateCreate("comp1 export", ESMF_STATEEXPORT, cname1)
+    call ESMF_GridCompInitialize(comp1, exportState=c1exp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp 1 Initialize finished, rc =", rc
  
-      c2imp = ESMF_StateCreate("comp2 import", ESMF_STATEIMPORT, cname2)
-      call ESMF_GridCompInitialize(comp2, importState=c2imp, clock=clock, rc=rc)
-      print *, "Comp 1a Initialize finished, rc =", rc
+    c2imp = ESMF_StateCreate("comp2 import", ESMF_STATEIMPORT, cname2)
+    call ESMF_GridCompInitialize(comp2, importState=c2imp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp 1a Initialize finished, rc =", rc
  
-      call ESMF_CplCompInitialize(cpl, c1exp, c2imp, clock=clock, rc=rc)
-      print *, "Coupler Initialize finished, rc =", rc
+    call ESMF_CplCompInitialize(cpl, c1exp, c2imp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Coupler Initialize finished, rc =", rc
  
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
@@ -180,21 +190,25 @@
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
 
-      do while (.not. ESMF_ClockIsStopTime(clock, rc))
+    do while (.not. ESMF_ClockIsStopTime(clock, rc))
 
-        call ESMF_GridCompRun(comp1, exportState=c1exp, clock=clock, rc=rc)
-        print *, "Comp 1 Run returned, rc =", rc
+      call ESMF_GridCompRun(comp1, exportState=c1exp, clock=clock, rc=rc)
+      if (rc .ne. ESMF_SUCCESS) goto 10
+      print *, "Comp 1 Run returned, rc =", rc
   
-        call ESMF_CplCompRun(cpl, c1exp, c2imp, clock=clock, rc=rc)
-        print *, "Coupler Run returned, rc =", rc
+      call ESMF_CplCompRun(cpl, c1exp, c2imp, clock=clock, rc=rc)
+      if (rc .ne. ESMF_SUCCESS) goto 10
+      print *, "Coupler Run returned, rc =", rc
   
-        call ESMF_GridCompRun(comp2, importState=c2imp, clock=clock, rc=rc)
-        print *, "Comp 2 Run returned, rc =", rc
+      call ESMF_GridCompRun(comp2, importState=c2imp, clock=clock, rc=rc)
+      if (rc .ne. ESMF_SUCCESS) goto 10
+      print *, "Comp 2 Run returned, rc =", rc
 
-        call ESMF_ClockAdvance(clock, rc=rc)
-        !call ESMF_ClockPrint(clock, rc=rc)
+      call ESMF_ClockAdvance(clock, rc=rc)
+      if (rc .ne. ESMF_SUCCESS) goto 10
+      !call ESMF_ClockPrint(clock, rc=rc)
 
-      enddo
+    enddo
  
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
@@ -203,27 +217,30 @@
 !-------------------------------------------------------------------------
 !     Print result
 
-      call ESMF_GridCompFinalize(comp1, exportState=c1exp, clock=clock, rc=rc)
-      print *, "Comp 1 Finalize finished, rc =", rc
+    call ESMF_GridCompFinalize(comp1, exportState=c1exp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp 1 Finalize finished, rc =", rc
 
-      call ESMF_GridCompFinalize(comp2, importState=c2imp, clock=clock, rc=rc)
-      print *, "Comp 2 Finalize finished, rc =", rc
+    call ESMF_GridCompFinalize(comp2, importState=c2imp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Comp 2 Finalize finished, rc =", rc
 
-      call ESMF_CplCompFinalize(cpl, c1exp, c2imp, clock=clock, rc=rc)
-      print *, "Coupler Finalize finished, rc =", rc
-
-
-      ! Figure out our local processor id for message below.
-      call ESMF_DELayoutGetDEID(layout1, de_id, rc)
+    call ESMF_CplCompFinalize(cpl, c1exp, c2imp, clock=clock, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    print *, "Coupler Finalize finished, rc =", rc
 
 
-      print *, "------------------------------------------------------------"
-      print *, "------------------------------------------------------------"
-      print *, "Test finished, de_id = ", de_id
-      print *, "------------------------------------------------------------"
-      print *, "------------------------------------------------------------"
+    ! Figure out our local processor id for message below.
+    call ESMF_VMGet(vm, localPet=pe_id, rc=rc)
 
-      print *, "Comp Finalize returned"
+
+    print *, "------------------------------------------------------------"
+    print *, "------------------------------------------------------------"
+    print *, "Test finished, pe_id = ", pe_id
+    print *, "------------------------------------------------------------"
+    print *, "------------------------------------------------------------"
+
+    print *, "Comp Finalize returned"
 
 !
 !-------------------------------------------------------------------------
@@ -233,27 +250,24 @@
 !-------------------------------------------------------------------------
 !     Clean up
 
-      call ESMF_StateDestroy(c1exp, rc)
-      call ESMF_StateDestroy(c2imp, rc)
+    call ESMF_StateDestroy(c1exp, rc)
+    call ESMF_StateDestroy(c2imp, rc)
 
-      call ESMF_ClockDestroy(clock, rc)
-      call ESMF_CalendarDestroy(gregorianCalendar, rc)
+    call ESMF_ClockDestroy(clock, rc)
+    call ESMF_CalendarDestroy(gregorianCalendar, rc)
 
-      call ESMF_GridCompDestroy(comp1, rc)
-      call ESMF_GridCompDestroy(comp2, rc)
-      call ESMF_CplCompDestroy(cpl, rc)
+    call ESMF_GridCompDestroy(comp1, rc)
+    call ESMF_GridCompDestroy(comp2, rc)
+    call ESMF_CplCompDestroy(cpl, rc)
 
-      call ESMF_DELayoutDestroy(layout2, rc)
-      call ESMF_DELayoutDestroy(layout3, rc)
-
-      print *, "All Destroy routines done"
+    print *, "All Destroy routines done"
 
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
 10    print *, "System Test CouplingOnExclDEs complete."
 
       ! Only on de 0 or any DE with an error. 
-      if ((de_id .eq. 0) .or. (rc .ne. ESMF_SUCCESS)) then
+      if ((pe_id .eq. 0) .or. (rc .ne. ESMF_SUCCESS)) then
 
         ! Normal ESMF Test output
         write(failMsg, *) "System Test failure"
