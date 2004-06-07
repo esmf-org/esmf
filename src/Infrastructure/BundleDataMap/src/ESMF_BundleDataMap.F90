6289afbae1f7d8300f76a05785ff4d95ad7ad146
Nancy Collins <nancy@ucar.edu>
2004-06-07 08:58:24 +0000

    fix define

--- src/Infrastructure/BundleDataMap/src/ESMF_BundleDataMap.F90
+++ src/Infrastructure/BundleDataMap/src/ESMF_BundleDataMap.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_BundleDataMap.F90,v 1.10 2004/06/04 13:13:55 nscollins Exp $
+! $Id: ESMF_BundleDataMap.F90,v 1.11 2004/06/07 08:58:24 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research, 
@@ -9,7 +9,7 @@
 ! Licensed under the GPL.
 !
 !------------------------------------------------------------------------------
-#define ESMF_FILE "ESMF_BundleDataMap.F90"
+#define ESMF_FILENAME "ESMF_BundleDataMap.F90"
 !
 ! ESMF BundleDataMap module
 !
@@ -124,7 +124,7 @@
 ! leave the following line as-is; it will insert the cvs ident string
 ! into the object file for tracking purposes.
      character(*), parameter, private :: version =  &
-       '$Id: ESMF_BundleDataMap.F90,v 1.10 2004/06/04 13:13:55 nscollins Exp $'
+       '$Id: ESMF_BundleDataMap.F90,v 1.11 2004/06/07 08:58:24 nscollins Exp $'
 !------------------------------------------------------------------------------
 
 
