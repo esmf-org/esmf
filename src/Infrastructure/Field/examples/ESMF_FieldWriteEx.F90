3b4f9c7f97d1cdb48b402a6581684b538663ec47
Spencer Swift <swift@sgi.com>
2004-06-09 14:42:15 +0000

    First cut at a FieldWrite example case.

---
+++ src/Infrastructure/Field/examples/ESMF_FieldWriteEx.F90
@@ -0,0 +1,227 @@
+ !
+! Example/test code which writes out a Field to a netCDF file.
+
+!-------------------------------------------------------------------------
+!EXAMPLE        String used by test script to count examples.
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+! See the following code fragments for examples of how to output a Field.
+!
+!
+!EOP
+!BOC
+
+!     ! Example program showing various ways to output a Field.
+program ESMF_FieldWriteEx
+
+  ! ESMF Framework module, defines all ESMF data types and procedures
+  use ESMF_Mod
+  
+  implicit none
+    
+!     ! Local variables
+  integer :: status, rc
+  type(ESMF_GridHorzStagger) :: horz_stagger
+  integer, dimension(2) :: counts
+  real(ESMF_KIND_R8), dimension(2) :: min, max
+  type(ESMF_DELayout) :: layout
+  type(ESMF_Grid) :: grid
+  type(ESMF_VM) :: vm
+  character (len = ESMF_MAXSTR) :: name
+  real (ESMF_KIND_R8), dimension(2) :: origin
+  real(kind=ESMF_KIND_R4), dimension(:,:), pointer :: u2
+  type(ESMF_Field) :: field_u2
+  type(ESMF_ArraySpec) :: arrayspec
+  type(ESMF_Array), pointer :: array_temp
+  type(ESMF_Time) :: timestamp
+  type(ESMF_IOSpec) :: iospec
+  integer i,j
+!EOC
+
+  integer :: finalrc
+  finalrc = ESMF_SUCCESS
+  
+  print *, 'Testing wrf write'
+!BOC
+  call ESMF_Initialize(rc=rc)
+!EOC
+  if (rc.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'call ESMF_Initialize' failed"
+  endif
+  
+!BOC
+  call ESMF_VMGetGlobal(vm, rc)
+!EOC
+  if (rc.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "' call ESMF_VMGetGlobal' failed"
+  endif
+    
+!BOC
+!-------------------------------------------------------------------------
+!   ! Example 1:
+!   !
+!   !  The user creates a simple horizontal Grid internally by passing all
+!   !  necessary information through the CreateInternal argument list.
+
+  counts(1) = 10
+  counts(2) = 12
+  horz_stagger = ESMF_GRID_HORZ_STAGGER_A
+  min(1) = 0.0
+  max(1) = 10.0
+  min(2) = 0.0
+  max(2) = 12.0
+  name = "test grid 1"
+ 
+  ! Create a 2 x 2 layout for the Grid
+  layout = ESMF_DELayoutCreate(vm, (/ 2, 2 /), rc=status)
+!EOC
+ 
+  if (status.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'layout=ESMF_DELayoutCreate' failed"
+  endif
+
+!BOC
+  grid = ESMF_GridCreateHorzXYUni(counts=counts, &
+                              minGlobalCoordPerDim=min, &
+                              maxGlobalCoordPerDim=max, &
+                              horzStagger=horz_stagger, &
+                              name=name, rc=status)
+      if (status.NE.ESMF_SUCCESS) then
+         print*, "'call ESMF_GridCreateHorzXYUni' failed"
+         finalrc = ESMF_FAILURE
+      else
+         print*, "'call ESMF_GridCreateHorzXYUni' succeeded"
+      end if
+
+  call ESMF_GridDistribute(grid, delayout=layout, rc=status)
+
+!EOC
+ 
+  if (status.NE.ESMF_SUCCESS) then
+     print*, "'call ESMF_GridDistribute' failed"
+         finalrc = ESMF_FAILURE
+      else
+         print*, "'call ESMF_GridDistribute' succeeded"
+  endif
+  
+  call ESMF_IOSpecSet(iospec, filename='foo.nc', &
+                      iofileformat=ESMF_IO_FILEFORMAT_UNSPECIFIED, &
+                      rc=Status)
+  if (Status.NE.ESMF_SUCCESS) then
+     print*, "'call ESMF_IOSpecSet' failed"
+     finalrc = ESMF_FAILURE
+  else
+     print*, "'call ESMF_IOSpecSet' succeeded"
+  endif
+
+  call ESMF_ArraySpecSet(arrayspec, rank=2, type=ESMF_DATA_REAL, &
+       kind=ESMF_R4, rc=Status)
+  if (Status.NE.ESMF_SUCCESS) then
+     print*, "' call ESMF_ArraySpecSet' failed"
+     finalrc = ESMF_FAILURE
+  else
+     print*, "' call ESMF_ArraySpecSet' succeeded"
+  endif
+  
+  field_u2  = ESMF_FieldCreate(grid, arrayspec, allocflag=ESMF_DO_ALLOCATE, &
+       horzRelloc=ESMF_CELL_CENTER, &
+       haloWidth=0, name="u2", iospec=iospec, rc=status)
+  if (Status.NE.ESMF_SUCCESS) then
+     print*, "'field_u2  = ESMF_FieldCreate' failed"
+  else
+     print*, "'field_u2  = ESMF_FieldCreate' succeeded"
+  endif
+
+!!$  call ESMF_FieldGetDataPointer(field_u2, u2, rc=status)
+  call ESMF_FieldGetArray( field_u2, array_temp, rc=status)
+  if (Status.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'call ESMF_FieldGetArray( field_u2, array_temp, rc=status)' failed"
+  endif
+
+call ESMF_ArrayPrint(array_temp)
+
+  call ESMF_ArrayGetData(array_temp, u2, ESMF_DATA_REF, status)
+  if (Status.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'call ESMF_ArrayGetData(array_temp, u2, ESMF_DATA_REF, status)' failed"
+  endif
+
+  print *,'testWRFWrite u2 (5,6)   = ',u2(5,6)
+
+!!$  u2 = 999.0
+
+!!$     do j= 6, 6
+!!$        do i= 5, 5
+!!$           u2(i,j) = float(10*i+j)
+!!$        enddo
+!!$     enddo
+!!$
+  u2(5,6) = float( 10*5+6)
+
+!!$  print*, shape(u2)
+
+  print *,'testWRFWrite u2 (5,6)   = ',u2(5,6)
+
+  call ESMF_ArrayPrint(array_temp)
+
+  call ESMF_TimeSet(timestamp, calendarType=ESMF_CAL_GREGORIAN, rc=status)
+  if (Status.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'call ESMF_TimeSet(timestamp, calendarType=ESMF_CAL_GREGORIAN, status)' failed"
+  else
+     print*, "'call ESMF_TimeSet(timestamp, calendarType=ESMF_CAL_GREGORIAN, status)' succeeded"
+  endif
+
+  call ESMF_TimeSyncToRealTime(timestamp, status)
+  if (Status.NE.ESMF_SUCCESS) then
+     finalrc = ESMF_FAILURE
+     print*, "'call ESMF_TimeSyncToRealTime(timestamp, status)' failed"
+  else
+     print*, "'call ESMF_TimeSyncToRealTime(timestamp, status)' succeeded"
+  endif
+
+  call ESMF_FieldWrite( field_u2, iospec, timestamp, status)
+  if (Status.NE.ESMF_SUCCESS) then
+     print*, "'call ESMF_FieldWrite( field_u2, iospec, timestamp, status)' failed"
+  else
+     print*, "'call ESMF_FieldWrite( field_u2, iospec, timestamp, status)' succeeded"
+  endif
+  
+!BOC
+      print *, "Grid example 1 returned"
+
+      call ESMF_GridDestroy(grid, rc)
+
+      print *, "Grid example 1 destroyed"
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+          print*, "'call ESMF_GridDestroy' failed"
+      end if
+
+!BOC
+      call ESMF_Finalize(rc)
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) then
+          finalrc = ESMF_FAILURE
+          print*, "'call ESMF_Finalize' failed"
+      end if
+
+     if (finalrc.EQ.ESMF_SUCCESS) then
+        print *, "PASS: ESMF_FieldWriteEx.F90"
+     else
+        print *, "FAIL: ESMF_FieldWriteEx.F90"
+     end if
+
+!BOC
+   end program ESMF_FieldWriteEx
+!EOC   
