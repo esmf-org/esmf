14d3728ce14f5a66b9d1a46806e57b44353cc745
Nancy Collins <nancy@ucar.edu>
2004-06-09 08:36:43 +0000

    examples in template format

---
+++ src/Infrastructure/ArraySpec/examples/ESMF_ArraySpecEx.F90
@@ -0,0 +1,94 @@
+! $Id: ESMF_ArraySpecEx.F90,v 1.1 2004/06/09 08:36:43 nscollins Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_ArraySpecEx
+
+!------------------------------------------------------------------------------
+!EXAMPLE	String used by test script to count examples.
+!==============================================================================
+!BOC
+! !PROGRAM: ESMF_ArraySpecEx - ArraySpec manipulation examples
+!
+! !DESCRIPTION:
+!
+! This program shows examples of ArraySpec set and get usage
+!-----------------------------------------------------------------------------
+
+      ! ESMF Framework module
+      use ESMF_Mod
+      implicit none
+
+      ! local variables 
+      type(ESMF_ArraySpec) :: arrayDS
+      integer :: myrank
+      type(ESMF_DataType) :: mytype
+      type(ESMF_DataKind) :: mykind
+
+
+      ! return code
+      integer:: rc
+!EOC
+
+      ! result code
+      integer :: finalrc
+      finalrc = ESMF_SUCCESS
+
+!BOC
+      ! initialize ESMF framework
+      call ESMF_Initialize(rc=rc)
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) finalrc = ESMF_FAILURE
+
+!BOP
+!\subsubsection{Setting ArraySpec Values}
+
+! This example shows how to set values in an {\tt ESMF\_ArraySpec}.
+!EOP
+
+!BOC
+      call ESMF_ArraySpecSet(arrayDS, rank=2, type=ESMF_DATA_REAL, &
+                             kind=ESMF_R8, rc=rc)
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) finalrc = ESMF_FAILURE
+
+!BOP
+!\subsubsection{Getting ArraySpec Values}
+
+! This example shows how to query an {\tt ESMF\_ArraySpec}.
+!EOP
+
+!BOC
+      call ESMF_ArraySpecGet(arrayDS, myrank, mytype, mykind, rc)
+      print *, "Returned values from ArraySpec:"
+      print *, "rank =", myrank
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) finalrc = ESMF_FAILURE
+
+!BOC
+      ! finalize ESMF framework
+      call ESMF_Finalize(rc)
+!EOC
+
+      if (rc.NE.ESMF_SUCCESS) finalrc = ESMF_FAILURE
+
+      if (finalrc.EQ.ESMF_SUCCESS) then
+        print *, "PASS: ESMF_ArraySpecEx.F90"
+      else
+        print *, "FAIL: ESMF_ArraySpecEx.F90"
+      end if
+
+!BOC
+      end program ESMF_ArraySpecEx
+!EOC
