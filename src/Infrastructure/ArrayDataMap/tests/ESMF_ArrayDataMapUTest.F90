f72bd7b61a85d7db4203ada51dcb81712a48b30a
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-06-13 03:54:32 +0000

    Original version.

--- src/Infrastructure/ArrayDataMap/tests/ESMF_ArrayDataMapUTest.F90
+++ src/Infrastructure/ArrayDataMap/tests/ESMF_ArrayDataMapUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ArrayDataMapUTest.F90,v 1.1 2004/05/03 16:12:58 nscollins Exp $
+! $Id: ESMF_ArrayDataMapUTest.F90,v 1.2 2004/06/13 03:54:32 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -10,22 +10,22 @@
 !
 !==============================================================================
 !
-      program ESMF_<Class>UTest
+      program ESMF_ArrayDataMapUTest
 
------------------------------------------------------------------------
+!-----------------------------------------------------------------------
 ! Include file, for ESMF_SRCLINE which must be done by the preprocessor
 
 #include <ESMF_Macros.inc>
 
------------------------------------------------------------------------
+!-----------------------------------------------------------------------
 !BOP
-! !PROGRAM: ESMF_<Class>UTest - One line general statement about this test
+! !PROGRAM: ESMF_ArrayDataMapUTest - One line general statement about this test
 !
 ! !DESCRIPTION:
 !
-! The code in this file drives F90 <Class> unit tests.
-! The companion file ESMF\_<Class>.F90 contains the definitions for the
-! <Class> methods.
+! The code in this file drives F90 ESMF_ArrayDataMap unit tests.
+! The companion file ESMF\_ArrayDataMap.F90 contains the definitions for the
+! ArrayDataMap methods.
 !
 !-----------------------------------------------------------------------------
 ! !USES:
@@ -36,14 +36,14 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_ArrayDataMapUTest.F90,v 1.1 2004/05/03 16:12:58 nscollins Exp $'
+      '$Id: ESMF_ArrayDataMapUTest.F90,v 1.2 2004/06/13 03:54:32 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
       integer :: result = 0
 
       ! individual test result code
-      integer :: rc
+      integer :: rc, datarank
 
       ! individual test name
       character(ESMF_MAXSTR) :: name
@@ -54,118 +54,120 @@
       ! local variables needed to pass into function/subroutine calls
       character(ESMF_MAXSTR) :: validate_options
       character(ESMF_MAXSTR) :: print_options
-      type(ESMF_<Class>Config) :: config_set
-      type(ESMF_<Class>Config) :: config_get
       ! when get/set value routines enabled, comment these in and set
       ! the appropriate values, and remove the temporary integers.
       !<value type> :: value_set, value_get
       integer :: value_set, value_get
 
-      ! instantiate a <Class> 
-      type(ESMF_<Class>) :: <class>
+      ! instantiate a ESMF_ArrayDataMap
+      type(ESMF_ArrayDataMap) :: ArrayDataMap
+
+      !-------------------------------------------------------------------------------
+      ! The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+      ! always run. When the environment variable, EXHAUSTIVE, is set to ON then
+      ! the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+      ! to OFF, then only the sanity unit tests.
+      ! Special strings (Non-exhaustive and exhaustive) have been
+      ! added to allow a script to count the number and types of unit tests.
+      !-------------------------------------------------------------------------------
+
+
+      call ESMF_Initialize(rc=rc)
+
+      datarank = 1
+
+      !-------------------------------------------------------------------------------
+      !NEX_UTest
+      ! Test ArrayDataMap Initialization
+      call ESMF_ArrayDataMapSetDefault(ArrayDataMap, datarank, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS"
+      write(name, *) "Set Default ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+      !-------------------------------------------------------------------------------
+
+      !NEX_UTest
+      ! Test ArrayDataMap Invalid
+      call ESMF_ArrayDataMapSetInvalid(ArrayDataMap, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS"
+      write(name, *) "Set Invalid ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
 
 #ifdef ESMF_EXHAUSTIVE
 
-      ! perform exhaustive tests here;
-      !   see #else below for non-exhaustive tests
-      ! future release will use run-time switching mechanism
 
-      ! for deep classes, keep create/construct routine and remove init
-      ! for shallow classes, keep init and remove create/construct
-     
-      ! test dynamic allocation of ESMF_<Class>
-      <class> = ESMF_<Class>Create(args, rc)
-      write(name, *) "ESMF_<Class>Create"
-      write(failMsg, *) "rc =", rc, ", args =", args
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
-    
-      ! test internal dynamic allocation within statically allocated
-      !   ESMF_<Class>
-      call ESMF_<Class>Construct(<class>, args, rc)
-      write(name, *) "ESMF_<Class>Construct"
-      write(failMsg, *) "rc =", rc, ", args =", args
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
+      !-------------------------------------------------------------------------------
+      !EX_UTest
+      ! Test ArrayDataMap Initialization
+      call ESMF_ArrayDataMapSetDefault(ArrayDataMap, datarank, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS"
+      write(name, *) "Set ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
-      ! test initialization of members of statically allocated ESMF_<Class>
-      !   may want to read back values via Get methods for comparison
-      call ESMF_<Class>Init(<class>, args, rc)
-      write(name, *) "ESMF_<Class>Init"
-      write(failMsg, *) "rc =", rc, ", args =", args
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
+      !-------------------------------------------------------------------------------
+      !EX_UTest
+      ! Test ArrayDataMap Get
+      call ESMF_ArrayDataMapGet(ArrayDataMap,  datarank=datarank, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS and/or datarank incorrect"
+      write(name, *) "Get ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(datarank.eq.1), name, failMsg, result, ESMF_SRCLINE)
 
-      ! test setting of configuration values
-      call ESMF_<Class>SetConfig(<class>, config_set, rc)
-      write(name, *) "ESMF_<Class>SetConfig"
-      write(failMsg, *) "rc =", rc, ", config_set =", config_set
-      call ESMF_Test((rc.eq.ESMF_SUCCESS),  &
-                      name, failMsg, result, ESMF_SRCLINE)
 
-      ! test getting of configuration values,
-      !  compare to values set previously
-      call ESMF_<Class>GetConfig(<class>, config_get, rc)
-      write(name, *) "ESMF_<Class>GetConfig"
-      write(failMsg, *) "rc =", rc, ", config_get =", config_get
-      call ESMF_Test((rc.eq.ESMF_SUCCESS .and. config_get .eq. config_set), &
-                      name, failMsg, result, ESMF_SRCLINE)
+      !-------------------------------------------------------------------------------
+      !This code crashes
+      ! Bug report 969243 opened
+      ! Test ArrayDataMap Print
+      !call ESMF_ArrayDataMapPrint(ArrayDataMap, rc=rc)
+      !write(failMsg, *) "Did not return ESMF_SUCCESS"
+      !write(name, *) "Print ArrayDataMap Test"
+      !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
-      ! test setting of ESMF_<Class> members values
-      !call ESMF_<Class>Set<Value>(<class>, value_set, rc)
-      rc = ESMF_FAILURE  ! remove this when this test enabled
-      write(name, *) "ESMF_<Class>Set<Value>"
-      write(failMsg, *) "rc =", rc, ", value_set =", value_set
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
 
-      ! test getting of ESMF_<Class> members values,
-      !   compare to values set previously
-      !call ESMF_<Class>Get<Value>(<class>, value_get, rc)
-      rc = ESMF_FAILURE  ! remove this when this test enabled
-      write(name, *) "ESMF_<Class>Get<Value>"
-      write(failMsg, *) "rc =", rc, ", value_get =", value_get
-      call ESMF_Test((rc.eq.ESMF_SUCCESS .and. value_get .eq. value_set), &
-                      name, failMsg, result, ESMF_SRCLINE)
-    
-      ! test validate method via option string
-      call ESMF_<Class>Validate(<class>, validate_options, rc)
-      write(name, *) "ESMF_<Class>Validate"
-      write(failMsg, *) "rc =",rc,", validate_options =", trim(validate_options)
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
 
-      ! test print method via option string
-      call ESMF_<Class>Print(<class>, print_options, rc)
-      write(name, *) "ESMF_<Class>Print"
-      write(failMsg, *) "rc =", rc, ", print_options =", trim(print_options)
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
+      !-------------------------------------------------------------------------------
+      !EX_UTest
+      ! Test ArrayDataMap Validate
+      call ESMF_ArrayDataMapValidate(ArrayDataMap, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS"
+      write(name, *) "Validate ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
-      ! test internal dynamic deallocation within statically allocated 
-      !   ESMF_<Class>.   only valid for deep classes; remove for shallow
-      call ESMF_<Class>Destruct(<class>, rc)
-      write(name, *) "ESMF_<Class>Destruct"
-      write(failMsg, *) "rc =", rc
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
 
-      ! test dynamic deallocation of ESMF_<Class>
-      !   also tests destructor
-      call ESMF_<Class>Destroy(<class>, rc)
-      write(name, *) "ESMF_<Class>Destroy"
-      write(failMsg, *) "rc =", rc
-      call ESMF_Test((rc.eq.ESMF_SUCCESS), &
-                      name, failMsg, result, ESMF_SRCLINE)
+      !-------------------------------------------------------------------------------
+      !EX_UTest
+      ! Test ArrayDataMap Set
+      call ESMF_ArrayDataMapSet(ArrayDataMap, datarank=2, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS"
+      write(name, *) "Set ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
-#else
 
-      ! perform non-exhaustive tests here;
-      !   use same templates as above
+      !-------------------------------------------------------------------------------
+      !EX_UTest
+      ! Test ArrayDataMap Get
+      call ESMF_ArrayDataMapGet(ArrayDataMap,  datarank, rc=rc)
+      write(failMsg, *) "Did not return ESMF_SUCCESS and/or horzRelloc incorrect"
+      write(name, *) "Get ArrayDataMap Test"
+      call ESMF_Test((rc.eq.ESMF_SUCCESS).and.(datarank.eq.2), name, failMsg, result, ESMF_SRCLINE)
+
+
+
+
+      !-------------------------------------------------------------------------------
+      !This code crashes
+      ! Bug report 969243 opened
+      ! Test ArrayDataMap Print
+      !call ESMF_ArrayDataMapPrint(ArrayDataMap, rc=rc)
+      !write(failMsg, *) "Did not return ESMF_SUCCESS"
+      !write(name, *) "Print ArrayDataMap Test"
+      !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
 
 #endif
 
       ! return number of failures to environment; 0 = success (all pass)
       ! return result  ! TODO: no way to do this in F90 ?
   
-      end program ESMF_<Class>UTest
+      end program ESMF_ArrayDataMapUTest
