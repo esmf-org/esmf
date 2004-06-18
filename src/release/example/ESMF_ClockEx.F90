9abc9383d5bc1d65de4b1639947825a86dcaba43
Earl Schwab <eschwab.esmf@gmail.com>
2004-06-18 20:58:10 +0000

    replaces ESMF_TimeEx.F90

---
+++ src/release/example/ESMF_ClockEx.F90
@@ -0,0 +1,85 @@
+! $Id: ESMF_ClockEx.F90,v 1.1 2004/06/18 20:58:10 eschwab Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_ClockEx
+
+!------------------------------------------------------------------------------
+!
+!==============================================================================
+!BOC
+! !PROGRAM: ESMF_ClockEx - Clock initialization and time-stepping
+!
+! !DESCRIPTION:
+!
+! This program shows an example of how to create, initialize, advance, and
+! examine a basic clock
+!-----------------------------------------------------------------------------
+
+      ! ESMF Framework module
+      use ESMF_Mod
+      implicit none
+
+      ! instantiate a clock 
+      type(ESMF_Clock) :: clock
+
+      ! instantiate time_step, start and stop times
+      type(ESMF_TimeInterval) :: timeStep
+      type(ESMF_Time) :: startTime
+      type(ESMF_Time) :: stopTime
+
+      ! local variables for Get methods
+      type(ESMF_Time) :: currTime
+      integer(ESMF_KIND_I8) :: advanceCount
+      integer :: YY, MM, DD, H, M, S
+
+      ! return code
+      integer :: rc
+
+      ! initialize ESMF framework
+      call ESMF_Initialize(defaultCalendar=ESMF_CAL_GREGORIAN, rc=rc)
+
+      ! initialize time interval to 2 days, 4 hours (6 timesteps in 13 days)
+      call ESMF_TimeIntervalSet(timeStep, d=2, h=4, rc=rc)
+
+      ! initialize start time to 4/1/2003 2:24:00 ( 1/10 of a day )
+      call ESMF_TimeSet(startTime, yy=2003, mm=4, dd=1, h=2, m=24, rc=rc)
+
+      ! initialize stop time to 4/14/2003 2:24:00 ( 1/10 of a day )
+      call ESMF_TimeSet(stopTime, yy=2003, mm=4, dd=14, h=2, m=24, rc=rc)
+
+      ! initialize the clock with the above values
+      clock = ESMF_ClockCreate("Clock 1", timeStep, startTime, stopTime, rc=rc)
+
+      ! time step clock from start time to stop time
+      do while (.not.ESMF_ClockIsStopTime(clock, rc))
+        call ESMF_ClockPrint(clock, "currTime string", rc)
+        call ESMF_ClockAdvance(clock, rc=rc)
+      end do
+
+      ! get the clock's final current time
+      call ESMF_ClockGet(clock, currTime=currTime, rc=rc)
+  
+      call ESMF_TimeGet(currTime, yy=YY, mm=MM, dd=DD, h=H, m=M, s=S, rc=rc) 
+      print *, "The clock's final current time is ", YY, "/", MM, "/", DD, &
+               " ", H, ":", M, ":", S
+
+      ! get the number of times the clock was advanced
+      call ESMF_ClockGet(clock, advanceCount=advanceCount, rc=rc)
+      print *, "The clock was advanced ", advanceCount, " times."
+
+      ! destroy clock
+      call ESMF_ClockDestroy(clock, rc)
+
+      ! finalize ESMF framework
+      call ESMF_Finalize(rc)
+
+      end program ESMF_ClockEx
