364e805ff302ee44092eed8e77882fb4360e549b
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-07-07 17:34:22 +0000

    Changed name from ESMF_CompCreateUTest.F90.

---
+++ src/Superstructure/Component/tests/ESMF_GridCompCreateUTest.F90
@@ -0,0 +1,171 @@
+! $Id: ESMF_GridCompCreateUTest.F90,v 1.1 2004/07/07 17:34:22 svasquez Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_GridCompCreateUTest
+
+!------------------------------------------------------------------------------
+
+#include <ESMF_Macros.inc>
+
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_GridCompCreateUTest - Unit test for Components.
+!
+! !DESCRIPTION:
+! Tests, cursory and exahustive, for Component Create code.
+!
+!-------------------------------------------------------------------------
+!
+! !USES:
+    use ESMF_TestMod     ! test methods
+    use ESMF_Mod
+    implicit none
+    
+!   ! Local variables
+    integer :: rc
+    integer , pointer:: pointer
+    character(ESMF_MAXSTR) :: cname, bname
+    type(ESMF_GridComp) :: comp1
+
+    ! individual test failure message
+    character(ESMF_MAXSTR) :: failMsg
+    character(ESMF_MAXSTR) :: name
+    integer :: result = 0
+
+!-------------------------------------------------------------------------------
+!   The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!   always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!   the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!   to OFF, then only the sanity unit tests.
+!   Special strings (Non-exhaustive and exhaustive) have been
+!   added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
+        
+    call ESMF_Initialize(rc=rc)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !NEX_UTest
+!   !  Test creation of a Component
+    cname = "Atmosphere"
+    comp1 = ESMF_GridCompCreate(name=cname, gridcompType=ESMF_ATM, &
+                                             configFile="grid.rc", rc=rc)  
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Creating a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !NEX_UTest
+!   !  Destroying a component
+
+    call ESMF_GridCompDestroy(comp1, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Destroying a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+#ifdef ESMF_EXHAUSTIVE
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test creation of a Component
+    cname = "Atmosphere"
+    comp1 = ESMF_GridCompCreate(name=cname, gridcompType=ESMF_ATM, &
+                                             configFile="grid.rc", rc=rc)  
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Creating a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test validate a component
+
+    call ESMF_GridCompValidate(comp1, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Validating a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test get a Component name
+
+    call ESMF_GridCompGet(comp1, name=bname, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Getting  a Component name Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Verify the name is correct
+
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Verifying the correct Component name was returned Test"
+    call ESMF_Test((bname.eq."Atmosphere"), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   Emailed Nancy about theses tests.
+!   !  Set Internal State
+
+!    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    !write(name, *) "Set Internal State Test"
+    !call ESMF_GridCompSetInternalState(comp1, dataPointer=pointer, rc=rc)
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+!   !  Get Internal State
+
+    !write(failMsg, *) "Did not return ESMF_SUCCESS"
+    !write(name, *) "Get Internal State Test"
+    !call ESMF_GridCompGetInternalState(comp1, dataPointer=pointer, rc=rc)
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test printing a component
+
+    call ESMF_GridCompPrint(comp1, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Printing a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Destroying a component
+
+    call ESMF_GridCompDestroy(comp1, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Destroying a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+#endif
+
+    call ESMF_Finalize(rc)
+
+    end program ESMF_GridCompCreateUTest
+    
