8ebddfa55fd354f476e7e2814427ebe6084c117e
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-07-07 17:26:42 +0000

    Original version.

---
+++ src/Superstructure/Component/tests/ESMF_CplCompCreateUTest.F90
@@ -0,0 +1,207 @@
+! $Id: ESMF_CplCompCreateUTest.F90,v 1.1 2004/07/07 17:26:42 svasquez Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_CplCompCreateUTest
+
+!------------------------------------------------------------------------------
+
+#include <ESMF_Macros.inc>
+
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_CplCompCreateUTest - Unit test for Components.
+!
+! !DESCRIPTION:
+! Tests, cursory and exahustive, for Coupler Component Create code.
+!
+!-------------------------------------------------------------------------
+!
+! !USES:
+    use ESMF_TestMod     ! test methods
+    use ESMF_Mod
+    implicit none
+    
+!   ! Local variables
+    integer :: rc
+    integer , pointer:: pointer
+    character(ESMF_MAXSTR) :: cname, bname, cplname
+    type(ESMF_CplComp) :: comp1
+    type(ESMF_CplComp) :: cpl
+    type(ESMF_VM):: vm
+
+    ! individual test failure message
+    character(ESMF_MAXSTR) :: failMsg
+    character(ESMF_MAXSTR) :: name
+    integer :: result = 0
+
+!-------------------------------------------------------------------------------
+!   The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!   always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!   the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!   to OFF, then only the sanity unit tests.
+!   Special strings (Non-exhaustive and exhaustive) have been
+!   added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
+        
+   ! Initialize framework and get back default global VM
+   call ESMF_Initialize(vm=vm, rc=rc)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !NEX_UTest
+!   !  Test creation of a Coupler Component
+    cplname = "One Way Coupler"
+    cpl = ESMF_CplCompCreate(vm, cplname, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Creating a Coupler Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !NEX_UTest
+!   !  Destroying a component
+
+    call ESMF_CplCompDestroy(cpl, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Destroying a Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+#ifdef ESMF_EXHAUSTIVE
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test creation of a Component
+    cplname = "One Way Coupler"
+    cpl = ESMF_CplCompCreate(vm, cplname, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Creating a Coupler Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test validate a Coupler Component
+
+    call ESMF_CplCompValidate(cpl, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Validating a Coupler Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test get a Coupler Component name
+
+    call ESMF_CplCompGet(cpl, name=bname, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Getting  a Coupler Component name Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Verify the name is correct
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Verifying the correct Component name was returned Test"
+    call ESMF_Test((bname.eq."One Way Coupler"), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test Set a Coupler Component name
+    cname = "Two Way Coupler"
+    call ESMF_CplCompSet(cpl, name=cname, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Setting  a Coupler Component name Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test get a Coupler Component name
+    call ESMF_CplCompGet(cpl, name=bname, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Getting  a Coupler Component name Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Verify the name is correct
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Verifying the correct Component name was returned Test"
+    call ESMF_Test((bname.eq."Two Way Coupler"), name, failMsg, result, ESMF_SRCLINE)
+
+
+!-------------------------------------------------------------------------
+!   Emailed Nancy about these tests.
+!   !  Set Internal State
+
+    !write(failMsg, *) "Did not return ESMF_SUCCESS"
+    !write(name, *) "Set Internal State Test"
+    !call ESMF_CplCompSetInternalState(cpl, dataPointer=pointer, rc=rc)
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+!   !  Get Internal State
+
+    !write(failMsg, *) "Did not return ESMF_SUCCESS"
+    !write(name, *) "Get Internal State Test"
+    !call ESMF_CplCompGetInternalState(comp1, dataPointer=pointer, rc=rc)
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Test printing a component
+
+    call ESMF_CplCompPrint(cpl, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Printing a Coupler Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+!-------------------------------------------------------------------------
+!   !
+    !This test is commented out because it crashes
+    ! Bug report 986728 opened
+!   !  Wait for a component
+
+    !call ESMF_CplCompWait(cpl, rc=rc)
+
+    !write(failMsg, *) "Did not return ESMF_SUCCESS"
+    !write(name, *) "Wait for a Coupler Component Test"
+    !call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+!-------------------------------------------------------------------------
+!   !
+    !EX_UTest
+!   !  Destroying a component
+
+    call ESMF_CplCompDestroy(cpl, rc=rc)
+
+    write(failMsg, *) "Did not return ESMF_SUCCESS"
+    write(name, *) "Destroying a Coupler Component Test"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+#endif
+
+    call ESMF_Finalize(rc)
+
+    end program ESMF_CplCompCreateUTest
+    
