e6b082ebf8bf9fe7e68f96c6ba5ef221dc44e8f5
Nancy Collins <nancy@ucar.edu>
2004-07-22 21:02:36 +0000

    more logic in grid destruct; remove unused local vars

--- src/Infrastructure/Grid/src/ESMF_LogRectGrid.F90
+++ src/Infrastructure/Grid/src/ESMF_LogRectGrid.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_LogRectGrid.F90,v 1.87 2004/06/23 16:25:50 jwolfe Exp $
+! $Id: ESMF_LogRectGrid.F90,v 1.87.2.1 2004/07/22 21:02:36 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -109,7 +109,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter, private :: version = &
-      '$Id: ESMF_LogRectGrid.F90,v 1.87 2004/06/23 16:25:50 jwolfe Exp $'
+      '$Id: ESMF_LogRectGrid.F90,v 1.87.2.1 2004/07/22 21:02:36 nscollins Exp $'
 
 !==============================================================================
 !
@@ -247,7 +247,7 @@
       type(ESMF_Grid) :: ESMF_GridCreateHorzLatLon
 !
 ! !ARGUMENTS:
-      real(ESMF_KIND_R8), dimension(:), intent(in) :: minGlobalCoordPerDim
+      real(ESMF_KIND_R8), dimension(:), intent(in), optional :: minGlobalCoordPerDim
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: delta1
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: delta2
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: coord1
@@ -497,7 +497,7 @@
       type(ESMF_Grid) :: ESMF_GridCreateHorzXY
 !
 ! !ARGUMENTS:
-      real(ESMF_KIND_R8), dimension(:), intent(in) :: minGlobalCoordPerDim
+      real(ESMF_KIND_R8), dimension(:), intent(in), optional :: minGlobalCoordPerDim
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: delta1
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: delta2
       real(ESMF_KIND_R8), dimension(:), intent(in), optional :: coord1
@@ -2567,6 +2567,14 @@
       ! Initialize return code; assume failure until success is certain
       if (present(rc)) rc = ESMF_FAILURE
 
+      ! If Grid is unitialized, return with warning
+      if (grid%gridStatus.eq.ESMF_GRID_STATUS_UNINIT) then
+        if (ESMF_LogWrite("destroying uninitialized grid", ESMF_LOG_WARNING, &
+                          ESMF_CONTEXT)) continue
+        if (present(rc)) rc = ESMF_SUCCESS
+        return
+      endif
+
       !TODO: destruct these
       !  type (ESMF_Base) :: base
       !  type (ESMF_Status) :: gridStatus
@@ -2582,36 +2590,38 @@
       grid%numPhysGrids    = 0
       grid%numDistGrids    = 0
 
-      do n = 1,grid%numPhysGridsAlloc
-        call ESMF_PhysGridDestroy(grid%physgrids(n), rc=localrc)
+      if (grid%gridStatus.eq.ESMF_GRID_STATUS_READY) then
+        do n = 1,grid%numPhysGridsAlloc
+          call ESMF_PhysGridDestroy(grid%physgrids(n), rc=localrc)
+          if (ESMF_LogMsgFoundError(localrc, &
+                                    ESMF_ERR_PASSTHRU, &
+                                    ESMF_CONTEXT, rc)) return
+        end do
+        grid%numPhysGridsAlloc = 0
+        deallocate(grid%physgrids, &
+                   grid%distGridIndex, stat=localrc)
+        if (ESMF_LogMsgFoundAllocError(localrc, "deallocating physgrids", &
+                                       ESMF_CONTEXT, rc)) return
+
+        do n = 1,grid%numDistGridsAlloc
+          call ESMF_DistGridDestroy(grid%distgrids(n), rc=localrc)
+          if (ESMF_LogMsgFoundError(localrc, &
+                                    ESMF_ERR_PASSTHRU, &
+                                    ESMF_CONTEXT, rc)) return
+        end do
+        grid%numDistGridsAlloc = 0
+        deallocate(grid%distgrids, stat=localrc)
+        if (ESMF_LogMsgFoundAllocError(localrc, "deallocating distgrids", &
+                                       ESMF_CONTEXT, rc)) return
+
+        grid%minGlobalCoordPerDim(:) = 0
+        grid%maxGlobalCoordPerDim(:) = 0
+
+        call ESMF_LocalArrayDestroy(grid%boundingBoxes, rc=localrc)
         if (ESMF_LogMsgFoundError(localrc, &
                                   ESMF_ERR_PASSTHRU, &
                                   ESMF_CONTEXT, rc)) return
-      end do
-      grid%numPhysGridsAlloc = 0
-      deallocate(grid%physgrids, &
-                 grid%distGridIndex, stat=localrc)
-      if (ESMF_LogMsgFoundAllocError(localrc, "deallocating physgrids", &
-                                     ESMF_CONTEXT, rc)) return
-
-      do n = 1,grid%numDistGridsAlloc
-        call ESMF_DistGridDestroy(grid%distgrids(n), rc=localrc)
-        if (ESMF_LogMsgFoundError(localrc, &
-                                  ESMF_ERR_PASSTHRU, &
-                                  ESMF_CONTEXT, rc)) return
-      end do
-      grid%numDistGridsAlloc = 0
-      deallocate(grid%distgrids, stat=localrc)
-      if (ESMF_LogMsgFoundAllocError(localrc, "deallocating distgrids", &
-                                     ESMF_CONTEXT, rc)) return
-
-      grid%minGlobalCoordPerDim(:) = 0
-      grid%maxGlobalCoordPerDim(:) = 0
-
-      call ESMF_LocalArrayDestroy(grid%boundingBoxes, rc=localrc)
-      if (ESMF_LogMsgFoundError(localrc, &
-                                ESMF_ERR_PASSTHRU, &
-                                ESMF_CONTEXT, rc)) return
+      endif
 
       if (present(rc)) rc = ESMF_SUCCESS
 
@@ -3073,7 +3083,8 @@
 !EOPI
 
       integer :: localrc                          ! Error status
-      integer :: i, i1, i2, gridBoundWidth, myDE(0:2)
+      integer :: i, i1, i2, gridBoundWidth 
+      !integer :: myDE(0:2)
       character(len=ESMF_MAXSTR) :: coordName, coordUnit
       logical :: coordAligned, coordEqualSpaced, coordCyclic
       integer :: count, compCount, localStart
@@ -3082,9 +3093,9 @@
       real(ESMF_KIND_R8) :: localMinCoord, localMaxCoord
       real(ESMF_KIND_R8), dimension(:), allocatable :: coordUse
       type(ESMF_CoordType) :: coordType
-      type(ESMF_DELayout) :: delayout
+      !type(ESMF_DELayout) :: delayout
       type(ESMF_PhysCoord) :: tempCoord
-      type(ESMF_Grid) :: gridp
+      !type(ESMF_Grid) :: gridp
       type(ESMF_PhysGrid) :: physGrid
       type(ESMF_CoordSystem) :: coordSystem
 
@@ -5763,7 +5774,7 @@
 ! !REQUIREMENTS:
 !EOPI
 
-      integer :: localrc                          ! Error status
+      !integer :: localrc                          ! Error status
       integer :: i                                ! loop index
 
       ! Initialize return code; assume failure until success is certain
@@ -6463,7 +6474,7 @@
 !
 !EOPI
 
-      character(len=ESMF_MAXSTR) :: name, str
+      character(len=ESMF_MAXSTR) :: name
       type(ESMF_GridClass), pointer :: gp
       integer :: localrc                          ! Error status
 
@@ -7026,7 +7037,7 @@
 ! !REQUIREMENTS:
 
       ! Local variables.
-      integer :: localrc                          ! Error status
+      !integer :: localrc                          ! Error status
       integer :: total, i
 
       ! Initialize return code; assume failure until success is certain
