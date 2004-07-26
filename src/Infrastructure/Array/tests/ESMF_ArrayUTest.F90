20b1232a09b03113775918fea86fb6147d17e41b
Nancy Collins <nancy@ucar.edu>
2004-07-26 22:01:45 +0000

    add test for query halo width

--- src/Infrastructure/Array/tests/ESMF_ArrayUTest.F90
+++ src/Infrastructure/Array/tests/ESMF_ArrayUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ArrayUTest.F90,v 1.4 2004/06/15 22:45:07 jwolfe Exp $
+! $Id: ESMF_ArrayUTest.F90,v 1.5 2004/07/26 22:01:45 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -35,13 +35,14 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_ArrayUTest.F90,v 1.4 2004/06/15 22:45:07 jwolfe Exp $'
+      '$Id: ESMF_ArrayUTest.F90,v 1.5 2004/07/26 22:01:45 nscollins Exp $'
 !------------------------------------------------------------------------------
 
 !   ! Local variables
     type(ESMF_Array) :: array1
     character(ESMF_MAXSTR) :: filename
     real, dimension(:,:), pointer :: f90ptr1
+    integer :: width
 
 
     ! individual test failure message
@@ -75,25 +76,12 @@
 #ifdef ESMF_EXHAUSTIVE
 !-------------------------------------------------------------------------------
 
-!   !  Create an Array Test
-    ! The following code core dumps it will be uncommented
-    ! when bug 932018 is fixed.
+!   !  Create an Array Test with data copy
     !EX_UTest
     allocate(f90ptr1(10,20))
     write(failMsg, *) "Did not return ESMF_SUCCESS" 
     write(name, *) "Create Array Test"
-    array1 =  ESMF_ArrayCreate(f90ptr1, ESMF_DATA_COPY, rc=rc)
-    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
-    print *, "array 1 create returned"
-
-!-------------------------------------------------------------------------------
-!   !  Create an Array Test
- 
-    !EX_UTest
-    allocate(f90ptr1(10,20))
-    write(failMsg, *) "Did not return ESMF_SUCCESS" 
-    write(name, *) "Create Array Test"
-    array1 =  ESMF_ArrayCreate(f90ptr1, ESMF_DATA_REF, rc=rc)
+    array1 = ESMF_ArrayCreate(f90ptr1, ESMF_DATA_COPY, rc=rc)
     call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
     print *, "array 1 create returned"
 
@@ -116,6 +104,40 @@
     call ESMF_ArrayGet(array1, name=array_name, rc=rc)
     call ESMF_Test((rc.eq.ESMF_SUCCESS.and.array_name.eq."SAM"), name, failMsg, result, ESMF_SRCLINE)
     print *, "array 1 is called ", array_name
+
+!-------------------------------------------------------------------------------
+!   !  Destroy an Array Test
+
+    !EX_UTest
+    write(failMsg, *) "Did not return ESMF_SUCCESS" 
+    write(name, *) "Destroy Array Test"
+    call ESMF_ArrayDestroy(array1, rc=rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+    print *, "array 1 destroy returned"
+
+!-------------------------------------------------------------------------------
+!
+!   !  Create an Array with a halo width
+ 
+    !EX_UTest
+    allocate(f90ptr1(10,20))
+    write(failMsg, *) "Did not return ESMF_SUCCESS" 
+    write(name, *) "Create Array with HaloWidth Test"
+    array1 = ESMF_ArrayCreate(f90ptr1, ESMF_DATA_COPY, haloWidth=2, rc=rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+    print *, "array 1 create returned"
+
+!-------------------------------------------------------------------------------
+!   !  Get Halo Width back
+ 
+    !EX_UTest
+    write(name, *) "Get Array HaloWidth Test"
+    call ESMF_ArrayGet(array1, haloWidth=width, rc=rc)
+    write(failMsg, *) "Did not return ESMF_SUCCESS" 
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+    write(failMsg, *) "Halo Width not 2" 
+    call ESMF_Test((width.eq.2), name, failMsg, result, ESMF_SRCLINE)
+    print *, "array 1 get halowidth returned"
 
 !-------------------------------------------------------------------------------
 !   !  Destroy an Array Test
