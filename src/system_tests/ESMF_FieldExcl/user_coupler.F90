25bb19d990c9f852265383e8037cf055ee8c13e1
Nancy Collins <nancy@ucar.edu>
2004-08-23 16:23:43 +0000

    new system test which starts testing exclusive components from the data perspective

---
+++ src/system_tests/ESMF_FieldExcl/user_coupler.F90
@@ -0,0 +1,172 @@
+! $Id: user_coupler.F90,v 1.1 2004/08/23 16:23:43 nscollins Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User-supplied Coupler
+!
+!
+!\begin{verbatim}
+
+    module user_coupler
+
+    ! ESMF Framework module
+    use ESMF_Mod
+    
+    implicit none
+    
+    public usercpl_register
+        
+    ! global data
+    type(ESMF_RouteHandle), save :: routehandle
+
+    contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+    subroutine usercpl_register(comp, rc)
+      type(ESMF_CplComp) :: comp
+      integer :: rc
+
+      print *, "in user setservices routine"
+
+      ! Register the callback routines.
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETINIT, user_init, &
+                                     ESMF_SINGLEPHASE, rc)
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETRUN, user_run, &
+                                     ESMF_SINGLEPHASE, rc)
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, &
+                                     ESMF_SINGLEPHASE, rc)
+
+      print *, "Registered Initialize, Run, and Finalize routines"
+
+      rc = ESMF_SUCCESS
+
+    end subroutine
+
+!-------------------------------------------------------------------------
+!   !User Comp Component created by higher level calls, here is the
+!   ! Initialization routine.
+ 
+    
+    subroutine user_init(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer :: rc
+
+      ! Local variables
+      integer :: itemcount
+      type(ESMF_Field) :: humidity1, humidity2
+      type(ESMF_VM) :: vm
+      type(ESMF_DELayout) :: cplDElayout
+
+      print *, "User Coupler Init starting"
+
+      call ESMF_StateGet(importState, itemcount=itemcount, rc=rc)
+      print *, "Import State contains ", itemcount, " items."
+       
+      ! Get input data
+      call ESMF_StateGetField(importState, "humidity", humidity1, rc=rc)
+      ! call ESMF_FieldPrint(humidity1, rc=rc)
+
+      ! Get location of output data
+      call ESMF_StateGetField(exportState, "humidity", humidity2, rc=rc)
+      ! call ESMF_FieldPrint(humidity2, rc=rc)
+
+      ! Get VM from coupler component and create a layout with all the PETs
+      call ESMF_CplCompGet(comp, vm=vm, rc=rc)
+      cplDElayout = ESMF_DELayoutCreate(vm, rc=rc)
+
+      ! These are fields on different Grids - call RegridStore to set
+      ! up the Regrid structure
+
+      call ESMF_FieldRegridStore(humidity1, humidity2, cplDElayout, &
+                                 routehandle, &
+                                 regridmethod=ESMF_REGRID_METHOD_BILINEAR, &
+                                 rc=rc)
+
+
+      print *, "User Coupler Init returning"
+   
+      rc = ESMF_SUCCESS
+
+    end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is coupled.
+!   !
+ 
+    subroutine user_run(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer :: rc
+
+      ! Local variables
+      type(ESMF_Field) :: humidity1, humidity2
+      integer :: status
+
+      print *, "User Coupler Run starting"
+
+      ! Get input data
+      call ESMF_StateGetField(importState, "humidity", humidity1, rc=rc)
+      ! call ESMF_FieldPrint(humidity1, rc=rc)
+
+      ! Get location of output data
+      call ESMF_StateGetField(exportState, "humidity", humidity2, rc=rc)
+      ! call ESMF_FieldPrint(humidity2, rc=rc)
+
+      ! These are fields on different Grids - call Regrid to rearrange
+      !  the data.   The communication pattern was computed at init,
+      !  this simply has to execute the send and receive equivalents.
+
+      call ESMF_FieldRegrid(humidity1, humidity2, routehandle, rc=status)
+
+      ! Data is moved directly to the field in the output state, so no
+      ! "put" is needed here.
+ 
+      print *, "User Coupler Run returning"
+
+      rc = status
+
+    end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+    subroutine user_final(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer :: rc
+
+      ! Local variables
+
+      print *, "User Coupler Final starting"
+   
+      ! Release resources stored for the Regridding.
+      call ESMF_FieldRegridRelease(routehandle, rc)
+
+      print *, "User Coupler Final returning"
+   
+      rc = ESMF_SUCCESS
+
+    end subroutine user_final
+
+
+    end module user_coupler
+    
+!\end{verbatim}
+    
