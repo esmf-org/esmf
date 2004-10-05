edcf873e6a0d62c9c266f829ac9d810bc19411a6
Silverio Vasquez <silverio.vasquez@noaa.gov>
2004-10-05 16:07:11 +0000

    Added newline before adding "# of procs" string.

--- src/Infrastructure/IOSpec/tests/ESMF_IOSpecUTest.F90
+++ src/Infrastructure/IOSpec/tests/ESMF_IOSpecUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_IOSpecUTest.F90,v 1.2 2004/08/26 20:56:21 svasquez Exp $
+! $Id: ESMF_IOSpecUTest.F90,v 1.3 2004/10/05 16:07:11 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_IOSpecUTest.F90,v 1.2 2004/08/26 20:56:21 svasquez Exp $'
+      '$Id: ESMF_IOSpecUTest.F90,v 1.3 2004/10/05 16:07:11 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -68,7 +68,7 @@
 
       call ESMF_Initialize(vm=vm, rc=rc)
       call ESMF_VMGet(vm, petCount=npets, rc=rc)
-      print *, "NUMBER_OF_PROCESSORS ", npets
+      print '(/, a, i3)' , "NUMBER_OF_PROCESSORS", npets
 
       !------------------------------------------------------------------------
       !NEX_UTest
