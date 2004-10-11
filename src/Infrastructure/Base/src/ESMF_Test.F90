fb814cd2edd351fb4f78d8f4009b0e0ab3be8c92
Nancy Collins <nancy@ucar.edu>
2004-10-11 20:13:37 +0000

    add a TestStart and TestEnd routine so we can localize changes to the messages

--- src/Infrastructure/Base/src/ESMF_Test.F90
+++ src/Infrastructure/Base/src/ESMF_Test.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_Test.F90,v 1.5 2004/10/08 23:04:39 eschwab Exp $
+! $Id: ESMF_Test.F90,v 1.6 2004/10/11 20:13:37 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -35,12 +35,14 @@
 
 ! !PUBLIC MEMBER FUNCTIONS:
       public ESMF_Test
+      public ESMF_TestStart
+      public ESMF_TestEnd
 !EOP
 
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter, private :: version = &
-      '$Id: ESMF_Test.F90,v 1.5 2004/10/08 23:04:39 eschwab Exp $'
+      '$Id: ESMF_Test.F90,v 1.6 2004/10/11 20:13:37 nscollins Exp $'
 
 !==============================================================================
 
@@ -91,6 +93,80 @@
       end if
 
       end subroutine ESMF_Test
+
+!------------------------------------------------------------------------------
+!BOP
+!
+! !IROUTINE:  ESMF_TestStart - Print information at the start of testing
+!
+! !INTERFACE:
+      subroutine ESMF_TestStart(numPETs, file, line, unit)
+
+! !ARGUMENTS:
+      integer, intent(in) :: numPETs        ! number of PETs in the test
+      character(*), intent(in) :: file      ! test file name
+      integer, intent(in) :: line           ! test file line number
+      integer, intent(in), optional :: unit ! additional output unit number
+
+! !DESCRIPTION:
+!     Prints a standard message; intended to be called at the start of any
+!     test code.  If {\tt unit}
+!     is specified, will in addition write the same message to that 
+!     Fortran unit number.
+!
+!EOP
+!-------------------------------------------------------------------------------
+
+      character(ESMF_MAXSTR) :: msg
+
+      write(msg, *) "Beginning Test, file ", trim(file), ", line", line
+      print *, msg
+      call ESMF_LogWrite(msg, ESMF_LOG_INFO)
+      if (present(unit)) write(unit, *) msg
+
+      write(msg, *) "NUMBER OF PROCESSORS", numPETs
+      print *, msg
+      call ESMF_LogWrite(msg, ESMF_LOG_INFO)
+      if (present(unit)) write(unit, *) msg
+
+      end subroutine ESMF_TestStart
+
+!------------------------------------------------------------------------------
+!BOP
+!
+! !IROUTINE:  ESMF_TestEnd - Print information at the end of testing
+!
+! !INTERFACE:
+      subroutine ESMF_TestEnd(result, file, line, unit)
+
+! !ARGUMENTS:
+      integer, intent(in) :: result         ! number of successful tests
+      character(*), intent(in) :: file      ! test file name
+      integer, intent(in) :: line           ! test file line number
+      integer, intent(in), optional :: unit ! additional output unit number
+
+! !DESCRIPTION:
+!     Prints a standard message; intended to be called at the end of any
+!     test code.  If {\tt unit}
+!     is specified, will in addition write the same message to that 
+!     Fortran unit number.
+!
+!EOP
+!-------------------------------------------------------------------------------
+
+      character(ESMF_MAXSTR) :: msg
+
+      write(msg, *) "Number of failed tests:", result
+      print *, msg
+      call ESMF_LogWrite(msg, ESMF_LOG_INFO)
+      if (present(unit)) write(unit, *) msg
+
+      write(msg, *) "Ending Test, file ", trim(file), ", line", line
+      print *, msg
+      call ESMF_LogWrite(msg, ESMF_LOG_INFO)
+      if (present(unit)) write(unit, *) msg
+
+      end subroutine ESMF_TestEnd
 
 !------------------------------------------------------------------------------
 
