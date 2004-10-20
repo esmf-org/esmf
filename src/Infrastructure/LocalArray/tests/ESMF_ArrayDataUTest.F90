79690aa42df2cee0b994742af351afff5092b8f5
Nancy Collins <nancy@ucar.edu>
2004-10-20 17:23:20 +0000

    add new test to verify array data is being maintained properly

---
+++ src/Infrastructure/LocalArray/tests/ESMF_ArrayDataUTest.F90
@@ -0,0 +1,129 @@
+! $Id: ESMF_ArrayDataUTest.F90,v 1.1 2004/10/20 17:23:20 nscollins Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+      program ESMF_ArrayDataUTest
+
+!------------------------------------------------------------------------------
+
+#include <ESMF_Macros.inc>
+
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_ArrayDataTest - One line general statement about this test
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 ArrayData unit tests.
+! The companion file ESMF\_Array.F90 contains the definitions for the
+! Array methods.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod     ! test methods
+      use ESMF_Mod
+
+      implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_ArrayDataUTest.F90,v 1.1 2004/10/20 17:23:20 nscollins Exp $'
+!------------------------------------------------------------------------------
+    
+    ! cumulative result: count failures; no failures equals "all pass"
+    integer :: result = 0
+
+    ! individual test result code
+    integer :: rc = 1
+
+    ! individual test failure message
+    character(ESMF_MAXSTR) :: failMsg
+    character(ESMF_MAXSTR) :: name
+
+    integer :: i, npets
+    type(ESMF_VM):: vm
+    type(ESMF_LocalArray) :: la
+
+
+    ! Array of data type Integer * 4 
+    type PtrIWrap1  
+    sequence
+      integer (ESMF_KIND_I4),dimension(:), pointer :: data
+    end type 
+    
+#ifdef ESMF_EXHAUSTIVE
+    ! Array of data type Real * 4 
+    type PtrRWrap2  
+    sequence
+      real (ESMF_KIND_R4),dimension(:,:), pointer :: data
+    end type 
+
+    ! Array of data type Real * 8 
+    type PtrR8Wrap3  
+    sequence
+      real (ESMF_KIND_R8),dimension(:,:,:), pointer :: data
+    end type 
+    
+    ! used below
+    type mytype
+    sequence
+       integer :: myint
+       real :: myreal
+    end type
+
+    ! Arrays of a derived data type
+    type PtrSWrap1  
+    sequence
+      type(mytype),dimension(:), pointer :: data
+    end type 
+#endif
+    
+
+    
+    type(PtrIWrap1) :: sizetest1I
+    
+#ifdef ESMF_EXHAUSTIVE
+    type(PtrRWrap2) :: sizetest2R
+    type(PtrR8Wrap3) :: sizetest3R8
+    type(PtrSWrap1) :: sizetest1S
+#endif
+    
+
+    call ESMF_Initialize(vm=vm, rc=rc)
+    call ESMF_VMGet(vm, petCount=npets, rc=rc)
+    call ESMF_TestStart(npets, ESMF_SRCLINE)
+    
+
+    !NEX_UTest
+    allocate(sizetest1I%data(-12:-6), stat=rc)
+    do i = -12, -6
+      sizetest1I%data(i) = i*1000
+    enddo
+
+    la = ESMF_LocalArrayCreate(sizetest1I%data, ESMF_DATA_REF, rc=rc)
+    write(failMsg, *) "cannot create LocalArray"
+    write(name, *) "Creating a LocalArray from an allocated F90 Pointer"
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+!------------------------------------------------------------------------------
+
+    call ESMF_LocalArrayPrint(la, rc=rc)
+
+#ifdef ESMF_EXHAUSTIVE
+    
+#endif
+
+    call ESMF_TestEnd(result, ESMF_SRCLINE)
+    call ESMF_Finalize(rc)
+
+    end program ESMF_ArrayDataUTest
+    
+    
