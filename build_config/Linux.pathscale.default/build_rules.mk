aa4fd2074f2dae8ba4ed60121cd5e81075cdd3d1
Nancy Collins <nancy@ucar.edu>
2005-02-03 17:51:44 +0000

    first stab at rules - not tested at all yet

---
+++ build_config/Linux.pathscale.default/build_rules.mk
@@ -0,0 +1,183 @@
+#  $Id: build_rules.mk,v 1.1 2005/02/03 17:51:44 nscollins Exp $
+#
+#  Linux.pathscale.default makefile
+#
+
+
+#
+#  ESMF_PREC defaults to 64 bits
+#
+ifndef ESMF_PREC
+export ESMF_PREC := 64
+endif
+ifeq ($(ESMF_PREC),default)
+export ESMF_PREC := 64
+endif
+
+#
+# Default MPI setting.
+#
+ifndef ESMF_COMM
+export ESMF_COMM := mpiuni
+endif
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+# if using PBS system, export this for run time
+ifdef PBS_NODEFILE
+export ESMF_NODES := -machinefile $(PBS_NODEFILE)
+endif
+
+
+############################################################
+#
+#  The following naming convention is used:
+#     XXX_LIB - location of library XXX
+#     XXX_INCLUDE - directory for include files needed for library XXX
+#
+# Location of BLAS and LAPACK.  See ${ESMF_DIR}/docs/instllation.html
+# for information on retrieving them.
+#
+#
+ifeq ($(ESMF_NO_IOCODE),true)
+BLAS_LIB         =
+LAPACK_LIB       =
+NETCDF_LIB       = -lnetcdf_stubs
+NETCDF_INCLUDE   = -I${ESMF_DIR}/src/Infrastructure/stubs/netcdf_stubs
+HDF_LIB          =
+HDF_INCLUDE      =
+else
+BLAS_LIB         = -L/usr/local/lib -latlas
+LAPACK_LIB       = -L/usr/local/lib -llapack
+NETCDF_LIB       = -L/usr/local/lib -lnetcdf
+NETCDF_INCLUDE   = -I/usr/local/include
+HDF_LIB          = -L/usr/local/lib/ -lmfhdf -ldf -ljpeg -lz
+HDF_INCLUDE      = -I/usr/local/include
+endif
+
+# Location of MPI (Message Passing Interface) software
+
+# comment in one or the other, depending on whether you have
+# installed the mpich or lam library. 
+
+ifeq ($(ESMF_COMM),lam)
+# with lam-mpi installed:
+MPI_HOME       = 
+MPI_LIB        = -lmpi -llam 
+MPI_INCLUDE    = 
+MPIRUN         =  mpirun
+endif
+
+ifeq ($(ESMF_COMM),mpich)
+# with mpich installed:
+MPI_LIB        = -lmpich
+MPI_INCLUDE    = -DESMF_MPICH
+MPIRUN         = mpirun $(ESMF_NODES)
+endif
+
+ifeq ($(ESMF_COMM),mpiuni)
+# without mpich installed:
+MPI_HOME       = ${ESMF_DIR}/src/Infrastructure/stubs/mpiuni
+MPI_LIB        = -lmpiuni
+MPI_INCLUDE    = -I${MPI_HOME}
+MPIRUN         =  ${MPI_HOME}/mpirun
+endif
+
+# MP_LIB is for openMP
+#MP_LIB          = 
+#MP_INCLUDE      = 
+# For pthreads (or omp)
+THREAD_LIB      = 
+
+
+############################################################
+#
+
+AR		   = ar
+AR_FLAGS	   = cr
+RM		   = rm -f
+OMAKE		   = ${MAKE}
+RANLIB		   = ranlib
+SHELL		   = /bin/sh
+SED		   = /bin/sed
+SH_LD		   = cc
+#
+# C and Fortran compiler 
+#
+ifneq ($(ESMF_COMM),mpich)
+C_CC                = cc
+C_FC                = f95
+CXX_CC              = g++ -fPIC
+CXX_FC              = f95
+FFLAGS              = -YEXT_NAMES=LCS -s  -YEXT_SFX=_
+endif
+
+ifeq ($(ESMF_COMM),mpich)
+C_CC               = mpicc
+C_FC               = mpif90 
+CXX_CC             = mpiCC -fPIC
+CXX_FC             = mpif90
+FFLAGS             = -YEXT_NAMES=LCS -s 
+endif
+
+C_FC_MOD           = -p
+C_CLINKER_SLFLAG   = -Wl,-rpath,
+C_FLINKER_SLFLAG   = -Wl,-rpath,
+C_CLINKER	   = ${C_CC}
+C_FLINKER	   = ${C_FC}
+C_CCV		   = ${C_CC} --version
+C_FCV              = f90fe -V    # docs say f95 -V should work but causes error
+C_SYS_LIB	   = ${MPI_LIB} -ldl -lc -lg2c -lm
+# ---------------------------- BOPT - g options ----------------------------
+G_COPTFLAGS	   = -g 
+G_FOPTFLAGS	   = -g 
+# ----------------------------- BOPT - O options -----------------------------
+O_COPTFLAGS	   = -O 
+O_FOPTFLAGS	   = -O
+# ########################## Fortran compiler ##############################
+#
+F_FREECPP       = -ffree
+F_FIXCPP        = -ffixed
+F_FREENOCPP     = -ffree
+F_FIXNOCPP      = -ffixed
+# ########################## C++ compiler ##################################
+#
+CXX_CLINKER_SLFLAG = -Wl,-rpath,
+CXX_FLINKER_SLFLAG = -Wl,-rpath,
+CXX_CLINKER	   = ${CXX_CC}
+CXX_FLINKER	   = ${CXX_CC}
+CXX_CCV		   = ${CXX_CC} --version
+CXX_SYS_LIB	   = ${MPI_LIB} -ldl -lc -lg2c -lm
+C_F90CXXLD         = ${CXX_FC}
+CXXLIBBASE         = /usr/lib/gcc-lib/i386-redhat-linux/2.96
+C_F90CXXLIBS       = ${MPI_LIB} -L${CXXLIBBASE} -lf90math -lfio -lf77math -lrt -lstdc++
+C_CXXF90LD         = ${CXX_CC} 
+F90LIBBASE         = /soft/com/packages/absoft-8.0/opt/absoft/lib
+C_CXXF90LIBS       = ${MPI_LIB} -lstdc++ -L${F90LIBBASE} -lf90math -lrt -lfio -lf77math
+# ------------------------- BOPT - g_c++ options ------------------------------
+GCXX_COPTFLAGS	   = -g 
+GCXX_FOPTFLAGS	   = -g
+# ------------------------- BOPT - O_c++ options ------------------------------
+OCXX_COPTFLAGS	   = -O 
+OCXX_FOPTFLAGS	   = -O
+# -------------------------- BOPT - g_complex options ------------------------
+GCOMP_COPTFLAGS	   = -g
+GCOMP_FOPTFLAGS	   = -g
+# --------------------------- BOPT - O_complex options -------------------------
+OCOMP_COPTFLAGS	   = -O
+OCOMP_FOPTFLAGS	   = -O
+##################################################################################
+
+PARCH		   = linux
+
+SL_LIBS_TO_MAKE = 
+
+SL_SUFFIX   = 
+SL_LIBOPTS  = 
+SL_LINKOPTS = 
+SL_F_LINKER = $(F90CXXLD)
+SL_C_LINKER = $(CXXF90LD)
+SL_LIB_LINKER = $(CXXF90LD)
+
+
