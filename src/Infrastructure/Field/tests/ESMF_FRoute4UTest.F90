3e1d737e062b3698f7c68034182a50f077dc5149
Silverio Vasquez <silverio.vasquez@noaa.gov>
2005-02-15 17:49:59 +0000

    Fixed unit tests tagging bug.

--- src/Infrastructure/Field/tests/ESMF_FRoute4UTest.F90
+++ src/Infrastructure/Field/tests/ESMF_FRoute4UTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FRoute4UTest.F90,v 1.9 2004/12/08 22:56:19 nscollins Exp $
+! $Id: ESMF_FRoute4UTest.F90,v 1.10 2005/02/15 17:49:59 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -35,7 +35,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_FRoute4UTest.F90,v 1.9 2004/12/08 22:56:19 nscollins Exp $'
+      '$Id: ESMF_FRoute4UTest.F90,v 1.10 2005/02/15 17:49:59 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -188,7 +188,7 @@
       call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
       !------------------------------------------------------------------------
-      !NEX_UTest_Multi_Proc_Only
+      !EX_UTest_Multi_Proc_Only
       write(failMsg, *) "Did not return ESMF_SUCESS"
       write(name, *) "Grid distribute Test "
       call ESMF_GridDistribute(grid1, delayout=layout1, rc=rc)
