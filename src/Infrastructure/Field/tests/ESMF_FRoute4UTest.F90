f6105c56f54d3d191544c524aabe293fbca34a01
Nancy Collins <nancy@ucar.edu>
2005-03-02 23:35:12 +0000

    use old interface for RedistStore() for now; next release will change over to using correct arg (a vm, not a delayout)

--- src/Infrastructure/Field/tests/ESMF_FRoute4UTest.F90
+++ src/Infrastructure/Field/tests/ESMF_FRoute4UTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FRoute4UTest.F90,v 1.9.2.1 2005/03/01 22:57:08 svasquez Exp $
+! $Id: ESMF_FRoute4UTest.F90,v 1.9.2.2 2005/03/02 23:35:12 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -35,7 +35,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_FRoute4UTest.F90,v 1.9.2.1 2005/03/01 22:57:08 svasquez Exp $'
+      '$Id: ESMF_FRoute4UTest.F90,v 1.9.2.2 2005/03/02 23:35:12 nscollins Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -291,7 +291,7 @@
       ! route test
       write(failMsg, *) "Did not return ESMF_SUCCESS"
       write(name, *) "Field Redist Store Test"
-      call ESMF_FieldRedistStore(f1, f2, vm, routehandle=rh, rc=rc)
+      call ESMF_FieldRedistStore(f1, f2, layout1, routehandle=rh, rc=rc)
       call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
       !------------------------------------------------------------------------
