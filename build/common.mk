e8197c5e50294c257494fe0b24220409d7cee4af
Gerhard Theurich <theurich@sourcespring.net>
2005-03-08 16:34:20 +0000

    Moved the evaluation of ESMF_COMM _after_ the platform-specific build_rules.mk file has been called. At that point ESMF_COMM is valid even for those cases where it is set to a platform-specific default value.

--- build/common.mk
+++ build/common.mk
@@ -1,4 +1,4 @@
-#  $Id: common.mk,v 1.100 2005/03/08 04:39:51 theurich Exp $
+#  $Id: common.mk,v 1.101 2005/03/08 16:34:20 theurich Exp $
 #===============================================================================
 #
 #  GNUmake makefile - cannot be used with standard unix make!!
@@ -115,14 +115,6 @@ endif
 #  ESMF_COMM set in site files.
 #
 
-# According to the above comment this is not the right place, but it's 
-# a quick fix for now... For how things are right now this will _not_ work
-# for those cases where the platform specific build_rules set ESMF_COMM to
-# mpiuni!
-ifeq ($(ESMF_COMM),mpiuni)
-CPPFLAGS       += -DESMF_MPIUNI
-endif
-
 # default compiler flag is optimized.
 ifndef ESMF_BOPT
 export ESMF_BOPT = O
@@ -139,7 +131,12 @@ include $(ESMF_TOP_DIR)/build_config/$(ESMF_ARCH).$(ESMF_COMPILER).$(ESMF_SITE)/
 
 #-------------------------------------------------------------------------------
 
-
+#
+# Now the ESMF_COMM variable is valid and can be used
+ifeq ($(ESMF_COMM),mpiuni)
+CPPFLAGS       += -DESMF_MPIUNI
+endif
+#
 
 #-------------------------------------------------------------------------------
 #  Common variables
