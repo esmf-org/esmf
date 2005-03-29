7d3af0b744b012be4bc6e11358fc3caba8ec74d1
Silverio Vasquez <silverio.vasquez@noaa.gov>
2005-03-29 17:24:12 +0000

    Fix typos.

--- src/Infrastructure/LogErr/tests/ESMF_LogErrUTest.F90
+++ src/Infrastructure/LogErr/tests/ESMF_LogErrUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_LogErrUTest.F90,v 1.16 2005/03/28 23:49:11 svasquez Exp $
+! $Id: ESMF_LogErrUTest.F90,v 1.17 2005/03/29 17:24:12 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -37,7 +37,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_LogErrUTest.F90,v 1.16 2005/03/28 23:49:11 svasquez Exp $'
+      '$Id: ESMF_LogErrUTest.F90,v 1.17 2005/03/29 17:24:12 svasquez Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -274,9 +274,9 @@
 
       !------------------------------------------------------------------------
       !EX_UTest
-      ! Test LogFlush of new non-opened log
+      ! Test LogFlush of new unopened log
       write(failMsg, *) "Did not return ESMF_SUCCESS"
-      write(name, *) " LogFlush of non-opened log Test"
+      write(name, *) " LogFlush of unopened log Test"
       call ESMF_LogFlush(log2, rc=rc)
       call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
@@ -412,9 +412,9 @@
 
       !------------------------------------------------------------------------
       !EX_UTest
-      ! Test LogFlush of new non-opened log
+      ! Test LogFlush of new unopened log
       write(failMsg, *) "Did not return ESMF_SUCCESS"
-      write(name, *) " LogFlush of non-opened log Test"
+      write(name, *) " LogFlush of unopened log Test"
       call ESMF_LogFlush(log2, rc=rc)
       call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
