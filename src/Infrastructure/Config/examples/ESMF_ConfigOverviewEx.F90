55139edecfb54834c8a7a50a6e5fafca6c417fd0
Nancy Collins <nancy@ucar.edu>
2005-03-31 23:56:05 +0000

    More doc clean-up.

--- src/Infrastructure/Config/examples/ESMF_ConfigOverviewEx.F90
+++ src/Infrastructure/Config/examples/ESMF_ConfigOverviewEx.F90
@@ -1,4 +1,3 @@
-! Id: ESMF_FieldWriteEx.F90,v 1.5 2004/06/16 14:03:13 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -15,7 +14,6 @@
 !-------------------------------------------------------------------------
 !EXAMPLE        String used by test script to count examples.
 !==============================================================================
-!BOC
 !
 ! !DESCRIPTION:
 ! Example/test code which performs simple Configuration File routines.
@@ -27,11 +25,15 @@
       ! Local variables
       integer             :: status, petcount, i, j
       type(ESMF_VM)       :: vm
-!EOC
 
-      ! {\em Common Arguments:}
 !BOE
-      character(ESMF_MAXSTR) :: fname              ! file name
+!\subsubsection{Common Code Arguments}
+
+! Common Arguments used in the following code fragments:
+!EOE
+
+!BOC
+      character(ESMF_MAXSTR) :: fname     ! file name
       character*20  :: fn1, fn2, fn3
       integer       :: rc                 ! error return code (0 is OK)
       integer       :: n 
@@ -39,8 +41,7 @@
       real          :: table(7,3)
 
       type(ESMF_Config)   :: cf
-      type(ESMF_DELayout) :: layout
-!EOE
+!EOC
 
       integer :: finalrc
       finalrc = ESMF_SUCCESS
@@ -63,57 +64,57 @@
 !   ! Example 1:
 !   !
 
-!BOC
+!BOE
 !\subsubsection{Creation of a Config}
 
 ! The first step is to create the {\tt ESMF\_Config} and load the
 ! ASCII resource (rc) file into memory\footnote{See next section
 ! for a complete description of parameters for each routine/function}:
-!EOC
-
-!BOE
-      cf = ESMF_ConfigCreate(rc)
 !EOE
+
+!BOC
+      cf = ESMF_ConfigCreate(rc)
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
         print*, "' call ESMF_ConfigCreate' failed"
       endif
 
-!BOE
+!BOC
       fname = "myResourceFile.rc"
       call ESMF_ConfigLoadFile(cf, fname, rc=rc)
-!EOE
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
         print*, "' call ESMF_ConfigLoadFile' failed"
       endif
 
-!BOC
+!BOE
 !\subsubsection{Retrieval of constants}
 
 ! The next step is to select the label (record) of interest, say
-!EOC
-
-!BOE
-      call ESMF_ConfigFindLabel(cf, 'constants:', rc=rc)
 !EOE
+
+!BOC
+      call ESMF_ConfigFindLabel(cf, 'constants:', rc=rc)
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
         print*, "' call ESMF_ConfigFindLabel' failed"
       endif
 
-!BOC
+!BOE
 ! Two constants, r and n, can be retrieved with the following code
 ! fragment:
-!EOC
+!EOE
 
-!BOE
+!BOC
       call ESMF_ConfigGetAttribute(cf, r, rc=rc)      ! results in r = 3.1415
       call ESMF_ConfigGetAttribute(cf, n, rc=rc)      ! results in n = 25
-!EOE
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
@@ -125,26 +126,26 @@
         print*, "bad results from ConfigGetAttribute"
       endif
 
-!BOC
+!BOE
 !\subsubsection{Retrieval of file names}
 
 ! File names can be retrieved with the following code fragment:
-!EOC
-
-!BOE
-       call ESMF_ConfigFindLabel(cf, 'my_file_names:', rc=rc)
 !EOE
+
+!BOC
+       call ESMF_ConfigFindLabel(cf, 'my_file_names:', rc=rc)
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
         print*, "' call ESMF_ConfigFindLabel' failed"
       endif
 
-!BOE
+!BOC
        call ESMF_ConfigGetAttribute(cf, fn1, rc=rc)   ! results in fn1 = 'jan87.dat'
        call ESMF_ConfigGetAttribute(cf, fn2, rc=rc)   ! results in fn2 = 'jan88.dat'
        call ESMF_ConfigGetAttribute(cf, fn3, rc=rc)   ! results in fn3 = 'jan89.dat'
-!EOE
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
@@ -157,47 +158,47 @@
         print*, "bad results from ConfigGetAttribute"
       endif
 
-!BOC
+!BOE
 !\subsubsection{Retrieval of tables}
 
 ! To access tabular data, the user first must use
 ! {\tt ESMF\_ConfigFindLabel()} to locate the beginning of the table, e.g.,
-!EOC
-
-!BOE
-      call ESMF_ConfigFindLabel(cf, 'my_table_name::', rc=rc)
 !EOE
+
+!BOC
+      call ESMF_ConfigFindLabel(cf, 'my_table_name::', rc=rc)
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
         print*, "' call ESMF_ConfigFindLabel' failed"
       endif
 
-!BOC
+!BOE
 ! Subsequently, {\tt call ESMF\_ConfigNextLine()} can be used to gain
 ! access to each row of the table. Here is a code fragment to read the above
 ! table (7 rows, 3 columns):
-!EOC
+!EOE
 
-!BOE
+!BOC
       do i = 1, 7
         call ESMF_ConfigNextLine(cf, rc=rc)
         do j = 1, 3
           call ESMF_ConfigGetAttribute(cf, table(i,j), rc=rc)
         enddo
       enddo
-!EOE
+!EOC
 
-!BOC
+!BOE
 !\subsubsection{Destruction of a Config}
 
 ! The work with the configuration file {\tt cf} is finalized by call to
 ! {\tt ESMF\_ConfigDestroy()}:
-!EOC
-
-!BOE
-      call ESMF_ConfigDestroy(cf, rc)
 !EOE
+
+!BOC
+      call ESMF_ConfigDestroy(cf, rc)
+!EOC
 
       if (rc.NE.ESMF_SUCCESS) then
         finalrc = ESMF_FAILURE
