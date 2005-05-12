287a25f08690c928c59c0999a192730d96946a8b
Nancy Collins <nancy@ucar.edu>
2005-05-12 16:07:29 +0000

    update version number

--- src/Infrastructure/Base/include/ESMC_Macros.h
+++ src/Infrastructure/Base/include/ESMC_Macros.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Macros.h,v 1.19.2.2 2005/03/02 17:52:25 nscollins Exp $
+// $Id: ESMC_Macros.h,v 1.19.2.3 2005/05/12 16:08:56 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -46,9 +46,9 @@
 #define ESMF_MAJOR_VERSION 2
 #define ESMF_MINOR_VERSION 1
 #define ESMF_REVISION      0
-#define ESMF_PATCHLEVEL    2
+#define ESMF_PATCHLEVEL    3
 
-#define ESMF_VERSION_STRING "2.1.0rp2"
+#define ESMF_VERSION_STRING "2.1.0rp3"
 
 #endif   // ESMC_MACROS_H
 
--- src/Infrastructure/Base/src/ESMF_BaseTypes.F90
+++ src/Infrastructure/Base/src/ESMF_BaseTypes.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_BaseTypes.F90,v 1.12.2.2 2005/03/02 17:51:56 nscollins Exp $
+! $Id: ESMF_BaseTypes.F90,v 1.12.2.3 2005/05/12 16:08:16 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -71,8 +71,8 @@
       integer, parameter :: ESMF_MAJOR_VERSION = 2
       integer, parameter :: ESMF_MINOR_VERSION = 1
       integer, parameter :: ESMF_REVISION      = 0
-      integer, parameter :: ESMF_PATCHLEVEL    = 2
-      character(8), parameter :: ESMF_VERSION_STRING = "2.1.0rp2"
+      integer, parameter :: ESMF_PATCHLEVEL    = 3
+      character(8), parameter :: ESMF_VERSION_STRING = "2.1.0rp3"
 
 !------------------------------------------------------------------------------
 !
