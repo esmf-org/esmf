c3e73663a3ebc35ff8155b03518b05ce07679312
Nancy Collins <nancy@ucar.edu>
2005-12-01 20:07:58 +0000

    make the tests work for different pointer sizes and make it a more valid test

--- src/prologue/tests/ESMC_WordsizeSubr.C
+++ src/prologue/tests/ESMC_WordsizeSubr.C
@@ -1,4 +1,4 @@
-// $Id: ESMC_WordsizeSubr.C,v 1.1 2005/08/30 21:31:40 nscollins Exp $
+// $Id: ESMC_WordsizeSubr.C,v 1.2 2005/12/01 20:07:58 nscollins Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2005, University Corporation for Atmospheric Research,
@@ -25,21 +25,36 @@
 
 extern "C" {
 
-// sizeof(short), sizeof(int), sizeof(long), sizeof(float), sizeof(double)
+// verify that we are using the same word sizes on each side of the language
+// boundary - F90 and C++.
 
-void FTN(c_ints)(int *i1, int *i2, int *i3, int *i4, int *i5, int *rc) {
+void FTN(c_ints)(int *i1, int *i2, int *i3, int *i4,
+                 int *i5, int *i6, int *i7, int *rc) {
 
-      *i1 = sizeof(short);
-      *i2 = sizeof(int);
-      *i3 = sizeof(long long);
-      *i4 = sizeof(float);
-      *i5 = sizeof(double);
+      printf("In C++: sizeof(char)     = %d\n", sizeof(char));
+      printf("In C++: sizeof(short)     = %d\n", sizeof(short));
+      printf("In C++: sizeof(int)       = %d\n", sizeof(int));
+      printf("In C++: sizeof(long)      = %d\n", sizeof(long));
+      printf("In C++: sizeof(long long) = %d\n", sizeof(long long));
+      printf("In C++: sizeof(float)     = %d\n", sizeof(float));
+      printf("In C++: sizeof(double)    = %d\n", sizeof(double));
+      printf("In C++: sizeof(pointer)   = %d\n", sizeof(*i1));
 
-      printf("In C++: sizeof(short)  = %d\n", *i1);
-      printf("In C++: sizeof(int)    = %d\n", *i2);
-      printf("In C++: sizeof(long long)   = %d\n", *i3);
-      printf("In C++: sizeof(float)  = %d\n", *i4);
-      printf("In C++: sizeof(double) = %d\n", *i5);
+      *i1 = sizeof(ESMF_KIND_I1);
+      *i2 = sizeof(ESMF_KIND_I2);
+      *i3 = sizeof(ESMF_KIND_I4);
+      *i4 = sizeof(ESMF_KIND_I8);
+      *i5 = sizeof(ESMF_KIND_R4);
+      *i6 = sizeof(ESMF_KIND_R8);
+      *i7 = sizeof(ESMF_KIND_POINTER);
+
+      printf("In C++: sizeof(ESMF_KIND_I1) = %d\n", *i1);
+      printf("In C++: sizeof(ESMF_KIND_I2) = %d\n", *i2);
+      printf("In C++: sizeof(ESMF_KIND_I4) = %d\n", *i3);
+      printf("In C++: sizeof(ESMF_KIND_I8) = %d\n", *i4);
+      printf("In C++: sizeof(ESMF_KIND_R4) = %d\n", *i5);
+      printf("In C++: sizeof(ESMF_KIND_R8) = %d\n", *i6);
+      printf("In C++: sizeof(ESMF_KIND_POINTER) = %d\n", *i7);
 
       *rc = ESMF_SUCCESS;
 
--- src/prologue/tests/ESMF_WordsizeUTest.F90
+++ src/prologue/tests/ESMF_WordsizeUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_WordsizeUTest.F90,v 1.3 2005/10/12 19:06:22 nscollins Exp $
+! $Id: ESMF_WordsizeUTest.F90,v 1.4 2005/12/01 20:07:58 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2005, University Corporation for Atmospheric Research,
@@ -21,8 +21,9 @@
 
     integer :: rc, result
     character(len=ESMF_MAXSTR) :: failMsg, name
-    integer(ESMF_KIND_I8) :: diff, i1, i2
-    integer :: i2size, i4size, i8size, r4size, r8size
+    integer :: diff
+    integer :: i1sizeF, i2sizeF, i4sizeF, i8sizeF, r4sizeF, r8sizeF, ptrsizeF
+    integer :: i1sizeC, i2sizeC, i4sizeC, i8sizeC, r4sizeC, r8sizeC, ptrsizeC
 
     type testi 
     sequence
@@ -37,6 +38,16 @@
     type testp 
     sequence
         integer, pointer :: fredp
+    end type
+
+    type testi1 
+    sequence
+        integer(ESMF_KIND_I1) :: fredi1
+    end type
+
+    type testi2 
+    sequence
+        integer(ESMF_KIND_I2) :: fredi2
     end type
 
     type testi4 
@@ -62,6 +73,8 @@
     type(testi)  :: vi(2)
     type(testr)  :: vr(2)
     type(testp)  :: vip(2)
+    type(testi1) :: vi1(2)
+    type(testi2) :: vi2(2)
     type(testi4) :: vi4(2)
     type(testi8) :: vi8(2)
     type(testr4) :: vr4(2)
@@ -79,119 +92,84 @@
     !------------------------------------------------------------------------
     !------------------------------------------------------------------------
     ! not a test - informational messages only.
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi(2), i2)
-    diff = i2 - i1
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vi(1), vi(2), diff)
     print *, "F90: Default Integer size = ", diff
 
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr(2), i2)
-    diff = i2 - i1
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vr(1), vr(2), diff)
     print *, "F90: Default Real size = ", diff
 
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vip(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vip(2), i2)
-    diff = i2 - i1
-    print *, "F90: Pointer size = ", diff
-
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi4(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi4(2), i2)
-    diff = i2 - i1
-    print *, "F90: Explicit Integer I4 size = ", diff
-
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi8(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi8(2), i2)
-    diff = i2 - i1
-    print *, "F90: Explicit Integer I8 size = ", diff
-
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr4(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr4(2), i2)
-    diff = i2 - i1
-    print *, "F90: Explicit Integer R4 size = ", diff
-
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr8(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr8(2), i2)
-    diff = i2 - i1
-    print *, "F90: Explicit Integer R8 size = ", diff
 
     !------------------------------------------------------------------------
     !------------------------------------------------------------------------
-    !NEX_UTest
-    write(failMsg,*) "Bad size for I4 variable"
-    write(name, *) "Verifying I4 is 4 bytes"
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi4(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi4(2), i2)
-    diff = i2 - i1
-    call ESMF_Test((diff .eq. 4), name, failMsg, result, ESMF_SRCLINE) 
+    ! Collect sizes on the Fortran side.
 
-    !------------------------------------------------------------------------
-    !NEX_UTest
-    write(failMsg,*) "Bad size for R4 variable"
-    write(name, *) "Verifying R4 is 4 bytes"
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr4(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr4(2), i2)
-    diff = i2 - i1
-    call ESMF_Test((diff .eq. 4), name, failMsg, result, ESMF_SRCLINE) 
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vip(1), vip(2), ptrsizeF)
+    print *, "F90: Pointer size = ", ptrsizeF
 
-    !------------------------------------------------------------------------
-    !------------------------------------------------------------------------
-    !NEX_UTest
-    write(failMsg,*) "Bad size for I8 variable"
-    write(name, *) "Verifying I8 is 8 bytes"
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi8(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vi8(2), i2)
-    diff = i2 - i1
-    call ESMF_Test((diff .eq. 8), name, failMsg, result, ESMF_SRCLINE) 
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vi1(1), vi1(2), i1sizeF)
+    print *, "F90: Explicit Integer I1 size = ", i1sizeF
 
-    !------------------------------------------------------------------------
-    !NEX_UTest
-    write(failMsg,*) "Bad size for R8 variable"
-    write(name, *) "Verifying R8 is 8 bytes"
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr8(1), i1)
-    call ESMF_PointerToInt(ESMC_POINTER_SIZE, vr8(2), i2)
-    diff = i2 - i1
-    call ESMF_Test((diff .eq. 8), name, failMsg, result, ESMF_SRCLINE) 
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vi2(1), vi2(2), i2sizeF)
+    print *, "F90: Explicit Integer I2 size = ", i2sizeF
 
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vi4(1), vi4(2), i4sizeF)
+    print *, "F90: Explicit Integer I4 size = ", i4sizeF
+
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vi8(1), vi8(2), i8sizeF)
+    print *, "F90: Explicit Integer I8 size = ", i8sizeF
+
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vr4(1), vr4(2), r4sizeF)
+    print *, "F90: Explicit Integer R4 size = ", r4sizeF
+
+    call ESMF_PointerDifference(ESMC_POINTER_SIZE, vr8(1), vr8(2), r8sizeF)
+    print *, "F90: Explicit Integer R8 size = ", r8sizeF
 
     !------------------------------------------------------------------------
     !------------------------------------------------------------------------
     !NEX_UTest
     ! get numbers from C for next set of tests
-    call c_ints(i2size, i4size, i8size, r4size, r8size, rc)
+    call c_ints(i1sizeC, i2sizeC, i4sizeC, i8sizeC, r4sizeC, r8sizeC, ptrsizeC, rc)
     write(failMsg,*) "Failed getting int/float sizes"
     write(name, *) "Getting C int/float word sizes"
     call ESMF_Test((rc .eq. ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE) 
 
     !------------------------------------------------------------------------
+    !------------------------------------------------------------------------
+    ! Compare F90 and C++ sizes; they must match.
     !NEX_UTest
-    write(failMsg,*) "Bad size for short variable"
-    write(name, *) "Verifying short is 2 bytes"
-    call ESMF_Test((i2size .eq. 2), name, failMsg, result, ESMF_SRCLINE) 
+    write(failMsg,*) "Size mismatch for I1 variable"
+    write(name, *) "Verifying F90 I1 matches C I1"
+    call ESMF_Test((i1sizeC .eq. i1sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
     !------------------------------------------------------------------------
     !NEX_UTest
-    write(failMsg,*) "Bad size for int variable"
-    write(name, *) "Verifying int is 4 bytes"
-    call ESMF_Test((i4size .eq. 4), name, failMsg, result, ESMF_SRCLINE) 
+    write(failMsg,*) "Size mismatch for I2 variable"
+    write(name, *) "Verifying F90 I2 matches C I2"
+    call ESMF_Test((i2sizeC .eq. i2sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
     !------------------------------------------------------------------------
     !NEX_UTest
-    write(failMsg,*) "Bad size for long long variable"
-    write(name, *) "Verifying long long is 8 bytes"
-    call ESMF_Test((i8size .eq. 8), name, failMsg, result, ESMF_SRCLINE) 
+    write(failMsg,*) "Size mismatch for I4 variable"
+    write(name, *) "Verifying F90 I4 matches C I4"
+    call ESMF_Test((i4sizeC .eq. i4sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
     !------------------------------------------------------------------------
     !NEX_UTest
-    write(failMsg,*) "Bad size for float variable"
-    write(name, *) "Verifying float is 4 bytes"
-    call ESMF_Test((r4size .eq. 4), name, failMsg, result, ESMF_SRCLINE) 
+    write(failMsg,*) "Size mismatch for I8 variable"
+    write(name, *) "Verifying F90 I8 matches C I8"
+    call ESMF_Test((i8sizeC .eq. i8sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
     !------------------------------------------------------------------------
     !NEX_UTest
-    write(failMsg,*) "Bad size for double variable"
-    write(name, *) "Verifying double is 8 bytes"
-    call ESMF_Test((r8size .eq. 8), name, failMsg, result, ESMF_SRCLINE) 
+    write(failMsg,*) "Size mismatch for R4 variable"
+    write(name, *) "Verifying F90 R4 matches C R4"
+    call ESMF_Test((r4sizeC .eq. r4sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    write(failMsg,*) "Size mismatch for R8 variable"
+    write(name, *) "Verifying F90 R8 matches C R8"
+    call ESMF_Test((r8sizeC .eq. r8sizeF), name, failMsg, result, ESMF_SRCLINE) 
 
 
     !------------------------------------------------------------------------
