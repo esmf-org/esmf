1789458a15c5e57f7a614bddf279d41d0f60dfa2
Silverio Vasquez <silverio.vasquez@noaa.gov>
2005-12-01 21:10:52 +0000

    Added EXHAUSTIVE tag and added needed variable definitions.

--- src/Infrastructure/Regrid/tests/ESMF_RegridToolUTest.F90
+++ src/Infrastructure/Regrid/tests/ESMF_RegridToolUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_RegridToolUTest.F90,v 1.4 2005/11/18 23:05:04 svasquez Exp $
+! $Id: ESMF_RegridToolUTest.F90,v 1.5 2005/12/01 21:10:52 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -23,182 +23,20 @@
     use ESMF_TestMod  ! test methods
     use ESMF_Mod      ! Framework module
 
-    use ESMF_RegridArgs
     use ESMF_RegridSubroutines
 
 
-
-    character(len=100           ) :: longString    ! [128]
-   !character(len=ESMF_MAXSTR-13) :: longString    ! [128]
+    integer :: result=0
 
     call ESMF_TestStart(ESMF_SRCLINE, rc=lrc)
     call ESMF_VMGetGlobal(vm, rc=lrc)
     !--- Get number of PETs we are running with
     call ESMF_VMGet(vm, petCount=npets, localPET=localPet,  rc=lrc)
 
-  !--LongString specifies all the adjustable parameters of the regrid test.
-  !  Possible options are as follows (Ref: Options_Table subroutine):
-  !   FUNCTION: C, D    (Note, A and B are not appropriate for a sphere)
-  !   REGSCHEME: BILINEAR, 1CONSERV
-  !   SRCGRID  : A, D_NE, C_NE
-  !   DSTGRID  : A, D_NE, C_NE
-  !   SRCDELAYOUT: 1DX, 1DY, 2D
-  !   DSTDELAYOUT: 1DX, 1DY, 2D
-
-    longString='FUNCTION:C:REGSCHEME:1CONSERV' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:1DY:DSTDELAYOUT:1DY' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
 #ifdef ESMF_EXHAUSTIVE
-   !Test for "success" of regridding
-   !--------------------------------
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
+    ! Get the test list file only if ESMF_EXHAUSTIVE = ON
+    call readTestList(testListFile ="ESMF_RegridTestList.rc", npets=npets, rc=lrc)
 
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:A:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:1DX:DSTDELAYOUT:1DX' &
-               //':DOMAIN:REGIONAL:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:A:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:1DX:DSTDELAYOUT:1DX' &
-               //':DOMAIN:REGIONAL:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:B:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:REGIONAL:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:C:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:1DX:DSTDELAYOUT:1DX' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:D:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:D:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:D:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:C:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:1DX:DSTDELAYOUT:1DX' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:D:REGSCHEME:BILINEAR' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
-
-   !Test for "success" of regridding
-   !--------------------------------
-    longString='FUNCTION:D:REGSCHEME:1CONSERV' &
-               //':SRCGRID:A:DSTGRID:A:SRCDELAYOUT:2D:DSTDELAYOUT:2D' &
-               //':DOMAIN:WHOLEGLOBE:SRCHALO:3:DSTHALO:0'
-
-    call setupRegridUTest(longString,ier)
-
-   !EX_UTest
-    write(failMsg, *) "Error in regrid"
-    write(name, *) "Regrid test: "//longString
-    call ESMF_Test((regrid_rc.eq.ESMF_SUCCESS),name, failMsg, result, &
-                    ESMF_SRCLINE)
 #endif
 
     call ESMF_TestEnd(result, ESMF_SRCLINE)
