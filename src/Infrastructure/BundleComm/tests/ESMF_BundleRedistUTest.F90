76f962aee031e5299957e274ef4efabe55a7171c
Nancy Collins <nancy@ucar.edu>
2006-01-04 21:13:45 +0000

    minor change to keep in line with helper changes

--- src/Infrastructure/BundleComm/tests/ESMF_BundleRedistUTest.F90
+++ src/Infrastructure/BundleComm/tests/ESMF_BundleRedistUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_BundleRedistUTest.F90,v 1.3 2005/12/19 21:43:48 nscollins Exp $
+! $Id: ESMF_BundleRedistUTest.F90,v 1.4 2006/01/04 21:13:45 nscollins Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2005, University Corporation for Atmospheric Research,
@@ -40,7 +40,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_BundleRedistUTest.F90,v 1.3 2005/12/19 21:43:48 nscollins Exp $'
+      '$Id: ESMF_BundleRedistUTest.F90,v 1.4 2006/01/04 21:13:45 nscollins Exp $'
 !------------------------------------------------------------------------------
 
       ! cumulative result: count failures; no failures equals "all pass"
@@ -358,9 +358,9 @@
       !NEX_UTest
       ! cleanup
       combined_rc = ESMF_SUCCESS
-      call BundleCleanup(bundle1, rc)
+      call BundleCleanup(bundle1, rc=rc)
       if (rc .ne. ESMF_SUCCESS) combined_rc = rc
-      call BundleCleanup(bundle2, rc)
+      call BundleCleanup(bundle2, rc=rc)
       if (rc .ne. ESMF_SUCCESS) combined_rc = rc
 
       write(name, *) "Deleting bundles at cleanup time"
