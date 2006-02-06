1cf1c8698cc777371e29e8461d75fb91d9806e5f
Earl Schwab <eschwab.esmf@gmail.com>
2006-02-06 05:20:54 +0000

    Changed int values to match those in f90ints(); added extra test to pass ints from f90 back to f90; and enhanced rc handling such that any single failure causes the routine to return failure.

--- src/prologue/tests/ESMC_StringSubr.C
+++ src/prologue/tests/ESMC_StringSubr.C
@@ -1,4 +1,4 @@
-// $Id: ESMC_StringSubr.C,v 1.2 2005/08/30 21:31:40 nscollins Exp $
+// $Id: ESMC_StringSubr.C,v 1.3 2006/02/06 05:20:54 eschwab Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2005, University Corporation for Atmospheric Research,
@@ -21,7 +21,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "ESMC_Conf.h"
+#include <ESMC_Macros.h>
+#include <ESMC_Conf.h>
 
 typedef void (*FUNC)(int *, int *, int *, int *, int *);
 typedef void (*FUNC2)(int *, int *, char *, int *, int *, int *, int);
@@ -38,6 +39,8 @@ void FTN(c_strings)(FUNC f90cb, FUNC2 f90cb2, FUNC3 f90cb3,
       static char *newstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       static char *newstr2 = "0123456789abcdefghijklmnopqrstuvwxyz";
       char tbuf[8192];
+      int result = ESMF_SUCCESS;
+      int local_rc;
 
 
       printf("\n\n-- entering c_strings\n");
@@ -47,34 +50,48 @@ void FTN(c_strings)(FUNC f90cb, FUNC2 f90cb2, FUNC3 f90cb3,
       printf(" %d %d %d %d %d\n", *i1, *i2, *i3, *i4, slen);
       printf(" strlen = %d, str='%s'\n", slen, tbuf);
 
-      ni1 = 199;
-      ni2 = 299;
-      ni3 = 399;
-      ni4 = 499;
-
+      ni1 = 102;
+      ni2 = 204;
+      ni3 = 409;
+      ni4 = 819;
   
-      printf("\n\n-- ready to call f90int by callback\n");
-      (*f90cb)(&ni1, &ni2, &ni3, &ni4, rc);
-      printf("\n\n-- returned from call of f90int by callback\n");
+      printf("\n\n-- ready to call f90int by callback with passed ints\n");
+      (*f90cb)(i1, i2, i3, i4, &local_rc);
+      printf("\n\n-- returned from call of f90int by callback with passed ints\n");
+      if (local_rc == ESMF_FAILURE) result = ESMF_FAILURE;
+
+      printf("\n\n-- ready to call f90int by callback with local ints\n");
+      (*f90cb)(&ni1, &ni2, &ni3, &ni4, &local_rc);
+      printf("\n\n-- returned from call of f90int by callback with local ints\n");
+      if (local_rc == ESMF_FAILURE) result = ESMF_FAILURE;
 
       clen = 15;
 
       printf("\n\n-- ready to call f90string2 by callback, passing 15\n");
-      (*f90cb2)(&ni1, &ni2, newstr, &ni3, &ni4, rc, clen);
+      (*f90cb2)(&ni1, &ni2, newstr, &ni3, &ni4, &local_rc, clen);
       printf("\n\n-- returned from call f90string2 by callback\n");
+
+      if (local_rc == ESMF_FAILURE) result = ESMF_FAILURE;
 
       clen = 25;
 
       printf("\n\n-- ready to call f90string2 by callback, passing 25\n");
-      (*f90cb2)(&ni1, &ni2, newstr, &ni3, &ni4, rc, clen);
+      (*f90cb2)(&ni1, &ni2, newstr, &ni3, &ni4, &local_rc, clen);
       printf("\n\n-- returned from call f90string2 by callback\n");
+
+      if (local_rc == ESMF_FAILURE) result = ESMF_FAILURE;
 
       clen = 14;
       clen2 = 24;
 
       printf("\n\n-- ready to call f90string3 by callback, passing 14, 24\n");
-      (*f90cb3)(&ni1, newstr, &ni2, newstr2, &ni3, &ni4, rc, clen, clen2);
+      (*f90cb3)(&ni1, newstr, &ni2, newstr2, &ni3, &ni4, &local_rc, clen, clen2);
       printf("\n\n-- returned from call f90string3 by callback\n");
+
+      if (local_rc == ESMF_FAILURE) result = ESMF_FAILURE;
+
+      /* pass back final result */
+      *rc = result;
 
       printf("\n\n-- leaving c_strings\n");
 
