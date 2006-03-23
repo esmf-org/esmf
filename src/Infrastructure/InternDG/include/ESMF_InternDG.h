311483ec9dbc4b9856f7ee57709ac1d33f977567
Gerhard Theurich <theurich@sourcespring.net>
2006-03-23 00:06:16 +0000

    mv ESMF_DistGrid.h ESMF_InternDG.h

---
+++ src/Infrastructure/InternDG/include/ESMF_InternDG.h
@@ -0,0 +1,56 @@
+! $Id: ESMF_InternDG.h,v 1.1 2006/03/23 00:06:16 theurich Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+! ESMF DistGrid include file for F90
+
+! these lines prevent this file from being read more than once if it
+! ends up being included multiple times
+
+#ifndef ESMF_DistGrid_H
+#define ESMF_DistGrid_H
+
+!------------------------------------------------------------------------------
+!
+! !DESCRIPTION:
+!
+! The code in this file implements constants and macros for the DistGrid...
+!
+! 
+!
+! (all lines below between the !BOP and !EOP markers will be included in 
+!  the automated document processing.)
+!
+!------------------------------------------------------------------------------
+!
+
+
+! 
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!
+! This section contains ...
+!
+!
+
+#define ESMF_DistGridMACRO1(arg1, arg2)  foo(arg1, arg2)
+
+
+! 
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
+!
+! This section contains ...
+!
+!
+
+#define ESMF_DistGridCONSTANT_FRED   23
+
+
+#endif  // ESMF_DistGrid_H
