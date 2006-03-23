37cf4b782a7909963a0ebff1fc30ea5cb776685f
Donald Stark <stark@ucar.edu>
2006-03-23 22:27:12 +0000

    Fixed a compiler warning in one of the testing modules by removing unused character variable.

--- src/Infrastructure/Regrid/tests/ESMF_RegridSubroutines.F90
+++ src/Infrastructure/Regrid/tests/ESMF_RegridSubroutines.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_RegridSubroutines.F90,v 1.13 2006/03/03 20:40:15 nscollins Exp $
+! $Id: ESMF_RegridSubroutines.F90,v 1.14 2006/03/23 22:27:12 donstark Exp $
 !-------------------------------------------------------------------------
 !-------------------------------------------------------------------------
 
@@ -47,7 +47,7 @@ contains
 
     integer :: openStatus, readStatus, startTesting, regrid_rc
     integer :: testCount
-    character(ESMF_MAXSTR) :: failMsg, name, testMsg, halo
+    character(ESMF_MAXSTR) :: failMsg, name, testMsg
     ! cumulative result: count failures; no failures equals "all pass"
     integer :: result = 0
     real(ESMF_KIND_R8) :: err_threshold
