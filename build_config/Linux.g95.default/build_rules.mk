abbc74ce663cd79090d95873937fde1fd2ed97dc
Gerhard Theurich <theurich@sourcespring.net>
2006-05-31 22:57:59 +0000

    massive update: - don't consider LD_LIBRARY_PATH, instead determine correct library path using GCC option to compiler front-end. - separate mpich and mpich2 settings

--- build_config/Linux.g95.default/build_rules.mk
+++ build_config/Linux.g95.default/build_rules.mk
@@ -1,10 +1,7 @@
-# $Id: build_rules.mk,v 1.5 2005/08/05 20:09:01 nscollins Exp $
+# $Id: build_rules.mk,v 1.6 2006/05/31 22:57:59 theurich Exp $
 #
 #  Linux.g95.default
-#  T. Wainwright, April 2005, based on Linux.pgi.default
-#  updated by nancy collins, after massive makefile updates
 #
-
 #
 # Default MPI setting.
 #
@@ -12,7 +9,7 @@ ifeq ($(ESMF_COMM),default)
 export ESMF_COMM := mpiuni
 endif
 
-# these have not been tested.
+# lam has not been tested.
 ifeq ($(ESMF_COMM),lam)
 # with lam-mpi installed:
 MPI_LIB        += -lmpi -llam
@@ -20,68 +17,74 @@ endif
 
 ifeq ($(ESMF_COMM),mpich)
 # with mpich installed:
-MPI_LIB        += -lmpich
+MPI_LIB        += -lpmpich++ -lmpich
 MPI_INCLUDE    += -DESMF_MPICH
 MPIRUN         += $(ESMF_NODES)
 endif
 
-THREAD_LIB     = -lpthread
+ifeq ($(ESMF_COMM),mpich2)
+# with mpich installed:
+MPI_LIB        +=
+MPI_INCLUDE    +=
+MPIRUN         += $(ESMF_NODES)
+MPIMPMDRUN      = mpiexec
+endif
 
-#
-# compilers or mpich wrappers
-#
+THREAD_LIB     =
+
+# straight compilers front-ends
 ifneq ($(ESMF_COMM),mpich)
 C_CC		= gcc 
 C_CXX		= g++
 C_FC		= g95
 endif
 
+# mpich compiler front-end wrappers
 ifeq ($(ESMF_COMM),mpich)
 C_CC    	= mpicc
-# MPICH_IGNORE_CXX_SEEK is workaround for MPI-2 bug (see MPICH2 docs)
-C_CXX   	= mpiCC -DMPICH_IGNORE_CXX_SEEK
+C_CXX   	= mpiCC
 C_FC    	= mpif90
 endif
 
+# mpich2 compiler front-end wrappers
+ifeq ($(ESMF_COMM),mpich2)
+C_CC    	= mpicc
+# MPICH_IGNORE_CXX_SEEK is workaround for MPI-2 bug (see MPICH2 docs)
+C_CXX   	= mpicxx -DMPICH_IGNORE_CXX_SEEK
+C_FC    	= mpif90
+endif
+
+# how to get version string out of compiler front-ends
 C_CCV		= ${C_CC} -dumpversion
 C_CXXV		= ${C_CXX} -dumpversion
 C_FCV           = ${C_FC} -dumpversion
 
+# this must match the Fortran symbol convention of any other libraries used
 FFLAGS          = -fno-second-underscore
+
+# not tested 64-bit option
 ifeq ($(ESMF_PREC),64)
 CFLAGS         += -march=k8 -m64 -mcmodel=medium
 FFLAGS         += -march=k8 -m64 -mcmodel=medium
 endif
+
+# options for how to process four different flavors of Fortran source
 F_FREECPP       = -cpp -ffree-form
 F_FIXCPP        = -cpp -ffixed-form
 F_FREENOCPP     = -ffree-form
 F_FIXNOCPP      = -ffixed-form
 
-# by default append each directory which is in LD_LIBRARY_PATH to
-# the -L flag and also to the run-time load flag.  (on systems which
-# support the 'module' command, that is how it works - by adding dirs
-# to LD_LIBRARY_PATH.)  if it is not set, default to one of the many
-# possible places the gcc compilers try to install themselves.  
-# if your compiler installs these libs someplace else
-# either set LD_LIBRARY_PATH first, or make a site specific file and
-# edit the paths explicitly.
-ifneq ($(origin LD_LIBRARY_PATH), environment)
-CXXLIB_PATHS   = -L/usr/lib
-F90LIB_PATHS   = -L/usr/lib
-C_LIB_PATHS      = ${CXXLIB_PATHS} ${F90LIB_PATHS}
-C_LD_PATHS       = ${CXXLIB_PATHS} ${F90LIB_PATHS}
-else
-C_LIB_PATHS      = $(ENV_LIB_PATHS)
-C_LD_PATHS       = $(ENV_LD_PATHS)
-endif
+# use the GCC -print-file-name option to determine location of stdc++ and 
+# Fortran libraries and thus define the C_LIB_PATHS
+C_LIB_PATHS     := -L$(dir $(shell $(C_CXX) -print-file-name=libstdc++.so)) \
+  -L$(dir $(shell $(C_FC) -print-file-name=libf95.a))
 
-
-C_F90CXXLIBS    = -lrt -lc -lstdc++
-C_CXXF90LIBS    = -lrt -lf95 -lstdc++
+# flags required to link against libesmf.a using the F90 linker front-end
+C_F90CXXLIBS    = -lrt -lstdc++
+# flags required to link against libesmf.a using the C++ linker front-end
+C_CXXF90LIBS    = -lrt -lf95
 
 
 PARCH	        = linux_g95
 
 SL_LIBS_TO_MAKE = 
-
-
