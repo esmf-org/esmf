de8ac90798cbc0442e60430a4f869952f57a4f44
Gerhard Theurich <theurich@sourcespring.net>
2006-07-17 19:07:51 +0000

    merge in the g95 work from trunk and morph it to fit enhanced common.mk

---
+++ build_config/Linux.g95.default/build_rules.mk
@@ -0,0 +1,145 @@
+# $Id: build_rules.mk,v 1.5.2.1 2006/07/17 19:07:51 theurich Exp $
+#
+#  Linux.g95.default
+#
+
+############################################################
+# Default compiler setting.
+#
+ESMF_F90DEFAULT         = g95
+ESMF_CXXDEFAULT         = g++
+
+############################################################
+# Default MPI setting.
+#
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90LINKLIBS       += -lmpiuni
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_CXXLINKLIBS       += -lmpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),mpich)
+# Mpich ----------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lpmpich++ -lmpich
+ESMF_CXXDEFAULT         = mpiCC
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPICH
+ESMF_MPIRUNDEFAULT      = mpirun
+else
+ifeq ($(ESMF_COMM),mpich2)
+# Mpich2 ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),lam)
+# LAM (assumed to be built with g95) -----------------------
+ESMF_F90DEFAULT         = mpif77
+ESMF_CXXDEFAULT         = mpic++
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),openmpi)
+# OpenMPI --------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+endif
+endif
+endif
+
+############################################################
+#
+# location of external libs.  if you want to use any of these,
+# define ESMF_SITE to my_site so the build system can find it,
+# copy this file into Linux.intel.my_site, and uncomment the
+# libs you want included.  remove the rest of this file since
+# both this file and the site file will be included.
+
+# LAPACK_INCLUDE   = 
+# LAPACK_LIB       = -L/usr/local/lib -llapack
+# NETCDF_INCLUDE   = -I/usr/local/include/netcdf
+# NETCDF_LIB       = -L/usr/local/lib -lnetcdf
+# HDF_INCLUDE      = -I/usr/local/include/hdf
+# HDF_LIB          = -L/usr/local/lib/ -lmfhdf -ldf -ljpeg -lz
+# BLAS_INCLUDE     = 
+# BLAS_LIB         = -latlas -lscs
+
+############################################################
+#
+# Fortran symbol convention must match other libraries used
+ESMF_F90COMPILEOPTS       += -fno-second-underscore
+ESMF_F90LINKOPTS          += -fno-second-underscore
+
+############################################################
+#
+# On IA64 set long and pointer types to 64-bit
+ifeq ($(ESMF_PREC),64)
+ESMF_CXXCOMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_CXXLINKOPTS          += -march=k8 -m64 -mcmodel=medium
+ESMF_F90COMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_F90LINKOPTS          += -march=k8 -m64 -mcmodel=medium
+endif
+
+############################################################
+#
+# Compiler options to print version string
+ESMF_CXXVOPT        = -dumpversion
+ESMF_F90VOPT        = -dumpversion
+
+############################################################
+#
+# Need this until the file convention is fixed (then remove these two lines)
+ESMF_F90COMPILEFREENOCPP = -ffree-form
+ESMF_F90COMPILEFIXCPP    = -cpp -ffixed-form
+
+############################################################
+#
+# Determine where gcc's libraries are located
+ESMF_F90LINKPATHS += \
+  -L$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.so))
+ESMF_F90LINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.so))
+
+############################################################
+#
+# Determine where g95's libraries are located
+ESMF_CXXLINKPATHS += \
+  -L$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libf95.a))
+ESMF_CXXLINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libf95.a))
+
+############################################################
+#
+# Link against libesmf.a using the F90 linker front-end
+ESMF_F90LINKLIBS += -lrt -lstdc++
+
+############################################################
+#
+# Link against libesmf.a using the C++ linker front-end
+ESMF_CXXLINKLIBS += -lrt -lf95
+
+###############################################################################
+
+SL_LIBS_TO_MAKE = 
+C_SL_LIBOPTS  =
+
