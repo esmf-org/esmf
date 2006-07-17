b45ebb6a677bc6a48b1e3137c411c70e114b1ae4
Gerhard Theurich <theurich@sourcespring.net>
2006-07-17 18:05:34 +0000

    rework for enhanced common.mk

--- build_config/Linux.pgi.default/build_rules.mk
+++ build_config/Linux.pgi.default/build_rules.mk
@@ -1,151 +1,128 @@
-# $Id: build_rules.mk,v 1.6 2004/03/16 18:00:54 nscollins Exp $
+# $Id: build_rules.mk,v 1.25.2.1 2006/07/17 18:05:34 theurich Exp $
 #
-#  Linux.pgi.default.mk
+#  Linux.pgi.default
 #
 
+############################################################
+# Default compiler setting.
 #
-#  Make sure precision is 32.
-#
-ESMF_PREC = 32
+ESMF_F90DEFAULT         = pgf90
+ESMF_CXXDEFAULT         = pgCC
 
-#
+############################################################
 # Default MPI setting.
 #
-ifndef ESMF_COMM
-export ESMF_COMM := mpiuni
-endif
 ifeq ($(ESMF_COMM),default)
 export ESMF_COMM := mpiuni
 endif
 
-# if using PBS system, export this for run time
-ifdef PBS_NODEFILE
-export ESMF_NODES := -machinefile $(PBS_NODEFILE)
-endif
-
-#############
-
-
-# Location of MPI (Message Passing Interface) software
-
-ifeq ($(ESMF_COMM),lam)
-# this section is set up for LAM mpi
-MPI_HOME       =
-MPI_LIB        = -lmpi -llam
-MPI_INCLUDE    =
-MPIRUN         =  mpirun
-endif
-
-ifeq ($(ESMF_COMM),mpich)
-# set up to use MPICH
-MPI_HOME       = 
-MPI_LIB        = -lmpich
-MPI_INCLUDE    = -DESMF_MPICH=1
-MPIRUN         = mpirun
-endif
-
+############################################################
+# MPI dependent settings.
+#
 ifeq ($(ESMF_COMM),mpiuni)
-# this section is set up to bypass all MPI
-MPI_HOME       = ${ESMF_DIR}/src/Infrastructure/mpiuni
-MPI_LIB        = -lmpiuni
-MPI_INCLUDE    = -I${MPI_HOME}
-MPIRUN         =  ${MPI_HOME}/mpirun
-endif
-
-# MP_LIB is for openMP
-MP_LIB          = -lpgmp
-# For pthreads (or omp)
-THREAD_LIB      = -lpthread
-
-#############
-#
-AR		   = ar
-AR_FLAGS	   = cr
-RM		   = rm -f
-OMAKE		   = ${MAKE}
-RANLIB		   = ranlib
-SHELL		   = /bin/sh
-SED		   = /bin/sed
-SH_LD		   = pgcc 
-#
-# C and Fortran compiler 
-#
-ifneq ($(ESMF_COMM),mpich)
-C_CC		   = pgcc -mp
-C_FC		   = pgf90 -mp
-CXX_CC		   = pgCC -tlocal
-CXX_FC		   = pgf90 -mp 
-endif
-
+# MPI stub library -----------------------------------------
+ESMF_F90LINKLIBS       += -lmpiuni
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_CXXLINKLIBS       += -lmpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
 ifeq ($(ESMF_COMM),mpich)
-C_CC		   = mpicc -mp
-C_FC		   = mpif90 -mp
-CXX_CC		   = mpiCC -tlocal
-CXX_FC		   = mpif90 -mp
+# Mpich ----------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lpmpich++ -lmpich
+ESMF_CXXDEFAULT         = mpiCC
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPICH
+ESMF_MPIRUNDEFAULT      = mpirun
+else
+ifeq ($(ESMF_COMM),mpich2)
+# Mpich2 ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),lam)
+# LAM (assumed to be built with pgf90) ---------------------
+ESMF_F90DEFAULT         = mpif77
+ESMF_CXXDEFAULT         = mpic++
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),openmpi)
+# OpenMPI --------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+endif
+endif
 endif
 
-C_FC_MOD           = -I
-C_CLINKER_SLFLAG   = -Wl,-rpath,
-C_FLINKER_SLFLAG   = -Wl,-rpath,
-C_CLINKER	   = ${C_CC}
-C_FLINKER	   = ${C_FC}
-C_CCV		   = ${C_CC} -V
-C_FCV              = ${C_FC} -V
-C_SYS_LIB	   = -ldl -lc -lg2c -lm
+############################################################
+#
+# location of external libs.  if you want to use any of these,
+# define ESMF_SITE to my_site so the build system can find it,
+# copy this file into Linux.intel.my_site, and uncomment the
+# libs you want included.  remove the rest of this file since
+# both this file and the site file will be included.
 
-F_FREECPP          = -Mpreprocess -Mfreeform
-F_FIXCPP           = -Mpreprocess -Mnofreeform
-F_FREENOCPP        = -Mfreeform
-F_FIXNOCPP         = -Mnofreeform
-# ---------------------------- BOPT - g options ----------------------------
-G_COPTFLAGS	   = -g 
-G_FOPTFLAGS	   = -g
-# ----------------------------- BOPT - O options -----------------------------
-O_COPTFLAGS	   = -O 
-O_FOPTFLAGS	   = -O
+# LAPACK_INCLUDE   = 
+# LAPACK_LIB       = -L/usr/local/lib -llapack
+# NETCDF_INCLUDE   = -I/usr/local/include/netcdf
+# NETCDF_LIB       = -L/usr/local/lib -lnetcdf
+# HDF_INCLUDE      = -I/usr/local/include/hdf
+# HDF_LIB          = -L/usr/local/lib/ -lmfhdf -ldf -ljpeg -lz
+# BLAS_INCLUDE     = 
+# BLAS_LIB         = -latlas -lscs
+
+############################################################
 #
-# C++ compiler 
+# Compiler options to print version string
+ESMF_CXXVOPT        = -V -v
+ESMF_F90VOPT        = -V -v
+
+############################################################
 #
-CXX_CLINKER_SLFLAG = -Wl,-rpath,
-CXX_FLINKER_SLFLAG = -Wl,-rpath,
-CXX_CLINKER	   = ${CXX_CC}
-CXX_FLINKER	   = ${CXX_CC}
-CXX_CCV		   = ${CXX_CC} -V
-CXX_SYS_LIB	   = -ldl -lc -lg2c -lm
-C_F90CXXLD         = ${CXX_FC} -mp
-C_F90CXXLIBS       = -lpgc -lstd -lC
-C_CXXF90LD         = ${CXX_CC} 
-C_CXXF90LIBS       = -lpgf90 -lpgf90_rpm1 -lpgf902 -lpgf90rtl -lpgftnrtl
-C_CXXSO            = ${CXX_CC} -shared
-# ------------------------- BOPT - g_c++ options ------------------------------
-GCXX_COPTFLAGS	   = -g 
-GCXX_FOPTFLAGS	   = -g
-# ------------------------- BOPT - O_c++ options ------------------------------
-OCXX_COPTFLAGS	   = -O 
-OCXX_FOPTFLAGS	   = -O
-# -------------------------- BOPT - g_complex options ------------------------
-GCOMP_COPTFLAGS	   = -g
-GCOMP_FOPTFLAGS	   = -g
-# --------------------------- BOPT - O_complex options -------------------------
-OCOMP_COPTFLAGS	   = -O
-OCOMP_FOPTFLAGS	   = -O
+# Need this until the file convention is fixed (then remove these two lines)
+ESMF_F90COMPILEFREENOCPP = -Mfreeform
+ESMF_F90COMPILEFIXCPP    = -Mpreprocess -Mnofreeform
+
+############################################################
+#
+# Determine where pgf90's libraries are located
+ESMF_CXXLINKPATHS += -L$(dir $(shell $(ESMF_DIR)/scripts/pgf90-libpath $(ESMF_F90COMPILER)))
+ESMF_CXXLINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_DIR)/scripts/pgf90-libpath $(ESMF_F90COMPILER)))
+
+############################################################
+#
+# Determine where pgCC's libraries are located
+ESMF_F90LINKPATHS += -L$(dir $(shell $(ESMF_DIR)/scripts/pgCC-libpath $(ESMF_CXXCOMPILER)))
+ESMF_F90LINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_DIR)/scripts/pgCC-libpath $(ESMF_CXXCOMPILER)))
+
+############################################################
+#
+# Link against libesmf.a using the F90 linker front-end
+ESMF_F90LINKLIBS += -lrt -lC -lc
+
+############################################################
+#
+# Link against libesmf.a using the C++ linker front-end
+ESMF_CXXLINKLIBS += -lrt -lC -lpgf90 -lpgf90_rpm1 -lpgf902 -lpgf90rtl -lpgftnrtl
+
 ###############################################################################
 
-PARCH		   = linux_pgi
-
-SL_SUFFIX   =
-SL_LIBOPTS  =
-SL_LINKOPTS =
-SL_F_LINKER = $(F90CXXLD)
-SL_C_LINKER = $(CXXF90LD)
-SL_LIB_LINKER = $(CXXF90LD)
-SL_LIBS_TO_MAKE = libesmf 
-
-
-#############
-#
-# Set shared dependent on build_shared to build .so lib.
-#
-shared:
+SL_LIBS_TO_MAKE = 
+C_SL_LIBOPTS  = -shared
 
 
