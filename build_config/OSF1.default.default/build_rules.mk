deed8a06024ea695f8a3729e427f74454d3dd6f4
Gerhard Theurich <theurich@sourcespring.net>
2006-07-20 20:13:24 +0000

    adapt to changes in common.mk

--- build_config/OSF1.default.default/build_rules.mk
+++ build_config/OSF1.default.default/build_rules.mk
@@ -1,149 +1,122 @@
-#  $Id: build_rules.mk,v 1.7 2004/03/16 18:00:54 nscollins Exp $
+#  $Id: build_rules.mk,v 1.31.2.1 2006/07/20 20:13:24 theurich Exp $
 #
-#  OSF1.default.default.mk
-#
+#  OSF1.default.default
 #
 
+############################################################
+# Default compiler setting.
 #
-#  Make sure that ESMF_PREC is set to 64
-#
-ESMF_PREC = 64
+ESMF_F90DEFAULT         = f90
+ESMF_CXXDEFAULT         = cxx
 
-#
+############################################################
 # Default MPI setting.
 #
-ifndef ESMF_COMM
-export ESMF_COMM := mpi
-endif
 ifeq ($(ESMF_COMM),default)
 export ESMF_COMM := mpi
 endif
 
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90LINKLIBS       += -lmpiuni
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_CXXLINKLIBS       += -lmpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),mpi)
+# Vendor MPI -----------------------------------------------
+ESMF_F90LINKLIBS       += -lmpi
+ESMF_CXXLINKLIBS       += -lmpi
+ESMF_MPIRUNDEFAULT      = ${ESMF_DIR}/scripts/mpirun.alpha
+ESMF_MPIMPMDRUNDEFAULT  = ${ESMF_DIR}/scripts/mpimpmdrun.alpha
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
 
 ############################################################
 #
-#  The following naming convention is used:
-#     XXX_LIB - location of library XXX
-#     XXX_INCLUDE - directory for include files needed for library XXX
-#
-# Location of BLAS and LAPACK.  See ${ESMF_DIR}/docs/instllation.html
-# for information on retrieving them.
-#
-#
-BLAS_LIB         = 
-LAPACK_LIB       = -L/usr/lib -lcxml
-NETCDF_LIB       = -L/usr/local/unsupported/netcdf-3.5.0/lib -lnetcdf
-NETCDF_INCLUDE   = -I/usr/local/unsupported/netcdf-3.5.0/include
-NETCDF64_LIB     = -L/usr/local/unsupported/netcdf-3.5.0/lib -lnetcdf
-NETCDF64_INCLUDE = -I/usr/local/unsupported/netcdf-3.5.0/include
-HDF_LIB          = /usr/local/unsupported/HDF4.1r5/lib/libmfhdf.a \
-                   /usr/local/unsupported/HDF4.1r5/lib/libdf.a \
-                   /usr/local/unsupported/HDF4.1r5/lib/libjpeg.a \
-                   /usr/local/unsupported/HDF4.1r5/lib/libz.a
-HDF_INCLUDE      = -I/usr/local/unsupported/HDF4.1r5/include
-#
-# Location of MPI (Message Passing Interface) software
-#
-MPI_LIB          = -L/usr/opt/mpi/lib -lmpi
-MPI_INCLUDE      = -I/usr/opt/mpi/include
-MPIRUN           = ${ESMF_TOP_DIR}/scripts/mpirun.alpha
-MPI64_LIB        = 
+# location of external libs.  if you want to use any of these,
+# define ESMF_SITE to my_site so the build system can find it,
+# copy this file into Linux.intel.my_site, and uncomment the
+# libs you want included.  remove the rest of this file since
+# both this file and the site file will be included.
 
-# For pthreads (or omp)
-THREAD_LIB        = -lpthread -lrt
-
+# LAPACK_INCLUDE   = 
+# LAPACK_LIB       = -L/usr/local/lib -llapack
+# NETCDF_INCLUDE   = -I/usr/local/include/netcdf
+# NETCDF_LIB       = -L/usr/local/lib -lnetcdf
+# HDF_INCLUDE      = -I/usr/local/include/hdf
+# HDF_LIB          = -L/usr/local/lib/ -lmfhdf -ldf -ljpeg -lz
+# BLAS_INCLUDE     = 
+# BLAS_LIB         = -latlas -lscs
 
 ############################################################
+# Conditionally add pthread compiler and linker flags
 #
-AR			= ar
-AR_FLAGS		= cr
-AR_EXTRACT              = -x 
-RM			= rm -f
-OMAKE			= ${MAKE}
-RANLIB			= ranlib
-SHELL			= /bin/sh
-SED			= /bin/sed
-#
-# Fortran compiler options 
-#
-AR32_64			= ${AR}
-BIG_ENDIAN		= -convert big_endian
-C_64BIT			= 
-DARCH			= -Dalpha -DOSF1
-DsysARCH		= -DsysOSF1
-INF_CONTROL		= -fpe1
-NO_LINE_DIRECTIVES	= -wl,P
-NO_AUTO_PARALLEL	= -nomp
-NO_INLINING		= -noinline
-REAL8			= -r8
-STRICT			= -nopipeline
-EXPAND_TEMPLATES        = -tweak
-FPPFLAGS		= $(FPPOPTS)
-F_FREECPP               = -free -cpp
-F_FIXCPP                = -cpp -extend_source
-F_FREENOCPP             = -free
-F_FIXNOCPP              = -extend_source
-#
-# C and Fortran compiler 
-#
-C_CC			= cc
-C_FC			= f90
-C_FC_MOD		= -I
-C_CLINKER_SLFLAG	= -Wl,-rpath,
-C_FLINKER_SLFLAG	= -Wl,-rpath,
-C_CLINKER		= cc
-C_FLINKER		= f90
-C_CCV			= $(C_CC) -V
-C_FCV			= $(C_FC) -version
-C_SYS_LIB		= -lfor -lutil -lFutil -lots
-# ---------------------------- BOPT - g options ----------------------------
-G_COPTFLAGS		= -g -assume gfullpath -pthread
-G_FOPTFLAGS		= -g -assume gfullpath 
-# ----------------------------- BOPT - O options -----------------------------
-O_COPTFLAGS		= -O3 -w -pthread
-O_FOPTFLAGS		= -O3 -w
-#
-# C++ compiler 
-#
-CXX_CC		   = cxx -x cxx
-CXX_FC		   = f90
-CXX_CLINKER_SLFLAG = -Wl,-rpath,
-CXX_FLINKER_SLFLAG = -Wl,-rpath,
-CXX_CLINKER	   = cxx
-CXX_FLINKER	   = f90
-CXX_CCV		   = $(CXX_CC) -V
-CXX_SYS_LIB	   = -lcomplex -lfor -lutil -lFutil -lots
-C_F90CXXLIBS       = -L/usr/ccs/lib/cmplrs/cxx -lcxx 
-C_F90CXXLD         = f90
-C_CXXF90LD         = cxx
-C_CXXF90LIBS       = -lfor
-# ------------------------- BOPT - g_c++ options ------------------------------
-GCXX_COPTFLAGS		= -g  -qfullpath
-GCXX_FOPTFLAGS		= -g  -qfullpath 
-# ------------------------- BOPT - O_c++ options ------------------------------
-OCXX_COPTFLAGS		= -O3  -qmaxmem=4000 -qspill=3000
-OCXX_FOPTFLAGS		= -O3
-# -------------------------- BOPT - g_complex options ------------------------
-GCOMP_COPTFLAGS		= -g  -qfullpath
-GCOMP_FOPTFLAGS		= -g  -qfullpath
-# --------------------------- BOPT - O_complex options -------------------------
-OCOMP_COPTFLAGS		= -O3  -qmaxmem=4000 -qspill=3000
-OCOMP_FOPTFLAGS		= -O3
-###############################################################################
+ifeq ($(ESMF_PTHREADS),ON)
+ESMF_F90COMPILEOPTS += -pthread -reentrancy threaded
+ESMF_F90LINKOPTS    += -pthread -reentrancy threaded
+ESMF_CXXCOMPILEOPTS += -pthread
+ESMF_CXXLINKOPTS    += -pthread
+endif
 
-PARCH			= alpha
-
-SL_SUFFIX   = so
-SL_LIBOPTS  = -shared -rpath .:$(ESMF_LIBDIR)  ${F90CXXLIBS} ${MPI_LIB} ${MP_LIB} ${THREAD_LIB} ${PCL_LIB} 
-SL_LINKOPTS = 
-SL_F_LINKER = $(F90CXXLD)
-SL_C_LINKER = $(CXXF90LD)
-SL_LIB_LINKER = $(CXXF90LD)
-SL_LIBS_TO_MAKE = libesmf 
-
-
+############################################################
+# Compiler options to print version string
 #
-# set shared dependent on build_shared to build .so lib
-#
-shared: build_shared
+ESMF_F90VOPT        = -version
+ESMF_CXXVOPT        = -V
 
+############################################################
+# Special debug flags
+#
+ESMF_F90OPTFLAG_G       += -assume gfullpath
+ESMF_CXXOPTFLAG_G       += -assume gfullpath
+
+############################################################
+# Special optimization flags
+#
+ESMF_F90OPTFLAG_O       += -w
+ESMF_CXXOPTFLAG_O       += -w
+
+############################################################
+# Help f90 to understand Fortran suffices
+#
+ESMF_F90COMPILEFREECPP   = -free -cpp
+ESMF_F90COMPILEFREENOCPP = -free
+ESMF_F90COMPILEFIXCPP    = -cpp -extend_source
+ESMF_F90COMPILEFIXNOCPP  = -extend_source
+
+############################################################
+# Blank out variables to prevent rpath encoding into f90
+#
+ESMF_RPATHPREFIX      = -rpath 
+
+############################################################
+# Determine where f90's libraries are located
+#
+ESMF_CXXLINKPATHS += $(addprefix -L, $(shell $(ESMF_DIR)/scripts/libpath.compaqf90 $(ESMF_F90COMPILER)))
+
+############################################################
+# Link against libesmf.a using the F90 linker front-end
+#
+ESMF_F90LINKLIBS += -lcxx -lrt -lm
+
+############################################################
+# Link against libesmf.a using the C++ linker front-end
+#
+ESMF_CXXLINKLIBS += -lfor -lrt -lm
+
+############################################################
+# Shared library options
+#
+ESMF_SL_LIBOPTS  += -shared $(ESMF_CXXCOMPILEOPTS)
+ESMF_SL_LIBLIBS  += $(ESMF_CXXLINKPATHS) $(ESMF_CXXLINKLIBS)
