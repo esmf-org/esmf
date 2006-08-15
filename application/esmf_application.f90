ac9517f5af74f8c0b550d86a506e02e632310569
Gerhard Theurich <theurich@sourcespring.net>
2006-08-15 20:17:29 +0000

    initial revision

---
+++ application/esmf_application.f90
@@ -0,0 +1,21 @@
+! Simple ESMF application
+
+program esmf_test0
+
+  ! modules
+  use ESMF_Mod
+  
+  implicit none
+  
+  ! local variables
+  integer:: rc
+  
+  call ESMF_Initialize(rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  
+  print *, "hi from program esmf_test0"
+  print *, "======================= finished ================================="
+
+  call ESMF_Finalize(rc=rc)
+  
+end program
