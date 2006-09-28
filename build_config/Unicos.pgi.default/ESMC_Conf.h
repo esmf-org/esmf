f9cdd6a398cc773fe70ee0d4fca36a5957fd6dfe
Gerhard Theurich <theurich@sourcespring.net>
2006-09-28 05:36:56 +0000

    initial revision

---
+++ build_config/Unicos.pgi.default/ESMC_Conf.h
@@ -0,0 +1,104 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1 2006/09/28 05:37:42 theurich Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2003, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the GPL.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_linux
+#define ESMF_ARCH_NAME "linux"
+
+#define ESMC_HAVE_LIMITS_H
+#define ESMC_HAVE_PWD_H 
+#define ESMC_HAVE_MALLOC_H 
+#define ESMC_HAVE_STRING_H 
+#define ESMC_HAVE_GETDOMAINNAME
+#define ESMC_HAVE_DRAND48 
+#define ESMC_HAVE_UNAME 
+#define ESMC_HAVE_UNISTD_H 
+#define ESMC_HAVE_SYS_TIME_H 
+#define ESMC_HAVE_STDLIB_H
+
+#define ESMC_HAVE_FORTRAN_UNDERSCORE 
+#define ESMC_HAVE_FORTRAN_UNDERSCORE_UNDERSCORE
+#define FTN(func) func##_
+
+#define ESMC_HAVE_OMP_THREADS 1
+
+#define ESMC_HAVE_MPI 1
+
+#define ESMC_HAVE_READLINK
+#define ESMC_HAVE_MEMMOVE
+#define ESMC_HAVE_TEMPLATED_COMPLEX
+
+#define ESMC_HAVE_DOUBLE_ALIGN_MALLOC
+#define ESMC_HAVE_MEMALIGN
+#define ESMC_HAVE_SYS_RESOURCE_H
+#define ESMC_SIZEOF_VOIDP 4
+#define ESMC_SIZEOF_INT 4
+#define ESMC_SIZEOF_DOUBLE 8
+
+#if defined(fixedsobug)
+#define ESMC_USE_DYNAMIC_LIBRARIES 1
+#define ESMC_HAVE_RTLD_GLOBAL 1
+#endif
+
+#define ESMC_HAVE_SYS_UTSNAME_H
+
+#if 0
+PGI method for marking F90 "not present" optional arguments uses
+the address of compiler-generated global array element pghpf_0_[8] for
+non-character data.  However, PGI says check against entire array to be safe.
+For character data, "not present" optional arguments are marked with the
+address of global compiler-generated variable pghpf_0c_.
+#endif
+#if 0
+This file is included by one F90 file (the test for F90 pointer size), so
+try to protect the F90 file from C declarations.
+#endif
+#ifndef _FROM_FORTRAN
+extern char pghpf_0_[];
+extern char pghpf_0c_;
+#define ESMC_PRESENT(arg) ( ! ( ( (char*)(arg) >=  pghpf_0_ && \
+                                  (char*)(arg) <= &pghpf_0_[12] ) || \
+		                  (char*)(arg) == &pghpf_0c_ ) )
+#endif
+
+#if S32
+#define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 72
+#define ESMF_F90_PTR_PLUS_RANK 24
+#define ESMC_POINTER_SIZE 4
+#endif
+#if S32_x86_64
+#define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 72
+#define ESMF_F90_PTR_PLUS_RANK 24
+#define ESMC_POINTER_SIZE 4
+#endif
+#if S64
+#define ESMF_IS_64BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 88
+#define ESMF_F90_PTR_PLUS_RANK 24
+#define ESMC_POINTER_SIZE 8
+#endif
+#if S64_x86_64
+#define ESMF_IS_64BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 88
+#define ESMF_F90_PTR_PLUS_RANK 24
+#define ESMC_POINTER_SIZE 8
+#endif
+
+
+#endif
