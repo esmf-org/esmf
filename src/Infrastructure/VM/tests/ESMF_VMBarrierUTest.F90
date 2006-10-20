9a3b622d006443508c1df6c5b359721b3b0b37a1
Gerhard Theurich <theurich@sourcespring.net>
2006-10-20 22:45:57 +0000

    reworked this code to better take timer precision into account and use ESMF_VMWTimeDelay()

--- src/Infrastructure/VM/tests/ESMF_VMBarrierUTest.F90
+++ src/Infrastructure/VM/tests/ESMF_VMBarrierUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_VMBarrierUTest.F90,v 1.7.2.1 2006/08/11 18:03:10 theurich Exp $
+! $Id: ESMF_VMBarrierUTest.F90,v 1.7.2.2 2006/10/20 22:45:57 theurich Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2003, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_VMBarrierUTest.F90,v 1.7.2.1 2006/08/11 18:03:10 theurich Exp $'
+      '$Id: ESMF_VMBarrierUTest.F90,v 1.7.2.2 2006/10/20 22:45:57 theurich Exp $'
 !------------------------------------------------------------------------------
       ! cumulative result: count failures; no failures equals "all pass"
       integer :: result = 0
@@ -50,9 +50,9 @@
       integer:: i, rc, loop_rc
       type(ESMF_VM):: vm
       integer:: localPet, petCount
-      real(ESMF_KIND_R8)  t_a, t0, t1, t_b, dt, max_time, delay, dt_prec
-      real(ESMF_KIND_R8), allocatable:: delay_time(:)
-
+      real(ESMF_KIND_R8):: t_a, t_b, dt, delay, dt_prec
+      real(ESMF_KIND_R8):: dt_prec_max(1), dt_prec_local(1)
+      real(ESMF_KIND_R8), parameter:: delay_time = 1.0 ! 1 second delay
 
 !------------------------------------------------------------------------------
 !   The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
@@ -67,58 +67,57 @@
 
       ! Get count of PETs and which PET number we are
       call ESMF_VMGetGlobal(vm, rc=rc)
-      call ESMF_VMGet(vm, localPet, petCount=petCount, rc=rc)
+      call ESMF_VMGet(vm, localPet=localPet, petCount=petCount, rc=rc)
 
-      ! Allocate localData
-      allocate(delay_time(0:petCount-1))
-      
       ! Determine the precision of the timer function
-      call ESMF_VMWTimePrec(dt_prec)
-      print *, "timing precision is: ", dt_prec
+      call ESMF_VMWTimePrec(dt_prec_local(1))
+      print *, "timing precision is: ", dt_prec_local(1)
+      
+      ! Find the maximum dt_prec across all PETs
+      call ESMF_VMAllReduce(vm, dt_prec_local, dt_prec_max, 1, ESMF_MAX, rc=rc)
+      dt_prec = dt_prec_max(1)
+      print *, "max timing precision is: ", dt_prec
+      
+      !NEX_UTest
+      ! delay_time must at least be one order of magnitude longer than precis.
+      write(name, *) "Timer Precision test"
+      write(failMsg, *) "Timer precision is too low!"
+      call ESMF_Test((delay_time >= 10. * dt_prec), &
+        name, failMsg, result, ESMF_SRCLINE)
 
-      !Loop to test delaying each of the processors in turn
+      ! Loop to test delaying each of the processors in turn
       ! Preset loop_rc to ESMF_SUCCESS
       loop_rc=ESMF_SUCCESS 
       do i=0,petCount-1
-        delay_time=0.
-        delay_time(i)=1.
-        !t_a=starting time in each processor
-        call ESMF_VMWTime(t_a)
+        call ESMF_VMWTime(t_a)  ! t_a is start time for each PET
 
+        ! double barrier construct
+        call ESMF_VMBarrier(vm, rc)
+        if (localPet==i) then
+          ! delay PET i by delay_time
+          dt = delay_time + 4*dt_prec ! 4*dt_prec compensates for 4x taking time
+          call ESMF_VMWTimeDelay(dt)
+        endif
         call ESMF_VMBarrier(vm, rc)
 
-        !Delay each process for dt=delay_time seconds.
-        call ESMF_VMWTime(t0)
-        t1 = t0
-        dt = delay_time(localPet)
+        call ESMF_VMWTime(t_b)  ! t_b is end time for each PET
 
-        Do While (t1-t0 < dt) 
-          call ESMF_VMWTime(t1)
-        End do
-
-        call ESMF_VMBarrier(vm, rc)
-
-        !t_b=ending time at each processor
-        call ESMF_VMWTime(t_b)
-
-        max_time = 1.
-      !----------------------------------------------------------------
-      ! Test Barrier method 
-        write(failMsg, *) "Barrier did not hold. Slow PET=", i
-        write(name, *) "Barrier Test"
-
-        if ((t_b - t_a) < (max_time - dt_prec)) then
+        ! test that measured delay is at least as long as delay_time
+        delay = t_b - t_a
+        if (delay < delay_time) then
           loop_rc=ESMF_FAILURE
         end if
 
-
-           delay= t_b - t_a
-           print*, 'slow Pet=', i,'Pet=', localPet,'time delay = ', &
-         &         delay,'[  -- should be >= ',max_time-dt_prec,'--  ]'
+        print*, 'slow Pet=', i,'Pet=', localPet,'time delay = ', &
+          delay,'[  -- should be >= ', delay_time,'--  ]'
       end do   !Loop over i -- "slow" Pet's
 
+      !----------------------------------------------------------------
+      ! Test Barrier method 
       !NEX_UTest
       !Verify loop test results
+      write(name, *) "Barrier Test"
+      write(failMsg, *) "Barrier did not hold!"
       call ESMF_Test((loop_rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
       call ESMF_TestEnd(result, ESMF_SRCLINE)
