c78fb3e00fbc9843c57b473354bf91cf47b8ca90
Robert Oehmke <oehmke@ucar.edu>
2006-11-18 00:28:35 +0000

    Adding Init Macro tests.

---
+++ src/Infrastructure/Util/tests/ESMF_InitMacrosTestTypes.F90
@@ -0,0 +1,124 @@
+! $Id: ESMF_InitMacrosTestTypes.F90,v 1.1 2006/11/18 00:28:35 oehmke Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2003, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+!     ESMF InitMacro Module
+      module ESMF_InitMacrosTestTypesMod
+
+
+!
+!==============================================================================
+!
+! This file contains funtions to support the Initialization Macros
+!
+!------------------------------------------------------------------------------
+! INCLUDES
+#include "ESMF.h"
+
+!------------------------------------------------------------------------------
+!
+! !USES:
+  use ESMF_UtilTypesMod
+
+  ! make contents of module private
+  private
+
+!!!!!!!!!!!!!  Test Types !!!!!!!!!!!!!!!!!!!!
+
+!  Example Shallow Type
+   type ESMF_Shallow
+
+#ifdef ESMF_NO_INITIALIZERS    
+    integer :: num
+#else
+    integer :: num=4
+#endif
+
+    ESMF_INIT_DECLARE
+   end type ESMF_Shallow
+
+
+!  Example Deep Type
+   type ESMF_Deep
+    private
+    integer :: num
+
+    ESMF_INIT_DECLARE
+   end type ESMF_Deep
+
+!--------------------------------------------------------------------------
+!
+! PUBLIC TYPES:
+   public ESMF_Shallow
+   public ESMF_Deep
+
+
+!--------------------------------------------------------------------------
+!
+! !PUBLIC MEMBER FUNCTIONS:
+
+   ! Function for getting init value from shallow type
+   public ESMF_ShallowGetInitVal, ESMF_ShallowInit
+   public ESMF_DeepGetInitVal, ESMF_DeepCreate, ESMF_DeepDestroy
+
+contains
+
+   ! Function for getting init value from shallow type
+   function ESMF_ShallowGetInitVal(s)
+      type(ESMF_Shallow) :: s
+      integer (ESMF_KIND_I8) :: ESMF_ShallowGetInitVal 
+       
+       ESMF_ShallowGetInitVal = s%isInit 
+
+   end function ESMF_ShallowGetInitVal
+
+   ! Initialization routine for shallow type
+   subroutine ESMF_ShallowInit(s)
+      type(ESMF_Shallow), intent(inout) :: s
+       
+       s%num=4
+
+       ESMF_INIT_SET_DEFINED(s)
+   end subroutine ESMF_ShallowInit
+
+
+
+
+   ! Function for getting init value from deep type
+   function ESMF_DeepGetInitVal(s)
+      type(ESMF_Deep) :: s
+      integer (ESMF_KIND_I8) :: ESMF_DeepGetInitVal 
+       
+       ESMF_DeepGetInitVal = s%isInit 
+
+   end function ESMF_DeepGetInitVal
+
+
+   ! Deep type constructor function 
+   function ESMF_DeepCreate()
+       type(ESMF_Deep) :: ESMF_DeepCreate
+
+       ESMF_DeepCreate%num=7
+
+       ESMF_INIT_SET_CREATED(ESMF_DeepCreate)
+   end function ESMF_DeepCreate
+
+
+   ! Deep type destructor subroutine
+   subroutine ESMF_DeepDestroy(d)
+      type(ESMF_Deep), intent(inout) :: d
+       
+       ESMF_INIT_SET_DELETED(d)
+   end subroutine ESMF_DeepDestroy
+
+end module ESMF_InitMacrosTestTypesMod
+
+
---
+++ src/Infrastructure/Util/tests/ESMF_InitMacrosUTest.F90
@@ -0,0 +1,132 @@
+! $Id: ESMF_InitMacrosUTest.F90,v 1.1 2006/11/18 00:28:35 oehmke Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2005, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the GPL.
+!
+!==============================================================================
+!
+#define ESMF_FILENAME "ESMF_InitMacrosUTest.F90"
+
+
+      program ESMF_InitMacrosTest
+
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF.h>
+!
+#undef  ESMF_METHOD
+#define ESMF_METHOD "ESMF_InitMacrosUTest"
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_InitMacrosTest - tests the init macros
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 Util unit tests.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod     ! test methods
+      use ESMF_Mod         ! the ESMF Framework
+      use ESMF_InitMacrosTestTypesMod
+      use ESMF_InitMacrosMod
+      implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_InitMacrosUTest.F90,v 1.1 2006/11/18 00:28:35 oehmke Exp $'
+!------------------------------------------------------------------------------
+
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+
+      ! individual test result code
+      integer :: rc
+
+      ! individual test name
+      character(ESMF_MAXSTR) :: name
+
+      ! individual test failure messages
+      character(ESMF_MAXSTR*2) :: failMsg
+
+      ! local variables needed to pass into function/subroutine calls
+      !character(ESMF_MAXSTR) :: validate_options
+      !character(ESMF_MAXSTR) :: print_options
+      !type(ESMF_UtilConfig) :: config_set
+      !type(ESMF_UtilConfig) :: config_get
+      !character(ESMF_MAXSTR) :: name_set, name_get
+
+      type(ESMF_Shallow) :: s
+      type(ESMF_Deep) :: d
+
+
+!-------------------------------------------------------------------------------
+!  The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!  always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!  the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!  to OFF, then only the sanity unit tests.
+!  Special strings (Non-exhaustive and exhaustive) have been
+!  added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
+
+      call ESMF_TestStart(ESMF_SRCLINE, rc=rc)
+
+      ! add tests here
+
+      ! Shallow type test
+      ESMF_INIT_CHECK_SHALLOW(ESMF_Shallow,s)
+      write(name,*) "Testing ESMF_INIT_CHECK_SHALLOW"
+      write(failMsg,*) "Did not initialize value"
+      call ESMF_Test((s%num .eq. 4), name, failMsg, result, ESMF_SRCLINE)
+
+
+
+      ! Deep type tests     
+      !! Test Undefined Check
+      call DeepTest(d,rc)
+      write(name,*) "Testing undefined object in ESMF_INIT_CHECK_DEEP"
+      write(failMsg,*) "Did not recognize undefined state"
+      call ESMF_Test((rc .eq. ESMF_RC_OBJ_NOT_CREATED),name,failMsg, &
+                 result,ESMF_SRCLINE)
+
+      !! Test Created Check
+      d=ESMF_DeepCreate()
+      call DeepTest(d,rc)
+      write(name,*) "Testing created object in ESMF_INIT_CHECK_DEEP"
+      write(failMsg,*) "Did not recognize created state"
+      call ESMF_Test((rc .eq. ESMF_SUCCESS),name,failMsg, &
+                     result,ESMF_SRCLINE)         
+
+      !! Test Deleted Check
+      call ESMF_DeepDestroy(d)
+      call DeepTest(d,rc)
+      write(name,*) "Testing deleted object in ESMF_INIT_CHECK_DEEP"
+      write(failMsg,*) "Did not recognize deleted state"
+      call ESMF_Test((rc .eq. ESMF_RC_OBJ_DELETED),name,failMsg, &
+                result,ESMF_SRCLINE)
+
+#ifdef ESMF_EXHAUSTIVE
+
+
+#endif
+
+      ! This calls finalize before returning, so it must be the last
+      ! ESMF-related thing the test does.
+      call ESMF_TestEnd(result, ESMF_SRCLINE)
+
+      contains 
+         subroutine DeepTest(d,rc)
+           type(ESMF_Deep), intent(in) :: d
+           integer, intent(inout) :: rc
+
+            ESMF_INIT_CHECK_DEEP(ESMF_Deep,d,rc)
+   
+         end subroutine DeepTest
+ 
+      end program ESMF_InitMacrosTest
