fbe1c87c4dbaba74037a4f6bdb02d11ce3489ea2
Robert Oehmke <oehmke@ucar.edu>
2006-11-26 06:02:28 +0000

    Added more concise error handling for Init Macros to solve 132 character limit on SGI.

--- src/Infrastructure/Util/src/ESMF_InitMacros.F90
+++ src/Infrastructure/Util/src/ESMF_InitMacros.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_InitMacros.F90,v 1.4 2006/11/22 06:34:52 oehmke Exp $
+! $Id: ESMF_InitMacros.F90,v 1.5 2006/11/26 06:02:28 oehmke Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2008, University Corporation for Atmospheric Research,
@@ -28,14 +28,17 @@
 ! !USES:
     ! inherit from ESMF base class
     use ESMF_UtilTypesMod
-
+    use ESMF_LogErrMod
 
 
 ! !PUBLIC MEMBER FUNCTIONS:
-   public ESMF_InitCheckDeep
+   public ESMF_InitCheckDeep,ESMF_IMErr
 
 
 contains
+
+
+
 !--------------------------------------------------------------------------
 !BOP
 ! !IROUTINE: ESMF_InitCheckDeep - Translate isInit value to return code
@@ -74,6 +77,63 @@ contains
     endif	
 
 end function ESMF_InitCheckDeep
+
+!--------------------------------------------------------------------------
+!BOP
+! !IROUTINE: ESMF_IMErr - Init Macros Error Handling
+
+! !INTERFACE: 
+	function ESMF_IMErr(rcToCheck, line, file, method, &
+                                       rc)
+!
+! !RETURN VALUE:
+	logical                                         ::ESMF_IMErr
+! !ARGUMENTS:
+!	
+	integer, intent(in)                             :: rcToCheck
+	integer, intent(in), optional                   :: line
+	character(len=*), intent(in), optional          :: file
+	character(len=*), intent(in), optional	        :: method
+	integer, intent(out),optional                   :: rc
+	
+
+! !DESCRIPTION:
+!      This function returns a logical true for ESMF return codes that indicate
+!      an error.  A predefined error message will added to the {\tt ESMF\_Log} 
+!      along with
+!      a user added {\tt line}, {\tt file} and {\tt method}.  
+!      Additionally, {\tt rcToReturn} will be set to {\tt rcToCheck}.
+!
+!      The arguments are:
+!      \begin{description}
+! 	
+!      \item [rcToCheck]
+!            Return code to check.
+!      \item [{[line]}]
+!            Integer source line number.  Expected to be set by
+!            using the preprocessor macro {\tt \_\_LINE\_\_} macro.
+!      \item [{[file]}]
+!            User-provided source file name. 
+!      \item [{[method]}]
+!            User-provided method string.
+!      \item [{[rc]}]
+!            If specified, copy the {\tt rcToCheck} value to {\tt rc}.
+!            This is not the return code for this function; it allows
+!            the calling code to do an assignment of the error code
+!            at the same time it is testing the value.
+!            of the default Log.
+!      
+!      \end{description}
+! 
+!EOP
+	
+
+    ESMF_IMErr=ESMF_LogMsgFoundError(rcToCheck,"Bad Object", &
+                                   line, file, method, &
+                                   rc)
+       
+end function ESMF_IMErr
+
 
 
 end module ESMF_InitMacrosMod
