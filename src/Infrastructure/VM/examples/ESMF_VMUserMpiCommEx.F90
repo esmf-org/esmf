fa2677a3f0ba927ce147a04f7f26a36c7c920c93
Gerhard Theurich <theurich@sourcespring.net>
2006-11-29 22:40:13 +0000

    initial revision

---
+++ src/Infrastructure/VM/examples/ESMF_VMUserMpiCommEx.F90
@@ -0,0 +1,76 @@
+! $Id: ESMF_VMUserMpiCommEx.F90,v 1.1 2006/11/29 22:40:13 theurich Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2008, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
+
+!==============================================================================
+!EXAMPLE        String used by test script to count examples.
+!==============================================================================
+
+!------------------------------------------------------------------------------
+!BOE
+!
+! \subsubsection{ESMF inside user defined MPI communicator}
+!
+! The following example code demonstrates how ESMF can run inside of a 
+! user defined MPI communicator.
+!
+!EOE
+!------------------------------------------------------------------------------
+
+program ESMF_VMUserMpiCommEx
+
+  use ESMF_Mod
+  
+  implicit none
+#ifndef ESMF_MPIUNI     
+  include 'mpif.h'
+#endif
+  
+  ! local variables
+  integer:: rc, ierr
+  integer:: esmfComm, rank
+  ! result code
+  integer :: finalrc
+  finalrc = ESMF_SUCCESS
+  ! user code initializes MPI
+#ifndef ESMF_MPIUNI     
+  call MPI_Init(ierr)
+  if (ierr/=0) finalrc = ESMF_FAILURE
+  call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+  if (ierr/=0) finalrc = ESMF_FAILURE
+  if (rank < 2) then
+    call MPI_COMM_SPLIT(MPI_COMM_WORLD, 0, 0, esmfComm, ierr)
+  else
+    call MPI_COMM_SPLIT(MPI_COMM_WORLD, 1, 0, esmfComm, ierr)
+  endif
+#endif
+  ! user code initializes ESMF
+  if (rank < 2) then
+    call ESMF_Initialize(mpiCommunicator=esmfComm, rc=rc)
+    if (rc/=ESMF_SUCCESS) finalrc = ESMF_FAILURE
+    ! user code only to execute on the local MPI communicator
+    print *, "ESMF application on MPI rank:", rank
+    ! user code finalizes ESMF
+    call ESMF_Finalize(terminationflag=ESMF_KEEPMPI, rc=rc)
+    if (rc/=ESMF_SUCCESS) finalrc = ESMF_FAILURE
+    ! user code finalizes MPI
+  endif
+#ifndef ESMF_MPIUNI     
+  call MPI_Finalize(ierr)
+  if (ierr/=0) finalrc = ESMF_FAILURE
+#endif
+  ! print result
+  if (finalrc==ESMF_SUCCESS) then
+    print *, "PASS: ESMF_VMUserMpiCommEx.F90"
+  else
+    print *, "FAIL: ESMF_VMUserMpiCommEx.F90"
+  endif
+end program
