e997b2fa4575ead666baaa19ea354073a902492d
Rosalinda de Fainchtein <rosalinda.fainchtein@comcast.net>
2007-01-12 21:32:49 +0000

    Added unit test for the new macro created routines in ESMF_FortranWordsize.F90

---
+++ src/Infrastructure/Util/tests/ESMF_FortranWordsizeUTest.F90
@@ -0,0 +1,134 @@
+! $Id: ESMF_FortranWordsizeUTest.F90,v 1.1 2007/01/12 21:32:49 rosalind Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2006, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+
+
+    program ESMF_FortranWordsizeUTest
+
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF.h>
+!
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_FortranWordsizeUTest - One line general statement about this test
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 FortranWordsize unit tests.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod         ! test methods
+      use ESMF_Mod             ! the ESMF Framework
+      use ESMF_FortranWordsizeMod     !  the ESMF_Wordsize function.
+
+    implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_FortranWordsizeUTest.F90,v 1.1 2007/01/12 21:32:49 rosalind Exp $'
+!------------------------------------------------------------------------------
+
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+
+      ! individual test result code
+      integer :: rc
+
+      ! individual test failure message
+      character(ESMF_MAXSTR) :: failMsg
+      character(ESMF_MAXSTR) :: name
+      character(len=8) :: strvalue
+
+      ! local variables
+      integer :: vint
+      integer(ESMF_KIND_I1) :: vint1(5)
+      integer(ESMF_KIND_I2) :: vint2
+      integer(ESMF_KIND_I4) :: vint4
+      integer(ESMF_KIND_I8) :: vint8
+
+      real (ESMF_KIND_R4) :: vreal4
+      real (ESMF_KIND_R8) :: vreal8
+      integer :: ESMF_FortranWordsize, datasize
+
+!-------------------------------------------------------------------------------
+!  The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!  always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!  the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!  to OFF, then only the sanity unit tests.
+!  Special strings (Non-exhaustive and exhaustive) have been
+!  added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
+
+      call ESMF_TestStart(ESMF_SRCLINE, rc=rc)
+
+!Test Integer Data Sizes
+!=======================
+    !NEX_UTest
+    ! Default integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of default integer"
+    datasize=ESMF_FortranWordsize(vint, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_I1 Integer size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_I1 integer"
+    datasize=ESMF_FortranWordsize(vint1(1), rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    !ESMF_KIND_I2 Integer size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_I2 integer"
+    datasize=ESMF_FortranWordsize(vint2, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    !ESMF_KIND_I4 Integer size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_I4 integer"
+    datasize=ESMF_FortranWordsize(vint4, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    !ESMF_KIND_I8 Integer size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_I8 integer"
+    datasize=ESMF_FortranWordsize(vint8, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    !ESMF_KIND_R4 Real size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_4 real"
+    datasize=ESMF_FortranWordsize(vreal4, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    !ESMF_KIND_R8 Real size
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting size of ESMF_KIND_R8 real"
+    datasize=ESMF_FortranWordsize(vreal8, rc )
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    call ESMF_TestEnd(result, ESMF_SRCLINE)
+
+    end program ESMF_FortranWordsizeUTest
+
