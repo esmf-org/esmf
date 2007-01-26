50f6389628d833ac519e66875d112668df1055b6
Gerhard Theurich <theurich@sourcespring.net>
2007-01-26 18:33:54 +0000

    initial revision

---
+++ build_config/Linux.absoftintel.default/ESMC_Conf.h
@@ -0,0 +1,38 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1 2007/01/26 18:33:54 theurich Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2008, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the University of Illinois-NCSA License.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_linux
+
+#define FTN(func) func##_
+
+#define ESMC_PRESENT(arg) ( (arg) != 0 )
+
+#if S32
+#define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 36
+#define ESMF_F90_PTR_PLUS_RANK 12
+#define ESMC_POINTER_SIZE 4
+#endif
+#if S64
+#define ESMF_IS_64BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE xx
+#define ESMF_F90_PTR_PLUS_RANK xx
+#define ESMC_POINTER_SIZE 8
+#endif
+
+#endif
---
+++ build_config/Linux.absoftintel.default/build_rules.mk
@@ -0,0 +1,134 @@
+# $Id: build_rules.mk,v 1.1 2007/01/26 18:33:54 theurich Exp $
+#
+# Linux.absoftintel.default
+#
+
+############################################################
+# Default compiler setting.
+#
+ESMF_F90DEFAULT         = f90
+ESMF_CXXDEFAULT         = icpc
+
+############################################################
+# Default MPI setting.
+#
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90COMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),mpich)
+# Mpich ----------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += 
+ESMF_CXXDEFAULT         = mpiCC
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPICH
+ESMF_MPIRUNDEFAULT      = mpirun
+else
+ifeq ($(ESMF_COMM),mpich2)
+# Mpich2 ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),lam)
+# LAM (assumed to be built with absoft f90) ----------------
+ESMF_F90DEFAULT         = mpif77
+ESMF_CXXDEFAULT         = mpic++
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),openmpi)
+# OpenMPI --------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+endif
+endif
+endif
+
+############################################################
+# Print compiler version string
+#
+ESMF_F90COMPILER_VERSION    = ${ESMF_F90COMPILER} -v
+ESMF_CXXCOMPILER_VERSION    = ${ESMF_CXXCOMPILER} -V -v
+
+############################################################
+# How to specify module directories
+#
+ESMF_F90IMOD        = -p
+
+############################################################
+# Force Fortran symbols lower case
+#
+ESMF_F90COMPILEOPTS += -YEXT_NAMES=LCS -YEXT_SFX=_
+
+############################################################
+# To compile with Intel's icpc but link with GCC's stdc++ lib
+# set ESMF_STDCXX_LIBRARY to gcc before building
+#
+ifeq ($(ESMF_STDCXX_LIBRARY),gcc)
+ESMF_F90LINKPATHS   += -L$(dir $(shell gcc -print-file-name=libstdc++.so))
+ESMF_F90LINKLIBS    += -lstdc++
+ESMF_CXXCOMPILEOPTS += -cxxlib-gcc
+ESMF_CXXLINKOPTS    += -cxxlib-gcc
+else
+ESMF_F90LINKLIBS    += -lcprts
+ESMF_CXXCOMPILEOPTS += -cxxlib-icc
+ESMF_CXXLINKOPTS    += -cxxlib-icc
+endif
+
+############################################################
+# Conditionally add pthread compiler and linker flags
+#
+ifeq ($(ESMF_PTHREADS),ON)
+ESMF_CXXCOMPILEOPTS +=  -pthread
+ESMF_CXXLINKOPTS    += -pthread
+endif
+
+############################################################
+# Need this until the file convention is fixed (then remove these two lines)
+#
+ESMF_F90COMPILEFREENOCPP = -ffree
+ESMF_F90COMPILEFIXCPP    = -ffixed
+
+############################################################
+# Determine where absoft f90's libraries are located
+#
+ESMF_CXXLINKPATHS += $(addprefix -L,$(shell $(ESMF_DIR)/scripts/libpath.absoft $(ESMF_F90COMPILER)))
+ESMF_RPATHPREFIXFIXED := $(ESMF_RPATHPREFIX)
+ESMF_CXXLINKRPATHS += $(addprefix $(ESMF_RPATHPREFIXFIXED), $(shell $(ESMF_DIR)/scripts/libpath.absoft $(ESMF_F90COMPILER)))
+
+############################################################
+# Link against libesmf.a using the F90 linker front-end
+#
+ESMF_F90LINKLIBS += -lrt
+
+############################################################
+# Link against libesmf.a using the C++ linker front-end
+#
+ESMF_CXXLINKLIBS += -lrt -ldl $(shell $(ESMF_DIR)/scripts/libs.absoft $(ESMF_F90COMPILER))
+
+############################################################
+# Blank out shared library options
+#
+ESMF_SL_LIBS_TO_MAKE  =
