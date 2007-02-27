a525a9b2d458f63ddbc310a9b317acff10fca302
Rosalinda de Fainchtein <rosalinda.fainchtein@comcast.net>
2007-02-27 20:50:47 +0000

    Added unit tests for ESMF_TypeKindGet.

---
+++ src/Infrastructure/Util/tests/ESMF_TypeKindGetUTest.F90
@@ -0,0 +1,190 @@
+! $Id: ESMF_TypeKindGetUTest.F90,v 1.1 2007/02/27 20:50:47 rosalind Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2006, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+
+    program ESMF_TypeKindGetUTest
+
+!------------------------------------------------------------------------------
+! INCLUDES
+#include <ESMF.h>
+!
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_TypeKindGetUTest - One line general statement about this test
+!
+! !DESCRIPTION:
+!
+! The code in this file drives F90 TypeKindGet unit tests.
+!
+!-----------------------------------------------------------------------------
+! !USES:
+      use ESMF_TestMod         ! test methods
+      use ESMF_Mod             ! the ESMF Framework
+      use ESMF_TypeKindGetMod     !  the ESMF_TypeKindGet function.
+
+    implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+      character(*), parameter :: version = &
+      '$Id: ESMF_TypeKindGetUTest.F90,v 1.1 2007/02/27 20:50:47 rosalind Exp $'
+!------------------------------------------------------------------------------
+
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+
+      ! individual test result code
+      integer :: rc
+
+      ! individual test failure message
+      character(ESMF_MAXSTR) :: failMsg
+      character(ESMF_MAXSTR) :: name
+      character(len=8) :: strvalue
+
+    ! local variables
+    integer :: vint
+#ifndef ESMF_NO_INTEGER_1_BYTE
+    integer(ESMF_KIND_I1) :: vint1(5)
+#endif
+#ifndef ESMF_NO_INTEGER_2_BYTE
+    integer(ESMF_KIND_I2) :: vint2
+#endif
+    integer(ESMF_KIND_I4) :: vint4
+    integer(ESMF_KIND_I8) :: vint8
+
+    real (ESMF_KIND_R4) :: vreal4
+    real (ESMF_KIND_R8) :: vreal8
+    type(ESMF_TypeKind) :: typekind
+
+!-------------------------------------------------------------------------------
+!  The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
+!  always run. When the environment variable, EXHAUSTIVE, is set to ON then
+!  the EXHAUSTIVE and sanity tests both run. If the EXHAUSTIVE variable is set
+!  to OFF, then only the sanity unit tests.
+!  Special strings (Non-exhaustive and exhaustive) have been
+!  added to allow a script to count the number and types of unit tests.
+!-------------------------------------------------------------------------------
+
+      call ESMF_TestStart(ESMF_SRCLINE, rc=rc)
+
+!Test Integer Data TypeKind Values
+!=================================
+    !NEX_UTest
+    ! Default integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) " Getting ESMF_TypeKind parameter of default integer"
+    typekind=ESMF_TypeKindGet(vint, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of default integer is", typekind
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_I1 integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) " Getting ESMF_TypeKind parameter of ESMF_KIND_I1 integer"
+    typekind=ESMF_TypeKindGet(vint1(1), rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_I1 integer is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_I1%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_I1 (=', &
+                 ESMF_TYPEKIND_I1,')'
+     end if
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_I2 integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) " Getting ESMF_TypeKind parameter of ESMF_KIND_I2 integer"
+    typekind=ESMF_TypeKindGet(vint2, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_I2 integer is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_I2%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_I2 (=', &
+                 ESMF_TYPEKIND_I2,')'
+     end if
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_I4 integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) " Getting ESMF_TypeKind parameter of ESMF_KIND_I4 integer"
+    typekind=ESMF_TypeKindGet(vint4, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_I4 integer is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_I4%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_I4 (=', &
+                 ESMF_TYPEKIND_I4,')'
+     end if
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_I8 integer
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting ESMF_TypeKind parameter of ESMF_KIND_I8 integer"
+    typekind=ESMF_TypeKindGet(vint8, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_I8 integer is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_I8%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_I8 (=', &
+                 ESMF_TYPEKIND_I8,')'
+     end if
+
+!Test Real Data TypeKind Values
+!=================================
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_R4 real
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting ESMF_TypeKind parameter of ESMF_KIND_R4 real"
+    typekind=ESMF_TypeKindGet(vreal4, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_R4 real is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_R4%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_R4 (=', &
+                 ESMF_TYPEKIND_R4,')'
+     end if
+
+    !------------------------------------------------------------------------
+    !NEX_UTest
+    ! ESMF_KIND_R8 real
+    write(failMsg, *) "Did not RETURN ESMF_SUCCESS"
+    write(name, *) "Getting ESMF_TypeKind parameter of ESMF_KIND_R8 real"
+    typekind=ESMF_TypeKindGet(vreal8, rc)
+    call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+    print *, "The typekind of ESMF_KIND_R8 real is", typekind
+
+    if(typekind%dkind /= ESMF_TYPEKIND_R8%dkind) then
+       print*,'ERROR: typekind (=',typekind, &
+              ') should equal ESMF_TYPEKIND_R8 (=', &
+                 ESMF_TYPEKIND_R8,')'
+     end if
+
+    call ESMF_TestEnd(result, ESMF_SRCLINE)
+
+    end program ESMF_TypeKindGetUTest
+
