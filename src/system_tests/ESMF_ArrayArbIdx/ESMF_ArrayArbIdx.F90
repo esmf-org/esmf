f8490c176d53b6fe6bede89d8198306e0835dd08
David Neckels <dneckels@ucar.edu>
2007-05-24 22:06:26 +0000

    Initial test driver for 1D user supplied indices

---
+++ src/system_tests/ESMF_ArrayArbIdx/ESMF_ArrayArbIdx.F90
@@ -0,0 +1,23 @@
+! $Id: ESMF_ArrayArbIdx.F90,v 1.1 2007/05/24 22:06:26 dneckels Exp $
+!
+!-------------------------------------------------------------------------
+!SYSTEM_TEST        String used by test script to count system tests.
+!=========================================================================
+
+!BOP
+!
+! !DESCRIPTION:
+! System test ArrayArbIdx
+!     Test the use of 1D arrays with 'user supplied' arbitrary indexing for
+!  the array entries.
+!
+!\begin{verbatim}
+
+program ArrayArbIdx
+
+#include <ESMF_Macros.inc>
+
+end program ArrayArbIdx
+    
+!\end{verbatim}
+    
