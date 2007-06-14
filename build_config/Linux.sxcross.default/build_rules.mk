8317a4f8af909cff680c01c8b054937aaee0b6b2
Gerhard Theurich <theurich@sourcespring.net>
2007-06-14 16:14:20 +0000

    clean up "mpi" section

--- build_config/Linux.sxcross.default/build_rules.mk
+++ build_config/Linux.sxcross.default/build_rules.mk
@@ -1,4 +1,4 @@
-# $Id: build_rules.mk,v 1.12 2007/06/12 22:24:44 svasquez Exp $
+# $Id: build_rules.mk,v 1.13 2007/06/14 16:14:20 theurich Exp $
 #
 # Linux.sxcross.default
 #
@@ -29,10 +29,7 @@ ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
 else
 ifeq ($(ESMF_COMM),mpi)
 # Vendor MPI -----------------------------------------------
-MPI_INCLUDE     = -DESMC_HAVE_INT_MPI_COMM
-MPIRUN          = ${ESMC_MPIRUN}
 ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/scripts/mpirun.sx
-MPIRUN      = ${ESMF_DIR}/scripts/mpirun.sx
 else
 ifeq ($(ESMF_COMM),user)
 # User specified flags -------------------------------------
