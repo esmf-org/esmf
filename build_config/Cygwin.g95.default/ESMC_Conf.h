89f14ceb0e2372a09aabc964a94288e5c3d2a635
Gerhard Theurich <theurich@sourcespring.net>
2007-06-27 18:09:19 +0000

    initial revision

---
+++ build_config/Cygwin.g95.default/ESMC_Conf.h
@@ -0,0 +1,73 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1 2007/06/27 18:09:19 theurich Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2006, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the University of Illinois-NCSA License.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_linux
+#define ESMF_ARCH_NAME "CYGWIN"
+
+#define ESMC_HAVE_LIMITS_H
+#define ESMC_HAVE_PWD_H
+#define ESMC_HAVE_MALLOC_H
+#define ESMC_HAVE_STRING_H
+#define ESMC_HAVE_GETDOMAINNAME
+#define ESMC_HAVE_DRAND48
+#define ESMC_HAVE_UNAME
+#define ESMC_HAVE_UNISTD_H
+#define ESMC_HAVE_SYS_TIME_H
+#define ESMC_HAVE_STDLIB_H
+
+#define FTN(func) func##__
+
+#define ESMC_HAVE_OMP_THREADS 1
+
+#define ESMC_HAVE_MPI 1
+
+#define ESMC_HAVE_READLINK
+#define ESMC_HAVE_MEMMOVE
+#define ESMC_HAVE_TEMPLATED_COMPLEX
+
+#define ESMC_HAVE_DOUBLE_ALIGN_MALLOC
+#define ESMC_HAVE_MEMALIGN
+#define ESMC_HAVE_SYS_RESOURCE_H
+#define ESMC_SIZEOF_VOIDP 4
+#define ESMC_SIZEOF_INT 4
+#define ESMC_SIZEOF_DOUBLE 8
+
+#if defined(fixedsobug)
+#define ESMC_USE_DYNAMIC_LIBRARIES 1
+#define ESMC_HAVE_RTLD_GLOBAL 1
+#endif
+
+#define ESMC_HAVE_SYS_UTSNAME_H
+
+#define ESMC_PRESENT(arg) ( (arg) != 0 )
+
+#if S32
+#define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 28
+#define ESMF_F90_PTR_PLUS_RANK 12
+#define ESMC_POINTER_SIZE 4
+#endif
+#if S64
+#define ESMF_IS_64BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 40
+#define ESMF_F90_PTR_PLUS_RANK 12
+#define ESMC_POINTER_SIZE 8
+#endif
+
+
+#endif
---
+++ build_config/Cygwin.g95.default/build_rules.mk
@@ -0,0 +1,132 @@
+# $Id: build_rules.mk,v 1.1 2007/06/27 18:09:19 theurich Exp $
+#
+# Linux.g95.default
+#
+
+############################################################
+# Default compiler setting.
+#
+ESMF_F90DEFAULT         = g95
+ESMF_CXXDEFAULT         = g++
+
+############################################################
+# Default MPI setting.
+#
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90COMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),mpich)
+# Mpich ----------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lpmpich -lmpich
+ESMF_CXXDEFAULT         = mpiCC
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPICH
+ESMF_MPIRUNDEFAULT      = mpirun
+else
+ifeq ($(ESMF_COMM),mpich2)
+# Mpich2 ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),lam)
+# LAM (assumed to be built with g95) -----------------------
+ESMF_F90DEFAULT         = mpif77
+ESMF_CXXDEFAULT         = mpic++
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),openmpi)
+# OpenMPI --------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lmpi_cxx
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+endif
+endif
+endif
+
+############################################################
+# Print compiler version string
+#
+ESMF_F90COMPILER_VERSION    = ${ESMF_F90COMPILER} -v --version
+ESMF_CXXCOMPILER_VERSION    = ${ESMF_CXXCOMPILER} -v --version
+
+############################################################
+# Cygwin 1.5.24 does not yet support POSIX IPC (memory mapped files)
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_POSIXIPC
+ 
+############################################################
+# Fortran symbol convention must match other libraries used
+#
+ESMF_F90COMPILEOPTS       += 
+ESMF_F90LINKOPTS          += 
+
+############################################################
+# On IA64 set long and pointer types to 64-bit
+#
+ifeq ($(ESMF_ABI),64)
+ESMF_CXXCOMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_CXXLINKOPTS          += -march=k8 -m64 -mcmodel=medium
+ESMF_F90COMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_F90LINKOPTS          += -march=k8 -m64 -mcmodel=medium
+endif
+
+############################################################
+# Need this until the file convention is fixed (then remove these two lines)
+#
+ESMF_F90COMPILEFREENOCPP = -ffree-form
+ESMF_F90COMPILEFIXCPP    = -cpp -ffixed-form
+
+############################################################
+# Determine where gcc's libraries are located
+#
+ESMF_F90LINKPATHS += \
+  -L$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.a))
+ESMF_F90LINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.a))
+
+############################################################
+# Determine where g95's libraries are located
+#
+ESMF_CXXLINKPATHS += \
+  -L$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libf95.a))
+ESMF_CXXLINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libf95.a))
+
+############################################################
+# Link against libesmf.a using the F90 linker front-end
+#
+ESMF_F90LINKLIBS += -lstdc++
+
+############################################################
+# Link against libesmf.a using the C++ linker front-end
+#
+ESMF_CXXLINKLIBS += -lf95
+
+############################################################
+# Blank out shared library options
+#
+ESMF_SL_LIBS_TO_MAKE  =
