481d122dd507a77f26d980829dde227f63ee4ceb
Gerhard Theurich <theurich@sourcespring.net>
2007-07-17 18:11:10 +0000

    initial revision

---
+++ build_config/Cygwin.gfortran.default/ESMC_Conf.h
@@ -0,0 +1,43 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1 2007/07/17 18:11:10 theurich Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2007, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the University of Illinois-NCSA License.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_cygwin
+
+#ifdef ESMF_LOWERCASE_SINGLEUNDERSCORE 
+#define FTN(func) func##_
+#endif
+#ifdef ESMF_LOWERCASE_DOUBLEUNDERSCORE 
+#define FTN(func) func##__
+#endif
+
+#define ESMC_PRESENT(arg) ( (arg) != 0 )
+
+#if S32
+#define ESMF_IS_32BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 24
+#define ESMF_F90_PTR_PLUS_RANK 12
+#define ESMC_POINTER_SIZE 4
+#endif
+#if S64
+#define ESMF_IS_64BIT_MACHINE 1
+#define ESMF_F90_PTR_BASE_SIZE 40
+#define ESMF_F90_PTR_PLUS_RANK 12
+#define ESMC_POINTER_SIZE 8
+#endif
+
+#endif
---
+++ build_config/Cygwin.gfortran.default/build_rules.mk
@@ -0,0 +1,149 @@
+# $Id: build_rules.mk,v 1.1 2007/07/17 18:11:11 theurich Exp $
+#
+# Cygwin.gfortran.default
+#
+
+############################################################
+# Default compiler setting.
+#
+ESMF_F90DEFAULT         = gfortran
+ESMF_CXXDEFAULT         = g++
+
+############################################################
+# Default MPI setting.
+#
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90COMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),mpich)
+# Mpich ----------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lpmpich -lmpich
+ESMF_CXXDEFAULT         = mpiCC
+ESMF_CXXCOMPILEOPTS    += -DESMF_MPICH
+ESMF_MPIRUNDEFAULT      = mpirun
+else
+ifeq ($(ESMF_COMM),mpich2)
+# Mpich2 ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),lam)
+# LAM (assumed to be built with gfortran) -----------------------
+ESMF_F90DEFAULT         = mpif77
+ESMF_CXXDEFAULT         = mpic++
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),openmpi)
+# OpenMPI --------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_F90LINKLIBS       += -lmpi_cxx
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+endif
+endif
+endif
+
+############################################################
+# Print compiler version string
+#
+ESMF_F90COMPILER_VERSION    = ${ESMF_F90COMPILER} -v --version
+ESMF_CXXCOMPILER_VERSION    = ${ESMF_CXXCOMPILER} -v --version
+
+############################################################
+# Cygwin 1.5.24 does not yet support POSIX IPC (memory mapped files)
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_POSIXIPC
+ 
+############################################################
+# Fortran symbol convention
+#
+ifeq ($(ESMF_FORTRANSYMBOLS),default)
+ESMF_F90COMPILEOPTS       += -fno-second-underscore
+ESMF_F90LINKOPTS          += -fno-second-underscore
+ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_SINGLEUNDERSCORE
+else
+ifeq ($(ESMF_FORTRANSYMBOLS),lowercase_singleunderscore)
+ESMF_F90COMPILEOPTS       += -fno-second-underscore
+ESMF_F90LINKOPTS          += -fno-second-underscore
+ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_SINGLEUNDERSCORE
+else
+ifeq ($(ESMF_FORTRANSYMBOLS),lowercase_doubleunderscore)
+ESMF_F90COMPILEOPTS       +=
+ESMF_F90LINKOPTS          +=
+ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_DOUBLEUNDERSCORE
+else
+$(error "ESMF_FORTRANSYMBOLS = $(ESMF_FORTRANSYMBOLS)" not supported by ESMF and/or this platform)
+endif
+endif
+endif
+
+############################################################
+# On IA64 set long and pointer types to 64-bit
+#
+ifeq ($(ESMF_ABI),64)
+ESMF_CXXCOMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_CXXLINKOPTS          += -march=k8 -m64 -mcmodel=medium
+ESMF_F90COMPILEOPTS       += -march=k8 -m64 -mcmodel=medium
+ESMF_F90LINKOPTS          += -march=k8 -m64 -mcmodel=medium
+endif
+
+############################################################
+# Need this until the file convention is fixed (then remove these two lines)
+#
+ESMF_F90COMPILEFREENOCPP = -ffree-form
+ESMF_F90COMPILEFIXCPP    = -cpp -ffixed-form
+
+############################################################
+# Determine where gcc's libraries are located
+#
+ESMF_F90LINKPATHS += \
+  -L$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.a))
+ESMF_F90LINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_CXXCOMPILER) -print-file-name=libstdc++.a))
+
+############################################################
+# Determine where gfortran's libraries are located
+#
+ESMF_CXXLINKPATHS += \
+  -L$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libgfortran.a))
+ESMF_CXXLINKRPATHS += \
+  -Wl,-rpath,$(dir $(shell $(ESMF_F90COMPILER) -print-file-name=libgfortran.a))
+
+############################################################
+# Link against libesmf.a using the F90 linker front-end
+#
+ESMF_F90LINKLIBS += -lstdc++
+
+############################################################
+# Link against libesmf.a using the C++ linker front-end
+#
+ESMF_CXXLINKLIBS += -lrt -lgfortran
+
+############################################################
+# Blank out shared library options
+#
+ESMF_SL_LIBS_TO_MAKE  =
