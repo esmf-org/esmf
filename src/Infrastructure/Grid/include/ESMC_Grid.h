244b215a67b576e04e40e2ed9591bced78ef03f1
Robert Oehmke <oehmke@ucar.edu>
2007-08-21 22:24:20 +0000

    Added Public Grid class header file.

---
+++ src/Infrastructure/Grid/include/ESMC_Grid.h
@@ -0,0 +1,58 @@
+// $Id: ESMC_Grid.h,v 1.11 2007/08/21 22:24:20 oehmke Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2007, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+
+// ESMC Grid class public include file
+//
+// (all lines below between the !BOP and !EOP markers will be included in 
+//  the automated document processing.)
+//-----------------------------------------------------------------------------
+//
+ // these lines prevent this file from being read more than once if it
+ // ends up being included multiple times
+
+#ifndef ESMC_Grid_H
+#define ESMC_Grid_H
+
+//-----------------------------------------------------------------------------
+//BOP
+// !CLASS:  ESMC_Grid - Public C interface to the Grid object
+//
+// !DESCRIPTION:
+//
+// The code in this file defines the C public Grid class and declares method 
+// signatures (prototypes).  The companion file ESMC\_Grid.C contains
+// the definitions (full code bodies) for the Grid methods.
+//
+//-----------------------------------------------------------------------------
+// 
+// !USES:
+#include "ESMCI_Grid.h"
+
+
+extern "C" {
+
+// class declaration type
+typedef struct {
+      ESMCI::Grid *grid;
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+} ESMC_Grid ; // end class ESMC_Grid
+
+
+// prototypes for the ESMC_Grid API
+
+
+}; // end extern "C"
+
+
+#endif  // ESMC_Grid_H
+
