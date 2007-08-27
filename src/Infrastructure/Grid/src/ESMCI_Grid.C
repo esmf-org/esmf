7a85418d54fe0530732de89a86275cf0241389fb
Robert Oehmke <oehmke@ucar.edu>
2007-08-27 20:59:12 +0000

    Testing something.

--- src/Infrastructure/Grid/src/ESMCI_Grid.C
+++ src/Infrastructure/Grid/src/ESMCI_Grid.C
@@ -1,4 +1,4 @@
-// $Id: ESMCI_Grid.C,v 1.18 2007/08/27 20:54:57 oehmke Exp $
+// $Id: ESMCI_Grid.C,v 1.19 2007/08/27 20:59:12 oehmke Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2007, University Corporation for Atmospheric Research, 
@@ -38,7 +38,7 @@
 //-----------------------------------------------------------------------------
 // leave the following line as-is; it will insert the cvs ident string
 // into the object file for tracking purposes.
-static const char *const version = "$Id: ESMCI_Grid.C,v 1.18 2007/08/27 20:54:57 oehmke Exp $";
+static const char *const version = "$Id: ESMCI_Grid.C,v 1.19 2007/08/27 20:59:12 oehmke Exp $";
 //-----------------------------------------------------------------------------
 
 #define VERBOSITY             (1)       // 0: off, 10: max
@@ -216,7 +216,7 @@ int Grid::allocCoordArray(
       }
     } else {
       for (int i=0; i<rank; i++) {
-	staggerLWidth[i] = 0; 
+        staggerLWidth[i] = 0; 
       }
     }
   } else {
@@ -253,7 +253,7 @@ int Grid::allocCoordArray(
       }
     } else {
       for (int i=0; i<rank; i++) {
-	staggerUWidth[i] = 0; 
+        staggerUWidth[i] = 0; 
       }
     }
   } else {
