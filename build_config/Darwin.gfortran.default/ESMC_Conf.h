e4523368e73669e304a19cb50b1dd319050531cc
Gerhard Theurich <theurich@sourcespring.net>
2007-10-19 21:31:54 +0000

    - adjust ESMF_F90_PTR_BASE_SIZE

--- build_config/Darwin.gfortran.default/ESMC_Conf.h
+++ build_config/Darwin.gfortran.default/ESMC_Conf.h
@@ -1,5 +1,5 @@
 #ifdef ESMC_RCS_HEADER
-"$Id: ESMC_Conf.h,v 1.6 2007/07/17 18:03:33 theurich Exp $"
+"$Id: ESMC_Conf.h,v 1.7 2007/10/19 21:31:54 theurich Exp $"
 "Defines the configuration for this machine"
 #endif
 
@@ -29,7 +29,7 @@ Licensed under the University of Illinois-NCSA License.
 
 #if S32
 #define ESMF_IS_32BIT_MACHINE 1
-#define ESMF_F90_PTR_BASE_SIZE 28
+#define ESMF_F90_PTR_BASE_SIZE 24
 #define ESMF_F90_PTR_PLUS_RANK 12
 #define ESMC_POINTER_SIZE 4
 #endif
