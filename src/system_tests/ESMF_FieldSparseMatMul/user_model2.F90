2f6ebaab7d4d756093652bcf37198b0350e92a32
Cecelia DeLuca <cecelia.deluca@noaa.gov>
2007-10-29 01:53:54 +0000

    add system test that creates fields, stores them in states, and extracts arrays from the fields in order to do a sparse mat mul.

---
+++ src/system_tests/ESMF_FieldSparseMatMul/user_model2.F90
@@ -0,0 +1,188 @@
+! $Id: user_model2.F90,v 1.1 2007/10/29 01:53:54 cdeluca Exp $
+!
+! Example/test code which shows User Component calls.
+
+!--------------------------------------------------------------------------------
+!--------------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User-supplied Component
+!
+!
+!\begin{verbatim}
+
+module user_model2
+
+  ! ESMF Framework module
+  use ESMF_Mod
+
+  implicit none
+    
+  public userm2_register
+        
+  contains
+
+!--------------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine userm2_register(comp, rc)
+    type(ESMF_GridComp), intent(inout) :: comp
+    integer, intent(out) :: rc
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Register starting"
+
+    ! Register the callback routines.
+
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, user_init, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, user_run, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "Registered Initialize, Run, and Finalize routines"
+
+#ifdef ESMF_TESTWITHTHREADS
+    ! The following call will turn on ESMF-threading (single threaded)
+    ! for this component. If you are using this file as a template for 
+    ! your own code development you probably don't want to include the 
+    ! following call unless you are interested in exploring ESMF's 
+    ! threading features.
+    call ESMF_GridCompSetVMMinThreads(comp, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+#endif
+
+    print *, "User Comp2 Register returning"
+    
+  end subroutine
+
+!--------------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   Initialization routine.
+ 
+  subroutine user_init(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp), intent(inout) :: comp
+    type(ESMF_State), intent(inout) :: importState, exportState
+    type(ESMF_Clock), intent(in) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_ArraySpec)  :: arrayspec
+    type(ESMF_Grid)       :: grid
+    type(ESMF_Field)      :: field
+    type(ESMF_VM)         :: vm
+    integer               :: petCount
+    real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)   ! matching F90 array pointer
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Init starting"
+
+    ! Determine petCount
+    call ESMF_GridCompGet(comp, vm=vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_VMGet(vm, petCount=petCount, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    
+    ! Create the destination Field and add it to the import State
+    call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    grid = ESMF_GridCreate(minIndex=(/1,1/), maxIndex=(/100,150/), &
+      regDecomp=(/1,petCount/), rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    array = ESMF_FieldCreate(grid=grid, arrayspec=arrayspec,
+      indexflag=ESMF_INDEX_GLOBAL, name="field data", rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_StateAddField(importState, field, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    print *, "User Comp2 Init returning"
+
+  end subroutine user_init
+
+
+!--------------------------------------------------------------------------------
+!   !  The Run routine where data is validated.
+!   !
+ 
+  subroutine user_run(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp), intent(inout) :: comp
+    type(ESMF_State), intent(inout) :: importState, exportState
+    type(ESMF_Clock), intent(in) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    real(ESMF_KIND_R8)    :: pi
+    type(ESMF_Field)      :: field
+    type(ESMF_Array)      :: array
+    real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)   ! matching F90 array pointer
+    integer               :: i, j
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Run starting"
+
+    pi = 3.14159d0
+
+    ! Get the destination Field from the import State
+    call ESMF_StateGetField(importState, "field data", field, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    ! Get the Array from the Field
+    call ESMF_FieldGet(field, array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Gain access to actual data via F90 array pointer
+    call ESMF_ArrayGet(array, farrayPtr=farrayPtr, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+      
+    ! Test Array in import state against exact solution
+    do j = lbound(farrayPtr, 2), ubound(farrayPtr, 2)
+      do i = lbound(farrayPtr, 1), ubound(farrayPtr, 1)
+        if (abs(farrayPtr(i,j) - (10.0d0 &
+          + 5.0d0 * sin(real(i,ESMF_KIND_R8)/100.d0*pi) &
+          + 2.0d0 * sin(real(j,ESMF_KIND_R8)/150.d0*pi))) > 1.d-8) then
+          rc=ESMF_FAILURE
+          return ! bail out
+        endif
+      enddo
+    enddo
+ 
+    print *, "User Comp2 Run returning"
+
+  end subroutine user_run
+
+
+!--------------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+  subroutine user_final(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp), intent(inout) :: comp
+    type(ESMF_State), intent(inout) :: importState, exportState
+    type(ESMF_Clock), intent(in) :: clock
+    integer, intent(out) :: rc
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Final starting"
+
+    print *, "User Comp2 Final returning"
+
+  end subroutine user_final
+
+end module user_model2
+!\end{verbatim}
