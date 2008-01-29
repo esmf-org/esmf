9f372823c97b71dd5e36ff1c5c95bf3858562e42
Gerhard Theurich <theurich@sourcespring.net>
2008-01-29 06:40:55 +0000

    - Correct x86_64_small settings

--- build_config/Linux.gfortran.default/ESMC_Conf.h
+++ build_config/Linux.gfortran.default/ESMC_Conf.h
@@ -1,5 +1,5 @@
 #ifdef ESMC_RCS_HEADER
-"$Id: ESMC_Conf.h,v 1.7.2.4 2008/01/28 17:33:47 theurich Exp $"
+"$Id: ESMC_Conf.h,v 1.7.2.5 2008/01/29 06:40:55 theurich Exp $"
 "Defines the configuration for this machine"
 #endif
 
@@ -54,9 +54,9 @@ typedef int ESMCI_FortranStrLenArg;
 #endif
 #if Sx86_64_small
 #define ESMF_IS_32BIT_MACHINE 1
-#define ESMF_F90_PTR_BASE_SIZE 24
-#define ESMF_F90_PTR_PLUS_RANK 12
-#define ESMC_POINTER_SIZE 4
+#define ESMF_F90_PTR_BASE_SIZE 48
+#define ESMF_F90_PTR_PLUS_RANK 24
+#define ESMC_POINTER_SIZE 8
 #endif
 #if Sx86_64_medium
 #define ESMF_IS_64BIT_MACHINE 1
