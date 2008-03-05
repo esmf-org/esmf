17d7006d7f4df2037ccb6dabe7ca26ddf592ca86
Gerhard Theurich <theurich@sourcespring.net>
2008-03-05 21:31:51 +0000

    - Revert back to strict counts == ubounds-lbounds+1 constraint in   LocalArrayAdjust()

--- src/Infrastructure/LocalArray/src/ESMC_LocalArray.C
+++ src/Infrastructure/LocalArray/src/ESMC_LocalArray.C
@@ -1,4 +1,4 @@
-// $Id: ESMC_LocalArray.C,v 1.27.2.3 2008/02/29 18:27:03 theurich Exp $
+// $Id: ESMC_LocalArray.C,v 1.27.2.4 2008/03/05 21:31:51 theurich Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2007, University Corporation for Atmospheric Research, 
@@ -45,7 +45,7 @@
 //-----------------------------------------------------------------------------
 // leave the following line as-is; it will insert the cvs ident string
 // into the object file for tracking purposes.
-static const char *const version = "$Id: ESMC_LocalArray.C,v 1.27.2.3 2008/02/29 18:27:03 theurich Exp $";
+static const char *const version = "$Id: ESMC_LocalArray.C,v 1.27.2.4 2008/03/05 21:31:51 theurich Exp $";
 //-----------------------------------------------------------------------------
 
 // prototypes for Fortran calls
@@ -640,7 +640,7 @@ int ESMC_LocalArray::ESMC_LocalArrayConstruct(
 
   // adjust the lbound and ubound members while checking counts
   for (int i=0; i<rank; i++){
-    if (larray->counts[i] < ubounds[i] - lbounds[i] + 1){
+    if (larray->counts[i] != ubounds[i] - lbounds[i] + 1){
       ESMC_LogDefault.ESMC_LogMsgFoundError(ESMC_RC_NOT_VALID,
         "- Mismatch of lbounds, ubounds and counts", rc);
       return NULL;
