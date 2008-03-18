ee525ce5bc9b7e81bc4074b682bd7bf7a05e2b71
Fei Liu <Fei.Liu@gmail.com>
2008-03-18 20:07:33 +0000

    improved test logging

--- src/Infrastructure/Bundle/tests/ESMF_BundleCreateGetUTest.F90
+++ src/Infrastructure/Bundle/tests/ESMF_BundleCreateGetUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_BundleCreateGetUTest.F90,v 1.1.2.1 2008/03/18 18:39:31 feiliu Exp $
+! $Id: ESMF_BundleCreateGetUTest.F90,v 1.1.2.2 2008/03/18 20:07:33 feiliu Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2007, University Corporation for Atmospheric Research,
@@ -76,33 +76,42 @@ contains
 
 #define ESMF_METHOD "ESMF_TESTS"
 
-    subroutine bundle_test1(finalrc)
+    subroutine bundle_test1(rc)
         type(ESMF_Bundle)   :: bundle
         type(ESMF_Grid)     :: grid
-        integer           :: rc, finalrc
+        integer             :: rc, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
         grid = ESMF_GridCreateShapeTile(minIndex=(/1,1/), maxIndex=(/10,20/), &
                                 gridEdgeLWidth=(/0,0/), gridEdgeUWidth=(/0,0/), &
-                                regDecomp=(/2,2/), name="landgrid", rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+                                regDecomp=(/2,2/), name="landgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        bundle = ESMF_BundleCreate(grid, 'mybundle', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        bundle = ESMF_BundleCreate(grid, 'mybundle', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
+
         call assemble_bundle(bundle, grid, rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         call retrieve_bundle_dataptr(bundle, rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine bundle_test1
 
-    subroutine assemble_bundle(bundle, grid, finalrc)
+    subroutine assemble_bundle(bundle, grid, rc)
 
         type(ESMF_Bundle)   :: bundle
         type(ESMF_Grid)     :: grid
-        integer, optional, intent(out)   :: finalrc
+        integer, optional, intent(out)   :: rc
 
         real(ESMF_KIND_R8), dimension(:,:), pointer :: farray1
         real(ESMF_KIND_R4), dimension(:,:), pointer :: farray2
@@ -110,12 +119,14 @@ contains
         type(ESMF_Field)    :: f1, f2, f3
         type(ESMF_DistGrid) :: distgrid
         type(ESMF_Array)  :: array8, array
-        integer           :: rc, i, j
+        integer           :: i, j, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
-        call ESMF_GridGet(grid, distgrid=distgrid, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_GridGet(grid, distgrid=distgrid, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         allocate(farray1(5,10))
         allocate(farray2(5,10))
@@ -129,23 +140,31 @@ contains
             enddo
         enddo
 
-        f1 = ESMF_FieldCreate(grid, farray1, name='field1', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        f1 = ESMF_FieldCreate(grid, farray1, name='field1', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleAddField(bundle, f1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleAddField(bundle, f1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        f2 = ESMF_FieldCreate(grid, farray2(:,:), name='field2', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        f2 = ESMF_FieldCreate(grid, farray2(:,:), name='field2', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleAddField(bundle, f2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleAddField(bundle, f2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine assemble_bundle
 
-    subroutine retrieve_bundle_dataptr(bundle, finalrc)
+    subroutine retrieve_bundle_dataptr(bundle, rc)
         type(ESMF_Bundle)   :: bundle
-        integer, optional   :: finalrc
+        integer, optional   :: rc
 
         real(ESMF_KIND_R8), dimension(:,:), pointer :: farray1
         real(ESMF_KIND_R4), dimension(:,:), pointer :: farray2
@@ -154,61 +173,80 @@ contains
         type(ESMF_Grid)     :: grid
         type(ESMF_DistGrid) :: distgrid
         type(ESMF_Array)  :: array8, array
-        integer           :: rc, fc, i, j
+        integer           :: fc, i, j, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
-        call ESMF_BundleGetField(bundle, 'field1', f1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGetField(bundle, 'field1', f1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_FieldGetDataPtr(f1, farray1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_FieldGetDataPtr(f1, farray1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleGetField(bundle, 'field2', f2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGetField(bundle, 'field2', f2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_FieldGetDataPtr(f2, farray2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_FieldGetDataPtr(f2, farray2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         do i = 1, 5
             do j = 1, 10
-                if( farray1(i, j) .ne. i + j * 2) finalrc = ESMF_FAILURE
-                if( farray2(i, j) .ne. i + j * 3) finalrc = ESMF_FAILURE
+                if( farray1(i, j) .ne. i + j * 2) rc = ESMF_FAILURE
+                if( farray2(i, j) .ne. i + j * 3) rc = ESMF_FAILURE
             enddo
         enddo
 
-        call ESMF_BundleGet(bundle, fieldcount=fc, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGet(bundle, fieldcount=fc, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine retrieve_bundle_dataptr
 
-    subroutine bundle_test2(finalrc)
+    subroutine bundle_test2(rc)
         type(ESMF_Bundle)   :: bundle
         type(ESMF_Grid)     :: grid
-        integer           :: rc, finalrc
+        integer             :: rc, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
         grid = ESMF_GridCreateShapeTile(minIndex=(/1,1/), maxIndex=(/10,20/), &
                                 gridEdgeLWidth=(/0,0/), gridEdgeUWidth=(/0,0/), &
-                                regDecomp=(/2,2/), name="landgrid", rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+                                regDecomp=(/2,2/), name="landgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        bundle = ESMF_BundleCreate(grid, 'mybundle', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        bundle = ESMF_BundleCreate(grid, 'mybundle', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
+
         call assemble_bundle2(bundle, grid, rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         call retrieve_bundle_dataptr2(bundle, rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine bundle_test2
 
-    subroutine assemble_bundle2(bundle, grid, finalrc)
+    subroutine assemble_bundle2(bundle, grid, rc)
 
         type(ESMF_Bundle)   :: bundle
         type(ESMF_Grid)     :: grid
-        integer, optional, intent(out)   :: finalrc
+        integer, optional, intent(out)   :: rc
 
         real(ESMF_KIND_R8), dimension(:,:), pointer :: farray1
         real(ESMF_KIND_R4), dimension(:,:), pointer :: farray2
@@ -216,12 +254,14 @@ contains
         type(ESMF_Field)    :: f1, f2, f3
         type(ESMF_DistGrid) :: distgrid
         type(ESMF_Array)  :: array8, array
-        integer           :: rc, i, j
+        integer           :: i, j, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
-        call ESMF_GridGet(grid, distgrid=distgrid, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_GridGet(grid, distgrid=distgrid, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         allocate(farray1(5,10))
         allocate(farray2(5,10))
@@ -235,29 +275,41 @@ contains
             enddo
         enddo
 
-        f1 = ESMF_FieldCreate(grid, farray1, name='field1', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        f1 = ESMF_FieldCreate(grid, farray1, name='field1', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleAddField(bundle, f1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleAddField(bundle, f1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        f2 = ESMF_FieldCreate(grid, farray2(:,:), name='field2', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        f2 = ESMF_FieldCreate(grid, farray2(:,:), name='field2', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleAddField(bundle, f2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleAddField(bundle, f2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        f3 = ESMF_FieldCreate(grid, farray3(3:7, 4:13), name='field3', rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        f3 = ESMF_FieldCreate(grid, farray3(3:7, 4:13), name='field3', rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleAddField(bundle, f3, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleAddField(bundle, f3, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine assemble_bundle2
 
-    subroutine retrieve_bundle_dataptr2(bundle, finalrc)
+    subroutine retrieve_bundle_dataptr2(bundle, rc)
         type(ESMF_Bundle)   :: bundle
-        integer, optional   :: finalrc
+        integer, optional   :: rc
 
         real(ESMF_KIND_R8), dimension(:,:), pointer :: farray1
         real(ESMF_KIND_R4), dimension(:,:), pointer :: farray2
@@ -266,38 +318,55 @@ contains
         type(ESMF_Grid)     :: grid
         type(ESMF_DistGrid) :: distgrid
         type(ESMF_Array)  :: array8, array
-        integer           :: rc, fc, i, j
+        integer           :: fc, i, j, localrc
 
-        finalrc = ESMF_SUCCESS
+        rc = ESMF_SUCCESS
 
-        call ESMF_BundleGetField(bundle, 'field1', f1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGetField(bundle, 'field1', f1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_FieldGetDataPtr(f1, farray1, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_FieldGetDataPtr(f1, farray1, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleGetField(bundle, 'field2', f2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGetField(bundle, 'field2', f2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_FieldGetDataPtr(f2, farray2, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_FieldGetDataPtr(f2, farray2, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleGetField(bundle, 'field3', f3, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGetField(bundle, 'field3', f3, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_FieldGetDataPtr(f3, farray3, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_FieldGetDataPtr(f3, farray3, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
         do i = 1, 5
             do j = 1, 10
-                if( farray1(i, j) .ne. i + j * 2) finalrc = ESMF_FAILURE
-                if( farray2(i, j) .ne. i + j * 3) finalrc = ESMF_FAILURE
-                if( farray3(i*2, j*2) .ne. i + j * 4) finalrc = ESMF_FAILURE
+                if( farray1(i, j) .ne. i + j * 2) localrc = ESMF_FAILURE
+                if( farray2(i, j) .ne. i + j * 3) localrc = ESMF_FAILURE
+                if( farray3(i*2, j*2) .ne. i + j * 4) localrc = ESMF_FAILURE
             enddo
         enddo
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
-        call ESMF_BundleGet(bundle, fieldcount=fc, rc=rc)
-        if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
+        call ESMF_BundleGet(bundle, fieldcount=fc, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+                ESMF_ERR_PASSTHRU, &
+                ESMF_CONTEXT, rc)) return
 
     end subroutine retrieve_bundle_dataptr2
 end program ESMF_BundleCreateGetUTest
