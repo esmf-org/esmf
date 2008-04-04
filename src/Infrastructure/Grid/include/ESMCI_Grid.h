3729f92a012191d30187dadca28ba501eeb6cca7
Fei Liu <Fei.Liu@gmail.com>
2008-04-04 20:54:10 +0000

    remove redundant const declaration from value semantic return type

--- src/Infrastructure/Grid/include/ESMCI_Grid.h
+++ src/Infrastructure/Grid/include/ESMCI_Grid.h
@@ -1,4 +1,4 @@
-// $Id: ESMCI_Grid.h,v 1.21.2.5 2008/04/01 05:32:31 oehmke Exp $
+// $Id: ESMCI_Grid.h,v 1.21.2.6 2008/04/04 20:54:10 feiliu Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2007, University Corporation for Atmospheric Research, 
@@ -163,14 +163,14 @@ class Grid : public ESMC_Base {    // inherits from ESMC_Base class
   // accessor methods
   // NOTE: For efficiencies sake the following functions don't error check
   //       so be sure to error check the grid or any input before using.  
-  const ESMC_GridStatus getStatus(void) const {return status;}
-  const int getDimCount(void) const {return dimCount;}
-  const int getDistDimCount(void) const {return distDimCount;}
-  const int getUndistDimCount(void) const {return undistDimCount;}
-  const int getTileCount(void) const {return distgrid->getPatchCount();}
-  const int getStaggerLocCount(void) const {return staggerLocCount;}
-  const ESMC_IndexFlag getIndexFlag(void) const {return indexflag;}
-  const ESMC_TypeKind getTypeKind(void) const {return typekind;}
+  ESMC_GridStatus getStatus(void) const {return status;}
+  int getDimCount(void) const {return dimCount;}
+  int getDistDimCount(void) const {return distDimCount;}
+  int getUndistDimCount(void) const {return undistDimCount;}
+  int getTileCount(void) const {return distgrid->getPatchCount();}
+  int getStaggerLocCount(void) const {return staggerLocCount;}
+  ESMC_IndexFlag getIndexFlag(void) const {return indexflag;}
+  ESMC_TypeKind getTypeKind(void) const {return typekind;}
   const DistGrid *getDistGrid(void) const {return distgrid;}
   const int *getDistgridToGridMap(void) const {return distgridToGridMap;}
   const int *getUndistLBound(void) const {return undistLBound;}
