3c481157a60be8dc7710380f44d81200c7c8bfba
Gerhard Theurich <theurich@sourcespring.net>
2008-04-24 18:04:39 +0000

    - Initial revision of ArrayBundleSparseMatMul STest

---
+++ src/system_tests/ESMF_ArrayBundleSparseMatMul/user_model1.F90
@@ -0,0 +1,213 @@
+! $Id: user_model1.F90,v 1.1.2.1 2008/04/24 18:04:40 theurich Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!
+! !DESCRIPTION:
+!  User-supplied Component, most recent interface revision.
+!
+!
+!\begin{verbatim}
+
+module user_model1
+
+  ! ESMF Framework module
+  use ESMF_Mod
+
+  implicit none
+    
+  public userm1_register
+        
+  contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine userm1_register(comp, rc)
+    type(ESMF_GridComp) :: comp
+    integer, intent(out) :: rc
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Register starting"
+
+    ! Register the callback routines.
+
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, user_init, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, user_run, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "Registered Initialize, Run, and Finalize routines"
+
+#ifdef ESMF_TESTWITHTHREADS
+    ! The following call will turn on ESMF-threading (single threaded)
+    ! for this component. If you are using this file as a template for 
+    ! your own code development you probably don't want to include the 
+    ! following call unless you are interested in exploring ESMF's 
+    ! threading features.
+    call ESMF_GridCompSetVMMinThreads(comp, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+#endif
+
+    print *, "User Comp1 Register returning"
+    
+  end subroutine
+
+!-------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   Initialization routine.
+ 
+    
+  subroutine user_init(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_ArraySpec)  :: arrayspec
+    type(ESMF_DistGrid)   :: distgrid
+    type(ESMF_Array)      :: array(2)
+    type(ESMF_ArrayBundle):: arraybundle
+    type(ESMF_VM)         :: vm
+    integer               :: petCount
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Init starting"
+
+    ! Determine petCount
+    call ESMF_GridCompGet(comp, vm=vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_VMGet(vm, petCount=petCount, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    
+    ! Create the source Array and add it to the export State
+    call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    distgrid = ESMF_DistGridCreate(minIndex=(/1,1/), maxIndex=(/100,150/), &
+      regDecomp=(/petCount,1/), rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    array(1) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+      indexflag=ESMF_INDEX_GLOBAL, name="array data 1", rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    array(2) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+      indexflag=ESMF_INDEX_GLOBAL, name="array data 2", rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    arraybundle = ESMF_ArrayBundleCreate(arrayList=array, name="srcAryBndl", &
+      rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_StateAddArrayBundle(exportState, arraybundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    print *, "User Comp1 Init returning"
+
+  end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is computed.
+!   !
+ 
+  subroutine user_run(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    real(ESMF_KIND_R8)    :: pi
+    type(ESMF_Array)      :: array(2)
+    type(ESMF_ArrayBundle):: arraybundle
+    real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)   ! matching F90 array pointer
+    integer               :: i, j
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Run starting"
+
+    pi = 3.14159d0
+
+    ! Get the source ArrayBundle from the export State
+    call ESMF_StateGetArrayBundle(exportState, "srcAryBndl", arraybundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get the source Arrays from the ArrayBundle
+    call ESMF_ArrayBundleGet(arraybundle, arrayList=array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Gain access to actual data via F90 array pointer
+    call ESMF_ArrayGet(array(1), localDe=0, farrayPtr=farrayPtr, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Fill source Array with data
+    do j = lbound(farrayPtr, 2), ubound(farrayPtr, 2)
+      do i = lbound(farrayPtr, 1), ubound(farrayPtr, 1)
+        farrayPtr(i,j) = 10.0d0 &
+          + 5.0d0 * sin(real(i,ESMF_KIND_R8)/100.d0*pi) &
+          + 2.0d0 * sin(real(j,ESMF_KIND_R8)/150.d0*pi)
+      enddo
+    enddo
+ 
+    print *, "User Comp1 Run returning"
+
+  end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+  subroutine user_final(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_DistGrid)   :: distgrid
+    type(ESMF_Array)      :: array(2)
+    type(ESMF_ArrayBundle):: arraybundle
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Final starting"
+
+    call ESMF_StateGetArrayBundle(exportState, "srcAryBndl", arraybundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayBundleGet(arraybundle, arrayList=array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayBundleDestroy(arraybundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayGet(array(1), distgrid=distgrid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayDestroy(array(1), rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayDestroy(array(2), rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_DistGridDestroy(distgrid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "User Comp1 Final returning"
+
+  end subroutine user_final
+
+
+end module user_model1
+    
+!\end{verbatim}
