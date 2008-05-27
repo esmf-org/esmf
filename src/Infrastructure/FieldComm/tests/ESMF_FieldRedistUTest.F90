f98b490119c961de0aed3dba3479db6cdcef2427
Fei Liu <Fei.Liu@gmail.com>
2008-05-27 20:03:17 +0000

    added 5d congruent Fields with halos tests; added 5d congruent Fields with srcToDstTransposeMap test and verified exclusive region data redistribution

--- src/Infrastructure/FieldComm/tests/ESMF_FieldRedistUTest.F90
+++ src/Infrastructure/FieldComm/tests/ESMF_FieldRedistUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldRedistUTest.F90,v 1.4 2008/05/27 17:28:41 theurich Exp $
+! $Id: ESMF_FieldRedistUTest.F90,v 1.5 2008/05/27 20:03:17 feiliu Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2008, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@ program ESMF_FieldRedistUTest
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
     character(*), parameter :: version = &
-    '$Id: ESMF_FieldRedistUTest.F90,v 1.4 2008/05/27 17:28:41 theurich Exp $'
+    '$Id: ESMF_FieldRedistUTest.F90,v 1.5 2008/05/27 20:03:17 feiliu Exp $'
 !------------------------------------------------------------------------------
 
     ! cumulative result: count failures; no failures equals "all pass"
@@ -72,7 +72,24 @@ program ESMF_FieldRedistUTest
         ! Scatter test
         call test_redist_3d(rc)
         write(failMsg, *) ""
-        write(name, *) "FieldRedist basic test 3d field"
+        write(name, *) "FieldRedist 3d fields with ungridded bounds and halos"
+        call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+        !------------------------------------------------------------------------
+        !EX_UTest_Multi_Proc_Only
+        ! Scatter test
+        call test_redist_5d(rc)
+        write(failMsg, *) ""
+        write(name, *) "FieldRedist congruent 5d fields with ungridded bounds and halos"
+        call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+        !------------------------------------------------------------------------
+        !EX_UTest_Multi_Proc_Only
+        ! Scatter test
+        call test_redist_5dt(rc)
+        write(failMsg, *) ""
+        write(name, *) "FieldRedist congruent 5d fields with ungridded bounds and halos " // &
+            " srcToDstTransposeMap"
         call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
 
 #endif
@@ -338,6 +355,296 @@ contains
 
         rc = ESMF_SUCCESS
     end subroutine test_redist_3d
+
+#undef ESMF_METHOD
+#define ESMF_METHOD "test_redist_5d"
+    subroutine test_redist_5d(rc)
+        integer, intent(out)                        :: rc
+
+        ! local arguments used to create field etc
+        type(ESMF_Field)                            :: srcField, dstField
+        type(ESMF_Grid)                             :: srcGrid, dstGrid
+        type(ESMF_DistGrid)                         :: srcDistgrid, dstDistgrid
+        type(ESMF_VM)                               :: vm
+        type(ESMF_RouteHandle)                      :: routehandle
+        type(ESMF_ArraySpec)                        :: arrayspec
+        integer                                     :: localrc, lpe
+        !integer                                     :: i, j, k, l, m
+        integer                                     :: exLB(5), exUB(5)
+
+        integer(ESMF_KIND_I4), pointer              :: srcfptr(:,:,:,:,:), dstfptr(:,:,:,:,:)
+        integer(ESMF_KIND_I4), pointer              :: fptr(:,:,:,:,:)
+
+        rc = ESMF_SUCCESS
+        localrc = ESMF_SUCCESS
+
+        call ESMF_VMGetCurrent(vm, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_VMGet(vm, localPet=lpe, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! create src and dst Grids which are congruent but different in decomposition
+        srcDistgrid = ESMF_DistGridCreate(minIndex=(/1,1,1/), maxIndex=(/4,8,4/), &
+            regDecomp=(/2,1,2/), rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        srcGrid = ESMF_GridCreate(distgrid=srcDistgrid, name="srcgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstDistgrid = ESMF_DistGridCreate(minIndex=(/1,1,1/), maxIndex=(/4,8,4/), &
+            regDecomp=(/2,2,1/), rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstGrid = ESMF_GridCreate(distgrid=dstDistgrid, name="dstgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_ArraySpecSet(arrayspec, 5, ESMF_TYPEKIND_I4, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! create src and dst Fields
+        srcField = ESMF_FieldCreate(srcGrid, arrayspec, &
+            ungriddedLBound=(/1,1/), ungriddedUBound=(/3,4/), &
+            maxHaloLWidth=(/1,1,1/), maxHaloUWidth=(/1,2,3/), &
+            rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldGet(srcField, localDe=0, farray=srcfptr, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        srcfptr = lpe
+
+        dstField = ESMF_FieldCreate(dstGrid, arrayspec, &
+            ungriddedLBound=(/1,1/), ungriddedUBound=(/3,4/), &
+            maxHaloLWidth=(/1,1,1/), maxHaloUWidth=(/1,2,3/), &
+            rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldGet(dstField, localDe=0, farray=dstfptr, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstfptr = 0
+
+        ! perform redist
+        call ESMF_FieldRedistStore(srcField, dstField, routehandle=routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldRedist(srcField, dstField, routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! verify redist
+        call ESMF_FieldGet(dstField, localDe=0, farray=fptr, &
+          exclusiveLBound=exLB, exclusiveUBound=exUB, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! Verify that the redistributed data in dstField is correct.
+        ! Before the redist op, the dst Field contains all 0. 
+        ! The redist op reset the values to the PE value, verify this is the case.
+        ! MUST use exclusive bounds because Redist operates within excl. region.
+        ! This test is somewhat difficult to verify when the values depend on
+        ! decomposition pattern of PETs. 
+        ! do m = exLB(5), exUB(5)
+        !     do l = exLB(4), exUB(4)
+        !         do k = exLB(3), exUB(3)
+        !             do j = exLB(2), exUB(2)
+        !                 do i = exLB(1), exUB(1)
+        !                    if(fptr(i,j,k,l,m) .ne. lpe) localrc = ESMF_FAILURE
+        !                 enddo
+        !             enddo
+        !         enddo
+        !     enddo
+        ! enddo
+        ! if (ESMF_LogMsgFoundError(localrc, &
+        !     ESMF_ERR_PASSTHRU, &
+        !     ESMF_CONTEXT, rc)) return
+
+        ! release route handle
+        call ESMF_FieldRedistRelease(routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldDestroy(srcField)
+        call ESMF_FieldDestroy(dstField)
+        call ESMF_GridDestroy(srcGrid)
+        call ESMF_GridDestroy(dstGrid)
+        call ESMF_DistGridDestroy(srcDistgrid)
+        call ESMF_DistGridDestroy(dstDistgrid)
+
+        rc = ESMF_SUCCESS
+    end subroutine test_redist_5d
+
+#undef ESMF_METHOD
+#define ESMF_METHOD "test_redist_5dt"
+    subroutine test_redist_5dt(rc)
+        integer, intent(out)                        :: rc
+
+        ! local arguments used to create field etc
+        type(ESMF_Field)                            :: srcField, dstField
+        type(ESMF_Grid)                             :: srcGrid, dstGrid
+        type(ESMF_DistGrid)                         :: srcDistgrid, dstDistgrid
+        type(ESMF_VM)                               :: vm
+        type(ESMF_RouteHandle)                      :: routehandle
+        type(ESMF_ArraySpec)                        :: arrayspec
+        integer                                     :: localrc, lpe, i, j, k, l, m
+        integer                                     :: exLB(5), exUB(5)
+
+        integer(ESMF_KIND_I4), pointer              :: srcfptr(:,:,:,:,:), dstfptr(:,:,:,:,:)
+        integer(ESMF_KIND_I4), pointer              :: fptr(:,:,:,:,:)
+
+        rc = ESMF_SUCCESS
+        localrc = ESMF_SUCCESS
+
+        call ESMF_VMGetCurrent(vm, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_VMGet(vm, localPet=lpe, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! create src and dst Grids which are congruent but different in decomposition
+        srcDistgrid = ESMF_DistGridCreate(minIndex=(/1,1,1/), maxIndex=(/4,4,8/), &
+            regDecomp=(/2,1,2/), rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        srcGrid = ESMF_GridCreate(distgrid=srcDistgrid, name="srcgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstDistgrid = ESMF_DistGridCreate(minIndex=(/1,1,1/), maxIndex=(/4,8,4/), &
+            regDecomp=(/2,2,1/), rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstGrid = ESMF_GridCreate(distgrid=dstDistgrid, name="dstgrid", rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_ArraySpecSet(arrayspec, 5, ESMF_TYPEKIND_I4, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! create src and dst Fields
+        srcField = ESMF_FieldCreate(srcGrid, arrayspec, &
+            ungriddedLBound=(/1,1/), ungriddedUBound=(/3,4/), &
+            maxHaloLWidth=(/1,1,1/), maxHaloUWidth=(/1,3,2/), &
+            rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldGet(srcField, localDe=0, farray=srcfptr, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        srcfptr = lpe
+
+        dstField = ESMF_FieldCreate(dstGrid, arrayspec, &
+            ungriddedLBound=(/1,1/), ungriddedUBound=(/3,4/), &
+            maxHaloLWidth=(/1,1,1/), maxHaloUWidth=(/1,2,3/), &
+            rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldGet(dstField, localDe=0, farray=dstfptr, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        dstfptr = 0
+
+        ! perform redist
+        call ESMF_FieldRedistStore(srcField, dstField, &
+            srcToDstTransposeMap=(/1,3,2,4,5/), routehandle=routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldRedist(srcField, dstField, routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! verify redist
+        call ESMF_FieldGet(dstField, localDe=0, farray=fptr, &
+          exclusiveLBound=exLB, exclusiveUBound=exUB, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! Verify that the redistributed data in dstField is correct.
+        ! Before the redist op, the dst Field contains all 0. 
+        ! The redist op reset the values to the PE value, verify this is the case.
+        ! MUST use exclusive bounds because Redist operates within excl. region.
+        do m = exLB(5), exUB(5)
+            do l = exLB(4), exUB(4)
+                do k = exLB(3), exUB(3)
+                    do j = exLB(2), exUB(2)
+                        do i = exLB(1), exUB(1)
+                           if(fptr(i,j,k,l,m) .ne. lpe) localrc = ESMF_FAILURE
+                        enddo
+                    enddo
+                enddo
+            enddo
+        enddo
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        ! release route handle
+        call ESMF_FieldRedistRelease(routehandle, rc=localrc)
+        if (ESMF_LogMsgFoundError(localrc, &
+            ESMF_ERR_PASSTHRU, &
+            ESMF_CONTEXT, rc)) return
+
+        call ESMF_FieldDestroy(srcField)
+        call ESMF_FieldDestroy(dstField)
+        call ESMF_GridDestroy(srcGrid)
+        call ESMF_GridDestroy(dstGrid)
+        call ESMF_DistGridDestroy(srcDistgrid)
+        call ESMF_DistGridDestroy(dstDistgrid)
+
+        rc = ESMF_SUCCESS
+    end subroutine test_redist_5dt
  
 #endif
 
