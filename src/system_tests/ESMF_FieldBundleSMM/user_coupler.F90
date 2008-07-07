8e62d4a7c84fa0e2913e529a36dc33b407a43061
Fei Liu <Fei.Liu@gmail.com>
2008-07-07 19:47:49 +0000

    initial revision of FieldBundle SMM system test

---
+++ src/system_tests/ESMF_FieldBundleSMM/user_coupler.F90
@@ -0,0 +1,231 @@
+! $Id: user_coupler.F90,v 1.1 2008/07/07 19:47:49 feiliu Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!
+! !DESCRIPTION:
+!  User-supplied Coupler
+!
+!
+!\begin{verbatim}
+
+module user_coupler
+
+  ! ESMF Framework module
+  use ESMF_Mod
+    
+  implicit none
+   
+  public usercpl_register
+        
+  ! global data
+  type(ESMF_RouteHandle), save :: routehandle
+
+  contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine usercpl_register(comp, rc)
+    type(ESMF_CplComp) :: comp
+    integer, intent(out) :: rc
+#ifdef ESMF_TESTWITHTHREADS
+    type(ESMF_VM) :: vm
+    logical :: supportPthreads
+#endif
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Coupler Register starting"
+    
+    ! Register the callback routines.
+    call ESMF_CplCompSetEntryPoint(comp, ESMF_SETINIT, user_init, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_CplCompSetEntryPoint(comp, ESMF_SETRUN, user_run, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_CplCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "Registered Initialize, Run, and Finalize routines"
+
+#ifdef ESMF_TESTWITHTHREADS
+    ! The following call will turn on ESMF-threading (single threaded)
+    ! for this component. If you are using this file as a template for
+    ! your own code development you probably don't want to include the
+    ! following call unless you are interested in exploring ESMF's
+    ! threading features.
+
+    ! First test whether ESMF-threading is supported on this machine
+    call ESMF_VMGetGlobal(vm, rc=rc)
+    call ESMF_VMGet(vm, supportPthreadsFlag=supportPthreads, rc=rc)
+    if (supportPthreads) then
+      call ESMF_CplCompSetVMMinThreads(comp, rc=rc)
+    endif
+#endif
+
+    print *, "User Coupler Register returning"
+
+  end subroutine
+
+!-------------------------------------------------------------------------
+!   !User Comp Component created by higher level calls, here is the
+!   ! Initialization routine.
+    
+  subroutine user_init(comp, importState, exportState, clock, rc)
+    type(ESMF_CplComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    integer :: itemcount, localPet
+    type(ESMF_FieldBundle) :: srcFieldBundle, dstFieldBundle
+    type(ESMF_VM) :: vm
+    real(ESMF_KIND_R8):: factorList(10000)
+    integer:: i, factorIndexList(2,10000)
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Coupler Init starting"
+
+    call ESMF_StateGet(importState, itemcount=itemcount, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    print *, "Import State contains ", itemcount, " items."
+
+    ! Need to reconcile import and export states
+    call ESMF_CplCompGet(comp, vm=vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_StateReconcile(importState, vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_StateReconcile(exportState, vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get source FieldBundle out of import State
+    call ESMF_StateGet(importState, "fieldbundle data", srcFieldBundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get destination FieldBundle out of export State
+    call ESMF_StateGet(exportState, "fieldbundle data", dstFieldBundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! get localPet
+    call ESMF_VMGet(vm, localPet=localPet, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Setup identity sparse matrix as a combination of PET 0 and PET 4
+    ! there are 15000 elements on the diagonal, defined as two overlapping
+    ! lists of each 10000 elements (overlapping 5000).
+    if (localPet==0) then
+      factorIndexList(1,:) = (/(i,i=1,10000)/)
+      factorIndexList(2,:) = factorIndexList(1,:)
+      do i=1, 5000
+        factorList(i) = 1.d0
+      enddo
+      do i=5001, 10000
+        factorList(i) = .253d0
+      enddo
+    endif
+    if (localPet==4) then
+      factorIndexList(1,:) = (/(i,i=5001,15000)/)
+      factorIndexList(2,:) = factorIndexList(1,:)
+      do i=1, 5000
+        factorList(i) = .747d0
+      enddo
+      do i=5001, 10000
+        factorList(i) = 1.d0
+      enddo
+    endif
+
+    ! Precompute and store an FieldBundleSMM operation
+    if (localPet==0 .or. localPet==4) then
+      ! only PET 0 and PET 4 provide factors
+      call ESMF_FieldBundleSMMStore(srcFieldBundle=srcFieldBundle, &
+        dstFieldBundle=dstFieldBundle, &
+        routehandle=routehandle, factorList=factorList, &
+        factorIndexList=factorIndexList, rc=rc)
+      if (rc/=ESMF_SUCCESS) return ! bail out
+    else
+      call ESMF_FieldBundleSMMStore(srcFieldBundle=srcFieldBundle, dstFieldBundle=dstFieldBundle, &
+        routehandle=routehandle, rc=rc)
+      if (rc/=ESMF_SUCCESS) return ! bail out
+    endif
+    
+    print *, "User Coupler Init returning"
+   
+  end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is coupled.
+!   !
+ 
+  subroutine user_run(comp, importState, exportState, clock, rc)
+    type(ESMF_CplComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_FieldBundle) :: srcFieldBundle, dstFieldBundle
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Coupler Run starting"
+
+    ! Get source FieldBundle out of import State
+    call ESMF_StateGet(importState, "fieldbundle data", srcFieldBundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get destination FieldBundle out of export State
+    call ESMF_StateGet(exportState, "fieldbundle data", dstFieldBundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Use FieldBundleSMM() to take data from srcFieldBundle to dstFieldBundle
+    call ESMF_FieldBundleSMM(srcFieldBundle=srcFieldBundle, dstFieldBundle=dstFieldBundle, &
+      routehandle=routehandle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+  
+    print *, "User Coupler Run returning"
+
+  end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+  subroutine user_final(comp, importState, exportState, clock, rc)
+    type(ESMF_CplComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Coupler Final starting"
+  
+    ! Release resources stored for the FieldBundleSMM.
+    call ESMF_FieldBundleSMMRelease(routehandle=routehandle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "User Coupler Final returning"
+  
+  end subroutine user_final
+
+
+end module user_coupler
+    
+!\end{verbatim}
+
---
+++ src/system_tests/ESMF_FieldBundleSMM/user_model1.F90
@@ -0,0 +1,243 @@
+! $Id: user_model1.F90,v 1.1 2008/07/07 19:47:49 feiliu Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!
+! !DESCRIPTION:
+!  User-supplied Component, most recent interface revision.
+!
+!
+!\begin{verbatim}
+
+module user_model1
+
+  ! ESMF Framework module
+  use ESMF_Mod
+
+  implicit none
+    
+  public userm1_register
+        
+  contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine userm1_register(comp, rc)
+    type(ESMF_GridComp) :: comp
+    integer, intent(out) :: rc
+#ifdef ESMF_TESTWITHTHREADS
+    type(ESMF_VM) :: vm
+    logical :: supportPthreads
+#endif
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Register starting"
+
+    ! Register the callback routines.
+
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, user_init, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, user_run, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, &
+      ESMF_SINGLEPHASE, rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "Registered Initialize, Run, and Finalize routines"
+
+#ifdef ESMF_TESTWITHTHREADS
+    ! The following call will turn on ESMF-threading (single threaded)
+    ! for this component. If you are using this file as a template for
+    ! your own code development you probably don't want to include the
+    ! following call unless you are interested in exploring ESMF's
+    ! threading features.
+
+    ! First test whether ESMF-threading is supported on this machine
+    call ESMF_VMGetGlobal(vm, rc=rc)
+    call ESMF_VMGet(vm, supportPthreadsFlag=supportPthreads, rc=rc)
+    if (supportPthreads) then
+      call ESMF_GridCompSetVMMinThreads(comp, rc=rc)
+    endif
+#endif
+
+    print *, "User Comp1 Register returning"
+    
+  end subroutine
+
+!-------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   Initialization routine.
+ 
+    
+  subroutine user_init(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_ArraySpec)  :: arrayspec
+    type(ESMF_Grid)       :: grid
+    type(ESMF_Field)      :: field(3)
+    type(ESMF_FieldBundle):: fieldbundle
+    type(ESMF_VM)         :: vm
+    integer               :: petCount, i
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Init starting"
+
+    ! Determine petCount
+    call ESMF_GridCompGet(comp, vm=vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_VMGet(vm, petCount=petCount, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    
+    ! Create the source FieldBundle and add it to the export State
+    call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    grid = ESMF_GridCreateShapeTile(minIndex=(/1,1/), maxIndex=(/100,150/), &
+      regDecomp=(/petCount,1/), &
+      gridEdgeLWidth=(/0,0/), gridEdgeUWidth=(/0,0/), & ! no stagger padding
+      indexflag=ESMF_INDEX_GLOBAL, rc=rc)
+
+    do i = 1, 3
+        field(i) = ESMF_FieldCreate(grid, arrayspec=arrayspec, &
+              staggerloc=ESMF_STAGGERLOC_CENTER, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
+    enddo
+
+    fieldbundle = ESMF_FieldBundleCreate(fieldCount=3, fieldList=field, &
+        name="fieldbundle data", rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    call ESMF_FieldBundleSetGrid(fieldbundle, grid=grid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    call ESMF_StateAdd(exportState, fieldbundle=fieldbundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    print *, "User Comp1 Init returning"
+
+  end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is computed.
+!   !
+ 
+  subroutine user_run(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    real(ESMF_KIND_R8)    :: pi
+    type(ESMF_FieldBundle):: fieldbundle
+    type(ESMF_Field)      :: field
+    type(ESMF_Array)      :: array
+    type(ESMF_Grid)      :: grid
+    real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)   ! matching F90 array pointer
+    integer               :: i, j, k
+    integer               :: compLBnd(2),compUBnd(2)
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Run starting"
+
+    pi = 3.14159d0
+
+    ! Get the source Field from the export State
+    call ESMF_StateGet(exportState, "fieldbundle data", fieldbundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get Grid from fieldbundle
+    call ESMF_FieldBundleGet(fieldbundle, grid=grid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    ! Get Bounds
+    call ESMF_GridGet(grid, localDe=0, staggerloc=ESMF_STAGGERLOC_CENTER, &
+           computationalLBound=compLBnd, computationalUBound=compUBnd, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    do k = 1, 3
+        ! Get the k-th field from the FieldBundle
+        call ESMF_FieldBundleGet(fieldbundle, fieldIndex=k, field=field, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
+
+        ! Gain access to actual data via F90 array pointer
+        call ESMF_FieldGet(field, localDe=0, farray=farrayPtr, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
+
+        ! Fill source FieldBundle with data
+        do j = compLBnd(2), compUbnd(2)
+          do i = compLBnd(1), compUBnd(1)
+            farrayPtr(i,j) = 10.0d0 &
+              + 5.0d0 * sin(real(i,ESMF_KIND_R8)/100.d0*pi) &
+              + 2.0d0 * sin(real(j,ESMF_KIND_R8)/150.d0*pi)
+          enddo
+        enddo
+    enddo
+ 
+    print *, "User Comp1 Run returning"
+
+  end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+  subroutine user_final(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_Grid)        :: grid
+    type(ESMF_Field)       :: field
+    type(ESMF_FieldBundle) :: fieldbundle
+    integer                :: k
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp1 Final starting"
+
+    call ESMF_StateGet(exportState, "fieldbundle data", fieldbundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_FieldBundleGet(fieldbundle, grid=grid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    do k = 1, 3
+        call ESMF_FieldBundleGet(fieldbundle, fieldIndex=k, field=field, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
+        call ESMF_FieldDestroy(field, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
+    enddo
+    call ESMF_FieldBundleDestroy(fieldbundle, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridDestroy(grid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "User Comp1 Final returning"
+
+  end subroutine user_final
+
+
+end module user_model1
+    
+!\end{verbatim}
