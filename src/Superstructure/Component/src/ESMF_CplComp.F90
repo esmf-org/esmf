c03c7d2f3d04c037fa0ee9c68e17bfe207258119
Peggy Li <p.p.li@jpl.nasa.gov>
2008-07-22 21:51:30 +0000

    Adding the description of the performance impact of setting parentVm to ESMF_CHILD_IN_PARENT_VM

--- src/Superstructure/Component/src/ESMF_CplComp.F90
+++ src/Superstructure/Component/src/ESMF_CplComp.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_CplComp.F90,v 1.90 2008/07/03 23:07:56 eschwab Exp $
+! $Id: ESMF_CplComp.F90,v 1.91 2008/07/22 21:54:14 peggyli Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2008, University Corporation for Atmospheric Research, 
@@ -101,7 +101,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter, private :: version = &
-      '$Id: ESMF_CplComp.F90,v 1.90 2008/07/03 23:07:56 eschwab Exp $'
+      '$Id: ESMF_CplComp.F90,v 1.91 2008/07/22 21:54:14 peggyli Exp $'
 
 !==============================================================================
 !
@@ -235,7 +235,12 @@
       integer,                intent(out), optional :: rc 
 !
 ! !DESCRIPTION:
-!  Create an {\tt ESMF\_CplComp} object.
+!  This interface creates an {\tt ESMF\_CplComp} object. By default, a separate VM context will be created for
+!  each component.  This implies creating a new MPI communicator and allocating additional memory to manage the VM resources.
+!  When running on a large number of processors, creating a separate VM for each component could be both
+!  time and memory inefficient.  If the application is sequential, i.e., each component is running on all
+!  the PETs of the global VM, it will be more efficient to use the global VM instead of creating a new one.  This can be
+!  done by setting parentVm to ESMF_CHILD_IN_PARENT_VM.
 !
 !  The return value is the new {\tt ESMF\_CplComp}.
 !    
