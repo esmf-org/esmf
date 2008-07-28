cb3a15a789b49cc54ca56133562fd2719c68c2ac
Gerhard Theurich <theurich@sourcespring.net>
2008-07-28 17:42:55 +0000

    - merge changes from trunk

--- build_config/Linux.absoftintel.default/ESMC_Conf.h
+++ build_config/Linux.absoftintel.default/ESMC_Conf.h
@@ -1,5 +1,5 @@
 #ifdef ESMC_RCS_HEADER
-"$Id: ESMC_Conf.h,v 1.3.2.2 2008/04/05 03:12:16 cdeluca Exp $"
+"$Id: ESMC_Conf.h,v 1.3.2.3 2008/07/28 17:42:56 theurich Exp $"
 "Defines the configuration for this machine"
 #endif
 
@@ -23,7 +23,8 @@ Licensed under the University of Illinois-NCSA License.
 #if defined (__cplusplus)
 // Typedef to match the data type of the 'hidden' string length
 // argument that Fortran uses when passing CHARACTER strings.
-typedef int ESMCI_FortranStrLenArg;
+// Absoft changes between 32-bit and 64-bit ABIs, so use 'long'.
+typedef long ESMCI_FortranStrLenArg;
 #endif
 
 #define ESMC_PRESENT(arg) ( (arg) != 0 )
@@ -36,7 +37,7 @@ typedef int ESMCI_FortranStrLenArg;
 #endif
 #if S64
 #define ESMF_IS_64BIT_MACHINE 1
-#define ESMF_F90_PTR_BASE_SIZE 72
+#define ESMF_F90_PTR_BASE_SIZE 64
 #define ESMF_F90_PTR_PLUS_RANK 24
 #define ESMC_POINTER_SIZE 8
 #endif
