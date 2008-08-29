65c4e562daa451942c8c9a4e30c5e3a7dc53c091
Robert Oehmke <oehmke@ucar.edu>
2008-08-29 04:58:32 +0000

    Add a couple of new files

---
+++ src/Infrastructure/LocStream/interface/ESMCI_LocStream_F.C
@@ -0,0 +1,393 @@
+// $Id: ESMCI_LocStream_F.C,v 1.1 2008/08/29 04:58:32 oehmke Exp $
+//
+// Earth System Modeling Framework
+// Copyright 2002-2008, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+
+// ESMC interface routines
+
+//-----------------------------------------------------------------------------
+//
+// !DESCRIPTION:
+//
+//
+//-----------------------------------------------------------------------------
+//
+// insert any higher level, 3rd party or system includes here
+
+#include <cstring>
+using namespace std;
+
+#include "ESMC_Start.h"
+#include "ESMCI_LogErr.h"
+#include "ESMCI_DistGrid.h"
+#include "ESMCI_Array.h"
+
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+ static const char *const version = 
+             "$Id: ESMCI_LocStream_F.C,v 1.1 2008/08/29 04:58:32 oehmke Exp $";
+//-----------------------------------------------------------------------------
+
+extern "C" {
+//
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//
+// This section includes all the LocStream routines
+//
+//
+
+
+// non-method functions
+void FTN(c_esmc_locstreamgetkeybnds)(ESMCI::Array **_array, 
+				     int *_localDE, 
+                                     int *exclusiveLBound, 
+                                     int *exclusiveUBound,
+                                     int *exclusiveCount,  
+                                     int *computationalLBound, 
+                                     int *computationalUBound,
+                                     int *computationalCount,  
+                                     int *totalLBound, 
+                                     int *totalUBound,
+                                     int *totalCount,  
+				     int *rc){
+
+#undef  ESMC_METHOD
+#define ESMC_METHOD "c_esmc_locstreamgetkeybnds()"
+
+  ESMCI::Array *array;
+  int localDE;
+  int localrc;
+  
+  // Initialize return code; assume routine not implemented
+  if (rc != NULL) *rc = ESMC_RC_NOT_IMPL;
+  
+  // Dereference variables
+  array=*_array;
+  localDE=*_localDE;
+  
+  // Input Error Checking
+  if ((localDE < 0) || (localDE >=array->getDistGrid()->getDELayout()->getLocalDeCount())) {
+    ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_WRONG,
+	  "- localDE outside range on this processor", ESMC_NOT_PRESENT_FILTER(rc));
+    return;
+  }
+
+  // ExclusiveLBound
+  if (ESMC_NOT_PRESENT_FILTER(exclusiveLBound) != ESMC_NULL_POINTER) {
+    *exclusiveLBound=*(array->getExclusiveLBound()+localDE);
+  }
+
+  // ExclusiveUBound
+  if (ESMC_NOT_PRESENT_FILTER(exclusiveUBound) != ESMC_NULL_POINTER) {
+    *exclusiveUBound=*(array->getExclusiveUBound()+localDE);
+  }
+
+  // ExclusiveCount
+  if (ESMC_NOT_PRESENT_FILTER(exclusiveCount) != ESMC_NULL_POINTER) {
+    *exclusiveCount=*(array->getExclusiveUBound()+localDE) -
+                    *(array->getExclusiveLBound()+localDE) + 1;
+  }
+
+  // ComputationalLBound
+  if (ESMC_NOT_PRESENT_FILTER(computationalLBound) != ESMC_NULL_POINTER) {
+    *computationalLBound=*(array->getComputationalLBound()+localDE);
+  }
+
+  // ComputationalUBound
+  if (ESMC_NOT_PRESENT_FILTER(computationalUBound) != ESMC_NULL_POINTER) {
+    *computationalUBound=*(array->getComputationalUBound()+localDE);
+  }
+
+  // ComputationalCount
+  if (ESMC_NOT_PRESENT_FILTER(computationalCount) != ESMC_NULL_POINTER) {
+    *computationalCount=*(array->getComputationalUBound()+localDE) -
+                        *(array->getComputationalLBound()+localDE) + 1;
+  }
+
+
+  // TotalLBound
+  if (ESMC_NOT_PRESENT_FILTER(totalLBound) != ESMC_NULL_POINTER) {
+    *totalLBound=*(array->getTotalLBound()+localDE);
+  }
+  
+  // TotalUBound
+  if (ESMC_NOT_PRESENT_FILTER(totalUBound) != ESMC_NULL_POINTER) {
+    *totalUBound=*(array->getTotalUBound()+localDE);
+  }
+  
+  // TotalCount
+  if (ESMC_NOT_PRESENT_FILTER(totalCount) != ESMC_NULL_POINTER) {
+    *totalCount=*(array->getTotalUBound()+localDE) -
+                *(array->getTotalLBound()+localDE) + 1;
+  }
+  
+
+  
+  // Return ESMF_SUCCESS
+  if (rc != NULL) *rc = ESMF_SUCCESS;
+    
+  return;
+} 
+
+// non-method functions
+void FTN(c_esmc_locstreamgetelbnd)(ESMCI::DistGrid **_distgrid, 
+				 int *_localDE, 
+				 ESMC_IndexFlag *_indexflag, 
+                                 int *exclusiveLBound, 
+				 int *rc){
+
+#undef  ESMC_METHOD
+#define ESMC_METHOD "c_esmc_locstreamgetelbnd()"
+
+  ESMCI::DistGrid *distgrid;
+  int localDE;
+  int localrc;
+  ESMC_IndexFlag indexflag;
+  
+  // Initialize return code; assume routine not implemented
+  if (rc != NULL) *rc = ESMC_RC_NOT_IMPL;
+  
+  // Dereference variables
+  distgrid=*_distgrid;
+  localDE=*_localDE;
+  indexflag=*_indexflag;
+  
+  // Input Error Checking
+  if ((localDE < 0) || (localDE >=distgrid->getDELayout()->getLocalDeCount())) {
+    ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_WRONG,
+	  "- localDE outside range on this processor", ESMC_NOT_PRESENT_FILTER(rc));
+    return;
+  }
+
+  // Set lower bound based on indexflag
+  if (indexflag==ESMF_INDEX_DELOCAL) {
+      *exclusiveLBound = 1; // excl. region starts at (1,1,1...) 
+  } else {
+    // Get some useful information
+    const int *localDeList = distgrid->getDELayout()->getLocalDeList();
+
+    // Get the Global DE from the local DE
+    int de = localDeList[localDE];
+
+    // obtain indexList for this DE and dim
+    const int *indexList =
+      distgrid->getIndexListPDimPLocalDe(localDE, 1, &localrc);
+    if (ESMC_LogDefault.MsgFoundError(localrc,ESMF_ERR_PASSTHRU, ESMC_NOT_PRESENT_FILTER(rc)))
+        return;
+      
+      // make sure this dimension is contiguous         
+      const int contig=distgrid->getContigFlagPDimPDe(de, 1, &localrc);
+      if (ESMC_LogDefault.MsgFoundError(localrc,
+                                    ESMF_ERR_PASSTHRU, ESMC_NOT_PRESENT_FILTER(rc))) return;
+      if (!contig) {
+        ESMC_LogDefault.MsgFoundError(ESMC_RC_NOT_IMPL,
+				      "- doesn't handle non-contiguous DEs yet ",  ESMC_NOT_PRESENT_FILTER(rc));
+        return;
+      }
+      
+      // Set lower bounds of exclusive region to match indexList[0]
+      *exclusiveLBound = indexList[0];
+  }
+  
+  // Return ESMF_SUCCESS
+  if (rc != NULL) *rc = ESMF_SUCCESS;
+    
+  return;
+} 
+
+
+void FTN(c_esmc_locstreamgeteubnd)(ESMCI::DistGrid **_distgrid, 
+				 int *_localDE, 
+				 ESMC_IndexFlag *_indexflag, 
+                                 int *exclusiveUBound, 
+				 int *rc){
+
+#undef  ESMC_METHOD
+#define ESMC_METHOD "c_esmc_locstreamgeteubnd()"
+
+  ESMCI::DistGrid *distgrid;
+  int localDE;
+  int localrc;
+  ESMC_IndexFlag indexflag;
+  
+  // Initialize return code; assume routine not implemented
+  if (rc != NULL) *rc = ESMC_RC_NOT_IMPL;
+  
+  // Dereference variables
+  distgrid=*_distgrid;
+  localDE=*_localDE;
+  indexflag=*_indexflag;
+  
+  // Input Error Checking
+  if ((localDE < 0) || (localDE >=distgrid->getDELayout()->getLocalDeCount())) {
+    ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_WRONG,
+	  "- localDE outside range on this processor", ESMC_NOT_PRESENT_FILTER(rc));
+    return;
+  }
+
+  // Get some useful information
+  const int *localDeList = distgrid->getDELayout()->getLocalDeList();
+  const int *indexCountPDimPDe = distgrid->getIndexCountPDimPDe();
+
+  // Get the Global DE from the local DE
+  int de = localDeList[localDE];
+
+  // exlc. region for each DE ends at indexCountPDimPDe of the associated
+  // DistGrid
+  *exclusiveUBound = indexCountPDimPDe[de];
+  
+  // Set upper bound based on indexflag
+  if (indexflag==ESMF_INDEX_GLOBAL) {
+
+    // obtain indexList for this DE and dim
+    const int *indexList =
+      distgrid->getIndexListPDimPLocalDe(localDE, 1, &localrc);
+    if (ESMC_LogDefault.MsgFoundError(localrc,ESMF_ERR_PASSTHRU, ESMC_NOT_PRESENT_FILTER(rc)))
+      return;
+    
+    // make sure is contiguous         
+    const int contig=distgrid->getContigFlagPDimPDe(de, 1, &localrc);
+    if (ESMC_LogDefault.MsgFoundError(localrc,
+				      ESMF_ERR_PASSTHRU, ESMC_NOT_PRESENT_FILTER(rc))) return;
+    if (!contig) {
+      ESMC_LogDefault.MsgFoundError(ESMC_RC_NOT_IMPL,
+				    "- doesn't handle non-contiguous DEs yet ", ESMC_NOT_PRESENT_FILTER(rc));
+      return;
+    }
+    
+    // shift bounds of exclusive region to match indexList[0]
+    *exclusiveUBound += indexList[0] - 1;
+  }
+  
+  // Return ESMF_SUCCESS
+  if (rc != NULL) *rc = ESMF_SUCCESS;
+    
+  return;
+} 
+  
+
+
+#if 0
+// non-method functions
+void FTN(c_esmc_fieldserialize)(ESMC_Status *fieldstatus, 
+                ESMC_Status *gridstatus, 
+                ESMC_Status *datastatus, 
+                ESMC_Status *iostatus,
+                int * staggerloc,
+                int * gridToFieldMap,
+                int * ungriddedLBound,
+                int * ungriddedUBound,
+                int * maxHaloLWidth,
+                int * maxHaloUWidth,
+				void *buffer, int *length, int *offset, int *localrc){
+    int i;
+
+
+    // Initialize return code; assume routine not implemented
+    if (localrc) *localrc = ESMC_RC_NOT_IMPL;
+    // either put the code here, or call into a real C++ function
+    ESMC_Status *sp;
+
+    // TODO: verify length > 4 status vars, and if not, make room.
+    int fixedpart = 4 * sizeof(int *) + sizeof(int) + 5 * ESMF_MAXDIM * sizeof(int);
+    if ((*length - *offset) < fixedpart) {
+         
+         ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_BAD,
+                            "Buffer too short to add a Field object", localrc);
+         return;
+ 
+        //buffer = (char *)realloc((void *)buffer,
+        //                         *length + 2*fixedpart + byte_count);
+        //*length += 2 * fixedpart;
+    }
+
+
+    sp = (ESMC_Status *)((char *)(buffer) + *offset);
+    *sp++ = *fieldstatus;
+    *sp++ = *gridstatus; 
+    *sp++ = *datastatus; 
+    *sp++ = *iostatus; 
+
+    // copy the rest of the field parameters
+    // we are explicitly assuming Fortran-integer is of size C-int
+    // This might be probelmatic on 64bit machines depending how compiler flag is used
+    // e.g. we know gridToFieldMap is of type cpu_word *, its element maybe int32 or int64
+    // depending on the size of Fortran-integer
+    char * ptr = (char *)sp;
+    memcpy((void *)ptr, (const void *)staggerloc, sizeof(int));
+    ptr += sizeof(int);
+    memcpy((void *)ptr, (const void *)gridToFieldMap, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ptr, (const void *)ungriddedLBound, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ptr, (const void *)ungriddedUBound, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ptr, (const void *)maxHaloLWidth, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ptr, (const void *)maxHaloUWidth, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+
+    *offset = ptr - (char *)buffer;
+
+    if (localrc) *localrc = ESMF_SUCCESS;
+
+    return;
+} 
+
+
+void FTN(c_esmc_fielddeserialize)(ESMC_Status *fieldstatus, 
+                ESMC_Status *gridstatus, 
+                ESMC_Status *datastatus, 
+                ESMC_Status *iostatus, 
+                int * staggerloc,
+                int * gridToFieldMap,
+                int * ungriddedLBound,
+                int * ungriddedUBound,
+                int * maxHaloLWidth,
+                int * maxHaloUWidth,
+				void *buffer, int *offset, int *localrc){
+
+    int i;
+
+    // Initialize return code; assume routine not implemented
+    if (localrc) *localrc = ESMC_RC_NOT_IMPL;
+    // either put the code here, or call into a real C++ function
+    ESMC_Status *sp;
+
+    sp = (ESMC_Status *)((char *)(buffer) + *offset);
+    *fieldstatus = *sp++;
+    *gridstatus = *sp++;
+    *datastatus = *sp++;
+    *iostatus = *sp++;
+
+    char * ptr = (char *)sp;
+    memcpy((void *)staggerloc, (const void *)ptr, sizeof(int));
+    ptr += sizeof(int);
+    memcpy((void *)gridToFieldMap, (const void *)ptr, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ungriddedLBound, (const void *)ptr, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)ungriddedUBound, (const void *)ptr, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)maxHaloLWidth, (const void *)ptr, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+    memcpy((void *)maxHaloUWidth, (const void *)ptr, ESMF_MAXDIM*sizeof(int));
+    ptr += ESMF_MAXDIM*sizeof(int);
+
+    *offset = ptr - (char *)buffer;
+
+    if (localrc) *localrc = ESMF_SUCCESS;
+
+    return;
+} 
+#endif
+
+}
