655c3341be83bb95ec2e98265d7ff018a4593b5d
Gerhard Theurich <theurich@sourcespring.net>
2008-09-12 17:05:42 +0000

    - fix error handling to work on glass/absoft

--- src/system_tests/ESMF_CompFortranAndC/user_FortranComponent.F90
+++ src/system_tests/ESMF_CompFortranAndC/user_FortranComponent.F90
@@ -1,4 +1,4 @@
-! $Id: user_FortranComponent.F90,v 1.4 2008/09/10 18:27:15 rosalind Exp $
+! $Id: user_FortranComponent.F90,v 1.5 2008/09/12 17:05:42 theurich Exp $
 !
 ! Example/test code which shows User Component calls.
 
@@ -39,6 +39,9 @@
         logical :: supportPthreads
 #endif
 
+        ! Initialize return code
+        rc = ESMF_SUCCESS
+
         print *, "In myRegistrationInFortran routine"
 
         ! Register the callback routines.
@@ -69,8 +72,6 @@
         endif
 #endif
 
-        rc = ESMF_SUCCESS
-
     end subroutine
 
 
@@ -85,7 +86,6 @@
         type(ESMF_State) :: importState, exportState
         type(ESMF_Clock) :: clock
         type(ESMF_Array) :: array
-        type(ESMF_Array) :: array0
         type(ESMF_ArraySpec) :: arrayspec
         type (ESMF_DistGrid) :: distgrid
         type (ESMF_VM) :: vm
@@ -95,6 +95,10 @@
 
         !local data
         integer :: i
+
+        ! Initialize return code
+        rc = ESMF_SUCCESS
+
         print *, "Fortran User Comp Init starting"
 
         ! Allocate the Fortran array
@@ -112,11 +116,11 @@
         ! Add an array state to the export state.
         call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, &
           rc=rc)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+        if (rc/=ESMF_SUCCESS) return ! bail out
 
         distgrid = ESMF_DistGridCreate(minIndex=(/1,1/), &
           maxIndex=(/5*petCount,2/), rc=rc)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+        if (rc/=ESMF_SUCCESS) return ! bail out
 
         do i=1,5
           farray(i,:) = float(i)
@@ -124,23 +128,16 @@
 
         array = ESMF_ArrayCreate(farray=farray, distgrid=distgrid, &
           name="array1", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
 
-       !call ESMF_ArrayPrint(array)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
-
-        call ESMF_StateAdd(exportState, array, rc)
         write(name, *) "Adding an Array to a State Test"
-
-        call ESMF_StateGet(exportState,"array1", array0, rc=rc)
-        call ESMF_ArrayPrint(array0)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+        call ESMF_StateAdd(exportState, array, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
 
         call ESMF_StatePrint(exportState, rc=rc)
 
         print *, "User routine myInitInFortran returning"
    
-        rc = ESMF_SUCCESS
-
     end subroutine myInitInFortran
 
 
@@ -156,28 +153,30 @@
 
         integer, save :: onetime=1              ! static variable
 
-        type(ESMF_Array) :: array0
+        type(ESMF_Array) :: array
         real(ESMF_KIND_R8), pointer, dimension(:,:) :: farrayPtr
         integer :: status
         integer :: i,j
+
+        ! Initialize return code
+        rc = ESMF_SUCCESS
 
         print *, "User Fortran Comp Run starting"
 
         ! static data array farray modified in my_InitInC
         print *, "In Fortran Component Run, farray= ",farray
 
-        call ESMF_StateGet(exportState,"array1", array0, rc=rc)
-        call ESMF_ArrayPrint(array0)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+        call ESMF_StateGet(exportState,"array1", array, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail out
 
-        call ESMF_ArrayGet(array0, 0, farrayPtr=farrayPtr, rc=rc)
+        call ESMF_ArrayGet(array, 0, farrayPtr=farrayPtr, rc=rc)
 
           ! values must be as set in "myInitInC"
           do j=1,2
             do i=1,5
               if ( abs(farrayPtr(i,j)-float(j-1)) > 1.e-8 ) then
                 print *, "ERROR! farrayPtr has wrong value at i,j=",i,j
-                call ESMF_Finalize(terminationflag=ESMF_ABORT)
+                if (rc/=ESMF_SUCCESS) return ! bail out
               end if
             end do
           end do 
@@ -195,13 +194,7 @@
        print *," data in exp state data modified in myRunInFortran in Fortran", &
                " component"
 
-       call ESMF_ArrayPrint(array0)
-
-       !call ESMF_StatePrint(exportState, rc=status)
- 
-        print *, "User routine myRunInFortran returning"
-
-        rc = ESMF_SUCCESS
+       print *, "User routine myRunInFortran returning"
 
     end subroutine myRunInFortran
 
@@ -219,6 +212,9 @@
         ! Internal data
         type(ESMF_Array) :: array
 
+        ! Initialize return code
+        rc = ESMF_SUCCESS
+
         print *, "User Comp Final starting"
     
         ! Extract the exportState array so it can be destroyed
@@ -226,13 +222,11 @@
 
         ! Free up memory
         call ESMF_ArrayDestroy(array, rc)
-        if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+        if (rc/=ESMF_SUCCESS) return ! bail out
         deallocate (farray)    
  
         print *, "User routine myFinalInFortran returning"
    
-        rc = ESMF_SUCCESS
-
     end subroutine myFinalInFortran
 
 
