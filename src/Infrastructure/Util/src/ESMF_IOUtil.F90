8df6157383006c28f7309a1f3bc98847677c6752
Walter Spector <walter.spector@hpe.com>
2008-10-29 23:57:21 +0000

    New file name, and change names to use 'IO' instead of 'FIO'. Move the actual flush statement/call to the various build_config .inc files, and use macro substitution to insert the correct call here.  Also, NAG requires the use of the (non-Standard) f90_unix_io module in order to call FLUSH correctly.

---
+++ src/Infrastructure/Util/src/ESMF_IOUtil.F90
@@ -0,0 +1,263 @@
+! $Id: ESMF_IOUtil.F90,v 1.1 2008/10/29 23:57:21 w6ws Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2008, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+#define ESMF_FILENAME "ESMF_IOUtil.F90"
+
+!
+! ESMF IOUtil Module
+!
+! (all lines between the !BOP and !EOP markers will be included in the
+! automated document processing.)
+!------------------------------------------------------------------------------
+! one blank line for protex processing - in case all routines here are
+! marked internal (BOPI/EOPI), the output file will still have contents.
+!BOP
+
+!EOP
+
+!------------------------------------------------------------------------------
+! module definition
+
+module ESMF_IOUtilMod
+
+!BOPI
+! !MODULE: ESMF_IOUtilMod - Fortran I/O utility routines
+!
+! !DESCRIPTION:
+!
+!  Fortran I/O-specific utility routines
+!
+! See the ESMF Developers Guide document for more details.
+!
+!------------------------------------------------------------------------------
+
+! !USES:
+
+  use ESMF_UtilTypesMod
+#include "ESMF.h"
+#ifdef ESMF_NAG_UNIXIO_MODULE
+  use f90_unix_io
+#endif
+
+  implicit none
+!
+! !PRIVATE TYPES:
+      private
+
+!------------------------------------------------------------------------------
+!
+! !DESCRIPTION:
+!
+! !PUBLIC MEMBER SUBROUTINES:
+!
+  public ESMF_IOUnitFlush
+  public ESMF_IOUnitGet
+  public ESMF_IOUnitInit
+
+!==============================================================================
+!
+! MODULE SCOPE DATA
+!
+!==============================================================================
+
+! Standard I/O Units
+
+! (F2003 TODO: Eventually, for compilers which support the F2003
+! ISO_FORTRAN_ENV intrinsic module, these should access the constants
+! 'input_unit', 'output_unit', and 'error_unit'.)
+
+  integer, parameter, public :: ESMF_IOstdin  = 5
+  integer, parameter, public :: ESMF_IOstdout = 6
+
+#ifdef sysHP_UX
+
+! Special setting for HP_UX
+
+  integer, parameter, public :: ESMF_IOstderr = 7
+#else
+! Generic setting for UNIX other than HP-UX
+
+  integer, parameter, public :: ESMF_IOstderr = 0
+#endif
+
+! Unit number range for ESMF internal use.
+
+  integer, private :: ESMF_IOUnitLower = ESMF_LOG_FORT_UNIT_NUMBER
+  integer, private :: ESMF_IOUnitUpper = ESMF_LOG_UPPER
+
+!------------------------------------------------------------------------------
+! leave the following line as-is; it will insert the cvs ident string
+! into the object file for tracking purposes.
+  character(*), parameter, private :: version = &
+      '$Id: ESMF_IOUtil.F90,v 1.1 2008/10/29 23:57:21 w6ws Exp $'
+!------------------------------------------------------------------------------
+
+  contains
+
+!-------------------------------------------------------------------------
+#undef  ESMF_METHOD
+#define ESMF_METHOD "ESMF_IOUnitFlush"
+!BOP
+! !IROUTINE: ESMF\_IOUnitFlush - flush output on a unit number
+!
+! !INTERFACE:
+  subroutine ESMF_IOUnitFlush (unit, rc)
+!
+! !PARAMETERS:
+    integer, intent(in) :: unit
+    integer, intent(out), optional :: rc               ! return code
+
+!
+! !DESCRIPTION:
+!   Call the system-dependent routine to force output.
+
+!
+! !REQUIREMENTS:
+
+!EOP
+    integer :: status
+
+!   Preset status in advance, since some library versions of FLUSH do
+!   not support a status argument for detecting errors.
+
+    status = 0
+
+ESMF_IOFlushMacro(unit,status)
+
+    if (present(rc)) then
+      rc = merge (ESMF_SUCCESS, ESMF_FAILURE, status == 0)
+    end if
+
+  end subroutine ESMF_IOUnitFlush
+
+!-------------------------------------------------------------------------
+#undef  ESMF_METHOD
+#define ESMF_METHOD "ESMF_IOUnitGet"
+!BOP
+!  !IROUTINE:  ESMF\_IOUnitGet - Scan for a free I/O unit number
+!
+! !INTERFACE:
+  subroutine ESMF_IOUnitGet (unit, rc)
+!
+! !ARGUMENTS:
+    integer, intent(out) :: unit
+    integer, intent(out), optional :: rc
+
+!
+! !DESCRIPTION:
+!   Scan for, and return, a free Fortran I/O unit number.
+!
+!     The arguments are:
+!     \begin{description}
+!     \item[{[unit]}]
+!       A Fortran I/O unit number.
+!     \item[{[rc]}]
+!       Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+!     \end{description}
+!
+!   The Fortran unit which is returned is not reserved in any way.
+!   So successive calls without intervening OPEN or CLOSE statements (or
+!   other ways of connecting to units), may not return a unique unit
+!   number.  It is recommended that an OPEN statement immediately follow
+!   the call to ESMF\_IOUnitGet to activate the unit.
+!EOP
+
+    integer :: i, status
+    logical :: inuse
+  
+    if (present(rc)) rc = ESMF_FAILURE
+  
+    do, i=ESMF_IOUnitLower, ESMF_IOUnitUpper
+      inquire (unit=i, opened=inuse, iostat=status)
+      if (.not. inuse .and. status == 0) exit
+    end do
+  
+    if (i <= ESMF_IOUnitUpper) then
+      unit = i
+      if (present (rc)) rc = ESMF_SUCCESS
+    else
+      unit = -1
+    end if
+
+  end subroutine ESMF_IOUnitGet
+
+!-------------------------------------------------------------------------
+#undef  ESMF_METHOD
+#define ESMF_METHOD "ESMF_IOUnitInit"
+!BOPI
+!  !IROUTINE:  ESMF_IOUnitInit - Initialize ESMF Fortran I/O unit number range
+!
+! !INTERFACE:
+  subroutine ESMF_IOUnitInit (lower, upper, rc)
+!
+! !ARGUMENTS:
+    integer, intent(in), optional :: lower
+    integer, intent(in), optional :: upper
+    integer, intent(out), optional :: rc
+
+!
+! !DESCRIPTION:
+!   Initialize non-default range for Fortran I/O unit numbers used
+!   within ESMF.
+!
+!     The arguments are:
+!     \begin{description}
+!     \item[{[lower]}]
+!       A lower bound for Fortran unit numbers.
+!     \item[{[upper]}]
+!       A upper bound for Fortran unit numbers.
+!     \item[{[rc]}]
+!       Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+!     \end{description}
+!
+!
+!EOPI
+
+    if (present(rc)) rc = ESMF_FAILURE
+
+! Sanity checks
+
+    if (present (lower)) then
+      if (lower < 0) return
+    end if
+
+    if (present (upper)) then
+      if (upper < 0) return
+    end if
+
+    if (present (lower) .and. .not. present (upper)) then
+      if (lower > ESMF_IOUnitUpper) return
+    end if
+
+    if (present (upper) .and. .not. present (lower)) then
+      if (upper < ESMF_IOUnitLower) return
+    end if
+
+    if (present (upper) .and. present (lower)) then
+      if (upper < lower) return
+    end if
+
+!
+
+    if (present (lower)) then
+      ESMF_IOUnitLower = lower
+    end if
+
+    if (present (upper)) then
+      ESMF_IOUnitUpper = upper
+    end if
+
+    if (present (rc)) rc = ESMF_SUCCESS
+
+  end subroutine ESMF_IOUnitInit
+
+!------------------------------------------------------------------------------
+
+end module ESMF_IOUtilMod
