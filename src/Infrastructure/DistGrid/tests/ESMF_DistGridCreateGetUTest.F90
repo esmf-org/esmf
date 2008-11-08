c508b7ed6b1694ba0529a4c46166072310ccd251
Gerhard Theurich <theurich@sourcespring.net>
2008-11-08 16:35:52 +0000

    - remove accidental garbage

--- src/Infrastructure/DistGrid/tests/ESMF_DistGridCreateGetUTest.F90
+++ src/Infrastructure/DistGrid/tests/ESMF_DistGridCreateGetUTest.F90
@@ -1,4 +1,4 @@
-#include "ESMF_Macros.inc"! $Id: ESMF_DistGridCreateGetUTest.F90,v 1.4.2.6 2008/11/07 23:52:08 theurich Exp $
+! $Id: ESMF_DistGridCreateGetUTest.F90,v 1.4.2.7 2008/11/08 16:35:52 theurich Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2008, University Corporation for Atmospheric Research,
@@ -37,7 +37,7 @@ program ESMF_DistGridCreateGetUTest
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
   character(*), parameter :: version = &
-    '$Id: ESMF_DistGridCreateGetUTest.F90,v 1.4.2.6 2008/11/07 23:52:08 theurich Exp $'
+    '$Id: ESMF_DistGridCreateGetUTest.F90,v 1.4.2.7 2008/11/08 16:35:52 theurich Exp $'
 !------------------------------------------------------------------------------
 
   ! cumulative result: count failures; no failures equals "all pass"
