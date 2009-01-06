5c306541bb491c286710ebde2ac30e486527d0e3
Robert Oehmke <oehmke@ucar.edu>
2009-01-06 23:25:39 +0000

    Make staggerMemLBounds correct size when going into array create.

--- src/Infrastructure/Grid/src/ESMCI_Grid.C
+++ src/Infrastructure/Grid/src/ESMCI_Grid.C
@@ -1,4 +1,4 @@
-// $Id: ESMCI_Grid.C,v 1.36.2.21 2008/12/16 02:07:13 oehmke Exp $
+// $Id: ESMCI_Grid.C,v 1.36.2.22 2009/01/06 23:25:39 oehmke Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2008, University Corporation for Atmospheric Research, 
@@ -38,7 +38,7 @@
 //-----------------------------------------------------------------------------
 // leave the following line as-is; it will insert the cvs ident string
 // into the object file for tracking purposes.
-static const char *const version = "$Id: ESMCI_Grid.C,v 1.36.2.21 2008/12/16 02:07:13 oehmke Exp $";
+static const char *const version = "$Id: ESMCI_Grid.C,v 1.36.2.22 2009/01/06 23:25:39 oehmke Exp $";
 //-----------------------------------------------------------------------------
 
 #define VERBOSITY             (1)       // 0: off, 10: max
@@ -410,6 +410,10 @@ int Grid::addCoordArray(
 
     //// Optionally fix the lower memory bounds of each DE's memory chunk
     if (indexflag==ESMF_INDEX_USER) {
+      // Set size of array based on coord dim
+      staggerMemLBoundIntInt->extent[0]=coordDimCount[coord];
+
+      // Fill Array
       j=0;
       for (int i=0; i<coordDimCount[coord]; i++) {
 	int gi=coordDimMap[coord][i];
