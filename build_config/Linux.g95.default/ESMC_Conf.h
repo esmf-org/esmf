82cbaa8849ef4c33cad5adcda0331fe54352b404
Walter Spector <walter.spector@hpe.com>
2009-04-20 19:03:58 +0000

    Revise pointer sizes for x86_64 to match the 'stable' Feb 28, 2008 version of g95. Note that with ABI 64, g95 rounds the pointer size up to a multiple of 8 bytes.

--- build_config/Linux.g95.default/ESMC_Conf.h
+++ build_config/Linux.g95.default/ESMC_Conf.h
@@ -1,5 +1,5 @@
 #ifdef ESMC_RCS_HEADER
-"$Id: ESMC_Conf.h,v 1.16 2009/01/21 21:37:57 cdeluca Exp $"
+"$Id: ESMC_Conf.h,v 1.17 2009/04/20 19:03:58 w6ws Exp $"
 "Defines the configuration for this machine"
 #endif
 
@@ -66,7 +66,7 @@ typedef int ESMCI_FortranStrLenArg;
 #if Sx86_64_small
 #define ESMF_IS_64BIT_MACHINE 1
 #ifndef ESMF_F90_PTR_BASE_SIZE
-#define ESMF_F90_PTR_BASE_SIZE 40
+#define ESMF_F90_PTR_BASE_SIZE 36
 #endif
 #ifndef ESMF_F90_PTR_PLUS_RANK
 #define ESMF_F90_PTR_PLUS_RANK 12
@@ -76,7 +76,7 @@ typedef int ESMCI_FortranStrLenArg;
 #if Sx86_64_medium
 #define ESMF_IS_64BIT_MACHINE 1
 #ifndef ESMF_F90_PTR_BASE_SIZE
-#define ESMF_F90_PTR_BASE_SIZE 40
+#define ESMF_F90_PTR_BASE_SIZE 36
 #endif
 #ifndef ESMF_F90_PTR_PLUS_RANK
 #define ESMF_F90_PTR_PLUS_RANK 12
