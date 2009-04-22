d6f2db604f5087cd8c6d2f4a81a812e3818d0f82
Walter Spector <walter.spector@hpe.com>
2009-04-22 19:20:11 +0000

    Combine the IF test with the rounding calculation, so that (hopefully) certain versions of Absoft and PGI won't issue an error on the potential divide-by-zero.  (The IF test protects against this possibility, but the compilers could not tell because it was on a different line...)

--- src/prologue/tests/ESMF_F90ArrayPtrUTest.F90
+++ src/prologue/tests/ESMF_F90ArrayPtrUTest.F90
@@ -587,9 +587,8 @@
 
         get_compiled_size = ESMF_F90_PTR_BASE_SIZE + ESMF_F90_PTR_PLUS_RANK * (rank-1)
 
-        if (ALIGN_SIZE /= 0) then
+        if (ALIGN_SIZE /= 0)  &
           get_compiled_size = ((get_compiled_size + ALIGN_SIZE-1)/ALIGN_SIZE) * ALIGN_SIZE
-        end if
 
       end function get_compiled_size
 
