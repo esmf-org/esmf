942be72c1bc4c9a98618d67343f9db39cc4c015e
Silverio Vasquez <silverio.vasquez@noaa.gov>
2009-05-27 22:35:55 +0000

    Original version.

---
+++ src/system_tests/ESMF_ArrayRedistOpenMP/ESMF_ArrayRedistOpenMPSTest.F90
@@ -0,0 +1,324 @@
+! $Id: ESMF_ArrayRedistOpenMPSTest.F90,v 1.1 2009/05/27 22:35:55 svasquez Exp $
+!
+!-------------------------------------------------------------------------
+!ESMF_OPENMP_SYSTEM_TEST        String used by test script to count system tests.
+!=========================================================================
+
+!-------------------------------------------------------------------------
+!
+! !DESCRIPTION:
+! System test ArrayRedistOpenMP.  
+!    Two gridded components and one coupler component, one-way coupling.
+!    To verify the openmp functionallity a test a 4 element array is created
+!    where each openmp thread stores its thread number in its element location.
+!
+!    First gridded component runs on 4 PETs and defines a 2D source Array 
+!    100x150. Second gridded component defines a destination Array also 
+!    100x150 but runs on only 2 PETs. Both gridded components use DELayouts 
+!    with 1 DE per PET. The decomposition of the source Array is defined as 
+!    (petCount x 1) = (4 x 1) while the destination Array is decomposed as 
+!    (1 x petCount) = (1 x 2).
+!
+!    Using OpenMP directives the first component initializes the source 
+!    Array to a geometric function:
+!
+!       10.0 + 5.0*sin((I/Imax)*pi) + 2.0*sin((J/Jmax)*pi)
+!
+!    The coupler component runs on all 6 PETs and reconciles import and export
+!    states which contain source and destination Array, respectively. The 
+!    coupler component then calls ArrayRedist() to a redistribute he source
+!    Array data onto the destination Array.
+!    
+!    Finally the second gridded component compares the data stored in the
+!    destination Array to the exact solution of the above function as a measure
+!    of the accuracy of the ArrayRedist() method.
+!
+!-------------------------------------------------------------------------
+!\begin{verbatim}
+
+program ESMF_ArrayRedistOpenMPSTest
+#define ESMF_METHOD "program ESMF_ArrayRedistOpenMPSTest"
+
+#include "ESMF.h"
+
+  ! ESMF Framework module
+  use ESMF_Mod
+  use ESMF_TestMod
+
+  use user_model1, only : userm1_setvm, userm1_register
+  use user_model2, only : userm2_setvm, userm2_register
+  use user_coupler, only : usercpl_setvm, usercpl_register
+
+  implicit none
+    
+  ! Local variables
+  integer :: localPet, petCount, localrc, rc=ESMF_SUCCESS
+  integer, dimension(4) :: openmp_array
+  character(len=ESMF_MAXSTR) :: cname1, cname2, cplname
+  type(ESMF_VM):: vm
+  type(ESMF_State) :: c1exp, c2imp
+  type(ESMF_GridComp) :: comp1, comp2
+  type(ESMF_CplComp) :: cpl
+
+  ! cumulative result: count failures; no failures equals "all pass"
+  integer :: testresult = 0
+
+  ! individual test name
+  character(ESMF_MAXSTR) :: testname
+
+  ! individual test failure message, and final status msg
+  character(ESMF_MAXSTR) :: failMsg, finalMsg
+  integer :: omp_get_num_threads,  omp_get_thread_num, tid, i
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  write(failMsg, *) "System Test failure"
+  write(testname, *) "System Test ESMF_ArrayRedistOpenMPSTest"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  print *, "--------------------------------------- "
+  print *, "Start of ", trim(testname)
+  print *, "--------------------------------------- "
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Create section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!
+  ! Initialize framework and get back default global VM
+  call ESMF_Initialize(vm=vm, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+    call omp_set_num_threads(4)
+    ! Populate the openmp test array.
+!$omp parallel
+        tid = omp_get_thread_num()
+	openmp_array(tid)=tid
+!$omp end parallel
+  ! Verify openmp ran successfully
+  do i = 0,3
+	if (openmp_array(i)/=i)  &
+	call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  end do
+
+  ! Get number of PETs we are running with
+  call ESMF_VMGet(vm, petCount=petCount, localPet=localPet, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  ! Create the 2 model components and coupler
+  cname1 = "user model 1"
+  ! use petList to define comp1 on PET 0,1,2,3
+  comp1 = ESMF_GridCompCreate(name=cname1, petList=(/0,1,2,3/), rc=localrc)
+  print *, "Created component ", trim(cname1), "rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  cname2 = "user model 2"
+  ! use petList to define comp2 on PET 4,5
+  comp2 = ESMF_GridCompCreate(name=cname2, petList=(/4,5/), rc=localrc)
+  print *, "Created component ", trim(cname2), "rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  cplname = "user one-way coupler"
+  ! no petList means that coupler component runs on all PETs
+  cpl = ESMF_CplCompCreate(name=cplname, rc=localrc)
+  print *, "Created component ", trim(cplname), ", rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  print *, "Comp Creates finished"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Register section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_GridCompSetVM(comp1, userRoutine=userm1_setvm, rc=localrc)
+  print *, "Comp1 SetVM finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_GridCompSetServices(comp1, userRoutine=userm1_register, rc=localrc)
+  print *, "Comp1 SetServices finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_GridCompSetVM(comp2, userRoutine=userm2_setvm, rc=localrc)
+  print *, "Comp2 SetVM finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_GridCompSetServices(comp2, userRoutine=userm2_register, rc=localrc)
+  print *, "Comp2 SetServices finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_CplCompSetVM(cpl, userRoutine=usercpl_setvm, rc=localrc)
+  print *, "Cpl SetVM finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_CplCompSetServices(cpl, userRoutine=usercpl_register, rc=localrc)
+  print *, "Cpl SetServices finished, rc= ", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Init section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+ 
+  c1exp = ESMF_StateCreate("comp1 export", ESMF_STATE_EXPORT, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_GridCompInitialize(comp1, exportState=c1exp, rc=localrc)
+  print *, "Comp 1 Initialize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+ 
+  c2imp = ESMF_StateCreate("comp2 import", ESMF_STATE_IMPORT, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_GridCompInitialize(comp2, importState=c2imp, rc=localrc)
+  print *, "Comp 2 Initialize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+ 
+  ! note that the coupler's import is comp1's export state
+  ! and coupler's export is comp2's import state
+  call ESMF_CplCompInitialize(cpl, c1exp, c2imp, rc=localrc)
+  print *, "Coupler Initialize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+ 
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Run section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_GridCompRun(comp1, exportState=c1exp, rc=localrc)
+  print *, "Comp 1 Run returned, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_CplCompRun(cpl, c1exp, c2imp, rc=localrc)
+  print *, "Coupler Run returned, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_GridCompRun(comp2, importState=c2imp, rc=localrc)
+  print *, "Comp 2 Run returned, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+ 
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Finalize section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_CplCompFinalize(cpl, c1exp, c2imp, rc=localrc)
+  print *, "Coupler Finalize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_GridCompFinalize(comp1, exportState=c1exp, rc=localrc)
+  print *, "Comp 1 Finalize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_GridCompFinalize(comp2, importState=c2imp, rc=localrc)
+  print *, "Comp 2 Finalize finished, rc =", localrc
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Destroy section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_GridCompDestroy(comp1, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_GridCompDestroy(comp2, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_CplCompDestroy(cpl, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_StateDestroy(c1exp, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+  call ESMF_StateDestroy(c2imp, rc=localrc)
+  if (ESMF_LogMsgFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  print *, "All Destroy routines done"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+10 continue
+
+  ! Normal ESMF Test output
+  print *, testname, " complete."
+
+  ! IMPORTANT: TestGlobal() prints the PASS: string that the scripts grep for.
+  call ESMF_TestGlobal((rc.eq.ESMF_SUCCESS), testname, failMsg, testresult, &
+    ESMF_SRCLINE)
+
+  if ((localPet .eq. 0) .and. (rc .eq. ESMF_SUCCESS)) then
+    ! Separate message to console, for quick confirmation of success/failure
+    write(finalMsg, *) "SUCCESS: ",trim(testname)," finished correctly."
+    write(0, *) ""
+    write(0, *) trim(testname)
+    write(0, *) trim(finalMsg)
+    write(0, *) ""
+  endif
+  
+  print *, "------------------------------------------------------------"
+  print *, "------------------------------------------------------------"
+  print *, "Test finished, localPet = ", localPet
+  print *, "------------------------------------------------------------"
+  print *, "------------------------------------------------------------"
+
+  call ESMF_Finalize()
+
+end program ESMF_ArrayRedistOpenMPSTest
+    
+!\end{verbatim}
---
+++ src/system_tests/ESMF_ArrayRedistOpenMP/user_model2.F90
@@ -0,0 +1,215 @@
+! $Id: user_model2.F90,v 1.1 2009/05/27 22:35:55 svasquez Exp $
+!
+! Example/test code which shows User Component calls.
+
+!--------------------------------------------------------------------------------
+!--------------------------------------------------------------------------------
+
+!
+! !DESCRIPTION:
+!  User-supplied Component
+!
+!
+!\begin{verbatim}
+
+module user_model2
+
+  ! ESMF Framework module
+  use ESMF_Mod
+
+  implicit none
+    
+  public userm2_setvm, userm2_register
+        
+  contains
+
+!--------------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine userm2_setvm(comp, rc)
+    type(ESMF_GridComp) :: comp
+    integer, intent(out) :: rc
+#ifdef ESMF_TESTWITHTHREADS
+    type(ESMF_VM) :: vm
+    logical :: supportPthreads
+#endif
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+#ifdef ESMF_TESTWITHTHREADS
+    ! The following call will turn on ESMF-threading (single threaded)
+    ! for this component. If you are using this file as a template for
+    ! your own code development you probably don't want to include the
+    ! following call unless you are interested in exploring ESMF's
+    ! threading features.
+
+    ! First test whether ESMF-threading is supported on this machine
+    call ESMF_VMGetGlobal(vm, rc=rc)
+    call ESMF_VMGet(vm, supportPthreadsFlag=supportPthreads, rc=rc)
+    if (supportPthreads) then
+      call ESMF_GridCompSetVMMinThreads(comp, rc=rc)
+    endif
+#endif
+
+  end subroutine
+
+  subroutine userm2_register(comp, rc)
+    type(ESMF_GridComp) :: comp
+    integer, intent(out) :: rc
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Register starting"
+
+    ! Register the callback routines.
+
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, userRoutine=user_init, &
+      rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, userRoutine=user_run, &
+      rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, userRoutine=user_final, &
+      rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "Registered Initialize, Run, and Finalize routines"
+    print *, "User Comp2 Register returning"
+    
+  end subroutine
+
+!--------------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   Initialization routine.
+ 
+  subroutine user_init(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_ArraySpec)  :: arrayspec
+    type(ESMF_DistGrid)   :: distgrid
+    type(ESMF_Array)      :: array
+    type(ESMF_VM)         :: vm
+    integer               :: petCount
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Init starting"
+
+    ! Determine petCount
+    call ESMF_GridCompGet(comp, vm=vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_VMGet(vm, petCount=petCount, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    
+    ! Create the destination Array and add it to the import State
+    call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    distgrid = ESMF_DistGridCreate(minIndex=(/1,1/), maxIndex=(/1000,1500/), &
+      regDecomp=(/1,petCount/), rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    array = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+      indexflag=ESMF_INDEX_GLOBAL, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArraySet(array, name="array data", rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_StateAdd(importState, array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    print *, "User Comp2 Init returning"
+
+  end subroutine user_init
+
+
+!--------------------------------------------------------------------------------
+!   !  The Run routine where data is validated.
+!   !
+ 
+  subroutine user_run(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    real(ESMF_KIND_R8)    :: pi
+    type(ESMF_Array)      :: array
+    real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)   ! matching F90 array pointer
+    integer               :: i, j
+    
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Run starting"
+
+    pi = 3.14159d0
+
+    ! Get the destination Array from the import State
+    call ESMF_StateGet(importState, "array data", array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+   
+    ! Gain access to actual data via F90 array pointer
+    call ESMF_ArrayGet(array, localDe=0, farrayPtr=farrayPtr, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+      
+    ! Test Array in import state against exact solution
+!$omp parallel do
+    do j = lbound(farrayPtr, 2), ubound(farrayPtr, 2)
+      do i = lbound(farrayPtr, 1), ubound(farrayPtr, 1)
+        if (abs(farrayPtr(i,j) - (10.0d0 &
+          + 5.0d0 * sin(real(i,ESMF_KIND_R8)/100.d0*pi) &
+          + 2.0d0 * sin(real(j,ESMF_KIND_R8)/150.d0*pi))) > 1.d-8) then
+          rc=ESMF_FAILURE
+          return ! bail out
+        endif
+      enddo
+    enddo
+!$omp end parallel do
+ 
+    print *, "User Comp2 Run returning"
+
+  end subroutine user_run
+
+
+!--------------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+  subroutine user_final(comp, importState, exportState, clock, rc)
+    type(ESMF_GridComp) :: comp
+    type(ESMF_State) :: importState, exportState
+    type(ESMF_Clock) :: clock
+    integer, intent(out) :: rc
+
+    ! Local variables
+    type(ESMF_DistGrid) :: distgrid
+    type(ESMF_Array) :: array
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+
+    print *, "User Comp2 Final starting"
+
+    call ESMF_StateGet(importState, "array data", array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayGet(array, distgrid=distgrid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_ArrayDestroy(array, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+    call ESMF_DistGridDestroy(distgrid, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail out
+
+    print *, "User Comp2 Final returning"
+
+  end subroutine user_final
+
+end module user_model2
+!\end{verbatim}
