2c0f0c8910ce2d6c1b694fb61e86214b031b6fef
Walter Spector <walter.spector@hpe.com>
2009-06-06 01:05:11 +0000

    Replace incorrect reference to Fortran flush with a reference to the new ESMF_IOUnitFlush. This affects the commentary for th Print methods that appear in the reference manual.

--- src/Infrastructure/ArrayBundle/interface/ESMF_ArrayBundle.F90
+++ src/Infrastructure/ArrayBundle/interface/ESMF_ArrayBundle.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_ArrayBundle.F90,v 1.7 2009/01/21 21:37:58 cdeluca Exp $
+! $Id: ESMF_ArrayBundle.F90,v 1.8 2009/06/06 01:05:11 w6ws Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2009, University Corporation for Atmospheric Research, 
@@ -100,7 +100,7 @@ module ESMF_ArrayBundleMod
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
   character(*), parameter, private :: version = &
-    '$Id: ESMF_ArrayBundle.F90,v 1.7 2009/01/21 21:37:58 cdeluca Exp $'
+    '$Id: ESMF_ArrayBundle.F90,v 1.8 2009/06/06 01:05:11 w6ws Exp $'
 
 !==============================================================================
 ! 
@@ -449,9 +449,8 @@ contains
 !   Note:  Many {\tt ESMF\_<class>Print} methods are implemented in C++.
 !   On some platforms/compilers there is a potential issue with interleaving
 !   Fortran and C++ output to {\tt stdout} such that it doesn't appear in
-!   the expected order.  If this occurs, it is recommended to use the
-!   standard Fortran call {\tt flush(6)} as a workaround until this issue
-!   is fixed in a future release. \\
+!   the expected order.  If this occurs, the {\tt ESMF\_IOUnitFlush()} method
+!   may be used on unit 6 to get coherent output.  \\
 !
 !   The arguments are:
 !   \begin{description}
