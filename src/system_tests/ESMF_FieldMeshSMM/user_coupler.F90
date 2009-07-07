e131891b7dea96d6abcee889312ff29d33bea5b0
Fei Liu <Fei.Liu@gmail.com>
2009-07-07 14:04:23 +0000

    initial implementation of Mesh based Field SMM system test; note that the components must run on an inclusive set of PETs because Mesh is not yet reconcilable

---
+++ src/system_tests/ESMF_FieldMeshSMM/user_coupler.F90
@@ -0,0 +1,198 @@
+! $Id: user_coupler.F90,v 1.1 2009/07/07 14:04:23 feiliu Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User-supplied Coupler
+!
+!
+!\begin{verbatim}
+
+    module user_coupler
+
+    ! ESMF Framework module
+    use ESMF_Mod
+    
+    implicit none
+    
+    public usercpl_register
+        
+    ! global data
+    type(ESMF_RouteHandle), save :: routehandle
+
+    contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+    subroutine usercpl_register(comp, rc)
+      type(ESMF_CplComp) :: comp
+      integer :: rc
+
+      rc = ESMF_SUCCESS
+      print *, "in user setservices routine"
+
+      ! Register the callback routines.
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETINIT, user_init, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETRUN, user_run, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      call ESMF_CplCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+
+      print *, "Registered Initialize, Run, and Finalize routines"
+
+    end subroutine
+
+!-------------------------------------------------------------------------
+!   !User Comp Component created by higher level calls, here is the
+!   ! Initialization routine.
+ 
+    
+    subroutine user_init(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer, intent(out) :: rc
+
+      ! Local variables
+      integer :: itemcount
+      type(ESMF_Field) :: humidity1, humidity2
+      type(ESMF_VM) :: vm
+      integer(ESMF_KIND_I4), allocatable          :: factorList(:)
+      integer, allocatable                        :: factorIndexList(:,:)
+      integer       :: lpe
+
+      rc = ESMF_SUCCESS
+      print *, "User Coupler Init starting"
+
+      call ESMF_CplCompGet(comp, vm=vm, rc=rc)
+      call ESMF_VMGet(vm, localPET=lpe, rc=rc)
+
+      ! uncomment the following when locstream supports reconcile
+
+      ! Since the components we are coupling between are running concurrently,
+      ! they have each separately created ESMF objects.   We are planning to
+      ! use a communications call (SMM) here, so first we must make a new
+      ! call to reconcile the object lists in all the import and export states.
+
+      !call ESMF_StateReconcile(importState, vm, rc=rc)
+      !if(rc/=ESMF_SUCCESS) return
+
+      !call ESMF_StateReconcile(exportState, vm, rc=rc)
+      !if(rc/=ESMF_SUCCESS) return
+
+      call ESMF_StateGet(importState, itemcount=itemcount, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      print *, "Import State contains ", itemcount, " items."
+      call ESMF_StateGet(exportState, itemcount=itemcount, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      print *, "Export State contains ", itemcount, " items."
+       
+      ! Get input data
+      call ESMF_StateGet(importState, "humidity", humidity1, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      ! call ESMF_FieldPrint(humidity1, rc=rc)
+
+      ! Get location of output data
+      call ESMF_StateGet(exportState, "humidity", humidity2, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      ! call ESMF_FieldPrint(humidity2, rc=rc)
+
+      ! Get VM from coupler component to send down to SMM Store routine
+      call ESMF_CplCompGet(comp, vm=vm, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+
+      ! These are fields on different Grids - call SMMStore to set
+      ! up the SMM structure
+      allocate(factorList(3))
+      allocate(factorIndexList(2,3))
+      factorList = (/1,2,3/)
+      factorIndexList(1,:) = (/lpe*3+1,lpe*3+2,lpe*3+3/)
+      factorIndexList(2,:) = (/lpe*3+1,lpe*3+2,lpe*3+3/)
+
+      call ESMF_FieldSMMStore(humidity1, humidity2, routehandle, &
+            factorList, factorIndexList, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+
+      print *, "User Coupler Init returning"
+   
+    end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is coupled.
+!   !
+ 
+    subroutine user_run(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer, intent(out) :: rc
+
+      ! Local variables
+      type(ESMF_Field) :: humidity1, humidity2
+
+      rc = ESMF_SUCCESS
+      print *, "User Coupler Run starting"
+
+      ! Get input data
+      call ESMF_StateGet(importState, "humidity", humidity1, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      ! call ESMF_FieldPrint(humidity1, rc=rc)
+
+      ! Get location of output data
+      call ESMF_StateGet(exportState, "humidity", humidity2, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+      ! call ESMF_FieldPrint(humidity2, rc=rc)
+
+      ! These are fields on different Grids - call SMM to rearrange
+      !  the data.   The communication pattern was computed at init,
+      !  this simply has to execute the send and receive equivalents.
+
+      call ESMF_FieldSMM(humidity1, humidity2, routehandle, rc=rc)
+      if(rc/=ESMF_SUCCESS) return
+
+      ! Data is moved directly to the field in the output state, so no
+      ! "put" is needed here.
+ 
+      print *, "User Coupler Run returning"
+
+    end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+    subroutine user_final(comp, importState, exportState, clock, rc)
+      type(ESMF_CplComp) :: comp
+      type(ESMF_State) :: importState, exportState
+      type(ESMF_Clock) :: clock
+      integer, intent(out) :: rc
+
+      ! Local variables
+
+      rc = ESMF_SUCCESS
+      print *, "User Coupler Final starting"
+   
+      ! Release resources stored for the SMM
+      call ESMF_FieldSMMRelease(routehandle, rc)
+      if(rc/=ESMF_SUCCESS) return
+
+      print *, "User Coupler Final returning"
+
+    end subroutine user_final
+
+
+    end module user_coupler
+    
+!\end{verbatim}
+    
---
+++ src/system_tests/ESMF_FieldMeshSMM/user_model1.F90
@@ -0,0 +1,200 @@
+! $Id: user_model1.F90,v 1.1 2009/07/07 14:04:23 feiliu Exp $
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User-supplied Component, most recent interface revision.
+!
+!
+!\begin{verbatim}
+
+    module user_model1
+
+    ! ESMF Framework module
+    use ESMF_Mod
+
+    implicit none
+    
+    public userm1_register
+        
+    contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+    subroutine userm1_register(comp, rc)
+        type(ESMF_GridComp), intent(inout) :: comp
+        integer, intent(out) :: rc
+
+        ! local variables
+
+        rc = ESMF_SUCCESS
+        print *, "in user register routine"
+
+        ! Register the callback routines.
+
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, user_init, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, user_run, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, user_final, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+
+        print *, "Registered Initialize, Run, and Finalize routines"
+
+    end subroutine
+
+!-------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   Initialization routine.
+ 
+    
+    subroutine user_init(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp), intent(inout) :: comp
+        type(ESMF_State), intent(inout) :: importState, exportState
+        type(ESMF_Clock), intent(in) :: clock
+        integer, intent(out) :: rc
+
+       ! Local variables
+        type(ESMF_Field) :: humidity
+        type(ESMF_Mesh)  :: mesh
+        type(ESMF_VM) :: vm
+        type(ESMF_ArraySpec) :: arrayspec
+        integer(ESMF_KIND_I4), dimension(:), pointer :: srcfptr
+        integer :: npets, de_id
+
+        integer                                     :: n_node, n_elem, i, j, l
+        integer, allocatable :: nodeId(:)
+        real(ESMF_KIND_R8), allocatable :: nodeCoord(:)
+        integer, allocatable :: nodeOwner(:)
+
+        integer, allocatable :: elemId(:)
+        integer, allocatable :: elemType(:)
+        integer, allocatable :: elemConn(:)
+        integer              :: conn(16) = (/1,2,5,4,2,3,6,5,4,5,8,7,5,6,9,8/)
+
+        ! Query component for VM and create a layout with the right breakdown
+        call ESMF_GridCompGet(comp, vm=vm, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+        call ESMF_VMGet(vm, localPet=de_id, petCount=npets, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+
+        rc = ESMF_SUCCESS
+        print *, de_id, "User Comp 1 Init starting"
+
+        ! Add a "humidity" field to the export state.
+        n_node = 9
+        n_elem = 4
+        
+        allocate(nodeId(n_node), nodeCoord(2*n_node), nodeOwner(n_node))
+        allocate(elemId(n_elem), elemType(n_elem), elemConn(n_elem*4))
+
+        do i = 1, n_node
+            nodeId(i) = i
+        enddo
+        do i = 1, 3
+            do j = 1, 3
+                l = (i-1)*3+j
+                nodeCoord(2*l-1) = i
+                nodeCoord(2*l) = j
+            enddo
+        enddo
+        nodeOwner = 0
+        do i = 1, n_elem
+            elemId(i) = i
+            elemType(i) = 9             ! Quard
+        enddo
+        elemConn = conn
+
+        mesh = ESMF_MeshCreate(2,2,nodeId, nodeCoord, nodeOwner, elemId, elemType,elemConn,rc)
+        if(rc/=ESMF_SUCCESS) return
+
+        call ESMF_ArraySpecSet(arrayspec, 1, ESMF_TYPEKIND_I4, rc=rc)
+        if (rc .ne. ESMF_SUCCESS) return
+
+        humidity = ESMF_FieldCreate(mesh, arrayspec, &
+            name="humidity", rc=rc)
+        if (rc .ne. ESMF_SUCCESS) return
+
+        call ESMF_FieldGet(humidity, localDe=0, farray=srcfptr, rc=rc)
+        if (rc .ne. ESMF_SUCCESS) return
+
+        srcfptr = 1
+
+        ! Set up a 2D real array
+        call ESMF_ArraySpecSet(arrayspec, rank=2, &
+                               typekind=ESMF_TYPEKIND_R8)
+        if (rc .ne. ESMF_SUCCESS) return
+
+        call ESMF_StateAdd(exportState, humidity, rc=rc)
+        if (rc .ne. ESMF_SUCCESS) return
+     !   call ESMF_StatePrint(exportState, rc=rc)
+
+        print *, de_id, "User Comp 1 Init returning"
+
+    end subroutine user_init
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is computed.
+!   !
+ 
+    subroutine user_run(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp), intent(inout) :: comp
+        type(ESMF_State), intent(inout) :: importState, exportState
+        type(ESMF_Clock), intent(in) :: clock
+        integer, intent(out) :: rc
+
+       ! Local variables
+
+        rc = ESMF_SUCCESS
+        print *, "User Comp Run starting"
+
+        print *, "User Comp Run returning"
+
+    end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+    subroutine user_final(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp), intent(inout) :: comp
+        type(ESMF_State), intent(inout) :: importState, exportState
+        type(ESMF_Clock), intent(in) :: clock
+        integer, intent(out) :: rc
+
+        ! Local variables
+        type(ESMF_Field) :: humidity
+        type(ESMF_Mesh)  :: mesh
+
+        rc = ESMF_SUCCESS
+        print *, "User Comp Final starting"
+
+        ! Get our local info
+        call ESMF_StateGet(exportState, "humidity", humidity, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+        call ESMF_FieldGet(humidity, mesh=mesh, rc=rc)
+        if(rc/=ESMF_SUCCESS) return
+
+        call ESMF_FieldDestroy(humidity, rc)
+        if(rc/=ESMF_SUCCESS) return
+        call ESMF_MeshDestroy(mesh, rc)
+        if(rc/=ESMF_SUCCESS) return
+        print *, "User Comp Final returning"
+
+    end subroutine user_final
+
+
+    end module user_model1
+    
+!\end{verbatim}
+    
