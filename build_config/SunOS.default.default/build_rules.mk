91d8682d8e9a226e5a79922e15eaf41e344f0817
Silverio Vasquez <silverio.vasquez@noaa.gov>
2009-07-13 19:52:38 +0000

    Added "-template=no%extdef" option as recommened by Sun.

--- build_config/SunOS.default.default/build_rules.mk
+++ build_config/SunOS.default.default/build_rules.mk
@@ -1,4 +1,4 @@
-# $Id: build_rules.mk,v 1.19 2009/07/08 22:03:38 svasquez Exp $
+# $Id: build_rules.mk,v 1.20 2009/07/13 19:52:38 svasquez Exp $
 # 
 # SunOS.default.default
 #
@@ -61,14 +61,14 @@ ESMF_F90COMPILEOPTS += -xpp=cpp
 # 32- vs. 64-bit ABI
 #
 ifeq ($(ESMF_ABI),32)
-ESMF_CXXCOMPILEOPTS       += -m32 -library=stlport4
-ESMF_CXXLINKOPTS          += -m32 -library=stlport4
+ESMF_CXXCOMPILEOPTS       += -m32 -library=stlport4 -template=no%extdef
+ESMF_CXXLINKOPTS          += -m32 -library=stlport4 -template=no%extdef
 ESMF_F90COMPILEOPTS       += -m32
 ESMF_F90LINKOPTS          += -m32 -library=stlport4
 endif
 ifeq ($(ESMF_ABI),64)
-ESMF_CXXCOMPILEOPTS       += -m64 -library=stlport4
-ESMF_CXXLINKOPTS          += -m64 -library=stlport4
+ESMF_CXXCOMPILEOPTS       += -m64 -library=stlport4 -template=no%extdef
+ESMF_CXXLINKOPTS          += -m64 -library=stlport4 -template=no%extdef
 ESMF_F90COMPILEOPTS       += -m64
 ESMF_F90LINKOPTS          += -m64 -library=stlport4
 endif
