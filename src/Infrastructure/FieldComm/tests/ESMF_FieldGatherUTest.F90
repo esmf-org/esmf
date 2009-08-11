e6d1bb3fabfe9e8c833f56b30a8bf89ed527e8f4
Silverio Vasquez <silverio.vasquez@noaa.gov>
2009-08-11 20:47:07 +0000

    Separate exhaustive local variables.

--- src/Infrastructure/FieldComm/tests/ESMF_FieldGatherUTest.F90
+++ src/Infrastructure/FieldComm/tests/ESMF_FieldGatherUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldGatherUTest.F90,v 1.36 2009/01/21 21:37:59 cdeluca Exp $
+! $Id: ESMF_FieldGatherUTest.F90,v 1.37 2009/08/11 20:47:07 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2009, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@ program ESMF_FieldGatherUTest
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
     character(*), parameter :: version = &
-    '$Id: ESMF_FieldGatherUTest.F90,v 1.36 2009/01/21 21:37:59 cdeluca Exp $'
+    '$Id: ESMF_FieldGatherUTest.F90,v 1.37 2009/08/11 20:47:07 svasquez Exp $'
 !------------------------------------------------------------------------------
 
     ! cumulative result: count failures; no failures equals "all pass"
@@ -45,11 +45,15 @@ program ESMF_FieldGatherUTest
     ! individual test result code
     integer :: rc = ESMF_SUCCESS
 
+#ifdef ESMF_TESTEXHAUSTIVE
+
     ! individual test name
     character(ESMF_MAXSTR) :: name
 
     ! individual test failure messages
     character(ESMF_MAXSTR*2) :: failMsg
+
+#endif
 
     call ESMF_TestStart(ESMF_SRCLINE, rc=rc)
     if(rc /= ESMF_SUCCESS) &
