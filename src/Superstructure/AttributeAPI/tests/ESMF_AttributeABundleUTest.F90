38a339c4689c44e16ff0bca245913ca5d8909228
Silverio Vasquez <silverio.vasquez@noaa.gov>
2009-08-12 22:53:57 +0000

    DEleted unused local variables and separated exhuastive tests local variables from non-exhaustive.

--- src/Superstructure/AttributeAPI/tests/ESMF_AttributeABundleUTest.F90
+++ src/Superstructure/AttributeAPI/tests/ESMF_AttributeABundleUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_AttributeABundleUTest.F90,v 1.18 2009/08/05 00:06:21 rokuingh Exp $
+! $Id: ESMF_AttributeABundleUTest.F90,v 1.19 2009/08/12 22:53:57 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2009, University Corporation for Atmospheric Research,
@@ -35,7 +35,7 @@ program ESMF_AttributeArrayBundleUTest
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_AttributeABundleUTest.F90,v 1.18 2009/08/05 00:06:21 rokuingh Exp $'
+      '$Id: ESMF_AttributeABundleUTest.F90,v 1.19 2009/08/12 22:53:57 svasquez Exp $'
 !------------------------------------------------------------------------------
 
 !-------------------------------------------------------------------------
@@ -51,10 +51,24 @@ program ESMF_AttributeArrayBundleUTest
       type(ESMF_Array)               :: afb
       type(ESMF_ArraySpec) :: arrayspec
       type(ESMF_DistGrid) :: distgrid
-      character(ESMF_MAXSTR) :: conv, nestconv, purp, nestpurp, attrname, &
-                                attrnameOut, attrvalue
-      integer                :: rc, count, items, itemCount
+      character(ESMF_MAXSTR) :: attrname, attrnameOut, attrvalue
+      integer                :: rc, count, items
       type(ESMF_TypeKind)    :: attrTK
+
+      real(kind=8)                     :: inR8, outR8, defaultR8, dfltoutR8
+      real(kind=8), dimension(3)       :: inR8l, defaultR8l, dfltoutR8l, outR8l
+      character(ESMF_MAXSTR)                     :: inChar, outChar, defaultChar, dfltoutChar
+  
+
+  
+      ! cumulative result: count failures; no failures equals "all pass"
+      integer :: result = 0
+
+#ifdef ESMF_TESTEXHAUSTIVE
+
+      ! local variables
+      character(ESMF_MAXSTR) :: conv, nestconv, purp, nestpurp
+      integer                :: itemCount
 
       integer(kind=4)                     :: inI4, outI4, defaultI4, dfltoutI4
       integer(kind=4), dimension(3)       :: inI4l, outI4l, defaultI4l, dfltoutI4l
@@ -62,27 +76,23 @@ program ESMF_AttributeArrayBundleUTest
       integer(kind=8), dimension(3)       :: inI8l, outI8l, defaultI8l, dfltoutI8l
       real(kind=4)                     :: inR4, outR4, defaultR4, dfltoutR4
       real(kind=4), dimension(3)       :: inR4l, outR4l, defaultR4l, dfltoutR4l
-      real(kind=8)                     :: inR8, outR8, defaultR8, dfltoutR8
-      real(kind=8), dimension(3)       :: inR8l, defaultR8l, dfltoutR8l, outR8l
       real(kind=8), dimension(10)       :: outR8lLong
-      character(ESMF_MAXSTR)                     :: inChar, outChar, defaultChar, dfltoutChar, &
-                                                    inEmpty, outEmpty
+      character(ESMF_MAXSTR)                     :: inEmpty, outEmpty
       character(ESMF_MAXSTR), dimension(3)       :: inCharl, defaultCharl, dfltoutCharl, outCharl
       character(ESMF_MAXSTR), dimension(10)       :: outCharlLong
       logical                          :: inLog, outLog, defaultLog, dfltoutLog
       logical, dimension(3)            :: inLogl, defaultLogl, dfltoutLogl, outLogl
       logical, dimension(10)            :: outLoglLong
-  
-      character(ESMF_MAXSTR), dimension(3)  :: attpackList, attpackListOut, &
+
+      character(ESMF_MAXSTR), dimension(3)  :: attpackList, &
                                                attpackListOut2, attpackDfltList, &
                                                attpackListOut3, attpackListOut4, &
-                                               attpackDfltList2   
+                                               attpackDfltList2
       character(ESMF_MAXSTR), dimension(12) :: attpackListTNames, attpackListTNames2
 
-  
-      ! cumulative result: count failures; no failures equals "all pass"
-      integer :: result = 0
 
+
+#endif
 !-------------------------------------------------------------------------------
 !  The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
 !  always run. When the environment variable, EXHAUSTIVE, is set to ON then
