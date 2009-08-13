669bab3ea76eb27855b978624ffc5749c7992734
Walter Spector <walter.spector@hpe.com>
2009-08-13 23:27:25 +0000

    Add new target architecture - uses the Intel Fortran compiler and the Microsoft 'cl' C++ compiler.

---
+++ build_config/MinGW.intelcl.default/ESMC_Conf.h
@@ -0,0 +1,56 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1 2009/08/13 23:27:25 w6ws Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2009, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the University of Illinois-NCSA License.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_mingw
+
+#define FTN(func) func
+
+#if defined (__cplusplus)
+// Typedef to match the data type of the 'hidden' string length
+// argument that Fortran uses when passing CHARACTER strings.
+// On Windows, 'long' is always 32 bits.  So conditional code to
+// select between 'int' and 'long long' is needed.
+#if defined (S32)
+typedef int ESMCI_FortranStrLenArg;
+#elif defined (Sx86_64_32)
+typedef int ESMCI_FortranStrLenArg;
+#elif (defined (Sx86_64_small) || defined (S64))
+typedef long long ESMCI_FortranStrLenArg;
+#elif defined (Sx86_64_medium)
+typedef long long ESMCI_FortranStrLenArg;
+#else
+#error Can't typedef ESMCI_FortranStrLenArg
+#endif
+#endif
+
+#define ESMC_PRESENT(arg) ( (arg) != 0 )
+
+#ifdef S32
+#define ESMC_POINTER_SIZE 4
+#endif
+#ifdef Sx86_64_32
+#define ESMC_POINTER_SIZE 4
+#endif
+#if (defined (Sx86_64_small) || defined (S64))
+#define ESMC_POINTER_SIZE 8
+#endif
+#ifdef Sx86_64_medium
+#define ESMC_POINTER_SIZE 8
+#endif
+
+#endif
---
+++ build_config/MinGW.intelcl.default/build_rules.mk
@@ -0,0 +1,230 @@
+# $Id: build_rules.mk,v 1.1 2009/08/13 23:27:25 w6ws Exp $
+#
+# MinGW.intel.default
+#
+
+############################################################
+# Default compiler setting.
+#
+ESMF_F90DEFAULT         = ifort
+ESMF_F90COMPILEOPTS    += -unix -Qfpp
+ESMF_CXXDEFAULT         = cl
+
+# Turn off some noise
+ESMF_CXXCOMPILEOPTS    += -nologo
+ESMF_F90COMPILEOPTS    += -nologo
+# Specify that .C files are c++
+ESMF_CXXCOMPILEOPTS    += -TP
+# Specify the c++ exception model
+ESMF_CXXCOMPILEOPTS    += -EHsc
+# Eliminate warnings about using _s variants of library functions
+ESMF_CXXCOMPILEOPTS    += -D_CRT_SECURE_NO_WARNINGS
+
+############################################################
+# Default MPI setting.
+#
+ifeq ($(ESMF_COMM),default)
+export ESMF_COMM := mpiuni
+endif
+
+############################################################
+# MPI dependent settings.
+#
+ifeq ($(ESMF_COMM),mpiuni)
+# MPI stub library -----------------------------------------
+ESMF_F90COMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILECPPFLAGS+= -DESMF_MPIUNI
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_DIR)/src/Infrastructure/stubs/mpiuni
+ESMF_MPIRUNDEFAULT      = $(ESMF_DIR)/src/Infrastructure/stubs/mpiuni/mpirun
+else
+ifeq ($(ESMF_COMM),msmpi)
+# Microsofts version of MPICH2 on CCS 2003 is generally at:
+ESMF_MSMPIDIR        = /c/"Program Files"/"Microsoft Compute Cluster Pack"
+ESMF_MSMPIDIRW       = c:/"Program Files"/"Microsoft Compute Cluster Pack"
+# and on HPC 2008:
+# ESMF_MSMPIDIR        = /c/"Program Files"/"Microsoft HPC Pack 2008 SDK"
+ESMF_CXXCOMPILECPPFLAGS+= -D__int64="long long"
+ESMF_F90COMPILEPATHS   += -I$(ESMF_MSMPIDIR)/Include
+ESMF_CXXCOMPILEPATHS   += -I$(ESMF_MSMPIDIR)/Include
+ESMF_MPILIBPATH          = -libpath:$(ESMF_MSMPIDIRW)/Lib/amd64
+ESMF_F90LINKLIBS       += msmpi.lib
+ESMF_CXXLINKLIBS       += msmpi.lib
+ESMF_MPIRUNDEFAULT      = mpiexec $(ESMF_MPILAUNCHOPTIONS)
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec $(ESMF_MPILAUNCHOPTIONS)
+else
+ifeq ($(ESMF_COMM),user)
+# User specified flags -------------------------------------
+else
+$(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
+endif
+endif
+
+############################################################
+# Print compiler version string
+#
+ESMF_F90COMPILER_VERSION    = ${ESMF_F90COMPILER} -logo
+ESMF_CXXCOMPILER_VERSION    = ${ESMF_CXXCOMPILER}
+
+############################################################
+# Force Fortran symbols lower case
+#
+ESMF_F90COMPILEOPTS += -names:lowercase
+
+############################################################
+# Windows does not have support for POSIX IPC (memory mapped files)
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_POSIXIPC
+
+############################################################
+# Windows does not have support for POSIX dynamic linking
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_DLFCN
+
+############################################################
+# Windows does not have support for "gethostid()"
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_GETHOSTID
+
+############################################################
+# Windows does not have support for signals
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_SIGNALS
+
+############################################################
+# Windows does not have support for system(3c) call
+#
+ESMF_CXXCOMPILECPPFLAGS += -DESMF_NO_SYSTEMCALL
+
+############################################################
+# Windows does not have support for the times system call
+#
+ESMF_CXXCOMPILECPPFLAGS += -DNO_TIMES
+
+############################################################
+# Windows does not have support for Pthreads
+#
+ESMF_PTHREADS := OFF
+
+############################################################
+# Fortran symbol convention
+#
+#ifeq ($(ESMF_FORTRANSYMBOLS),default)
+#ESMF_F90COMPILEOPTS       += -fno-second-underscore
+#ESMF_F90LINKOPTS          += -fno-second-underscore
+#ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_SINGLEUNDERSCORE
+#else
+#ifeq ($(ESMF_FORTRANSYMBOLS),lowercase_singleunderscore)
+#ESMF_F90COMPILEOPTS       += -fno-second-underscore
+#ESMF_F90LINKOPTS          += -fno-second-underscore
+#ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_SINGLEUNDERSCORE
+#else
+#ifeq ($(ESMF_FORTRANSYMBOLS),lowercase_doubleunderscore)
+#ESMF_F90COMPILEOPTS       +=
+#ESMF_F90LINKOPTS          +=
+#ESMF_CXXCOMPILEOPTS       += -DESMF_LOWERCASE_DOUBLEUNDERSCORE
+#else
+#$(error "ESMF_FORTRANSYMBOLS = $(ESMF_FORTRANSYMBOLS)" not supported by ESMF and/or this platform)
+#endif
+#endif
+#endif
+
+############################################################
+# Construct the ABISTRING
+#
+ifeq ($(ESMF_MACHINE),x86_64)
+ifeq ($(ESMF_ABI),32)
+ESMF_ABISTRING := $(ESMF_MACHINE)_32
+endif
+ifeq ($(ESMF_ABI),64)
+ESMF_ABISTRING := x86_64_small
+endif
+endif
+
+############################################################
+# Set memory model compiler flags according to ABISTRING
+#
+ifeq ($(ESMF_ABISTRING),x86_64_32)
+ESMF_F90COMPILEOPTS     += -m32
+ESMF_F90LINKOPTS        += -m32
+ESMF_CXXCOMPILEOPTS     += -m32
+ESMF_CXXLINKOPTS        += -m32
+endif
+ifeq ($(ESMF_ABISTRING),x86_64_small)
+ESMF_F90COMPILEOPTS     += -m64 -mcmodel=small
+ESMF_F90LINKOPTS        += -m64 -mcmodel=small
+ESMF_CXXCOMPILEOPTS     += -m64 -mcmodel=small
+ESMF_CXXLINKOPTS        += -m64 -mcmodel=small
+endif
+ifeq ($(ESMF_ABISTRING),x86_64_medium)
+ESMF_F90COMPILEOPTS     += -mcmodel=medium
+ESMF_F90LINKOPTS        += -mcmodel=medium
+ESMF_CXXCOMPILEOPTS     += -mcmodel=medium
+ESMF_CXXLINKOPTS        += -mcmodel=medium
+endif
+
+############################################################
+# Need this until the file convention is fixed (then remove these two lines)
+#
+ESMF_F90COMPILEFREENOCPP = -fpp0 -FR
+ESMF_F90COMPILEFIXCPP    = -fpp
+
+###########################################################
+# Special debugging flags
+#
+ESMF_F90OPTFLAG_G           = -Od -debug -traceback
+ESMF_CXXOPTFLAG_G           = -Od -Zi
+
+###########################################################
+# Default optlevel
+#
+ESMF_OPTLEVELDEFAULT        = 2
+
+###########################################################
+# Change default tools, "true" is a noop executable
+#
+ESMF_ARDEFAULT              = lib
+ESMF_ARCREATEFLAGSDEFAULT   =
+ESMF_AREXTRACTDEFAULT       = $(ESMF_ARDEFAULT) -extract:
+ESMF_RANLIBDEFAULT          = true
+
+############################################################
+# Blank out variables to prevent rpath encoding
+#
+ESMF_F90LINKRPATHS      =
+ESMF_CXXLINKRPATHS      =
+
+###########################################################
+# OpenMP compiler and linker flags
+#
+ESMF_OPENMP_F90COMPILEOPTS += -Qopenmp
+ESMF_OPENMP_CXXCOMPILEOPTS +=  -openmp
+ESMF_OPENMP_F90LINKOPTS    += -Qopenmp
+ESMF_OPENMP_CXXLINKOPTS    +=  -openmp
+
+###########################################################
+# Determine where libraries are located
+#
+ESMF_F90LINKPATHS = -libpath:`$(ESMF_DIR)/scripts/path_mingw2win $(ESMF_LDIR)`
+
+############################################################
+# Determine where libraries are located
+#
+ESMF_CXXLINKPATHS = -libpath:`$(ESMF_DIR)/scripts/path_mingw2win $(ESMF_LDIR)`
+
+############################################################
+# Link against libesmf.a using the F90 linker front-end
+#
+ESMF_F90LINKLIBS += -link -debug -libpath:`$(ESMF_DIR)/scripts/path_mingw2win $(ESMF_LDIR)` $(ESMF_MPILIBPATH)
+ESMF_F90ESMFLINKLIBS = libesmf.a $(ESMF_F90LINKLIBS)
+
+############################################################
+# Link against libesmf.a using the C++ linker front-end
+#
+ESMF_CXXLINKLIBS += -link -libpath:`$(ESMF_DIR)/scripts/path_mingw2win $(ESMF_LDIR)` $(ESMF_MPILIBPATH)
+ESMF_CXXESMFLINKLIBS = libesmf.a $(ESMF_CXXLINKLIBS)
+
+############################################################
+# Blank out shared library options
+#
+ESMF_SL_LIBS_TO_MAKE  =
