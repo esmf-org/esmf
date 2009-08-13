ed1c55157fe40abc5a9d38697889e012ffb98e40
Silverio Vasquez <silverio.vasquez@noaa.gov>
2009-08-13 19:32:48 +0000

    Separated the exhaustive test local variables from the non-exhaustive.

--- src/Superstructure/AttributeAPI/tests/ESMF_AttReadFieldUTest.F90
+++ src/Superstructure/AttributeAPI/tests/ESMF_AttReadFieldUTest.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_AttReadFieldUTest.F90,v 1.3 2009/05/01 04:43:55 eschwab Exp $
+! $Id: ESMF_AttReadFieldUTest.F90,v 1.4 2009/08/13 19:32:48 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2009, University Corporation for Atmospheric Research,
@@ -36,7 +36,7 @@ program ESMF_AttReadFieldUTest
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
       character(*), parameter :: version = &
-      '$Id: ESMF_AttReadFieldUTest.F90,v 1.3 2009/05/01 04:43:55 eschwab Exp $'
+      '$Id: ESMF_AttReadFieldUTest.F90,v 1.4 2009/08/13 19:32:48 svasquez Exp $'
 !------------------------------------------------------------------------------
 
 !-------------------------------------------------------------------------
@@ -48,13 +48,16 @@ program ESMF_AttReadFieldUTest
 
       ! local variables
       type(ESMF_Field)       :: field
-      character(ESMF_MAXSTR) :: attrname, attrvalue, outChar
-      character(ESMF_MAXSTR) :: conv, purp
       logical                :: xercesNotPresent
       integer                :: rc
 
       ! cumulative result: count failures; no failures equals "all pass"
       integer :: result = 0
+#ifdef ESMF_TESTEXHAUSTIVE
+      character(ESMF_MAXSTR) :: attrname, attrvalue, outChar
+      character(ESMF_MAXSTR) :: conv, purp
+
+#endif
 
 !-------------------------------------------------------------------------------
 !  The unit tests are divided into Sanity and Exhaustive. The Sanity tests are
