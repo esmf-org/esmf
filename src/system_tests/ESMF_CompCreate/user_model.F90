719419f9a2fccfb6c7a5be6bf4b252ac0dfebb66
Gerhard Theurich <theurich@sourcespring.net>
2009-10-21 16:57:40 +0000

    - bail out of user code on error

--- src/system_tests/ESMF_CompCreate/user_model.F90
+++ src/system_tests/ESMF_CompCreate/user_model.F90
@@ -1,4 +1,4 @@
-! $Id: user_model.F90,v 1.26 2009/09/22 14:56:31 feiliu Exp $
+! $Id: user_model.F90,v 1.27 2009/10/21 16:57:40 theurich Exp $
 !
 ! Example/test code which shows User Component calls.
 
@@ -59,9 +59,12 @@
 
     ! First test whether ESMF-threading is supported on this machine
     call ESMF_VMGetGlobal(vm, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail on error
     call ESMF_VMGet(vm, pthreadsEnabledFlag=pthreadsEnabled, rc=rc)
+    if (rc/=ESMF_SUCCESS) return ! bail on error
     if (pthreadsEnabled) then
       call ESMF_GridCompSetVMMinThreads(comp, rc=rc)
+      if (rc/=ESMF_SUCCESS) return ! bail on error
     endif
 #endif
 
@@ -75,18 +78,24 @@
         type(mydata), pointer :: mydatablock
         type(wrapper) :: mywrapper
 
+        rc = ESMF_SUCCESS
+
         print *, "In user register routine"
 
         ! Register the callback routines.
 
         call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, userRoutine=user_init1,&
           phase=1, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_GridCompSetEntryPoint(comp, ESMF_SETINIT, userRoutine=user_init2,&
           phase=2, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_GridCompSetEntryPoint(comp, ESMF_SETRUN, userRoutine=user_run, &
           rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_GridCompSetEntryPoint(comp, ESMF_SETFINAL, userRoutine=user_final, &
           rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
         print *, "Registered Initialize, Run, and Finalize routines"
 
@@ -100,11 +109,11 @@
         mywrapper%wrap => mydatablock
 
         call ESMF_GridCompSetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
         print *, "Registered local datablock"
         print *, " initial data =", mydatablock%index, &
                      mydatablock%scale_factor, mydatablock%flag
-        rc = ESMF_SUCCESS
 
     end subroutine
 
@@ -119,8 +128,9 @@
         type(ESMF_Clock) :: clock
         integer, intent(out) :: rc
 
-        print *, "first init routine called"
         rc = ESMF_SUCCESS
+
+        print *, "first init routine called"
 
     end subroutine user_init1
 
@@ -140,17 +150,22 @@
         type(mydata), pointer :: mydatablock
         type(wrapper) :: mywrapper
 
+        rc = ESMF_SUCCESS
+
         print *, "User Comp Init 2 starting"
 
         ! This is where the model specific setup code goes.  
 
         call ESMF_GridCompPrint(comp, "", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_StatePrint(exportState, "", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
         print *, "init, ready to call get data ptr"
         nullify(mydatablock)
         mywrapper%wrap => mydatablock
         call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         mydatablock => mywrapper%wrap
         print *, "init, back from data ptr"
 
@@ -159,13 +174,14 @@
 
         ! Add an empty "humidity" field to the export state.
         humidity = ESMF_FieldCreateEmpty(name="humidity", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_StateAdd(exportState, humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_StatePrint(exportState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
         print *, "User Comp Init returning"
    
-        rc = ESMF_SUCCESS
-
     end subroutine user_init2
 
 
@@ -182,9 +198,10 @@
         type(ESMF_Field) :: humidity
         integer, save :: onetime=1              ! static variable
 
-        integer :: status
         type(mydata), pointer :: mydatablock
         type(wrapper) :: mywrapper
+
+        rc = ESMF_SUCCESS
 
         print *, "User Comp Run starting"
 
@@ -192,8 +209,10 @@
         ! one component to the import before this call.  For now, copy the
         ! field from the export state to import state by hand.
         if (onetime .gt. 0) then
-          call ESMF_StateGet(exportState, "humidity", humidity, rc=status)
-          call ESMF_StateAdd(importState, humidity, rc=status)
+          call ESMF_StateGet(exportState, "humidity", humidity, rc=rc)
+          if (rc/=ESMF_SUCCESS) return ! bail on error    
+          call ESMF_StateAdd(importState, humidity, rc=rc)
+          if (rc/=ESMF_SUCCESS) return ! bail on error    
           onetime = 0
         endif
 
@@ -202,24 +221,27 @@
         nullify(mydatablock)
         mywrapper%wrap => mydatablock
         call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         mydatablock => mywrapper%wrap
 
         print *, "run, local data =", mydatablock%index, &
                         mydatablock%scale_factor, mydatablock%flag
    
-        call ESMF_StatePrint(importState, rc=status)
-        call ESMF_StateGet(importState, "humidity", humidity, rc=status)
-        call ESMF_FieldPrint(humidity, rc=status)
+        call ESMF_StatePrint(importState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_StateGet(importState, "humidity", humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_FieldPrint(humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
         ! This is where the model specific computation goes.
 
         ! Here is where the output state is updated.
         !call ESMF_StateAdd(exportState, humidity, rc=status)
-        call ESMF_StatePrint(exportState, rc=status)
- 
-        print *, "User Comp Run returning"
+        call ESMF_StatePrint(exportState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
 
-        rc = status
+        print *, "User Comp Run returning"
 
     end subroutine user_run
 
@@ -239,6 +261,8 @@
         type(wrapper) :: mywrapper
         type(ESMF_Field) :: humidity
 
+        rc = ESMF_SUCCESS
+
         print *, "User Comp Final starting"
     
         ! Query component for information.
@@ -246,6 +270,7 @@
         nullify(mydatablock)
         mywrapper%wrap => mydatablock
         call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         mydatablock => mywrapper%wrap
    
         print *, "final, local data =", mydatablock%index, &
@@ -254,12 +279,12 @@
         ! garbage collection 
         deallocate(mydatablock)
         call ESMF_StateGet(importState, "humidity", humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         call ESMF_FieldDestroy(humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
         
         print *, "User Comp Final returning"
    
-        rc = ESMF_SUCCESS
-
     end subroutine user_final
 
 
