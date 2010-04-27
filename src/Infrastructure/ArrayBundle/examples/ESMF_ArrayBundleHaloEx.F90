fbcb6345b9f08a7c43b4e9c835c4533543324ac3
Gerhard Theurich <theurich@sourcespring.net>
2010-04-27 22:55:44 +0000

    - add ArrayBundleHaloEx.F90 code and link into ArrayBundle doc

---
+++ src/Infrastructure/ArrayBundle/examples/ESMF_ArrayBundleHaloEx.F90
@@ -0,0 +1,160 @@
+! $Id: ESMF_ArrayBundleHaloEx.F90,v 1.1 2010/04/27 22:55:44 theurich Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2010, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
+
+!==============================================================================
+!ESMF_MULTI_PROC_EXAMPLE        String used by test script to count examples.
+!==============================================================================
+
+program ESMF_ArrayBundleHaloEx
+
+  use ESMF_Mod
+  
+  implicit none
+  
+  ! local variables
+  integer:: rc, petCount, localPet, i
+  type(ESMF_VM):: vm
+  type(ESMF_DistGrid):: distgrid
+  type(ESMF_ArraySpec):: arrayspec
+  type(ESMF_Array):: arrayList(5)
+  type(ESMF_ArrayBundle):: arraybundle
+  type(ESMF_RouteHandle):: haloHandle
+  
+  real(ESMF_KIND_R8), pointer :: farrayPtr(:,:)
+  
+  ! result code
+  integer :: finalrc
+  
+  finalrc = ESMF_SUCCESS
+  call ESMF_Initialize(vm=vm, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  call ESMF_VMGet(vm, localPet=localPet, petCount=petCount, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  
+  if (petCount /= 4) then
+    finalrc = ESMF_FAILURE
+    goto 10
+  endif
+  
+!BOE
+! \subsubsection{Communication - Halo}
+!
+! One of the most fundamental communication pattern in domain decomposition
+! codes is the {\em halo} operation. The ESMF Array class supports halos
+! by allowing memory for extra elements to be allocated on each DE. See
+! section \ref{Array:Halo} for a discussion of the Array level halo operation.
+! The ArrayBundle level extents the Array halo operation to bundles of Arrays.
+!
+! First create an {\tt ESMF\_ArrayBundle} object containing a set of ESMF
+! Arrays.
+!EOE
+  call ESMF_ArraySpecSet(arrayspec, typekind=ESMF_TYPEKIND_R8, rank=2, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  distgrid = ESMF_DistGridCreate(minIndex=(/1,1/), maxIndex=(/4,4/), &
+    regDecomp=(/2,2/), rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  arrayList(1) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  arrayList(2) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  arrayList(3) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+    totalLWidth=(/1,1/), totalUWidth=(/1,1/), rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  arrayList(4) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+    totalLWidth=(/1,1/), totalUWidth=(/1,1/), rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  arrayList(5) = ESMF_ArrayCreate(arrayspec=arrayspec, distgrid=distgrid, &
+    totalLWidth=(/1,1/), totalUWidth=(/1,0/), rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+!BOC
+  arraybundle = ESMF_ArrayBundleCreate(arrayList=arrayList, &
+    name="MyArrayBundle", rc=rc)
+!EOC  
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  
+  ! initialize Arrays with values for verification
+  do i=1, 5
+    call ESMF_ArrayGet(arrayList(i), farrayPtr=farrayPtr, rc=rc)
+    if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+    farrayPtr(:,:) = real(localPet + 100 * i, ESMF_KIND_R8)
+  enddo
+  
+!BOE
+! The ArrayBundle object can be treated as a single entity. The
+! {\tt ESMF\_ArrayBundleHaloStore()} call determines the most efficient
+! halo exchange pattern for {\em all} Arrays that are part of
+! {\tt arraybundle}.
+!EOE
+  
+!BOC
+  call ESMF_ArrayBundleHaloStore(arraybundle=arraybundle, &
+    routehandle=haloHandle, rc=rc)
+!EOC
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+    
+!BOE
+! The halo exchange pattern stored in {\tt haloHandle} can now be applied to
+! the {\tt arraybundle} object, or any other ArrayBundle that is weakly
+! congruent to the one used during the {\tt ESMF\_ArrayBundleHaloStore()} call.
+!EOE
+    
+!BOC
+  call ESMF_ArrayBundleHalo(arraybundle=arraybundle, routehandle=haloHandle, &
+    rc=rc)
+!EOC
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  
+!BOE
+! Finally, when no longer needed, the resources held by {\tt haloHandle} need
+! to be returned to the system by calling {\tt ESMF\_ArrayBundleHaloRelease()}.
+!EOE
+
+!BOC
+  call ESMF_ArrayBundleHaloRelease(routehandle=haloHandle, rc=rc)
+!EOC
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  
+  ! print Arrays to allow verification by inspection
+  do i=1, 5
+    call ESMF_ArrayPrint(arrayList(i), rc=rc)
+    if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  enddo
+
+!BOE
+! Finally the ArrayBundle object can be destroyed.
+!EOE
+
+!BOC
+  call ESMF_ArrayBundleDestroy(arraybundle, rc=rc)
+!EOC  
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+
+  do i=1, 5
+    call ESMF_ArrayDestroy(arrayList(i), rc=rc)
+    if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+  enddo
+  
+  call ESMF_DistGridDestroy(distgrid, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(terminationflag=ESMF_ABORT)
+
+
+10 continue
+  call ESMF_Finalize(rc=rc)
+  
+  if (rc/=ESMF_SUCCESS) finalrc = ESMF_FAILURE
+  if (finalrc==ESMF_SUCCESS) then
+    print *, "PASS: ESMF_ArrayBundleHaloEx.F90"
+  else
+    print *, "FAIL: ESMF_ArrayBundleHaloEx.F90"
+  endif
+  
+end program
