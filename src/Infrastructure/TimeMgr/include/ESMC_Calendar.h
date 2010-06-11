27503403443243819a05e95e1976da539e8fac94
Gerhard Theurich <theurich@sourcespring.net>
2010-06-11 23:33:44 +0000

    - Cleanup C API instrumentation, removing ARGUMENTS section which is   redundant in case of C API.

--- src/Infrastructure/TimeMgr/include/ESMC_Calendar.h
+++ src/Infrastructure/TimeMgr/include/ESMC_Calendar.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Calendar.h,v 1.67 2010/04/30 22:39:28 svasquez Exp $
+// $Id: ESMC_Calendar.h,v 1.68 2010/06/11 23:33:44 theurich Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2010, University Corporation for Atmospheric Research,
@@ -48,60 +48,53 @@ extern "C" {
 #endif
 
 // Class declaration type
+
+//-----------------------------------------------------------------------------
 typedef struct {
   // private:  // members opaque on C side, philosophically.
     void *ptr;
     // TODO:  implement isInit initialization like in F90 API?
 } ESMC_Calendar;
+//-----------------------------------------------------------------------------
 
 // Class API
+
+//-----------------------------------------------------------------------------
 //BOP
 // !IROUTINE: ESMC_CalendarCreate - Create a Calendar
 //
 // !INTERFACE:
-ESMC_Calendar ESMC_CalendarCreate(const char *name,
-                                  enum ESMC_CalendarType calendarType, int *rc);  
+ESMC_Calendar ESMC_CalendarCreate(const char *name, enum ESMC_CalendarType
+  calendarType, int *rc);  
 
-// ARGUMENTS :
-// 	char	name,		
-//	enum ESMC_CalendarType calendarType,
-//	int *rc
-//
-// 
 // !DESCRIPTION:
 //	Create a Calendar of calendar type.
 //EOP
+//-----------------------------------------------------------------------------
 
-
+//-----------------------------------------------------------------------------
 //BOP
 // !IROUTINE: ESMC_CalendarDestroy - Destroy a Calendar
 //
 // !INTERFACE:
 int ESMC_CalendarDestroy(ESMC_Calendar *calendar);
 
-// ARGUMENTS :
-//	ESMC_Calendar *calendar
-//
-//
 // !DESCRIPTION:
 //      Destroy a Calendar.
 //EOP
+//-----------------------------------------------------------------------------
 
-
+//-----------------------------------------------------------------------------
 //BOP
 // !IROUTINE: ESMC_CalendarPrint - Print a Calendar
 //
 // !INTERFACE:
 int ESMC_CalendarPrint(ESMC_Calendar calendar);
 
-// ARGUMENTS :
-//      ESMC_Calendar calendar
-//
-//
 // !DESCRIPTION:
 //      Print a Calendar.
 //EOP
-
+//-----------------------------------------------------------------------------
 
 
 #ifdef __cplusplus
