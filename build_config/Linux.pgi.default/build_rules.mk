40dbf301842ec742ca242871cda9a281b836fc52
Silverio Vasquez <silverio.vasquez@noaa.gov>
2010-06-18 17:11:45 +0000

    Added support for mvapich.

--- build_config/Linux.pgi.default/build_rules.mk
+++ build_config/Linux.pgi.default/build_rules.mk
@@ -1,4 +1,4 @@
-# $Id: build_rules.mk,v 1.49.2.1 2010/01/09 18:18:12 svasquez Exp $
+# $Id: build_rules.mk,v 1.49.2.2 2010/06/18 17:11:45 svasquez Exp $
 #
 # Linux.pgi.default
 #
@@ -42,6 +42,13 @@ ESMF_CXXDEFAULT         = mpicxx
 ESMF_MPIRUNDEFAULT      = mpirun $(ESMF_MPILAUNCHOPTIONS)
 ESMF_MPIMPMDRUNDEFAULT  = mpiexec $(ESMF_MPILAUNCHOPTIONS)
 else
+ifeq ($(ESMF_COMM),mvapich)
+# Mvapich ---------------------------------------------------
+ESMF_F90DEFAULT         = mpif90
+ESMF_CXXDEFAULT         = mpicxx
+ESMF_MPIRUNDEFAULT      = mpirun $(ESMF_MPILAUNCHOPTIONS)
+ESMF_MPIMPMDRUNDEFAULT  = mpiexec $(ESMF_MPILAUNCHOPTIONS)
+else
 ifeq ($(ESMF_COMM),mvapich2)
 # Mvapich2 ---------------------------------------------------
 ESMF_F90DEFAULT         = mpif90
@@ -76,6 +83,7 @@ ifeq ($(ESMF_COMM),user)
 # User specified flags -------------------------------------
 else
 $(error Invalid ESMF_COMM setting: $(ESMF_COMM))
+endif
 endif
 endif
 endif
