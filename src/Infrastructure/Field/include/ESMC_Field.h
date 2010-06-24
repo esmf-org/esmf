78140e1871a9dd3ba09fdb048fca36f498998999
Fei Liu <Fei.Liu@gmail.com>
2010-06-24 02:47:06 +0000

    ESMC_Field C API documentation

--- src/Infrastructure/Field/include/ESMC_Field.h
+++ src/Infrastructure/Field/include/ESMC_Field.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Field.h,v 1.19 2010/03/04 18:57:42 svasquez Exp $
+// $Id: ESMC_Field.h,v 1.20 2010/06/24 02:47:06 feiliu Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2010, University Corporation for Atmospheric Research, 
@@ -49,17 +49,168 @@ extern "C" {
 typedef void * ESMC_Field;
 
 // Class API
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldCreate - Create a Field
+//
+// !INTERFACE:
 ESMC_Field ESMC_FieldCreate(ESMC_Mesh mesh, ESMC_ArraySpec arrayspec,
   ESMC_InterfaceInt gridToFieldMap, ESMC_InterfaceInt ungriddedLBound,
   ESMC_InterfaceInt ungriddedUBound, const char *name, int *rc);
+// !DESCRIPTION:
+//
+//  Creates a {\tt ESMC\_Field} object
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[mesh]
+//    A {\tt ESMC\_Mesh} object.
+//  \item[arrayspec]
+//    A {\tt ESMC\_ArraySpec} object describing data type and kind specification.
+//  \item[gridToFieldMap]
+//    List with number of elements equal to the grid's dimCount. The list
+//    elements map each dimension of the grid to a dimension in the field by
+//    specifying the appropriate field dimension index. The default is to map all of
+//    the grid's dimensions against the lowest dimensions of the field in sequence,
+//    i.e. gridToFieldMap = (/1,2,3,.../). The values of all gridToFieldMap entries
+//    must be greater than or equal to one and smaller than or equal to the field
+//    rank. It is erroneous to specify the same gridToFieldMap entry multiple times.
+//    The total ungridded dimensions in the field  are the total field dimensions
+//    less the dimensions in the grid. Ungridded dimensions must be in the same order
+//    they are stored in the field. If the Field dimCount is less than the Mesh
+//    dimCount then the default gridToFieldMap will contain zeros for the rightmost
+//    entries. A zero entry in the gridToFieldMap indicates that the particular Mesh
+//    dimension will be replicating the Field across the DEs along this direction.
+//  \item[ungriddedLBound]
+//    Lower bounds of the ungridded dimensions of the field. The number of elements
+//    in the ungriddedLBound is equal to the number of ungridded dimensions in the
+//    field. All ungridded dimensions of the field are also undistributed. When field
+//    dimension count is greater than grid dimension count, both ungriddedLBound and
+//    ungriddedUBound must be specified. When both are specified the values are
+//    checked for consistency. Note that the the ordering of these ungridded
+//    dimensions is the same as their order in the field.  
+//  \item[ungriddedUBound]
+//    Upper bounds of the ungridded dimensions of the field. The number of elements
+//    in the ungriddedUBound is equal to the number of ungridded dimensions in the
+//    field. All ungridded dimensions of the field are also undistributed. When field
+//    dimension count is greater than grid dimension count, both ungriddedLBound and
+//    ungriddedUBound must be specified. When both are specified the values are
+//    checked for consistency. Note that the the ordering of these ungridded
+//    dimensions is the same as their order in the field.  
+//  \item[{[name]}]
+//    The name for the newly created field.  If not specified, i.e. NULL,
+//    a default unique name will be generated: "FieldNNN" where NNN
+//    is a unique sequence number from 001 to 999.
+//  \item[{[rc]}]
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldDestroy - Destroy a Field
+//
+// !INTERFACE:
 int ESMC_FieldDestroy(ESMC_Field *field);
+// !DESCRIPTION:
+//
+//  Releases all resources associated with this {\tt ESMC\_Field}.
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[field]
+//    Destroy contents of this {\tt ESMC\_Field}.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldPrint - Print the internal information of a Field
+//
+// !INTERFACE:
 int ESMC_FieldPrint(ESMC_Field field);
+// !DESCRIPTION:
+//
+//  Print the internal information within this {\tt ESMC\_Field}.
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[field]
+//    Print contents of this {\tt ESMC\_Field}.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldGetMesh - Get the internal Mesh stored in the Field
+//
+// !INTERFACE:
 ESMC_Mesh ESMC_FieldGetMesh(ESMC_Field field, int *rc);
+// !DESCRIPTION:
+//
+//  Get the internal Mesh stored in the {\tt ESMC\_Field}.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[field]
+//    Get the internal Mesh stored in this {\tt ESMC\_Field}.
+//  \item[{[rc]}]
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldGetArray - Get the internal Array stored in the Field
+//
+// !INTERFACE:
 ESMC_Array ESMC_FieldGetArray(ESMC_Field field, int *rc);
+// !DESCRIPTION:
+//
+//  Get the internal Array stored in the {\tt ESMC\_Field}.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[field]
+//    Get the internal Array stored in this {\tt ESMC\_Field}.
+//  \item[{[rc]}]
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_FieldGetPtr - Get the internal Fortran data pointer stored in the Field
+//
+// !INTERFACE:
 void *ESMC_FieldGetPtr(ESMC_Field field, int localDe, int *rc);
+// !DESCRIPTION:
+//
+//  Get the internal Fortran data pointer stored in the {\tt ESMC\_Field}.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[field]
+//    Get the internal Fortran data pointer stored in this {\tt ESMC\_Field}.
+//  \item[{[rc]}]
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
 
 
 #if defined (__cplusplus)
