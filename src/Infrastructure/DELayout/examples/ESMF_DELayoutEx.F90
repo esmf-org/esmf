0049a0744b11b757a5f6d13aa4c6599c3a78b080
Silverio Vasquez <silverio.vasquez@noaa.gov>
2010-06-29 19:08:01 +0000

    Added creation of *Log file.

--- src/Infrastructure/DELayout/examples/ESMF_DELayoutEx.F90
+++ src/Infrastructure/DELayout/examples/ESMF_DELayoutEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_DELayoutEx.F90,v 1.20 2010/03/04 18:57:42 svasquez Exp $
+! $Id: ESMF_DELayoutEx.F90,v 1.21 2010/06/29 19:08:01 svasquez Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2010, University Corporation for Atmospheric Research,
@@ -31,7 +31,8 @@ program ESMF_DELayoutEx
   integer :: finalrc
   finalrc = ESMF_SUCCESS
   
-  call ESMF_Initialize(vm=vm, rc=rc)
+  call ESMF_Initialize(vm=vm, defaultlogfilename="DELayoutEx.Log", &
+                    defaultlogtype=ESMF_LOG_MULTI, rc=rc)
   if (rc /= ESMF_SUCCESS) goto 99
   call ESMF_VMGet(vm, localPET=localPET, petCount=petCount, rc=rc)
   if (rc /= ESMF_SUCCESS) goto 99
