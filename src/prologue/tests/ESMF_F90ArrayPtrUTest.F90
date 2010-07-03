e590ae5bf59ccabe7359fa9fdbdb2f61cc714c96
Gerhard Theurich <theurich@sourcespring.net>
2010-07-03 02:41:00 +0000

    - add missing ESMF preamble

--- src/prologue/tests/ESMF_F90ArrayPtrUTest.F90
+++ src/prologue/tests/ESMF_F90ArrayPtrUTest.F90
@@ -1,3 +1,14 @@
+! $Id: ESMF_F90ArrayPtrUTest.F90,v 1.10 2010/07/03 02:41:00 theurich Exp $
+!
+! Earth System Modeling Framework
+! Copyright 2002-2010, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
 
     program test
     
