c8e1ea730259fa5edd576fc37bcc848430135550
Walter Spector <walter.spector@hpe.com>
2010-07-22 00:20:11 +0000

    First pass at documenting the C API for the C Reference Manual.

--- src/Superstructure/State/include/ESMC_State.h
+++ src/Superstructure/State/include/ESMC_State.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_State.h,v 1.30 2010/04/08 19:14:46 w6ws Exp $
+// $Id: ESMC_State.h,v 1.31 2010/07/22 00:20:11 w6ws Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2010, University Corporation for Atmospheric Research, 
@@ -45,13 +45,199 @@ extern "C" {
 typedef void* ESMC_State;
 
 // Class API
-ESMC_State ESMC_StateCreate(const char *name, int *rc);
-int ESMC_StateDestroy(ESMC_State *state);
-int ESMC_StatePrint(ESMC_State state);
-int ESMC_StateAddArray(ESMC_State state, ESMC_Array array);
-int ESMC_StateAddField(ESMC_State state, ESMC_Field field);
-int ESMC_StateGetArray(ESMC_State state, const char *name, ESMC_Array *array);
-int ESMC_StateGetField(ESMC_State state, const char *name, ESMC_Field *field);
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateCreate - Create an Array
+//
+// !INTERFACE:
+ESMC_State ESMC_StateCreate(
+  const char *name,  // in
+  int *rc);          // out
+
+// !RETURN VALUE:
+//  Newly created ESMC_State object.
+//
+// !DESCRIPTION:
+//
+//  Create an {\tt ESMC\_State} object.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[name]}]
+//    The name for the State object. If not specified, i.e. NULL,
+//    a default unique name will be generated: "StateNNN" where NNN
+//    is a unique sequence number from 001 to 999.
+//  \item[{[rc]}]
+//    Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateDestroy - Destroy a State
+//
+// !INTERFACE:
+int ESMC_StateDestroy(
+  ESMC_State *state);  // in
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Destroy a {\tt ESMC\_State} object.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State to be destroyed.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StatePrint - Print the contents of a State
+//
+// !INTERFACE:
+int ESMC_StatePrint(
+  ESMC_State state);  // in
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Prints the contents of a {\tt ESMC\_State} object.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State to be printed.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateAddArray - Add an Array object to a State
+//
+// !INTERFACE:
+int ESMC_StateAddArray(
+  ESMC_State state,   // in
+  ESMC_Array array);  // in
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Add an Array object to a {\tt ESMC\_State} object.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State object.
+//  \item[{[array]}]
+//    The Array object to be included within the State.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateAddArray - Add a Field object to a State
+//
+// !INTERFACE:
+int ESMC_StateAddField(
+  ESMC_State state,   // in
+  ESMC_Field field);  // in
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Add an Array object to a {\tt ESMC\_State} object.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State object.
+//  \item[{[array]}]
+//    The Array object to be included within the State.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateGetArray - Obtains an Array object from a State
+//
+// !INTERFACE:
+int ESMC_StateGetArray(
+  ESMC_State state,    // in
+  const char *name,    // in
+  ESMC_Array *array);  // out
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Obtain a pointer to an {\tt ESMC\_Array} object contained within
+//  a State.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State object.
+//  \item[{[name]}]
+//    The name of the desired Array object.
+//  \item[{[array]}]
+//    A pointer to the Array object.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//BOP
+// !IROUTINE: ESMC_StateGetField - Obtains a Field object from a State
+//
+// !INTERFACE:
+int ESMC_StateGetField(
+  ESMC_State state,    // in
+  const char *name,    // in
+  ESMC_Field *field);  // out
+
+// !RETURN VALUE:
+//  Return code; equals ESMF_SUCCESS if there are no errors.
+//
+// !DESCRIPTION:
+//
+//  Obtain a pointer to a {\tt ESMC\_Field} object contained within
+//  a State.
+//
+//  The arguments are:
+//  \begin{description}
+//  \item[{[state]}]
+//    The State object.
+//  \item[{[name]}]
+//    The name of the desired Field object.
+//  \item[{[array]}]
+//    A pointer to the Field object.
+//  \end{description}
+//
+//EOP
+//-----------------------------------------------------------------------------
 
 #ifdef __cplusplus
 } // extern "C"
