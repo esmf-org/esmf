83603a8c3336bb413c739bff3f86631e3e1fae51
Silverio Vasquez <silverio.vasquez@noaa.gov>
2010-12-15 23:47:19 +0000

    Fix line too long problem.

--- src/Superstructure/AttributeAPI/examples/ESMF_AttributeUpdateMod.F90
+++ src/Superstructure/AttributeAPI/examples/ESMF_AttributeUpdateMod.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_AttributeUpdateMod.F90,v 1.26 2010/11/03 22:48:45 theurich Exp $
+! $Id: ESMF_AttributeUpdateMod.F90,v 1.27 2010/12/15 23:47:19 svasquez Exp $
 !
 ! Example/test code which shows User Component calls.
 
@@ -434,20 +434,32 @@ module ESMF_AttributeUpdateMod
 
     ! Create the Grid Attribute Package
     call ESMF_AttributeAdd(grid,convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'GridType','Cubed sphere',convention=convESMF, purpose=purpGen, rc=status)    
-    call ESMF_AttributeSet(grid,'CongruentTiles',.true.,convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'NumberOfGridTiles','1',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'DimensionOrder','YX',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'DiscretizationType','Logically Rectangular',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'GeometryType','Sphere',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'IsConformal',.false.,convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'IsRegular',.false.,convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'IsUniform',.false.,convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'NorthPoleLocation','long: 0.0 lat: 90.0',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'NumberOfCells','53457',convention=convESMF, purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'GridType','Cubed sphere',convention=convESMF, &
+			purpose=purpGen, rc=status)    
+    call ESMF_AttributeSet(grid,'CongruentTiles',.true.,convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'NumberOfGridTiles','1',convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'DimensionOrder','YX',convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'DiscretizationType','Logically Rectangular', &
+			convention=convESMF, purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'GeometryType','Sphere',convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'IsConformal',.false.,convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'IsRegular',.false.,convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'IsUniform',.false.,convention=convESMF, &
+			purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'NorthPoleLocation','long: 0.0 lat: 90.0', &
+			convention=convESMF, purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'NumberOfCells','53457',convention=convESMF, &
+			purpose=purpGen, rc=status)
     call ESMF_AttributeSet(grid,'NX','96',convention=convESMF, purpose=purpGen, rc=status)
     call ESMF_AttributeSet(grid,'NY','96',convention=convESMF, purpose=purpGen, rc=status)
-    call ESMF_AttributeSet(grid,'HorizontalResolution','C48',convention=convESMF, purpose=purpGen, rc=status)
+    call ESMF_AttributeSet(grid,'HorizontalResolution','C48',convention=convESMF, &
+			purpose=purpGen, rc=status)
     if (status .ne. ESMF_SUCCESS) return
 
 !EOC
