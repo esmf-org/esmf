107d0d68bdfc1692a821ebbe3044b75afecc7292
Fei Liu <Fei.Liu@gmail.com>
2010-12-27 17:23:48 +0000

    fix doc format for web display

--- src/Infrastructure/FieldComm/examples/ESMF_FieldSMMEx.F90
+++ src/Infrastructure/FieldComm/examples/ESMF_FieldSMMEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldSMMEx.F90,v 1.19 2010/12/27 16:06:33 feiliu Exp $
+! $Id: ESMF_FieldSMMEx.F90,v 1.20 2010/12/27 17:23:48 feiliu Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2010, University Corporation for Atmospheric Research,
@@ -35,7 +35,7 @@
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
     character(*), parameter :: version = &
-    '$Id: ESMF_FieldSMMEx.F90,v 1.19 2010/12/27 16:06:33 feiliu Exp $'
+    '$Id: ESMF_FieldSMMEx.F90,v 1.20 2010/12/27 17:23:48 feiliu Exp $'
 !------------------------------------------------------------------------------
 
     ! Local variables
@@ -263,11 +263,13 @@
     if(rc .ne. ESMF_SUCCESS) finalrc = ESMF_FAILURE
 !EOC
 !BOE
-    ! create src_farray, srcArray, and srcField
-    ! +  PET0  +  PET1  +  PET2  +  PET3  +
-    ! +--------+--------+--------+--------+
-    !      1        2        3        4            ! value
-    ! 1        4        8        12       16       ! bounds
+! create 1D src_farray, srcArray, and srcField
+!\begin {verbatim}
+! +  PET0  +  PET1  +  PET2  +  PET3  +
+! +--------+--------+--------+--------+
+!      1        2        3        4            ! value
+! 1        4        8        12       16       ! bounds of seq indices
+!\end {verbatim}
 !EOE
 !BOC
     allocate(src_farray2(tlb(1):tub(1)) )
@@ -283,21 +285,24 @@
 
 !EOC
 !BOE
-    ! Create dstField on the following distribution:
-    ! +  PET0  +  PET1  +  PET2  +  PET3  +
-    ! +--------+--------+--------+--------+
-    ! |        |        |        |        |
-    ! |   1    |   4    |   7    |   10   |
-    ! |        |        |        |        |
-    ! +--------+--------+--------+--------+
-    ! |        |        |        |        |
-    ! |   2    |   5    |   8    |   11   |
-    ! |        |        |        |        |
-    ! +--------+--------+--------+--------+
-    ! |        |        |        |        |
-    ! |   3    |   6    |   9    |   12   |
-    ! |        |        |        |        |
-    ! +--------+--------+--------+--------+
+! Create 2D dstField on the following distribution 
+! (numbers are the sequence indices):
+!\begin{verbatim}
+! +  PET0  +  PET1  +  PET2  +  PET3  +
+! +--------+--------+--------+--------+
+! |        |        |        |        |
+! |   1    |   4    |   7    |   10   |
+! |        |        |        |        |
+! +--------+--------+--------+--------+
+! |        |        |        |        |
+! |   2    |   5    |   8    |   11   |
+! |        |        |        |        |
+! +--------+--------+--------+--------+
+! |        |        |        |        |
+! |   3    |   6    |   9    |   12   |
+! |        |        |        |        |
+! +--------+--------+--------+--------+
+!\end{verbatim}
 !EOE
 !BOC
 
