149f83e040e6efbe028fff8601d793491364164f
Gerhard Theurich <theurich@sourcespring.net>
2011-01-11 00:00:39 +0000

    - caught 2 files that had old copyright date

--- src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
+++ src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
@@ -1,8 +1,8 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! $Id: ESMF_RegridWeightGen.F90,v 1.19 2011/01/10 17:57:36 peggyli Exp $
+! $Id: ESMF_RegridWeightGen.F90,v 1.20 2011/01/11 00:00:39 theurich Exp $
 !
 ! Earth System Modeling Framework
-! Copyright 2002-2010, University Corporation for Atmospheric Research,
+! Copyright 2002-2011, University Corporation for Atmospheric Research,
 ! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
 ! Laboratory, University of Michigan, National Centers for Environmental
 ! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
