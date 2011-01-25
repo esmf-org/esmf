2a5714867be79f43a48fabfaaef28de73d91a51b
Gerhard Theurich <theurich@sourcespring.net>
2011-01-25 05:08:19 +0000

    - added dummy version of ESMF_WebServController app

---
+++ src/apps/ESMF_WebServController/ESMF_WebServController.c
@@ -0,0 +1,10 @@
+/* $Id: ESMF_WebServController.c,v 1.1 2011/01/25 05:08:19 theurich Exp $ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+  printf("hello from ESMF_WebServController\n");
+    
+  return 0;
+}
