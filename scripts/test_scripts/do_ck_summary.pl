460c5eb1e9f3d04b05444fd9bd480596b920fabb
Silverio Vasquez <silverio.vasquez@noaa.gov>
2011-03-17 22:21:49 +0000

    Add check results summary of system and unit tests only.

---
+++ scripts/test_scripts/do_ck_summary.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+# $Id: do_ck_summary.pl,v 1.1 2011/03/17 22:21:49 svasquez Exp $
+# This prints a summary of system tests, unit tests and examples.
+# The script calls sys_tests_results, examples_results and 
+# unit_tests_results with the summary option turned on,
+# therefore the summary results are printed to the screen.
+
+
+# Options:
+#
+#  -h   ESMF_TESTSCRIPTS
+#  -d	TEST_DIR
+#  -e	EX_DIR
+#  -b   ESMF_BOPT
+#  
+
+use Getopt::Std;
+use Cwd;
+
+getopts("h:d:e:b:m:", \%options); 
+
+
+	$ESMF_TESTSCRIPTS = "$options{h}"; 
+	$TEST_DIR = "$options{d}"; 
+	$EX_DIR = "$options{e}"; 
+	$ESMF_BOPT = "$options{b}";
+
+
+#
+# Define location of test scripts.
+#
+unshift (@INC, "$ESMF_TESTSCRIPTS");
+#
+# Declare subroutines
+#
+require "sys_tests_results.pl";
+require "unit_tests_results.pl";
+
+# get pwd
+$dir = cwd();
+
+# Call sys_tests_result with SUMMARY request turned on.
+&sys_tests_results("$TEST_DIR","$ESMF_BOPT","1");
+
+# Go back tp pwd
+chdir $dir;
+
+# Call unit_tests_results with SUMMARY request turned on.
+&unit_tests_results("$TEST_DIR","$ESMF_BOPT","1");
+
+# Go back tp pwd
+chdir $dir;
+
+exit;
