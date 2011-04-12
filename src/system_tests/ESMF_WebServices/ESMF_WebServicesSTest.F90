1fcbf7051ab37d1394a7ccd0271a550951e9079b
Kathy Saint <ksaint@mindspring.com>
2011-04-12 15:20:05 +0000

    Initial checkin.

---
+++ src/system_tests/ESMF_WebServices/ESMF_WebServicesSTest.F90
@@ -0,0 +1,186 @@
+! $Id: ESMF_WebServicesSTest.F90,v 1.1 2011/04/12 15:20:05 ksaint Exp $
+!
+!-------------------------------------------------------------------------
+!ESMF_MULTI_PROC_SYSTEM_TEST        String used by test script to count system tests.
+!=========================================================================
+
+!-------------------------------------------------------------------------
+!
+! !DESCRIPTION:
+! System test WebServices.
+!    Component Service startup.
+!
+!-------------------------------------------------------------------------
+!\begin{verbatim}
+
+program ESMF_WebServicesSTest
+#define ESMF_METHOD "program ESMF_WebServicesSTest"
+
+#include "ESMF.h"
+
+  ! ESMF Framework module
+  use ESMF_Mod
+  use ESMF_TestMod
+  use ESMF_WebServMod
+  use ESMF_WebServUserModel
+
+  implicit none
+
+  ! Local variables
+  integer :: localPet, petCount, userrc, localrc, rc=ESMF_SUCCESS
+  character(len=ESMF_MAXSTR) :: cname1, cname2, cplname
+  type(ESMF_VM):: vm
+  type(ESMF_State) :: c1exp, c2imp
+  type(ESMF_GridComp) :: comp1
+  integer :: portNum
+
+  ! cumulative result: count failures; no failures equals "all pass"
+  integer :: result = 0
+
+  ! individual test name
+  character(ESMF_MAXSTR) :: testname
+
+  ! individual test failure message, and final status msg
+  character(ESMF_MAXSTR) :: failMsg, finalMsg
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  write(testname, *) "System Test ESMF_WebServicesSTest"
+  write(failMsg, *) "System Test failure"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  print *, "--------------------------------------- "
+  print *, "Start of ", trim(testname)
+  print *, "--------------------------------------- "
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Create section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!
+  ! Initialize framework and get back default global VM
+  call ESMF_Initialize(vm=vm, defaultlogfilename="WebServicesSTest.Log", &
+                        defaultlogtype=ESMF_LOG_MULTI, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  ! Create the model component
+  cname1 = "user model 1"
+  ! use petList to define comp1 on PET 0,1,2,3
+!  comp1 = ESMF_GridCompCreate(name=cname1, petList=(/0,1,2,3/), rc=localrc)
+  comp1 = ESMF_GridCompCreate(name=cname1, rc=localrc)
+  print *, "Created component ", trim(cname1), "rc =", localrc
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  print *, "Comp Creates finished"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Register section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_GridCompSetVM(comp1, userRoutine=userm1_setvm, rc=localrc)
+  print *, "Comp1 SetVM finished, rc= ", localrc
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_GridCompSetServices(comp1, &
+    userRoutine=ESMF_WebServUserModelRegister, rc=localrc)
+  print *, "Comp1 SetServices finished, rc= ", localrc
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Init section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  c1exp = ESMF_StateCreate(name="comp1 export",  &
+                           stateType=ESMF_STATE_EXPORT, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  c2imp = ESMF_StateCreate(name="comp2 import",  &
+                           stateType=ESMF_STATE_IMPORT, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Start Loop section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  rc = ESMF_SUCCESS
+  portNum = 27060
+  call ESMF_WebServicesLoop(comp1, portNum, rc=rc)
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+! Destroy section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+  call ESMF_GridCompDestroy(comp1, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_StateDestroy(c1exp, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  call ESMF_StateDestroy(c2imp, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+    ESMF_CONTEXT, rcToReturn=rc)) &
+    call ESMF_Finalize(rc=rc, terminationflag=ESMF_ABORT)
+
+  print *, "All Destroy routines done"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+10 continue
+
+  ! Normal ESMF Test output
+  print *, testname, " complete."
+
+  if (rc .eq. ESMF_SUCCESS) then
+    ! Separate message to console, for quick confirmation of success/failure
+    write(finalMsg, *) "SUCCESS: ",trim(testname)," finished correctly."
+    write(0, *) ""
+    write(0, *) trim(testname)
+    write(0, *) trim(finalMsg)
+    write(0, *) ""
+  endif
+
+  print *, "------------------------------------------------------------"
+  print *, "------------------------------------------------------------"
+  print *, "Test finished, localPet = ", localPet
+  print *, "------------------------------------------------------------"
+  print *, "------------------------------------------------------------"
+
+  ! IMPORTANT: ESMF_STest() prints the PASS string and the # of processors in the log
+  ! file that the scripts grep for.
+  call ESMF_STest((rc.eq.ESMF_SUCCESS), testname, failMsg, result, ESMF_SRCLINE)
+
+  call ESMF_Finalize()
+
+end program ESMF_WebServicesSTest
+
+!\end{verbatim}
