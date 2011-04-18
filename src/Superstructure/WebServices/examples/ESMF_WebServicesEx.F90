1aed74008309d4ac2b8aec7b7a280264a8676589
Gerhard Theurich <theurich@sourcespring.net>
2011-04-18 23:01:45 +0000

    - make the dummy code work

--- src/Superstructure/WebServices/examples/ESMF_WebServicesEx.F90
+++ src/Superstructure/WebServices/examples/ESMF_WebServicesEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_WebServicesEx.F90,v 1.1 2011/04/18 18:05:33 theurich Exp $
+! $Id: ESMF_WebServicesEx.F90,v 1.2 2011/04/18 23:01:45 theurich Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2011, University Corporation for Atmospheric Research,
@@ -22,14 +22,39 @@
 !  the WebServices interface.
 !EOE
 !BOC
-program WebServComp
-  ! arguments
-  type(ESMF_GridComp):: gcomp
-  type(ESMF_State):: importState, exportState
-  type(ESMF_Clock):: clock
-  integer, intent(out):: rc
+program WebServicesEx
+  ! ESMF Framework module
+  use ESMF_Mod
+!EOC
+  implicit none
 
-  ! here goes the code    
+  ! Local variables
+  integer :: rc
+  
+  integer :: finalrc
+  finalrc = ESMF_SUCCESS
 
-end program ESMF_AttachMethodsEx
+  call ESMF_Initialize(defaultlogfilename="WebServicesEx.Log", &
+                    defaultlogtype=ESMF_LOG_MULTI, rc=rc)
+  if (rc/=ESMF_SUCCESS) then
+    finalrc = ESMF_FAILURE
+    goto 10
+  endif
+
+!BOC
+  ! here goes the code 
+!EOC
+
+10 continue
+  call ESMF_Finalize(rc=rc)
+  
+  if (rc/=ESMF_SUCCESS) finalrc = ESMF_FAILURE
+  if (finalrc==ESMF_SUCCESS) then
+    print *, "PASS: ESMF_WebServicesEx.F90"
+  else
+    print *, "FAIL: ESMF_WebServicesEx.F90"
+  endif
+
+!BOC
+end program WebServicesEx
 !EOC
