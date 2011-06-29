8e6d88166d56b266ab724a70c7ed41e0e33133ff
Walter Spector <walter.spector@hpe.com>
2011-06-29 14:39:12 +0000

    Replace statetype argument in StateCreates with stateintent.

--- src/Superstructure/AppDriver/seq_pairwise_fdriver_spmd/ParentGridCompTemplate.F90
+++ src/Superstructure/AppDriver/seq_pairwise_fdriver_spmd/ParentGridCompTemplate.F90
@@ -1,4 +1,4 @@
-! $Id: ParentGridCompTemplate.F90,v 1.16 2011/06/27 22:30:36 rokuingh Exp $
+! $Id: ParentGridCompTemplate.F90,v 1.17 2011/06/29 14:39:12 w6ws Exp $
 !
 ! Template code for a Gridded Component which creates 3 child Components:
 !  two Gridded Components which perform a computation and a Coupler component
@@ -93,19 +93,19 @@
 
       ! Now create Import and Export State objects in order to pass data
       ! between the Coupler and the Gridded Components
-      G1imp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_IMPORT, &
+      G1imp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT, &
         name="GComp1 Import")
-      G1exp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_EXPORT, &
+      G1exp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT, &
         name="GComp1 Export")
 
-      G2imp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_IMPORT, &
+      G2imp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT, &
         name="GComp2 Import")
-      G2exp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_EXPORT, &
+      G2exp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT, &
         name="GComp2 Export")
 
-      Cplimp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_IMPORT, &
+      Cplimp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT, &
         name="Coupler Import")
-      Cplexp = ESMF_StateCreate(statetype=ESMF_STATEINTENT_EXPORT, &
+      Cplexp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT, &
         name="Coupler Export")
       call ESMF_StateAdd(Cplimp, G1imp, rc=rc)
       call ESMF_StateAdd(Cplimp, G2imp, rc=rc)
