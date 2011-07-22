18e8095186f28fb8c754ad6bf8914905ed3cf218
Gerhard Theurich <theurich@sourcespring.net>
2011-07-22 18:23:21 +0000

    TRUNK -> 520rbranch merge:
    "correct copyright end year to 2011"
    
    TRUNK files with revision:
    
    src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90 - 1.45

--- src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
+++ src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
@@ -1,8 +1,8 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! $Id: ESMF_RegridWeightGen.F90,v 1.44 2011/07/07 19:56:00 rokuingh Exp $
+! $Id: ESMF_RegridWeightGen.F90,v 1.44.2.1 2011/07/22 18:23:21 theurich Exp $
 !
 ! Earth System Modeling Framework
-! Copyright 2002-2010, University Corporation for Atmospheric Research,
+! Copyright 2002-2011, University Corporation for Atmospheric Research,
 ! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
 ! Laboratory, University of Michigan, National Centers for Environmental
 ! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
