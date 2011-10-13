45bfc4ef01c0b26e18c09708386771ef80fbc8cd
Fei Liu <Fei.Liu@gmail.com>
2011-10-13 15:55:51 +0000

    fix optional arguments doc

--- src/Infrastructure/Field/src/ESMF_FieldSMM.F90
+++ src/Infrastructure/Field/src/ESMF_FieldSMM.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldSMM.F90,v 1.2 2011/10/06 13:46:01 feiliu Exp $
+! $Id: ESMF_FieldSMM.F90,v 1.3 2011/10/13 15:55:51 feiliu Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2011, University Corporation for Atmospheric Research, 
@@ -58,7 +58,7 @@ module ESMF_FieldSMMMod
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
     character(*), parameter, private :: version = &
-      '$Id: ESMF_FieldSMM.F90,v 1.2 2011/10/06 13:46:01 feiliu Exp $'
+      '$Id: ESMF_FieldSMM.F90,v 1.3 2011/10/13 15:55:51 feiliu Exp $'
 
 !------------------------------------------------------------------------------
     interface ESMF_FieldSMMStore
@@ -113,6 +113,13 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
 !   using such Fields in a weakly congruent manner in Field communication methods
 !   leads to undefined behavior.
 !   \end{sloppypar}
+!
+!   The {\tt srcField} and {\tt dstField} arguments are optional in support of
+!   the situation where {\tt srcField} and/or {\tt dstField} are not defined on
+!   all PETs. The {\tt srcField} and {\tt dstField} must be specified on those
+!   PETs that hold source or destination DEs, respectively, but may be omitted
+!   on all other PETs. PETs that hold neither source nor destination DEs may
+!   omit both arguments.
 !
 !   It is erroneous to specify the identical Field object for {\tt srcField} and
 !   {\tt dstField} arguments.
@@ -302,13 +309,6 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
 ! Source and destination Fields may be of different <type><kind>. Further source 
 ! and destination Fields may differ in shape, however, the number of elements 
 ! must match. 
-!
-! The {\tt srcField} and {\tt dstField} arguments are optional in support of
-! the situation where {\tt srcField} and/or {\tt dstField} are not defined on
-! all PETs. The {\tt srcField} and {\tt dstField} must be specified on those
-! PETs that hold source or destination DEs, respectively, but may be omitted
-! on all other PETs. PETs that hold neither source nor destination DEs may
-! omit both arguments.
 !  
 ! It is erroneous to specify the identical Field object for srcField and dstField 
 ! arguments.
