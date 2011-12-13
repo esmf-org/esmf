9744fe0924a50ec2cd317ebb21822db3690149f5
Gerhard Theurich <theurich@sourcespring.net>
2011-12-13 23:14:26 +0000

    TRUNK -> 520rbranch merge:
    "Gerhard's: New Unicos/lc configurations."
    
    TRUNK files with revision up to ESMF_5_3_0_beta_snapshot_18.

---
+++ build_config/Unicos.cce.default/ESMC_Conf.h
@@ -0,0 +1,47 @@
+#ifdef ESMC_RCS_HEADER
+"$Id: ESMC_Conf.h,v 1.1.2.2 2011/12/13 23:14:26 theurich Exp $"
+"Defines the configuration for this machine"
+#endif
+
+#if 0
+Earth System Modeling Framework
+Copyright 2002-2011, University Corporation for Atmospheric Research,
+Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+Laboratory, University of Michigan, National Centers for Environmental
+Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+NASA Goddard Space Flight Center.
+Licensed under the University of Illinois-NCSA License.
+#endif
+
+#if !defined(INCLUDED_CONF_H)
+#define INCLUDED_CONF_H
+
+#define PARCH_linux
+
+#define FTN(func) func##_
+
+#if defined (__cplusplus)
+// Typedef to match the data type of the 'hidden' string length
+// argument that Fortran uses when passing CHARACTER strings.
+typedef int ESMCI_FortranStrLenArg;
+#endif
+
+#define ESMC_PRESENT(arg) ( (arg) != 0 )
+
+#ifdef S32
+#define ESMC_POINTER_SIZE 4
+#endif
+#ifdef S64
+#define ESMC_POINTER_SIZE 8
+#endif
+#ifdef Sx86_64_32
+#define ESMC_POINTER_SIZE 4
+#endif
+#ifdef Sx86_64_small
+#define ESMC_POINTER_SIZE 8
+#endif
+#ifdef Sx86_64_medium
+#define ESMC_POINTER_SIZE 8
+#endif
+
+#endif
