92c0527522e7442d2888f31e2dec315bbabff9ad
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2012-04-02 17:37:54 +0000

    fix docs: missing slash before underscore

--- src/Infrastructure/Mesh/include/ESMC_Mesh.h
+++ src/Infrastructure/Mesh/include/ESMC_Mesh.h
@@ -1,4 +1,4 @@
-// $Id: ESMC_Mesh.h,v 1.40 2012/04/02 16:44:58 rokuingh Exp $
+// $Id: ESMC_Mesh.h,v 1.41 2012/04/02 17:37:54 rokuingh Exp $
 //
 // Earth System Modeling Framework
 // Copyright 2002-2012, University Corporation for Atmospheric Research, 
@@ -124,7 +124,7 @@ int ESMC_MeshAddElements(
 //   \item[elementMask]
 //         An array containing values which can be used for element masking. 
 //         Which values indicate masking are chosen via the srcMaskValues or 
-//         dstMaskValues arguments to ESMF_FieldRegridStore() call. This input 
+//         dstMaskValues arguments to ESMF\_FieldRegridStore() call. This input 
 //         consists of a 1D array the size of the number of elements on this 
 //         PET.
 //   \end{description}
