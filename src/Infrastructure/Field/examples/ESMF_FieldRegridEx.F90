f64c0ba42f913eb2d1a3e0e330d3dd5e32ea7f01
Robert Oehmke <oehmke@ucar.edu>
2012-08-20 21:19:07 +0000

    Make things a bit clearer around pole options and put in new masking description to mirror that in Python.

--- src/Infrastructure/Field/examples/ESMF_FieldRegridEx.F90
+++ src/Infrastructure/Field/examples/ESMF_FieldRegridEx.F90
@@ -1,4 +1,4 @@
-! $Id: ESMF_FieldRegridEx.F90,v 1.69 2012/06/04 20:05:55 rokuingh Exp $
+! $Id: ESMF_FieldRegridEx.F90,v 1.70 2012/08/20 21:19:07 oehmke Exp $
 !
 ! Earth System Modeling Framework
 ! Copyright 2002-2012, University Corporation for Atmospheric Research,
@@ -37,7 +37,7 @@ program ESMF_FieldRegridEx
 !------------------------------------------------------------------------------
 ! The following line turns the CVS identifier string into a printable variable.
   character(*), parameter :: version = &
-    '$Id: ESMF_FieldRegridEx.F90,v 1.69 2012/06/04 20:05:55 rokuingh Exp $'
+    '$Id: ESMF_FieldRegridEx.F90,v 1.70 2012/08/20 21:19:07 oehmke Exp $'
 !------------------------------------------------------------------------------
     
 
@@ -298,18 +298,56 @@ program ESMF_FieldRegridEx
 ! {\tt ESMF\_FieldRegridRelease()} should be used to 
 ! free the associated memory. 
 !
-! In the case that the Grid is on a sphere (coordSys=ESMF\_COORDSYS\_SPH\_DEG or ESMF\_COORDSYS\_SPH\_RAD)
+! In the case that the Grid is on a sphere ({\tt coordSys=ESMF\_COORDSYS\_SPH\_DEG or ESMF\_COORDSYS\_SPH\_RAD})
 ! then the coordinates given in the Grid are interpretted as latitude and longitude values. The coordinates can either be in degrees or radians as indicated by the 
 ! {\tt coordSys} flag set during Grid creation. As is true with many global models, this application currently assumes the latitude and longitude refer to positions on a 
 ! perfect sphere, as opposed to a more complex and accurate representation of the earth's true shape such as would be used in a GIS system. (ESMF's current user base doesn't 
 ! require this level of detail in representing the earth's shape, but it could be added in the future if necessary.)
 !
-! In terms of masking, ESMF regrid currently supports masking for Fields built on structured Grids and element masking for Fields built on 
-! unstructured Meshes. The user may mask out points in the source Field or destination Field or both. To do masking the user sets mask information
-! in the Grid (see~\ref{sec:usage:items}) or Mesh (see~\ref{sec:mesh:mask}) upon which the Fields passed into the {\tt ESMF\_FieldRegridStore()} 
-! call are built. The {\tt srcMaskValues} and {\tt dstMaskValues} arguments can then be used to specify which values in that mask information
-! indicate that a location should be masked out. For example, if {\tt dstMaskValues} is set to 1, then any location that has a value of 1 in the 
-! Grid or Mesh upon which the destination Field is built will be masked out. 
+! For Grids on a sphere, the regridding occurs in 3D Cartesian to avoid
+! problems with periodicity and with the pole singularity. This library
+! supports four options for handling the pole region (i.e. the empty area above the top row of the source grid or below
+! the bottom row of the source grid).  Note that all of these pole options currently only work for the Fields build on the Grid class and not for those built on 
+! the Mesh class. The first option is to leave the pole region empty ({\tt polemethod=ESMF\_POLEMETHOD\_NONE}), in this 
+! case if a destination point lies above or below the 
+! top row of the source grid, it will fail to map, yielding an error (unless {\tt unmappedaction=ESMF\_UNMAPPEDACTION\_IGNORE} is specified).  
+! With the next two options, the pole region is handled by constructing 
+! an artificial pole in the center of the top and bottom row of grid points and then filling
+! in the region from this pole to the edges of the source grid with triangles. 
+! The pole is located at the average of the position of the points surrounding
+! it, but moved outward to be at the same radius as the rest of the points
+! in the grid. The difference between these two artificial pole options is what value is used at the pole. 
+! The default pole option ({\tt polemethod=ESMF\_POLEMETHOD\_ALLAVG}) sets the value at the pole to be the average of the values
+! of all of the grid points surrounding the pole. For the other option ({\tt polemethod=ESMF\_POLEMETHOD\_NPNTAVG}), the user chooses
+! a number N from 1 to the number of source grid points around the pole. The value N is set via the argument {\tt regridPoleNPnts}. For
+! each destination point, the value at the pole is then the average of the N source points
+! surrounding that destination point. For the last pole option ({\tt polemethod=ESMF\_POLEMETHOD\_TEETH}) no artificial pole is constructed, instead the
+! pole region is covered by connecting points across the top and bottom row of the source Grid into triangles. As 
+! this makes the top and bottom of the source sphere flat, for a big enough difference between the size of
+! the source and destination pole regions, this can still result in unmapped destination points.  
+! Only pole option {\tt ESMF\_POLEMETHOD\_NONE} is currently supported with the conservative interpolation method 
+!(i.e. {\tt regridmethod=ESMF\_REGRIDMETHOD\_CONSERVE}). 
+!
+! Masking is the process whereby parts of a grid can be marked to be ignored during an operation, such as regridding. Masking can be used on a source grid to 
+! indicate that certain portions of the grid should not be used to generate regridded data. This is useful, for example, if a portion of source grid contains 
+! unusable values. Masking can also be used on a destination grid to indicate that the portion of the field built on that part of the Grid should not receive 
+! regridded data. This is useful, for example, when part of the grid isn't being used (e.g. the land portion of an ocean grid).
+!
+! ESMF currently supports masking for Fields built on structured Grids and element masking for Fields built on unstructured Meshes. The user may mask out points 
+! in the source Field or destination Field or both. To do masking the user sets mask information in the Grid (see~\ref{sec:usage:items}) or 
+! Mesh (see~\ref{sec:mesh:mask}) upon which the Fields passed into the 
+! {\tt ESMF\_FieldRegridStore()} call are built. The `srcMaskValues' and `dstMaskValues' arguments to that call can then be used to specify which values in that mask 
+! information indicate that a location should be masked out. For example, if `dstMaskValues' is set to (/1,2/), then any location that has a value of 1 or 2 in 
+! the mask information of the Grid or Mesh upon which the destination Field is built will be masked out.
+!
+! Masking behavior differs slightly between regridding methods. For non-conservative regridding methods (e.g. bilinear or high-order patch), masking is done on
+! points. For these methods, masking a destination point means that the point won't participate in regridding (e.g. won't be interpolated to). For these methods, 
+! masking a source point means that the entire source cell using that point is masked out. In other words, if any corner point making up a source cell is masked 
+! then the cell is masked. For conservative regridding methods (e.g. first-order conservative) masking is done on cells. Masking a destination cell means that the 
+! cell won't participate in regridding (e.g. won't be interpolated to). Similarly, masking a source cell means that the cell won't participate in regridding 
+! (e.g. won't be interpolated from). For any type of interpolation method (conservative or non-conservative) the masking is set on the location upon which the 
+! Fields passed into the regridding call are built. For example, if Fields built on {\tt ESMF\_STAGGERLOC\_CENTER} are passed into the {\tt ESMF\_FieldRegridStore()} 
+! call then the masking should also be set in {\tt ESMF\_STAGGERLOC\_CENTER}.
 !
 ! If a destination point can't be mapped to a location in the source grid, the user has two options. The user may ignore those destination points
 ! that can't be mapped by setting the {\tt unmappedaction} argument to {\tt ESMF\_UNMAPPEDACTION\_IGNORE}. (Ignored points won't be included in
@@ -317,7 +355,6 @@ program ESMF_FieldRegridEx
 ! an error if unmapped destination points exist. This is the default behavior, so the user can either not set the {\tt unmappedaction} argument
 ! or the user can set it to {\tt ESMF\_UNMAPPEDACTION\_ERROR}. At this point ESMF does not support extrapolation to destination points outside 
 ! the unmasked source Field. 
-!
 !
 ! ESMF currently supports three options for interpolation: bilinear, patch, and conservative. 
 ! Bilinear interpolation calculates the value for the 
