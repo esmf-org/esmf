62bdafc12424518c68e896d9e3bc8a50f1bb1d1d
Joseph Jacob <jjacob@jpl.nasa.gov>
2013-09-10 16:21:45 -0700

    Reverted to previous commit to remove all of the code supporting the dryrun option from these files.

--- src/ESMF/test/regrid_test/regrid_from_file_test/regrid_check_driver.py
+++ src/ESMF/test/regrid_test/regrid_from_file_test/regrid_check_driver.py
@@ -14,7 +14,6 @@ import os
 import subprocess
 import urllib
 import traceback
-import getopt
 
 try:
     import ESMF
@@ -39,28 +38,10 @@ def cache_data_file (fname):
 # read from a control file.    Retrieve data files for each test from a remote
 # server if they do not exist locally.
 def main():
-    try:
-        # parse options
-        opts, args = getopt.getopt(sys.argv[1:], "", ["dryrun"])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err)
-        usage()
-        sys.exit(2)
-    dryrun = False
-    for o, a in opts:
-        if o == "--dryrun":
-            dryrun = True
-        else:
-            assert False, "unhandled option"
+    # Start up ESMF.
+    esmp = ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
 
-
-    wget_ok = True
-    if not dryrun:
-        # Start up ESMF.
-        esmp = ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
-
-        petCount = esmp.petCount
+    petCount = esmp.petCount
 
     # Create data subdirectory if it doesn't exist.
     if not os.path.exists(DATA_SUBDIR):
@@ -73,12 +54,11 @@ def main():
             (src_fname, dst_fname, regrid_method, options,
              max_err_str) = re.split('\s*:\s*', line)
             max_err = float(max_err_str)
-            if not dryrun:
-                test_str = 'Regrid %s to %s as %s with %s and max_err = %f' % \
-                (src_fname, dst_fname, regrid_method, options,
-                 max_err)
+            test_str = 'Regrid %s to %s as %s with %s and max_err = %f' % \
+                    (src_fname, dst_fname, regrid_method, options,
+                     max_err)
 
-                print '\n' + test_str + ' - START\n'
+            print '\n' + test_str + ' - START\n'
 
             src_fname_full = os.path.join(DATA_SUBDIR, src_fname)
             dst_fname_full = os.path.join(DATA_SUBDIR, dst_fname)
@@ -91,49 +71,41 @@ def main():
             except:
                 print "wget ERROR:\n"
                 traceback.print_exc(file=sys.stdout)
-                wget_ok = False
             else:
-                if not dryrun:
-                    try:
-                        correct = mesh_check(src_fname_full, dst_fname_full,
-                                             regrid_method, options, max_err)
-                    except:
-                        print "Regridding ERROR:\n"
-                        traceback.print_exc(file=sys.stdout)
+                try:
+                    correct = mesh_check(src_fname_full, dst_fname_full,
+                                                     regrid_method, options, max_err)
+                except:
+                    print "Regridding ERROR:\n"
+                    traceback.print_exc(file=sys.stdout)
 
-            if not dryrun:
-                skip = False
-                for i in range(petCount):
-                    for line in open("PET"+str(i)+".ESMF_LogFile"):
-                        if "ESMF_NETCDF not defined when lib was compiled" in line or \
-                            "File format is not supported" in line:
-                            # set skip
-                            skip = True
+            skip = False
+            for i in range(petCount):
+                for line in open("PET"+str(i)+".ESMF_LogFile"):
+                    if "ESMF_NETCDF not defined when lib was compiled" in line or \
+                       "File format is not supported" in line:
+                        # set skip
+                        skip = True
 
-                # print the file
-                print '\n***NOTE*** The log files must be deleted in this test case, they are printed below for future reference\n'
-                if skip:
-                    for line in open("PET"+str(i)+".ESMF_LogFile"):
-                        print line
+            # print the file
+            print '\n***NOTE*** The log files must be deleted in this test case, they are printed below for future reference\n'
+            if skip:
+                for line in open("PET"+str(i)+".ESMF_LogFile"):
+                    print line
 
-                # clean the log files
-                for i in range(petCount):
-                    os.system("echo ' ' > PET"+str(i)+".ESMF_LogFile")
-                    
-                print '\n' + test_str + ' - FINISH\n'
+            # clean the log files
+            for i in range(petCount):
+                os.system("echo ' ' > PET"+str(i)+".ESMF_LogFile")
 
-                if skip:
-                    print 'RESULT: SKIP\n\n'
-                elif correct:
-                    print 'RESULT: PASS\n\n'
-                else:
-                    print 'RESULT: FAIL\n\n'
+            print '\n' + test_str + ' - FINISH\n'
 
-    if dryrun:
-        if wget_ok:
-            print 'RESULT: PASS - wget ok\n\n'
-        else:
-            print 'RESULT: FAIL - wget error\n\n'
+            if skip:
+                print 'RESULT: SKIP\n\n'
+            elif correct:
+                print 'RESULT: PASS\n\n'
+            else:
+                print 'RESULT: FAIL\n\n'
+
     return 0
 
 if __name__ == '__main__':
