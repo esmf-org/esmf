f86f5a29b487e8ec34fc699db708e727eb8f43e6
Joseph Jacob <jjacob@jpl.nasa.gov>
2013-10-17 14:28:14 -0700

    Refactor main loop with subroutines to read the test cases from the control file and to cache the data files needed for the tests before running them.  Also, now call regrid_check instead of mesh_check since we will support Mesh or Grid regridding.

--- src/ESMF/test/regrid_test/regrid_from_file_test/regrid_check_driver.py
+++ src/ESMF/test/regrid_test/regrid_from_file_test/regrid_check_driver.py
@@ -14,25 +14,15 @@ import os
 import subprocess
 import urllib
 import traceback
-
 try:
     import ESMF
 except:
     raise ImportError('The ESMF library cannot be found!')
 import re
-from regrid_check import mesh_check
-
-TEST_REGRID_DIR = 'src/ESMF/test/regrid_test/regrid_from_file_test/'
-CONTROL_FNAME = 'regrid_test_data.txt'
-DATA_SUBDIR = os.path.join(TEST_REGRID_DIR,'data/')
-DATA_URL_ROOT = 'http://www.earthsystemmodeling.org/download/data'
-
-# If fname doesn't exist, retrieve it from the remote server via http.
-def cache_data_file (fname):
-    if not os.path.exists(fname):
-        url = os.path.join(DATA_URL_ROOT, os.path.basename(fname))
-        print 'Retrieving ' + url + '...\n'
-        urllib.urlretrieve(url, fname)
+from regrid_check import regrid_check
+from regrid_from_file_consts import DATA_SUBDIR
+from read_test_cases_from_control_file import read_test_cases_from_control_file
+from run_regrid_from_file_dryrun import cache_data_files_for_test_cases
 
 # Main program: Start up ESMF and run regrid test for each line of options
 # read from a control file.    Retrieve data files for each test from a remote
@@ -40,72 +30,60 @@ def cache_data_file (fname):
 def main():
     # Start up ESMF.
     esmp = ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
-
     petCount = esmp.petCount
+    
+    # Read the test case parameters from the control file.
+    test_cases = read_test_cases_from_control_file()
 
-    # Create data subdirectory if it doesn't exist.
-    if not os.path.exists(DATA_SUBDIR):
-        os.mkdir(DATA_SUBDIR)
+    # Retrieve the data files needed for the test cases from the remote server.
+    status_ok = cache_data_files_for_test_cases(test_cases)
 
     # For each test case line from the control file parse the line and call
     # the test subroutine.
-    for line in open(os.path.join(TEST_REGRID_DIR, CONTROL_FNAME), 'r'):
-        if line[0] != '#' and re.match('(\s*.+\s*:){3}', line):
-            (src_fname, dst_fname, regrid_method, options,
-             max_err_str) = re.split('\s*:\s*', line)
-            max_err = float(max_err_str)
-            test_str = 'Regrid %s to %s as %s with %s and max_err = %f' % \
-                    (src_fname, dst_fname, regrid_method, options,
-                     max_err)
+    for test_case in test_cases:
+        (src_fname, dst_fname, regrid_method, options, max_err, max_area_err) = test_case
+        test_str = 'Regrid %s to %s as %s with %s and max_err = %f' % \
+          (src_fname, dst_fname, regrid_method, options, max_err)
+        print '\n' + test_str + ' - START\n'
+        src_fname_full = os.path.join(DATA_SUBDIR, src_fname)
+        dst_fname_full = os.path.join(DATA_SUBDIR, dst_fname)
 
-            print '\n' + test_str + ' - START\n'
+        # run the data file retrieval and regridding through try/except
+        correct = False
+        try:
+            correct = regrid_check(src_fname_full, dst_fname_full,
+                                 regrid_method, options, max_err)
+        except:
+            print "Regridding ERROR:\n"
+            traceback.print_exc(file=sys.stdout)
 
-            src_fname_full = os.path.join(DATA_SUBDIR, src_fname)
-            dst_fname_full = os.path.join(DATA_SUBDIR, dst_fname)
+        skip = False
+        for i in range(petCount):
+            for line in open("PET"+str(i)+".ESMF_LogFile"):
+                if "ESMF_NETCDF not defined when lib was compiled" in line or \
+                  "File format is not supported" in line:
+                  # set skip
+                  skip = True
 
-            # run the data file retrieval and regridding through try/except
-            correct = False
-            try:
-                cache_data_file (src_fname_full)
-                cache_data_file (dst_fname_full)
-            except:
-                print "wget ERROR:\n"
-                traceback.print_exc(file=sys.stdout)
-            else:
-                try:
-                    correct = mesh_check(src_fname_full, dst_fname_full,
-                                                     regrid_method, options, max_err)
-                except:
-                    print "Regridding ERROR:\n"
-                    traceback.print_exc(file=sys.stdout)
+        # print the file
+        print '\n***NOTE*** The log files must be deleted in this test case, they are printed below for future reference\n'
+        if skip:
+            for line in open("PET"+str(i)+".ESMF_LogFile"):
+                print line
+                
+        # clean the log files
+        for i in range(petCount):
+            os.system("echo ' ' > PET"+str(i)+".ESMF_LogFile")
+            
+        print '\n' + test_str + ' - FINISH\n'
 
-            skip = False
-            for i in range(petCount):
-                for line in open("PET"+str(i)+".ESMF_LogFile"):
-                    if "ESMF_NETCDF not defined when lib was compiled" in line or \
-                       "File format is not supported" in line:
-                        # set skip
-                        skip = True
-
-            # print the file
-            print '\n***NOTE*** The log files must be deleted in this test case, they are printed below for future reference\n'
-            if skip:
-                for line in open("PET"+str(i)+".ESMF_LogFile"):
-                    print line
-
-            # clean the log files
-            for i in range(petCount):
-                os.system("echo ' ' > PET"+str(i)+".ESMF_LogFile")
-
-            print '\n' + test_str + ' - FINISH\n'
-
-            if skip:
-                print 'RESULT: SKIP\n\n'
-            elif correct:
-                print 'RESULT: PASS\n\n'
-            else:
-                print 'RESULT: FAIL\n\n'
-
+        if skip:
+            print 'RESULT: SKIP\n\n'
+        elif correct:
+            print 'RESULT: PASS\n\n'
+        else:
+            print 'RESULT: FAIL\n\n'
+            
     return 0
 
 if __name__ == '__main__':
