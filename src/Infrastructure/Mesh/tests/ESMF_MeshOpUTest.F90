1828e695242a5cc471ad8311475b847125e7c833
Fei Liu <Fei.Liu@gmail.com>
2014-01-22 11:39:31 -0500

    remove the call to ESMF_MeshDestroy for now since the unit test is testing a BOPI api

--- src/Infrastructure/Mesh/tests/ESMF_MeshOpUTest.F90
+++ src/Infrastructure/Mesh/tests/ESMF_MeshOpUTest.F90
@@ -94,10 +94,10 @@ contains
     !  ESMF_ERR_PASSTHRU, &
     !  ESMF_CONTEXT, rcToReturn=rc)) return
 
-    call ESMF_MeshDestroy(mesh, rc=localrc)
-    if (ESMF_LogFoundError(localrc, &
-      ESMF_ERR_PASSTHRU, &
-      ESMF_CONTEXT, rcToReturn=rc)) return
+    !call ESMF_MeshDestroy(mesh, rc=localrc)
+    !if (ESMF_LogFoundError(localrc, &
+    !  ESMF_ERR_PASSTHRU, &
+    !  ESMF_CONTEXT, rcToReturn=rc)) return
 
   end subroutine test3
 
