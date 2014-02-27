c984bb9af7b26d836852351acbe2acac738df829
Gerhard Theurich <theurich@sourcespring.net>
2014-02-27 12:43:22 -0800

    Add the ESMF copyright statement at the top of each NUOPC Layer source file.

--- src/addon/NUOPC/src/NUOPC_Base.F90
+++ src/addon/NUOPC/src/NUOPC_Base.F90
@@ -1,6 +1,16 @@
 ! $Id$
-
+!
+! Earth System Modeling Framework
+! Copyright 2002-2014, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
 #define FILENAME "src/addon/NUOPC/NUOPC_Base.F90"
+!==============================================================================
 
 !TODO: make this macros available through ESMF as parameter or find other way
 #define ESMF_INIT_CREATED 82949521
--- src/addon/NUOPC/src/NUOPC_DriverAtmOcn.F90
+++ src/addon/NUOPC/src/NUOPC_DriverAtmOcn.F90
@@ -1,6 +1,16 @@
 ! $Id$
-
+!
+! Earth System Modeling Framework
+! Copyright 2002-2014, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
 #define FILENAME "src/addon/NUOPC/NUOPC_DriverAtmOcn.F90"
+!==============================================================================
 
 module NUOPC_DriverAtmOcn
 
