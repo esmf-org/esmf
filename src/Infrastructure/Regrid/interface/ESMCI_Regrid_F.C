c1411fcde5cdfad745fccd09e7f23d63a5e05ced
Gerhard Theurich <theurich@sourcespring.net>
2014-07-21 20:28:06 -0400

    Implement progress logging and memory logging, but #ifdef both off.

--- src/Infrastructure/Regrid/interface/ESMCI_Regrid_F.C
+++ src/Infrastructure/Regrid/interface/ESMCI_Regrid_F.C
@@ -115,7 +115,16 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
   // Old Regrid conserve turned off for now
   int regridConserve=ESMC_REGRID_CONSERVE_OFF;
 
-  ESMC_LogDefault.Write("c_esmc_regrid_create(): Just entered routine.", ESMC_LOGMSG_TRACE);
+#define PROGRESSLOG_off
+#define MEMLOG_off
+
+#ifdef PROGRESSLOG_on
+  ESMC_LogDefault.Write("c_esmc_regrid_create(): Just entered routine.", ESMC_LOGMSG_INFO);
+#endif
+
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate1.0"));
+#endif
  
   try {
 
@@ -195,8 +204,14 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
           "collapses to a line or point", ESMC_CONTEXT, &localrc)) throw localrc;
       }
     }
+#ifdef PROGRESSLOG_on
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): Entering weight generation.", ESMC_LOGMSG_INFO);
+#endif
 
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): Entering weight generation.", ESMC_LOGMSG_TRACE);
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate2.0"));
+#endif
+
 
     // Compute Weights matrix
     IWeights wts;
@@ -217,8 +232,13 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
                         regridScheme, map_type, &temp_unmappedaction))
         Throw() << "Online regridding error" << std::endl;
     }
+#ifdef PROGRESSLOG_on
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): Done with weight generation... check unmapped dest,", ESMC_LOGMSG_INFO);
+#endif
 
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): Done with weight generation... check unmapped dest,", ESMC_LOGMSG_TRACE);
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate3.0"));
+#endif
 
     // If requested get list of unmapped destination points
     std::vector<int> unmappedDstList;
@@ -234,8 +254,9 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
         get_mesh_node_ids_not_in_wmat(dstmesh, wts, &unmappedDstList);
       }
     }
-
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): More unmapped points checking.", ESMC_LOGMSG_TRACE);
+#ifdef PROGRESSLOG_on
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): More unmapped points checking.", ESMC_LOGMSG_INFO);
+#endif
 
     // If user is worried about unmapped points then check that
     // here, because we have all the dest objects and weights
@@ -273,8 +294,13 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
         }
       }
     }
+#ifdef PROGRESSLOG_on
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): Prepare for ArraySMMStore().", ESMC_LOGMSG_INFO);
+#endif
 
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): Prepare for ArraySMMStore().", ESMC_LOGMSG_TRACE);
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate4.0"));
+#endif
 
     /////// We have the weights, now set up the sparsemm object /////
 
@@ -364,12 +390,16 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
       if (*norm_type==ESMC_NORMTYPE_FRACAREA) change_wts_to_be_fracarea(dstmesh, num_entries, iientries, factors);
     }
 
+#ifdef PROGRESSLOG_on
     char msgString[1024];
     sprintf(msgString, "c_esmc_regrid_create(): num_entries=%d.", num_entries);
-    
-    ESMC_LogDefault.Write(msgString, ESMC_LOGMSG_TRACE);
+    ESMC_LogDefault.Write(msgString, ESMC_LOGMSG_INFO);
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): Entering ArraySMMStore().", ESMC_LOGMSG_INFO);
+#endif
 
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): Entering ArraySMMStore().", ESMC_LOGMSG_TRACE);
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate5.0"));
+#endif
 
     // Build the ArraySMM
     if (*has_rh != 0) {
@@ -381,7 +411,13 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
         ESMC_CONTEXT, NULL)) throw localrc;  // bail out with exception
     }
 
-    ESMC_LogDefault.Write("c_esmc_regrid_create(): Returned from ArraySMMStore().", ESMC_LOGMSG_TRACE);
+#ifdef PROGRESSLOG_on
+    ESMC_LogDefault.Write("c_esmc_regrid_create(): Returned from ArraySMMStore().", ESMC_LOGMSG_INFO);
+#endif
+
+#ifdef MEMLOG_on
+  VM::logMemInfo(std::string("RegridCreate6.0"));
+#endif
 
     *nentries = num_entries;
     // Clean up.  If has_iw, then we will use the arrays to
@@ -451,7 +487,9 @@ extern "C" void FTN_X(c_esmc_regrid_create)(ESMCI::VM **vmpp,
     return;
   }
 
-  ESMC_LogDefault.Write("c_esmc_regrid_create(): Final return.", ESMC_LOGMSG_TRACE);
+#ifdef PROGRESSLOG_on
+  ESMC_LogDefault.Write("c_esmc_regrid_create(): Final return.", ESMC_LOGMSG_INFO);
+#endif
 
   // Set return code 
   if (rc!=NULL) *rc = ESMF_SUCCESS;
