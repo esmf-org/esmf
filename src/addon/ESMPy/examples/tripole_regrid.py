afc55192918ba416f2341c97614700e5d06bce00
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2014-08-08 14:31:17 -0600

    add a test example of tripole regridding (doesn't work)

---
+++ src/addon/ESMPy/examples/tripole_regrid.py
@@ -0,0 +1,43 @@
+# This example demonstrates how to regrid between a grid and a mesh.
+# The grid and mesh files are required, they can be retrieved from the ESMF data repository:
+#   wget http://www.earthsystemmodeling.org/download/data/ll2.5deg_grid.nc
+#   wget http://www.earthsystemmodeling.org/download/data/tripole.nc
+
+import ESMF
+
+ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
+
+
+grid1 = "ll2.5deg_grid.nc"
+grid2 = "tripole.nc"
+
+# Create a uniform global latlon grid from a SCRIP formatted file
+grid = ESMF.Grid(filename=grid1, filetype=ESMF.FileFormat.SCRIP,
+                 add_corner_stagger=True)
+
+# create a field on the center stagger locations of the source grid
+srcfield = ESMF.Field(grid, 'srcfield', staggerloc=ESMF.StaggerLoc.CENTER)
+
+# initialize the field to a constant value
+srcfield[...] = 25
+
+# create an ESMF formatted unstructured mesh with clockwise cells removed
+mesh = ESMF.Grid(filename=grid2, filetype=ESMF.FileFormat.SCRIP,
+                 add_corner_stagger=True)
+#mesh = ESMF.Mesh(filename=grid2,
+#                 filetype=ESMF.FileFormat.ESMFMESH)
+
+# create a field on the elements of the destination mesh
+dstfield = ESMF.Field(mesh, 'dstmesh', meshloc=ESMF.MeshLoc.ELEMENT)
+
+# create an object to regrid data from the source to the destination field
+regrid = ESMF.Regrid(srcfield, dstfield, \
+                     regrid_method=ESMF.RegridMethod.CONSERVE, \
+                     unmapped_action=ESMF.UnmappedAction.IGNORE)
+
+# do the regridding from source to destination field
+dstfield = regrid(srcfield, dstfield)
+
+print "Successfully read a grid and a mesh and did a regridding of a constant field!"
+print "The field values on PET (processor) # {0} are:".format(ESMF.local_pet())
+print dstfield.data
