e3db53229a27d3cc05a5203b3de31c1ef1ffc7f0
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2014-08-13 16:31:29 -0600

    modify tripole regridding example to use tripoles for source and destination, one GRIDSPEC and one SCRIP
    (cherry picked from commit 63709c2c96ec15785e30a8ff490e12ec4288a68c)

--- src/addon/ESMPy/examples/tripole_regrid.py
+++ src/addon/ESMPy/examples/tripole_regrid.py
@@ -1,15 +1,18 @@
 # This example demonstrates how to regrid between a grid and a mesh.
 # The grid and mesh files are required, they can be retrieved from the ESMF data repository:
-#   wget http://www.earthsystemmodeling.org/download/data/ll2.5deg_grid.nc
 #   wget http://www.earthsystemmodeling.org/download/data/GRIDSPEC_ACCESS1.nc
+#   wget http://www.earthsystemmodeling.org/download/data/tx0.1v2_070911.nc
 
 import ESMF
+import numpy as np
 
-grid1 = "ll2.5deg_grid.nc"
-grid2 = "GRIDSPEC_ACCESS1.nc"
+ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
+
+grid1 = "GRIDSPEC_ACCESS1.nc"
+grid2 = "tx0.1v2_070911.nc"
 
 # Create a uniform global latlon grid from a SCRIP formatted file
-grid = ESMF.Grid(filename=grid1, filetype=ESMF.FileFormat.SCRIP,
+grid = ESMF.Grid(filename=grid1, filetype=ESMF.FileFormat.GRIDSPEC,
                  add_corner_stagger=True)
 
 # create a field on the center stagger locations of the source grid
@@ -19,16 +22,19 @@ srcfield = ESMF.Field(grid, 'srcfield', staggerloc=ESMF.StaggerLoc.CENTER)
 srcfield[...] = 25
 
 # create an ESMF formatted unstructured mesh with clockwise cells removed
-tripole = ESMF.Grid(filename=grid2, filetype=ESMF.FileFormat.GRIDSPEC,
-                 add_corner_stagger=True)
+tripole = ESMF.Grid(filename=grid2, filetype=ESMF.FileFormat.SCRIP,
+                 add_corner_stagger=True, add_mask=True, varname="grid_imask")
 
 # create a field on the elements of the destination mesh
 dstfield = ESMF.Field(tripole, 'dstfield', meshloc=ESMF.StaggerLoc.CENTER)
 
+dstfield[...] = 0
+
 # create an object to regrid data from the source to the destination field
 regrid = ESMF.Regrid(srcfield, dstfield, \
                      regrid_method=ESMF.RegridMethod.CONSERVE, \
-                     unmapped_action=ESMF.UnmappedAction.IGNORE)
+                     unmapped_action=ESMF.UnmappedAction.IGNORE, \
+                     dst_mask_values=np.array([0]))
 
 # do the regridding from source to destination field
 dstfield = regrid(srcfield, dstfield)
