72f76178c4cdc09e86d41cc0db1840bccd0dbfed
Walter Spector <walter.spector@hpe.com>
2014-08-26 11:40:43 -0700

    Eliminate valgrind-noticed issues in ESMF_ArrayIOUTest when not using NetCDF.

--- src/Infrastructure/Field/tests/ESMF_FieldIOUTest.F90
+++ src/Infrastructure/Field/tests/ESMF_FieldIOUTest.F90
@@ -368,6 +368,8 @@ program ESMF_FieldIOUTest
 !------------------------------------------------------------------------
   !NEX_UTest_Multi_Proc_Only
 ! ! Compare readin and the existing file
+
+#if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
   Maxvalue = 0.0
   do j=exclusiveLBound(2),exclusiveUBound(2)
   do i=exclusiveLBound(1),exclusiveUBound(1)
@@ -375,6 +377,10 @@ program ESMF_FieldIOUTest
     if (Maxvalue.le.diff) Maxvalue=diff
   enddo
   enddo
+#else
+  Maxvalue = 1.0
+#endif
+
   write(name, *) "Compare readin data to the existing data"
   write(failMsg, *) "Comparison failed"
 #if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
@@ -445,6 +451,7 @@ program ESMF_FieldIOUTest
 !------------------------------------------------------------------------
   !NEX_UTest_Multi_Proc_Only
 ! ! Compare readin and the existing file
+#if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
   Maxvalue = 0.0
   do j=exclusiveLBound(2),exclusiveUBound(2)
   do i=exclusiveLBound(1),exclusiveUBound(1)
@@ -452,6 +459,10 @@ program ESMF_FieldIOUTest
     if (Maxvalue.le.diff) Maxvalue=diff
   enddo
   enddo
+#else
+  Maxvalue = 1.0
+#endif
+
   write(name, *) "Compare readin data to the existing data"
   write(failMsg, *) "Comparison failed"
 #if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
@@ -500,6 +511,7 @@ program ESMF_FieldIOUTest
 !------------------------------------------------------------------------
   !NEX_UTest_Multi_Proc_Only
 ! ! Compare readin and the existing file
+#if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
   Maxvalue = 0.0
   do j=exclusiveLBound(2),exclusiveUBound(2)
   do i=exclusiveLBound(1),exclusiveUBound(1)
@@ -507,6 +519,10 @@ program ESMF_FieldIOUTest
     if (Maxvalue.le.diff) Maxvalue=diff
   enddo
   enddo
+#else
+  Maxvalue = 1.0
+#endif
+
   write(name, *) "Compare readin data to the existing data"
   write(failMsg, *) "Comparison failed"
 #if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
@@ -520,6 +536,7 @@ program ESMF_FieldIOUTest
 
   deallocate (computationalLBound, computationalUBound)
   deallocate (exclusiveLBound, exclusiveUBound)
+  deallocate (Farray_sw)
 
 !------------------------------------------------------------------------
   !NEX_UTest_Multi_Proc_Only
