4e1e6313316c647e7e4e0deaf6af6cc4185efeea
Peggy Li <p.p.li@jpl.nasa.gov>
2014-10-22 12:41:08 -0600

    Added --help --version -V

--- src/apps/ESMF_Scrip2Unstruct/ESMF_Scrip2Unstruct.c
+++ src/apps/ESMF_Scrip2Unstruct/ESMF_Scrip2Unstruct.c
@@ -47,6 +47,22 @@ void orderit(int index, double lon, double lat, int numedges, double *latlonbuf,
 void convert3D(double lon, double lat, double *x, double *y);
 void orderit2(int index, double lon, double lat, int numedges, double *latlonbuf, int *next);
 
+int print_usage() {
+      /* standard --help argument was specified */
+      printf("ESMF_Scrip2Unstruct: Convert an unstructured grid file in SCRIP format into either a ESMF unstructured file or a UGRID file format.\n");
+      printf("Usage: ESMF_Scrip2Unstruct [--help] [--version] [-V] inputfile outputfile dualflag [fileformat]\n");
+      printf("    [--help]        Display this information and exit.\n");
+      printf("    [--version]     Display ESMF version and license information "
+        "and exit.\n");
+      printf("    [-V]            Display ESMF version string and exit.\n");
+      printf("    inputfile       input grid filename \n");
+      printf("    outputfile      output filename \n");
+      printf("    dualflag        1 to generate a dual mesh, 0 for non-dual mesh\n");
+      printf("    [fileformat]    Either ESMF or UGRID, the default is ESMF\n");
+      printf("\n");
+      return 0;
+}
+
 // Create a ESMF unstructured grid file and define all the dimension, variables and attributes
 int create_esmf(char* filename, char* infilename, int dualflag, size_t nnodes, size_t nelmts, size_t maxconnection, int nocenter, int nomask, int noarea)
 {
@@ -360,17 +376,86 @@ int main(int argc, char** argv)
   int totalsize;
   ESMC_VM vm;
   MPI_Comm mpi_comm;
+  int argIndex;
+  int argFlag;
   int pthreadflag, openmpflag;
+  int rc;
   
   ESMC_Initialize(&status, ESMC_ArgLast);
   vm = ESMC_VMGetGlobal(&status);
   status = ESMC_VMGet(vm, &myrank, &nprocs, &npes, &mpi_comm, &pthreadflag, &openmpflag);
 
+  if (myrank == 0){
+    argFlag = 0;
+    /* check for standard command line arguments */
+    argIndex = ESMC_UtilGetArgIndex(argc, argv, "--help", &rc);
+    if (argIndex >= 0){
+      argFlag=1;
+      print_usage();
+    }
+    argIndex = ESMC_UtilGetArgIndex(argc, argv, "--version", &rc);
+    if (argIndex >= 0){
+      argFlag=1;
+      /* standard --help argument was specified */
+      printf("  ESMF_VERSION_STRING:       %s\n", ESMF_VERSION_STRING);
+      printf("  ESMF_VERSION_MAJOR:        %d\n", ESMF_VERSION_MAJOR);
+      printf("  ESMF_VERSION_MINOR:        %d\n", ESMF_VERSION_MINOR);
+      printf("  ESMF_VERSION_REVISION:     %d\n", ESMF_VERSION_REVISION);
+      printf("  ESMF_VERSION_PATCHLEVEL:   %d\n", ESMF_VERSION_PATCHLEVEL);
+      printf("  ESMF_VERSION_PUBLIC:       %c\n", ESMF_VERSION_PUBLIC);
+      printf("  ESMF_VERSION_BETASNAPSHOT: %c\n", ESMF_VERSION_BETASNAPSHOT);
+      printf("\n");
+      printf("Earth System Modeling Framework\n");
+      printf("\n");
+      printf("Copyright (c) 2002-2014 University Corporation for Atmospheric Research,\n");
+      printf("Massachusetts Institute of Technology, Geophysical Fluid Dynamics Laboratory,\n");
+      printf("University of Michigan, National Centers for Environmental Prediction,\n");
+      printf("Los Alamos National Laboratory, Argonne National Laboratory,\n");
+      printf("NASA Goddard Space Flight Center.  All rights reserved.\n");
+      printf("\n");
+      printf("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
+      printf("of this software and associated documentation files (the \"Software\"), to\n");
+      printf("deal with the Software without restriction, including without limitation the\n");
+      printf("rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n");
+      printf("sell copies of the Software, and to permit persons to whom the Software is\n");
+      printf("furnished to do so, subject to the following conditions:\n");
+      printf("   1. Redistributions of source code must retain the above copyright notice,\n");
+      printf("      this list of conditions and the following disclaimers.\n");
+      printf("   2. Redistributions in binary form must reproduce the above copyright\n");
+      printf("      notice, this list of conditions and the following disclaimers in the\n");
+      printf("      documentation and/or other materials provided with the distribution.\n");
+      printf("   3. Neither the names of the organizations developing this software, nor\n");
+      printf("      its contributors may be used to endorse or promote products derived\n");
+      printf("      from this Software without specific prior written permission.\n");
+      printf("\n");
+      printf("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+      printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");
+      printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE\n");
+      printf("CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
+      printf("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n");
+      printf("FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n");
+      printf("WITH THE SOFTWARE.\n");
+      printf("\n");
+    }
+    argIndex = ESMC_UtilGetArgIndex(argc, argv, "-V", &rc);
+    if (argIndex >= 0){
+      argFlag=1;
+      /* standard --help argument was specified */
+      printf("  ESMF_VERSION_STRING:       %s\n", ESMF_VERSION_STRING);
+      printf("\n");
+    }
+  }
+  // broadcast the argIndex to all the processors
+  MPI_Bcast(&argFlag, 1, MPI_INT, 0, mpi_comm);
+  if (argFlag == 1) {
+    ESMC_Finalize();
+    exit(0);
+  }
+             
 #ifdef ESMF_NETCDF
   if (argc < 4) {
     if (myrank == 0) {
-      fprintf(stderr, "USAGE: Scrip2Unstruct inputfile outputfile dualflag [fileformat]\n");
-      fprintf(stderr,"  where dualflag = 1 or 0 and fileformat is either ESMF or UGRID\n  if fileformat is not given, the default file format is ESMF\n");
+      print_usage();
     }
     ESMC_Finalize();
     exit(1);
@@ -1080,4 +1165,5 @@ int main(int argc, char** argv)
   exit(1);
 #endif
   }
+
 
