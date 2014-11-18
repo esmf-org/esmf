ESMPy: fix a bug in masking and 3D grids with new implementation of extra dimensions on a field

src/addon/ESMPy/src/ESMF/api/field.py
@@ -92,7 +92,8 @@ class Field(ma.MaskedArray):
             lb = [1 for a in range(len(local_ndbounds))]
             ungridded_lower_bound = np.array(lb, dtype=np.int32)
             ungridded_upper_bound = np.array(local_ndbounds, dtype=np.int32)
-            grid_to_field_map = np.array([xd+1, xd+2], dtype=np.int32)
+            # set this to put gridded dimension in the last available dimensions of the field, dependent on grid rank
+            grid_to_field_map = np.array([i+xd+1 for i in range(grid.rank)], dtype=np.int32)
             rank += len(local_ndbounds)
 
         data = None
@@ -104,11 +105,28 @@ class Field(ma.MaskedArray):
                                           ungridded_lower_bound,
                                           ungridded_upper_bound)
 
-            # set the mask
+            # set the grid mask
             if (grid.item_done[staggerloc][GridItem.MASK]):
                 lbounds, ubounds = ESMP_FieldGetBounds(struct, rank)
-                if (list(grid.mask[staggerloc].shape) == list(ubounds-lbounds)):
-                    mask = grid.mask[staggerloc]
+                # if there are extra field dimensions, find out how many
+                if ungridded_lower_bound is not None:
+                    xd = len(ungridded_lower_bound)
+                else:
+                    xd = 0
+                # verify that grid mask is the same shape as the extra field dimensions
+                if (np.all(grid.mask[staggerloc].shape == ubounds[xd:xd+rank]-lbounds[xd:xd+rank])):
+                    # initialize the mask to all unmasked values
+                    mask = np.ones(ubounds-lbounds, dtype=np.int32)
+                    # reset the mask with integer values according to what is in the grid mask,
+                    # taking care to propagate masked values through the extra field dimensions
+                    if grid.rank == 2:
+                        mask[...,:,:] = grid.mask[staggerloc]
+                    elif grid.rank == 3:
+                        mask[...,:,:,:] = grid.mask[staggerloc]
+                    else:
+                        raise IndexError("Grid with rank less than 2 or greater than 3 is not allowed")
+                else:
+                    raise IndexError("grid mask bounds do not match field bounds")
 
         elif isinstance(grid, Mesh):
             # deal with the wacky ESMF node/element convention
@@ -132,7 +150,7 @@ class Field(ma.MaskedArray):
         else:
             raise FieldDOError
      
-        #  set field_mask based on the grid mask and the mask_values input argument
+        #  set field_mask to True or False values based on the integer valued grid mask
         field_mask = False
         if mask is not None and mask_values is not None:
             field_mask = [True if x in mask_values else False for x in mask.flatten().tolist()]

src/addon/ESMPy/src/ESMF/test/unit_test.py
@@ -157,8 +157,6 @@ def grid_coords_test():
                 print "FAIL - gridYCoord[,"+str(i)+","+str(j)+"] = "+\
                       str(gridYCoord_check[i,j])
 
-    #grid._write("gridcoords2DCart")
-
     # return correct from unit test
     return correct
 
@@ -226,8 +224,6 @@ def grid_coords_3D_test():
                           str(j)+","+str(k)+"] = "+\
                           str(gridZCoord_check[i, j, k])
 
-    #grid._write("gridcoord3DCart")
-
     # return correct from unit test
     return correct
 
@@ -285,8 +281,24 @@ def field_mask_test():
      
     if(not field.mask[2][0]):
         correct = False
-        print field.mask
         raise ValueError("field mask is incorrect")
+
+    # return True from unit test
+    return correct
+
+def field_mask_with_xd_test():
+
+    correct = True
+
+    grid = Grid(np.array([10,10], dtype=np.int32), coord_sys=CoordSys.CART)
+
+    mask = grid.add_item(GridItem.MASK)
+    mask[:] = 1
+    mask[0, 1] = 0
+
+    field = Field(grid, "name", ndbounds=[2, 5], mask_values=[0])
+
+    assert (np.all(field.mask[:, :, 0, 1]))
 
     # return True from unit test
     return correct
@@ -302,16 +314,64 @@ def grid_mask_3D_test():
 
     # Add Mask
     mask = grid.add_item(GridItem.MASK)
+    mask[...] = 1
+    mask[:,2] = 0
 
-    [x, y, z] = [0, 1, 2]
-    for i in range(mask.shape[x]):
-        if (i == 2.0):
-            mask[i, :, :] = 1
-        else:
-            mask[i, :, :] = 0;
+    assert(not np.all(mask[:,2]))
 
     # return True from unit test
     return True
+
+def field_mask_3D_test():
+
+    correct = True
+
+    max_index = np.array([10,20,30])
+
+    grid = Grid(max_index)
+
+    # Add coordinates
+    grid.add_coords(staggerloc=[StaggerLoc.CENTER])
+
+    # Add Mask
+    mask = grid.add_item(GridItem.MASK)
+
+    [x, y, z] = [0, 1, 2]
+    for i in xrange(mask.shape[x]):
+        for j in xrange(mask.shape[y]):
+            for k in xrange(mask.shape[z]):
+                if (i == 2.0):
+                    mask[i, j, k] = 2
+                elif (j == 3.0):
+                    mask[i, j, k] = 3
+                else:
+                    mask[i, j, k] = 0;
+
+    # create a Field on the Grid, should inherit the mask
+    field = Field(grid, "FIELD!", mask_values = [2, 3])
+
+    assert(np.all(field.mask[2,:,:]))
+    assert(np.all(field.mask[:,3,:]))
+
+    # return True from unit test
+    return correct
+
+def field_mask_3D_with_xd_test():
+
+    correct = True
+
+    grid = Grid(np.array([10,10,10], dtype=np.int32), coord_sys=CoordSys.CART)
+
+    mask = grid.add_item(GridItem.MASK)
+    mask[:] = 1
+    mask[0, 1, 0] = 0
+
+    field = Field(grid, "name", ndbounds=[2, 5], mask_values=[0])
+
+    assert (np.all(field.mask[:, :, 0, 1, 0]))
+
+    # return True from unit test
+    return correct
 
 def grid_area_test():
 
@@ -1557,7 +1617,10 @@ def main():
     (4.3,'Grid 3D coordinates') : grid_coords_3D_test,
     (4.4,'Grid masking') : grid_mask_test,
     (4.45,'Field masking') : field_mask_test,
+    (4.46,'Field masking with extra dimensions') : field_mask_with_xd_test,
     (4.5,'Grid 3D masking') : grid_mask_3D_test,
+    (4.55,'Field 3D masking') : field_mask_3D_test,
+    (4.56,'Field 3D masking with extra dimensions') : field_mask_3D_with_xd_test,
     (4.6,'Grid area') : grid_area_test,
     (4.7,'Grid 3D area') : grid_area_3D_test,
     (4.8,'Grid create from file - SCRIP') : grid_create_from_file_scrip_test,