080b975963d2d1d5ad1548ff852b477e497e89a9
Jayesh Krishna <jayesh@mcs.anl.gov>
2014-12-18 19:54:54 +0000

    Moving IntelMIC framework interface from Fortran to C
    The Intel MIC framework (software stack) now uses C instead of
    Fortran. This makes it easy to integrate with the ESMF VM
    software layer that is already implemented in C.

---
+++ src/Infrastructure/VM/src/ESMCI_IntelMICInfo.C
@@ -0,0 +1,19 @@
+// $Id$
+//
+// Earth System Modeling Framework
+// Copyright 2002-2014, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+//
+//==============================================================================
+#include "offload.h"
+
+int fw_get_num_devices(void )
+{
+    int ndevs = 0;
+    ndevs = _Offload_number_of_devices();
+    return ndevs;
+}
