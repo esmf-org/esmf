c6b35c90795d30d47e2f0872c8853637b5e2bf18
Gerhard Theurich <theurich@sourcespring.net>
2015-01-07 14:34:13 -0800

    Revert "Update Copyright date to 2015.", because of unwanted 'x' bit change in the permissions.
    This reverts commit 2d00755cd46879f3767838811b54c2e9d08c86bc.

--- src/Infrastructure/Util/src/ESMF_UtilTypes.F90
+++ src/Infrastructure/Util/src/ESMF_UtilTypes.F90
@@ -1,7 +1,7 @@
 ! $Id$
 !
 ! Earth System Modeling Framework
-! Copyright 2002-2015, University Corporation for Atmospheric Research,
+! Copyright 2002-2014, University Corporation for Atmospheric Research,
 ! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
 ! Laboratory, University of Michigan, National Centers for Environmental
 ! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
@@ -1856,7 +1856,7 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
         print *, ""
         print *, "Earth System Modeling Framework"
         print *, ""
-        print *, "Copyright (c) 2002-2015 University Corporation for Atmospheric Research,"
+        print *, "Copyright (c) 2002-2014 University Corporation for Atmospheric Research,"
         print *, "Massachusetts Institute of Technology, Geophysical Fluid Dynamics Laboratory,"
         print *, "University of Michigan, National Centers for Environmental Prediction,"
         print *, "Los Alamos National Laboratory, Argonne National Laboratory,"
