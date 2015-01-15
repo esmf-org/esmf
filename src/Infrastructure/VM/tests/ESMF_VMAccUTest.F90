b7e2742278ea34d696215b0cf5e569360a19d8fb
Jayesh Krishna <jayesh@mcs.anl.gov>
2015-01-15 12:50:18 -0600

    Adding unit test to query the number of acc devices
    Adding a simple unit test to query the number of accelerator
    devices in a system using the
    ESMF_VMGet(..., accDeviceCount=numAccDevices,...)
    call. The test checks if the function returns a success or
    not (but not the actual value returned by the function).

---
+++ src/Infrastructure/VM/tests/ESMF_VMAccUTest.F90
@@ -0,0 +1,75 @@
+! $Id$
+!
+! Earth System Modeling Framework
+! Copyright 2002-2014, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
+
+program ESMF_VMAccUTest
+
+!------------------------------------------------------------------------------
+ 
+#include "ESMF_Macros.inc"
+
+!==============================================================================
+!BOP
+! !PROGRAM: ESMF_VMAccUTest - Unit test for VM Accelerator Support Functions
+!
+! !DESCRIPTION:
+!
+! This file contains unit tests for the ESMF Accelerator Support functions.
+! The tests are run on multiple processes.
+!-----------------------------------------------------------------------------
+! !USES:
+  use ESMF_TestMod     ! test methods
+  use ESMF
+
+  implicit none
+
+!------------------------------------------------------------------------------
+! The following line turns the CVS identifier string into a printable variable.
+  character(*), parameter :: version = &
+    '$Id$'
+!------------------------------------------------------------------------------
+  ! cumulative result: count failures; no failures equals "all pass"
+  integer :: result = 0
+
+  ! individual test failure message
+  character(ESMF_MAXSTR) :: name
+  character(ESMF_MAXSTR) :: failMsg
+
+  ! local variables
+  integer::  rc
+  type(ESMF_VM):: vm
+  integer:: localPet, petCount, ssiId, accDeviceCount
+     
+  !------------------------------------------------------------------------
+  call ESMF_TestStart(ESMF_SRCLINE, rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  !------------------------------------------------------------------------
+
+  ! get global vm information
+  call ESMF_VMGetGlobal(vm, rc=rc)
+  call ESMF_VMGet(vm, localPet=localPet, petCount=petCount, rc=rc)
+
+
+  !Try getting number of accelerator devices
+  !===========================================
+  !------------------------------------------------------------------------
+  write(name, *) "Test getting the number of accelerator devices"
+  write(failMsg, *) "Did not return ESMF_SUCCESS"
+  call ESMF_VMGet(vm, localPet, accDeviceCount=accDeviceCount, ssiId=ssiId, rc=rc)
+  ! FIXME: Print number of devices found
+  call ESMF_Test((rc.eq.ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+
+
+  !------------------------------------------------------------------------
+  call ESMF_TestEnd(ESMF_SRCLINE)
+  !------------------------------------------------------------------------
+
+end program ESMF_VMAccUTest
