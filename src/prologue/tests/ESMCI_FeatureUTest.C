894a8a1530dfb6efa54da146bfc29726bec108a5
Walter Spector <walter.spector@hpe.com>
2015-04-13 12:15:55 -0700

    Add 1d and 2d utests to ESMCI_FeatureUTest.

--- src/prologue/tests/ESMCI_FeatureUTest.C
+++ src/prologue/tests/ESMCI_FeatureUTest.C
@@ -26,6 +26,14 @@ extern "C" {
   int FTN_X(esmf_optional_arg_pos)(int *, int *);
 }
 
+extern "C" {
+  int FTN_X(esmf_optional_arg_pos_a1d)(double a1[], double a2[]);
+}
+
+extern "C" {
+  int FTN_X(esmf_optional_arg_pos_a2d)(double a1[], int *, double a2[], int *);
+}
+
 int main(void){
 
   char name[80];
@@ -33,6 +41,9 @@ int main(void){
   int result = 0;
 
   int arg1=42, arg2=43;
+  double arg1_1d[10], arg2_1d[20];
+  double arg1_2d[10*10], arg2_2d[20*20];  // dense 2D arrays
+  int arg1_idim, arg2_idim;               // leading dimension for dense 2D arrays
   int returnVal;
 
   //----------------------------------------------------------------------------
@@ -43,7 +54,7 @@ int main(void){
 
   //----------------------------------------------------------------------------
   //NEX_UTest
-  strcpy(name, "Call Fortran with no optional args Test");
+  strcpy(name, "Call Fortran with both args present scalar Test");
   strcpy(failMsg, "Did not report correct arguments");
 
   // Fortran function returns a 1 bit for each argument that is present.
@@ -53,7 +64,7 @@ int main(void){
 
   //----------------------------------------------------------------------------
   //NEX_UTest
-  strcpy(name, "Call Fortran with arg #1 not present Test");
+  strcpy(name, "Call Fortran with arg #1 not present scalar Test");
   strcpy(failMsg, "Did not report correct arguments");
 
   // Fortran function returns a 1 bit for each argument that is present.
@@ -63,7 +74,7 @@ int main(void){
 
   //----------------------------------------------------------------------------
   //NEX_UTest
-  strcpy(name, "Call Fortran with arg #2 not present Test");
+  strcpy(name, "Call Fortran with arg #2 not present scalar Test");
   strcpy(failMsg, "Did not report correct arguments");
 
   // Fortran function returns a 1 bit for each argument that is present.
@@ -73,11 +84,99 @@ int main(void){
 
   //----------------------------------------------------------------------------
   //NEX_UTest
-  strcpy(name, "Call Fortran with no args present Test");
+  strcpy(name, "Call Fortran with no args present scalar Test");
   strcpy(failMsg, "Did not report correct arguments");
 
   // Fortran function returns a 1 bit for each argument that is present.
   returnVal = FTN_X(esmf_optional_arg_pos)(NULL, NULL);
+  ESMCI::Test(returnVal==0, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with both args present 1D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a1d)(arg1_1d, arg2_1d);
+  ESMCI::Test(returnVal==3, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with arg #1 not present 1D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a1d)(NULL, arg2_1d);
+  ESMCI::Test(returnVal==2, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with arg #2 not present 1D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a1d)(arg1_1d, NULL);
+  ESMCI::Test(returnVal==1, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with no args present 1D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a1d)(NULL, NULL);
+  ESMCI::Test(returnVal==0, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with both args present 2D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  arg1_idim = 10;
+  arg2_idim = 20;
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a2d)(arg1_1d, &arg1_idim, arg2_2d, &arg2_idim);
+  ESMCI::Test(returnVal==3, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with arg #1 not present 2D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  arg1_idim = 0;
+  arg2_idim = 20;
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a2d)(NULL, &arg1_idim, arg2_2d, &arg2_idim);
+  ESMCI::Test(returnVal==2, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with arg #2 not present 1D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  arg1_idim = 10;
+  arg2_idim = 0;
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a2d)(arg1_2d, &arg1_idim, NULL, &arg2_idim);
+  ESMCI::Test(returnVal==1, name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  strcpy(name, "Call Fortran with no args present 2D array Test");
+  strcpy(failMsg, "Did not report correct arguments");
+
+  arg1_idim = 0;
+  arg2_idim = 0;
+  // Fortran function returns a 1 bit for each argument that is present.
+  returnVal = FTN_X(esmf_optional_arg_pos_a2d)(NULL, &arg1_idim, NULL, &arg2_idim);
   ESMCI::Test(returnVal==0, name, failMsg, &result, __FILE__, __LINE__, 0);
   //----------------------------------------------------------------------------
   
--- src/prologue/tests/ESMF_FeatureSubr.F90
+++ src/prologue/tests/ESMF_FeatureSubr.F90
@@ -10,17 +10,39 @@
 !
 !==============================================================================
 
-integer function ESMF_Optional_arg_pos (i, j)
+integer function ESMF_Optional_arg_pos (i, j) result (ret)
   implicit none
 
   integer, intent(in), optional :: i, j
-  integer :: ret
 
   ret = 0
   if (present (i)) ret = 1
   if (present (j)) ret = ret + 2
 
-  ESMF_Optional_arg_pos = ret
+end function
+
+integer function ESMF_Optional_arg_pos_a1d (a1, a2) result (ret)
+  implicit none
+
+! Arrays are passed by assumed size, not shape
+  integer, intent(in), optional :: a1(*), a2(*)
+
+  ret = 0
+  if (present (a1)) ret = 1
+  if (present (a2)) ret = ret + 2
+
+end function
+
+integer function ESMF_Optional_arg_pos_a2d (a1, a1_size, a2, a2_size) result (ret)
+  implicit none
+
+! Arrays are passed by assumed size, not shape
+  integer, intent(in) :: a1_size, a2_size  ! leading dimensions
+  integer, intent(in), optional :: a1(a1_size,*), a2(a2_size,*)
+
+  ret = 0
+  if (present (a1)) ret = 1
+  if (present (a2)) ret = ret + 2
 
 end function
 
