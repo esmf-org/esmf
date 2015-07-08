f277dedd4db98eb4cc474e8d6b2247c9e3ec7904
Gerhard Theurich <theurich@sourcespring.net>
2015-07-07 17:53:50 -0700

    Continue implementation of NUOPC_FreeFormat.

--- src/addon/NUOPC/src/NUOPC_FreeFormatDef.F90
+++ src/addon/NUOPC/src/NUOPC_FreeFormatDef.F90
@@ -20,10 +20,18 @@ module NUOPC_FreeFormatDef
   
   private
   
+  ! type
   public NUOPC_FreeFormat
-  public NUOPC_FreeFormatCreate, NUOPC_FreeFormatDestroy
   
+  ! parameter
   public NUOPC_FreeFormatLen
+
+  ! methods  
+  public NUOPC_FreeFormatCreate
+  public NUOPC_FreeFormatDestroy
+  public NUOPC_FreeFormatGet
+  public NUOPC_FreeFormatPrint
+  
   
 !==============================================================================
 ! 
@@ -41,7 +49,7 @@ module NUOPC_FreeFormatDef
   
   type NUOPC_FreeFormat
     character(len=NUOPC_FreeFormatLen), pointer   :: stringList(:)
-    integer                                       :: size
+    integer                                       :: count
   end type
 
 !==============================================================================
@@ -74,24 +82,58 @@ module NUOPC_FreeFormatDef
     integer,                            optional, intent(out) :: rc
 ! !DESCRIPTION:
 !   Create a new FreeFormat object. If {\tt stringList} is provided, then the
-!   newly created object will hold the provided strings and the size is that 
+!   newly created object will hold the provided strings and the count is that 
 !   of {\tt size(stringList)}. If {\tt capacity} is provided, it is used to set
 !   the capacity of the newly created FreeFormat object. Providing a
 !   {\tt capacity} that is smaller than {\tt size(stringList)} triggers an
 !   error.
 !EOP
   !-----------------------------------------------------------------------------
-    integer                                :: stat
+    integer   :: stat, i
+    integer   :: stringCount, capacityOpt
     
     if (present(rc)) rc = ESMF_SUCCESS
-
-#if 0    
-    ! allocate a new run element
-    allocate(runElement, stat=stat)
+    
+    ! initialize members
+    NUOPC_FreeFormatCreate%stringList => NULL()
+    NUOPC_FreeFormatCreate%count      =  0;
+    
+    ! determine count
+    if (present(stringList)) then
+      stringCount = size(stringList)
+    else
+      stringCount = 0
+    endif
+    
+    ! determine capacity
+    if (present(capacity)) then
+      capacityOpt = capacity
+      if (stringCount>capacity) then
+        ! error condition
+        call ESMF_LogSetError(ESMF_RC_ARG_BAD, &
+          msg="Capacity if provided must at least be as large as stringCount", &
+          line=__LINE__, &
+          file=FILENAME, &
+          rcToReturn=rc)
+        return  ! bail out
+      endif
+    else
+      capacityOpt = stringCount
+    endif
+    
+    ! allocate
+    allocate(NUOPC_FreeFormatCreate%stringList(capacityOpt), stat=stat)
     if (ESMF_LogFoundAllocError(statusToCheck=stat, &
-      msg="Allocation of RunElement in NUOPC_RunElementAdd.", &
+      msg="Allocation of NUOPC_FreeFormat%stringList.", &
       line=__LINE__, file=FILENAME, rcToReturn=rc)) return  ! bail out
-#endif
+    
+    ! conditionally fill initial strings
+    NUOPC_FreeFormatCreate%count = stringCount
+    if (present(stringList)) then
+      do i=1, stringCount
+        NUOPC_FreeFormatCreate%stringList(i) = stringList(i)
+      enddo
+    endif
 
   end function
   !-----------------------------------------------------------------------------
@@ -108,20 +150,77 @@ module NUOPC_FreeFormatDef
 !   Destroy a FreeFormat object. All internal memory is deallocated.
 !EOP
   !-----------------------------------------------------------------------------
-    integer                                :: stat
+    integer   :: stat
     
     if (present(rc)) rc = ESMF_SUCCESS
-
-#if 0    
-    ! allocate a new run element
-    allocate(runElement, stat=stat)
-    if (ESMF_LogFoundAllocError(statusToCheck=stat, &
-      msg="Allocation of RunElement in NUOPC_RunElementAdd.", &
-      line=__LINE__, file=FILENAME, rcToReturn=rc)) return  ! bail out
-#endif
+    
+    ! conditionally deallocate members 
+    if (associated(freeFormat%stringList)) then
+      deallocate(freeFormat%stringList, stat=stat)
+      if (ESMF_LogFoundDeallocError(statusToCheck=stat, &
+        msg="Deallocation of NUOPC_FreeFormat%stringList.", &
+        line=__LINE__, file=FILENAME, rcToReturn=rc)) return  ! bail out
+    endif
 
   end subroutine
   !-----------------------------------------------------------------------------
 
+  !-----------------------------------------------------------------------------
+!BOP
+! !IROUTINE: NUOPC_FreeFormatGet - Get information from a FreeFormat object
+! !INTERFACE:
+  subroutine NUOPC_FreeFormatGet(freeFormat, count, capacity, stringList, rc)
+! !ARGUMENTS:
+    type(NUOPC_FreeFormat),                       intent(in)  :: freeFormat
+    integer,                            optional, intent(out) :: count
+    integer,                            optional, intent(out) :: capacity
+    character(len=NUOPC_FreeFormatLen), optional, pointer     :: stringList(:)
+    integer,                            optional, intent(out) :: rc
+! !DESCRIPTION:
+!   Get information from a FreeFormat object.
+!EOP
+  !-----------------------------------------------------------------------------
+    if (present(rc)) rc = ESMF_SUCCESS
+    
+    if (present(count)) then
+      count = freeFormat%count
+    endif
+
+    if (present(capacity)) then
+      capacity = size(freeFormat%stringList)
+    endif
+
+    if (present(stringList)) then
+      stringList => freeFormat%stringList
+    endif
+
+  end subroutine
+  !-----------------------------------------------------------------------------
+
+  !-----------------------------------------------------------------------------
+!BOP
+! !IROUTINE: NUOPC_FreeFormatPrint - Print a FreeFormat object
+! !INTERFACE:
+  subroutine NUOPC_FreeFormatPrint(freeFormat, rc)
+! !ARGUMENTS:
+    type(NUOPC_FreeFormat),           intent(in)    :: freeFormat
+    integer,                optional, intent(out)   :: rc
+! !DESCRIPTION:
+!   Print a FreeFormat object.
+!EOP
+  !-----------------------------------------------------------------------------
+    integer   :: i
+    
+    if (present(rc)) rc = ESMF_SUCCESS
+    
+    ! conditionally deallocate members 
+    if (associated(freeFormat%stringList)) then
+      do i=1, freeFormat%count
+        print *, freeFormat%stringList(i)
+      enddo
+    endif
+
+  end subroutine
+  !-----------------------------------------------------------------------------
 
 end module NUOPC_FreeFormatDef
