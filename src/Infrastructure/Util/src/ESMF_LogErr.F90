7dd6e61bcdae6e11ec77aa130d6a55feff323fe3
Gerhard Theurich <theurich@sourcespring.net>
2015-11-10 13:15:42 -0800

    Fix a bug in the determination of PET number width for petCount==1 case.

--- src/Infrastructure/Util/src/ESMF_LogErr.F90
+++ src/Infrastructure/Util/src/ESMF_LogErr.F90
@@ -1608,7 +1608,11 @@ end subroutine ESMF_LogInitialize
 
     call f_ESMF_VMGlobalGet(alog%petNumber, petCount)
     ! Convert PET to contiguous character label
-    digits = log10(real(petCount-1))+1
+    if (petCount>1) then
+      digits = log10(real(petCount-1))+1
+    else
+      digits = 1
+    endif
     write(formatString, "('(i',i1,'.',i1,')')") digits, digits
     write(petNumChar, formatString) alog%petNumber
     alog%petNumLabel = "PET" // trim(adjustl(petNumChar))
