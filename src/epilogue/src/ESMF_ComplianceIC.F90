c639614bcd7599b557704dad61e341d1f22e10c1
Rocky Dunlap <dunlap@ucar.edu>
2016-06-13 15:02:53 -0600

    Testing JSON output in log

--- src/epilogue/src/ESMF_ComplianceIC.F90
+++ src/epilogue/src/ESMF_ComplianceIC.F90
@@ -291,6 +291,12 @@ module ESMF_ComplianceICMod
     character(ESMF_MAXSTR)  :: output
     type(ESMF_Clock)        :: clockCopy
     integer                 :: phase
+    character(ESMF_MAXSTR)  :: conv, purp
+    type(ESMF_AttPack)      :: attpack
+    character(1024)         :: jsonstring
+    type(ESMF_Field)        :: dummyField
+    character(2)            :: phaseString
+    character(ESMF_MAXSTR)  :: compName
     
     ! Initialize user return code
     rc = ESMF_SUCCESS
@@ -302,6 +308,12 @@ module ESMF_ComplianceICMod
       return  ! bail out
     
     call ESMF_GridCompGet(comp, currentPhase=phase, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_GridCompGet(comp, name=compName, rc=rc)
     if (ESMF_LogFoundError(rc, &
       line=__LINE__, &
       file=FILENAME)) &
@@ -363,6 +375,100 @@ module ESMF_ComplianceICMod
       line=__LINE__, &
       file=FILENAME)) &
       return  ! bail out
+
+    ! JSON output to log
+    conv = "json"
+    purp = "general"
+
+    !call ESMF_AttributeAdd(comp, convention="json", purpose="firstLevel", &
+    !    attrList=(/"firstLevelAttr"/), attpack=attpack, &
+    !    rc=rc)
+    !if (ESMF_LogFoundError(rc, &
+    !  line=__LINE__, &
+    !  file=FILENAME)) &
+    !  return  ! bail out
+
+
+    !call ESMF_AttributeSet(comp, name="firstLevelAttr", value="silly", &
+    !                       attpack=attpack, rc=rc)
+    !if (ESMF_LogFoundError(rc, &
+    !  line=__LINE__, &
+    !  file=FILENAME)) &
+    !  return  ! bail out
+
+    !call ESMF_AttributeAdd(comp, convention="json", purpose="extended", &
+    !                       attpack=attpack, rc=rc)
+    !if (ESMF_LogFoundError(rc, &
+    !  line=__LINE__, &
+    !  file=FILENAME)) &
+    !  return  ! bail out
+
+    dummyField = ESMF_FieldEmptyCreate(name="dummyField", rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttributeAdd(dummyField, convention=conv, purpose=purp, &
+                           attrList=(/"event     ", &
+                                      "compName  ", &
+                                      "method    ", &
+                                      "phase     "/), &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttributeSet(dummyField, name="event", value="start_phase", &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttributeSet(dummyField, name="compName", value=compName, &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttributeSet(dummyField, name="method", value="init", &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    !call ESMF_AttributeSet(comp, name="phaseLabel", value="??", &
+    !                       attpack=attpack, rc=rc)
+    !if (ESMF_LogFoundError(rc, &
+    !  line=__LINE__, &
+    !  file=FILENAME)) &
+    !  return  ! bail out
+
+    write(phaseString, "(I0)") phase
+    call ESMF_AttributeSet(dummyField, name="phase", value=trim(phaseString), &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttPackStreamJSON(attpack, jsonstring, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_LogWrite(jsonstring, ESMF_LOGMSG_JSON, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+    ! end JSON write
+
     
     endif
     ! Stop Compliance Checking: InitializePrologue
@@ -381,6 +487,30 @@ module ESMF_ComplianceICMod
     !---------------------------------------------------------------------------
     ! Start Compliance Checking: InitializeEpilogue
     if (ccfDepth <= maxDepth .or. maxDepth < 0) then
+
+    ! output JSON
+
+    call ESMF_AttributeSet(dummyField, name="event", value="stop_phase", &
+                           attpack=attpack, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_AttPackStreamJSON(attpack, jsonstring, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    call ESMF_LogWrite(jsonstring, ESMF_LOGMSG_JSON, rc=rc)
+    if (ESMF_LogFoundError(rc, &
+      line=__LINE__, &
+      file=FILENAME)) &
+      return  ! bail out
+
+    ! end output JSON
+
 
     call prefixString(comp, prefix=prefix, forward=.false., rc=rc)
     if (ESMF_LogFoundError(rc, &
