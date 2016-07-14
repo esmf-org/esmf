00257a1d377b9c67ee206f917557c06f8460b835
Jayesh Krishna <jayesh@mcs.anl.gov>
2016-07-14 13:27:40 -0500

    App performs resource negotiation btw acc and non-acc comps
    First cut of an application that performs resource negotiation
    between two components
    
    * The first component, CAN_ACC - Can use accelerators, if avail
    * The second component, NO_ACC - Cannot use accelerators.
    
    This app uses the new ESMF_VMGet() interface to get the number
    of accelerator devices available to each PET and divides the
    PETs such that the CAN_ACC component gets PETs with access to
    accelerator devices and NO_ACC component gets the rest of the
    PETs.

---
+++ src/system_tests/ESMF_CompCreateTwoConcurrentAccWithResNeg/ESMF_CompCreateTwoConcurrentAccWithResNegSTest.F90
@@ -0,0 +1,267 @@
+! $Id$
+!
+! System test CompCreateTwoConcurrentAccWithResNeg
+!  Description on Sourceforge under System Test #63029
+
+!-------------------------------------------------------------------------
+!ESMF_SYSTEM_TEST        String used by test script to count system tests.
+!=========================================================================
+
+!BOP
+!
+! !DESCRIPTION:
+! System test CompCreateTwoConcurrentAccWithResNeg.
+!
+!
+!\begin{verbatim}
+
+    program CompCreateTwoConcurrentAccWithResNeg
+
+    ! ESMF Framework module
+    use ESMF
+    use ESMF_TestMod
+
+    use user_model1, only : user_register1 => user_register,&
+                            user_setvm1 => user_setvm
+    use user_model2, only : user_register2 => user_register,&
+                            user_setvm2 => user_setvm
+    use user_neg_info
+    use user_neg, only : user_neg_routine
+
+    implicit none
+
+!   Local variables
+    integer, parameter :: NUM_COMPONENTS = 2
+    integer, parameter :: NUM_PHASES = 2
+    integer :: my_pet, npets, rc, userrc
+    integer, dimension(:), allocatable :: petlist1, petlist2
+    integer :: npetlist1, npetlist2
+    type(ESMF_VM):: vm
+    !type(ESMF_GridComp) :: comp1, comp2
+    type(ESMF_GridComp) :: comps(NUM_COMPONENTS)
+    !type(ESMF_State) :: imp1, imp2, exp1, exp2
+    type(ESMF_State) :: imps(NUM_COMPONENTS), exps(NUM_COMPONENTS)
+    !character(len=ESMF_MAXSTR) :: cname1, cname2
+    character(len=ESMF_MAXSTR) :: cnames(NUM_COMPONENTS)
+    type(ESMF_SetVMInterfaceType) :: user_setvms(NUM_COMPONENTS)
+    
+    integer :: i, j
+    character(len=ESMF_MAXSTR) :: tmpstr
+
+    ! cumulative result: count failures; no failures equals "all pass"
+    integer :: result = 0
+
+    ! individual test name
+    character(ESMF_MAXSTR) :: testname
+
+    ! individual test failure message and status msg
+    character(ESMF_MAXSTR) :: failMsg, finalMsg
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+    print *, "System Test CompCreateTwoConcurrentAccWithResNeg:"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!    Create section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!
+
+    call ESMF_Initialize(defaultlogfilename="CompCreateTwoConcurrentAccWithResNegSTest.Log", &
+                        logkindflag=ESMF_LOGKIND_MULTI, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+
+    ! Get the default global VM
+    call ESMF_VMGetGlobal(vm, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+
+    ! Get our pet number for output print statements
+    call ESMF_VMGet(vm, petCount=npets, localPet=my_pet, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+
+      ! Check for correct number of PETs
+    if ( npets < 2 ) then
+      print *, "This system test does not run on fewer than 2 PETs"
+      rc = ESMF_RC_ARG_BAD
+      goto 10
+    endif
+
+    npetlist2 = npets/2
+    npetlist1 = npets - npetlist2
+
+    allocate(petlist1(npetlist1))
+    allocate(petlist2(npetlist2))
+
+    j = 1
+    do i=0,npets-1,2
+      petlist1(j) = i 
+      j = j + 1
+    end do
+    j = 1
+    do i=1,npets-1,2
+      petlist2(j) = i 
+      j = j + 1
+    end do
+
+    print *, "PET list comp 1 : ", petlist1
+    print *, "PET list comp 2 : ", petlist2
+    cnames(1) = "System Test CompCreateTwoConcurrentAccWithResNeg Comp 1"
+    comps(1) = ESMF_GridCompCreate(name=cnames(1), petList=petlist1, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    call ESMF_GridCompPrint(comps(1))
+
+    print *, "Comp Create finished, name = ", trim(cnames(1))
+
+    cnames(2) = "System Test CompCreateTwoConcurrentAccWithResNeg Comp 2"
+    comps(2) = ESMF_GridCompCreate(name=cnames(2), petList=petlist2, rc=rc)
+    if (rc .ne. ESMF_SUCCESS) goto 10
+    call ESMF_GridCompPrint(comps(2))
+
+    print *, "Comp Create finished, name = ", trim(cnames(2))
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!  User negotiation section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+    user_setvms(1)%pfunc => user_setvm1
+    user_setvms(2)%pfunc => user_setvm2
+    call user_neg_routine(vm, comps, user_setvms, rc)
+    if(rc /= ESMF_SUCCESS) then
+      print *, "user negotiation failed"
+    end if
+    print *, "User negotiation finished for all components"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!  Register section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+    call ESMF_GridCompSetServices(comps(1), userRoutine=user_register1, &
+    userRc=userrc, rc=rc)
+    if ((rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS)) goto 10
+    print *, "Comp Register finished (comp 1), rc= ", rc
+    call ESMF_GridCompSetServices(comps(2), userRoutine=user_register2, &
+    userRc=userrc, rc=rc)
+    if ((rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS)) goto 10
+    print *, "Comp Register finished (comp 2), rc= ", rc
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!  Init section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+    do i=1,NUM_COMPONENTS
+      write(tmpstr, *) "igrid import state Comp", i 
+      imps(i) = ESMF_StateCreate(name=tmpstr,  &
+                                  stateintent=ESMF_STATEINTENT_IMPORT, rc=rc)
+      if ( (rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS) ) goto 10
+      write(tmpstr, *) "igrid export state Comp", i 
+      exps(i) = ESMF_StateCreate(name=tmpstr,  &
+                                  stateintent=ESMF_STATEINTENT_EXPORT, rc=rc)
+      if (rc .ne. ESMF_SUCCESS) goto 10
+    end do
+    do i=1,NUM_COMPONENTS
+      do j=1,NUM_PHASES
+        call ESMF_GridCompInitialize(comps(i), importState=imps(i),&
+                                      exportState=exps(i), phase=j,&
+                                      userRc=userrc, rc=rc)
+        if ( (rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS) ) goto 10
+        print *, "Comp Initialize 1 finished : Comp ", i, " : Phase ", j
+      end do
+    end do
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!     Run section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+    do i=1,NUM_PHASES
+      do j=1,NUM_COMPONENTS
+        call ESMF_GridCompRun(comps(j), importState=imps(j), &
+                              exportState=exps(j), userRc=userrc, rc=rc)
+        if ((rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS)) goto 10
+        print *, "Comp Run succeeded : Comp ", j, " : Phase ", i
+      end do
+    end do
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!     Finalize section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!     Print result
+      do i=1,NUM_COMPONENTS
+        call ESMF_GridCompFinalize(comps(i), importState=imps(i), &
+          exportState=exps(i), userRc=userrc, rc=rc)
+        if ( (rc .ne. ESMF_SUCCESS) .or. (userrc .ne. ESMF_SUCCESS) ) goto 10
+      end do
+
+      print *, "-----------------------------------------------------------------"
+      print *, "-----------------------------------------------------------------"
+      print *, "Test finished, my_pet = ", my_pet
+      print *, "-----------------------------------------------------------------"
+      print *, "-----------------------------------------------------------------"
+
+      print *, "Comp Finalize returned"
+
+!
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!     Destroy section
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+!     Clean up
+
+      do i=1,NUM_COMPONENTS
+        call ESMF_GridCompDestroy(comps(i), rc=rc)
+        if (rc .ne. ESMF_SUCCESS) goto 10
+        call ESMF_StateDestroy(imps(i), rc=rc)
+        if (rc .ne. ESMF_SUCCESS) goto 10
+        call ESMF_StateDestroy(exps(i), rc=rc)
+        if (rc .ne. ESMF_SUCCESS) goto 10
+      end do
+
+      print *, "All Destroy routines done"
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+ 10   print *, "System Test CompCreateTwoConcurrentAccWithResNeg complete"
+
+      ! Standard ESMF Test output to log file
+      write(failMsg, *) "System Test failure"
+      write(testname, *) "System Test CompCreateTwoConcurrentAccWithResNeg: Component Create Test"
+
+      if (rc .ne. ESMF_SUCCESS) then
+        ! Separate message to console, for quick confirmation of success/failure
+        if (rc .eq. ESMF_SUCCESS) then
+          write(finalMsg, *) "SUCCESS: Component Create complete."
+        else
+          write(finalMsg, *) "System Test did not succeed.  Error code ", rc
+        endif
+        write(0, *) ""
+        write(0, *) trim(testname)
+        write(0, *) trim(finalMsg)
+        write(0, *) ""
+
+      endif
+
+      ! IMPORTANT: ESMF_STest() prints the PASS string and the # of processors
+      ! into the Log file that the scripts grep for.
+      call ESMF_STest((rc.eq.ESMF_SUCCESS), testname, failMsg, result, &
+        __FILE__, &
+        __LINE__)
+
+      call ESMF_Finalize(rc=rc)
+
+      end program CompCreateTwoConcurrentAccWithResNeg
+
+!\end{verbatim}
+
---
+++ src/system_tests/ESMF_CompCreateTwoConcurrentAccWithResNeg/user_model1.F90
@@ -0,0 +1,341 @@
+! $Id$
+!
+! Example/test code which shows User Component calls.
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User-supplied Component
+!
+!
+!\begin{verbatim}
+
+    module user_model1
+
+    ! ESMF Framework module
+    use ESMF
+    use user_neg_info
+
+    implicit none
+    
+    public user_setvm, user_register
+        
+    type mydata 
+      integer :: index
+      real :: scale_factor
+      integer :: flag
+    end type
+
+    type wrapper
+      type(mydata), pointer :: wrap
+    end type
+
+    contains
+
+!-------------------------------------------------------------------------
+!   !  The Register routine sets the subroutines to be called
+!   !   as the init, run, and finalize routines.  Note that these are
+!   !   private to the module.
+ 
+  subroutine user_setvm(comp, rc)
+    type(ESMF_GridComp) :: comp
+    integer, intent(out) :: rc
+
+    integer :: i, pet_count, pet_list_info_type
+    integer, allocatable :: pet_list(:)
+#ifdef ESMF_TESTWITHTHREADS
+    type(ESMF_VM) :: vm
+    logical :: pthreadsEnabled
+#endif
+
+    integer :: neg_state
+
+    ! Initialize return code
+    rc = ESMF_SUCCESS
+    print *, "USER: user_setvm called"
+
+    call ESMF_AttributeGet(comp, name="ESMF_COMP_USER_NEG_STATE",&
+          value=neg_state, rc=rc)
+    if(rc /= ESMF_SUCCESS) then
+      print *, "Getting Negotiation state failed"
+      return
+    end if
+
+    if(neg_state == ESMF_COMP_USER_NEG_INIT) then
+      print *, "Negotiation state : INIT"
+      call ESMF_AttributeSet(comp, name="ESMF_COMP_USER_NEG_ACC_INFO",&
+            value=ESMF_COMP_CAN_ACC, rc=rc)
+      if(rc /= ESMF_SUCCESS) then
+        print *, "Setting Negotiation acc info failed"
+        return
+      end if
+    else if(neg_state == ESMF_COMP_USER_NEG_INPROGRESS) then
+      print *, "Negotiation state : INPROGRESS"
+      call ESMF_AttributeGet(comp, name="ESMF_COMP_USER_NEG_PETLIST_INFO_TYPE",&
+        value=pet_list_info_type, rc=rc)
+      if(rc /= ESMF_SUCCESS) then
+        print *, "Getting pet list info type failed, exiting..."
+        return
+      end if
+      if(pet_list_info_type == ESMF_COMP_USER_NEG_PETLIST_INFO_ENUMERATE) then
+        call ESMF_AttributeGet(comp, name="ESMF_COMP_USER_NEG_PETLIST_INFO_SIZE",&
+          value=pet_count, rc=rc)
+        if(rc /= ESMF_SUCCESS) then
+          print *, "Getting pet list info type failed, exiting..."
+          return
+        end if
+        if(pet_count <= 0) then
+          print *, "Pet list info size <= 0, exiting"
+          rc = ESMF_FAILURE
+          return
+        end if
+        allocate(pet_list(pet_count))
+        call ESMF_AttributeGet(comp, name="ESMF_COMP_USER_NEG_PETLIST_INFO",&
+          valueList=pet_list, rc=rc)
+        if(rc /= ESMF_SUCCESS) then
+          print *, "Setting pet list info failed, exiting..."
+          return
+        end if
+        print *, "Got Petlist : ", pet_list
+        print *, "Accepting the petlist passed by driver/main"
+      else
+        print *, "ERROR: Only PETLIST_INFO of type ENUMERATE is supported for now"
+        return
+      end if
+
+      rc = ESMF_SUCCESS
+    else if(neg_state == ESMF_COMP_USER_NEG_FINALIZE) then
+      print *, "Negotiation state : FINALIZE"
+      rc = ESMF_SUCCESS
+    else
+      print *, "ERROR : INVALID Negotiation state"
+      rc = ESMF_FAILURE
+    end if
+
+  end subroutine
+
+    subroutine user_register(comp, rc)
+        type(ESMF_GridComp) :: comp
+        integer, intent(out) :: rc
+
+        ! Local variables
+        type(mydata), pointer :: mydatablock
+        type(wrapper) :: mywrapper
+
+        rc = ESMF_SUCCESS
+
+        print *, "In user register routine"
+
+        ! Register the callback routines.
+
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_METHOD_INITIALIZE, userRoutine=user_init1,&
+          phase=1, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_METHOD_INITIALIZE, userRoutine=user_init2,&
+          phase=2, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_METHOD_RUN, userRoutine=user_run, &
+          rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_GridCompSetEntryPoint(comp, ESMF_METHOD_FINALIZE, userRoutine=user_final, &
+          rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        print *, "Registered Initialize, Run, and Finalize routines"
+
+        ! Set Internal State
+
+        allocate(mydatablock)
+        mydatablock%index = 13
+        mydatablock%scale_factor = 0.66
+        mydatablock%flag = 25
+
+        mywrapper%wrap => mydatablock
+
+        call ESMF_GridCompSetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        print *, "Registered local datablock"
+        print *, " initial data =", mydatablock%index, &
+                     mydatablock%scale_factor, mydatablock%flag
+
+    end subroutine
+
+!-------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   first Initialization routine.
+ 
+    
+    subroutine user_init1(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp) :: comp
+        type(ESMF_State) :: importState, exportState
+        type(ESMF_Clock) :: clock
+        integer, intent(out) :: rc
+
+        rc = ESMF_SUCCESS
+
+        print *, "first init routine called"
+
+    end subroutine user_init1
+
+!-------------------------------------------------------------------------
+!   !  User Comp Component created by higher level calls, here is the
+!   !   second and main Initialization routine.
+ 
+    
+    subroutine user_init2(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp) :: comp
+        type(ESMF_State) :: importState, exportState
+        type(ESMF_Clock) :: clock
+        integer, intent(out) :: rc
+
+!     ! Local variables
+        type(ESMF_Field) :: humidity
+        type(mydata), pointer :: mydatablock
+        type(wrapper) :: mywrapper
+
+        rc = ESMF_SUCCESS
+
+        print *, "User Comp Init 2 starting"
+
+        ! This is where the model specific setup code goes.  
+
+        call ESMF_GridCompPrint(comp, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_StatePrint(exportState, options="", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        print *, "init, ready to call get data ptr"
+        nullify(mydatablock)
+        mywrapper%wrap => mydatablock
+        call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        mydatablock => mywrapper%wrap
+        print *, "init, back from data ptr"
+
+        print *, "init, data = ", mydatablock%index, &
+                     mydatablock%scale_factor, mydatablock%flag
+
+        ! Add an empty "humidity" field to the export state.
+        humidity = ESMF_FieldEmptyCreate(name="humidity", rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_StateAdd(exportState, (/humidity/), rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_StatePrint(exportState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        print *, "User Comp Init returning"
+   
+    end subroutine user_init2
+
+
+!-------------------------------------------------------------------------
+!   !  The Run routine where data is computed.
+!   !
+ 
+    subroutine user_run(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp) :: comp
+        type(ESMF_State) :: importState, exportState
+        type(ESMF_Clock) :: clock
+        integer, intent(out) :: rc
+
+        type(ESMF_Field) :: humidity
+        integer, save :: onetime=1              ! static variable
+
+        type(mydata), pointer :: mydatablock
+        type(wrapper) :: mywrapper
+
+        rc = ESMF_SUCCESS
+
+        print *, "User Comp Run starting"
+
+        ! In a real application, the coupler would move the export from
+        ! one component to the import before this call.  For now, copy the
+        ! field from the export state to import state by hand.
+        if (onetime .gt. 0) then
+          call ESMF_StateGet(exportState, "humidity", humidity, rc=rc)
+          if (rc/=ESMF_SUCCESS) return ! bail on error    
+          call ESMF_StateAdd(importState, (/humidity/), rc=rc)
+          if (rc/=ESMF_SUCCESS) return ! bail on error    
+          onetime = 0
+        endif
+
+        ! Get private data block
+        print *, "in run, ready to get data block"
+        nullify(mydatablock)
+        mywrapper%wrap => mydatablock
+        call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        mydatablock => mywrapper%wrap
+
+        print *, "run, local data =", mydatablock%index, &
+                        mydatablock%scale_factor, mydatablock%flag
+   
+        call ESMF_StatePrint(importState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_StateGet(importState, "humidity", humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_FieldPrint(humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        ! This is where the model specific computation goes.
+
+        ! Here is where the output state is updated.
+        !call ESMF_StateAdd(exportState, humidity, rc=status)
+        call ESMF_StatePrint(exportState, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+
+        print *, "User Comp Run returning"
+
+    end subroutine user_run
+
+
+!-------------------------------------------------------------------------
+!   !  The Finalization routine where things are deleted and cleaned up.
+!   !
+ 
+    subroutine user_final(comp, importState, exportState, clock, rc)
+        type(ESMF_GridComp) :: comp
+        type(ESMF_State) :: importState, exportState
+        type(ESMF_Clock) :: clock
+        integer, intent(out) :: rc
+
+!     ! Local variables
+        type(mydata), pointer :: mydatablock
+        type(wrapper) :: mywrapper
+        type(ESMF_Field) :: humidity
+
+        rc = ESMF_SUCCESS
+
+        print *, "User Comp Final starting"
+    
+        ! Query component for information.
+        print *, "final, ready to call get data ptr"
+        nullify(mydatablock)
+        mywrapper%wrap => mydatablock
+        call ESMF_GridCompGetInternalState(comp, mywrapper, rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        mydatablock => mywrapper%wrap
+   
+        print *, "final, local data =", mydatablock%index, &
+                        mydatablock%scale_factor, mydatablock%flag
+
+        ! garbage collection 
+        deallocate(mydatablock)
+        call ESMF_StateGet(importState, "humidity", humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        call ESMF_FieldDestroy(humidity, rc=rc)
+        if (rc/=ESMF_SUCCESS) return ! bail on error    
+        
+        print *, "User Comp Final returning"
+   
+    end subroutine user_final
+
+
+    end module user_model1
+    
+!\end{verbatim}
+    
