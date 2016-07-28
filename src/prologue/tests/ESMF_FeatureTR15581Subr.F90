b225afadba6d52618802126c2dbec274e76e0bb6
Walter Spector <walter.spector@hpe.com>
2016-07-27 18:42:53 -0700

    Add tests to ensure that newer TR15581 features are available.

---
+++ src/prologue/tests/ESMF_FeatureTR15581Subr.F90
@@ -0,0 +1,81 @@
+! $Id$
+!
+! Earth System Modeling Framework
+! Copyright 2002-2016, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!
+!==============================================================================
+
+module ESMF_FeatureTR15581Subr_mod
+  use ESMF
+  implicit none
+
+  type ESMF_AllocDType
+    ! F2003 and F95+TR15581 allocatable components
+    real,         allocatable :: a(:)
+    integer,      allocatable :: indicies(:)
+    logical,      allocatable :: tfs(:)
+    character(1), allocatable :: chars(:)
+  end type
+
+contains
+
+  subroutine ESMF_FeatureAllocArg (n, a, indicies, tfs, strings, dts, rc)
+    integer,                   intent(in)  :: n
+    ! F2003 and F95+TR15581 allocatable dummy arguments
+    real,         allocatable, intent(out) :: a(:)
+    integer,      allocatable, intent(out) :: indicies(:)
+    logical,      allocatable, intent(out) :: tfs(:)
+    character(*), allocatable, intent(out) :: strings(:)
+    type(ESMF_AllocDType), allocatable, intent(out) :: dts(:)
+    integer,                   intent(out) :: rc
+
+    integer :: i
+    integer :: memstat
+
+    allocate (a(n), indicies(n), tfs(n), strings(n), stat=memstat)
+    if (memstat /= 0) then
+      rc = ESMF_FAILURE
+      return
+    end if
+
+    do, i=1, n
+      a(i) = i
+      indicies(i) = i
+      tfs = mod (i, 1) == 1
+      strings(i) = achar (mod (i, 127))
+    end do
+
+    rc = ESMF_SUCCESS
+
+  end subroutine ESMF_FeatureAllocArg
+
+  function ESMF_FeatureAllocFRet (n, rc) result (ret)
+    integer, intent(in)  :: n
+    integer, intent(out) :: rc
+    ! F2003 and F95+TR15581 allocatable function result
+    real, allocatable :: ret(:)
+
+    integer :: i
+    integer :: memstat
+
+    allocate (ret(n), stat=memstat)
+    if (memstat /= 0) then
+      rc = ESMF_FAILURE
+      return
+    end if
+
+    do, i=1, n
+      ret(i) = i
+    end do
+
+    rc = ESMF_SUCCESS
+
+  end function  ESMF_FeatureAllocFRet
+
+end module ESMF_FeatureTR15581Subr_mod
+
