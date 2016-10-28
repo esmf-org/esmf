8fd13ae6bf85f38279761d9a0a8df341831a295b
Gerhard Theurich <theurich@sourcespring.net>
2016-10-28 16:27:51 -0700

    Revert "Change gfortran -Wextra to -Wcompare-reals to eliminate keywordenforcer warnings." The -Wcompare-reals seems to require GCC 4.8 or later, and breaks older compilers.
    This reverts commit 2c8d8bca618e39cacb072197163ca903c5dbf869.

--- build_config/Unicos.gfortran.default/build_rules.mk
+++ build_config/Unicos.gfortran.default/build_rules.mk
@@ -50,7 +50,7 @@ ESMF_CXXCOMPILER_VERSION    = ${ESMF_CXXCOMPILER} -v --version
 ############################################################
 # Special debug flags
 #
-ESMF_F90OPTFLAG_G       += -Wall -Wcompare-reals -Wno-unused -Wno-unused-dummy-argument -fbacktrace
+ESMF_F90OPTFLAG_G       += -Wall -Wextra -Wno-unused -Wno-unused-dummy-argument -fbacktrace
 ESMF_CXXOPTFLAG_G       += -Wall -Wextra -Wno-unused
 
 ############################################################
