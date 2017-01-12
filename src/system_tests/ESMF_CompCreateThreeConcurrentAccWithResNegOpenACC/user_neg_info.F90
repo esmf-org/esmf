79e7a31a5010ffdc04d6e2f5e947dc518875836c
Jayesh Krishna <jayesh@mcs.anl.gov>
2017-01-12 13:05:42 -0600

    OpenACC app that uses acc device list from driver
    A new app that uses accelerator devices passed by the driver.
    
    The run routine in model1 is modified to use the accelerator device
    list passed to the model by the driver. The run routine uses
    OpenACC to run code on the target device.

---
+++ src/system_tests/ESMF_CompCreateThreeConcurrentAccWithResNegOpenACC/user_neg_info.F90
@@ -0,0 +1,76 @@
+! $Id$
+!
+! Example code which contains the include info required by the user
+! model
+
+!-------------------------------------------------------------------------
+!-------------------------------------------------------------------------
+
+!BOP
+!
+! !DESCRIPTION:
+!  User negotitation info required by the user model
+!
+!
+!\begin{verbatim}
+
+    module user_neg_info
+
+    ! ESMF Framework module
+    use ESMF
+
+    implicit none
+
+!------------------------------------------------------------------------------
+! ! ESMF_GridCompUserNegInfo accInfo types
+
+  integer, parameter ::&
+    ESMF_COMP_NO_ACC=0, ESMF_COMP_CAN_ACC=1, ESMF_COMP_MUST_ACC=2
+
+!------------------------------------------------------------------------------
+! ! ESMF_GridCompUserNegInfo state types
+
+  integer, parameter ::&
+    ESMF_COMP_USER_NEG_NOTPRESENT=0,&
+    ESMF_COMP_USER_NEG_INIT=1, ESMF_COMP_USER_NEG_FINALIZE=2,&
+    ESMF_COMP_USER_NEG_INPROGRESS=3
+
+!------------------------------------------------------------------------------
+! ! ESMF_GridCompUserNegInfo petlist info types
+
+  integer, parameter ::&
+    ESMF_COMP_USER_NEG_LIST_INFO_ENUMERATE=0,&
+    ESMF_COMP_USER_NEG_LIST_INFO_TUPLE=1
+!------------------------------------------------------------------------------
+
+  abstract interface
+    subroutine set_vm_interface(gridcomp, rc)
+      use ESMF_CompMod
+      implicit none
+      type(ESMF_GridComp)  :: gridcomp
+      integer, intent(out)  :: rc
+    end subroutine set_vm_interface
+  end interface
+
+  type  ESMF_SetVMInterfaceType
+    procedure(set_vm_interface), pointer, nopass :: pfunc => NULL()
+  end type
+
+  public  ESMF_COMP_NO_ACC, ESMF_COMP_CAN_ACC, ESMF_COMP_MUST_ACC
+  public  ESMF_COMP_USER_NEG_NOTPRESENT,&
+          ESMF_COMP_USER_NEG_INIT, ESMF_COMP_USER_NEG_FINALIZE,&
+          ESMF_COMP_USER_NEG_INPROGRESS
+
+  public  ESMF_COMP_USER_NEG_LIST_INFO_ENUMERATE,&
+          ESMF_COMP_USER_NEG_LIST_INFO_TUPLE
+
+  public  set_vm_interface, ESMF_SetVMInterfaceType
+
+  type ESMF_PetListInfo !pet_list_info
+    integer, allocatable :: pet_list(:)
+    integer, allocatable :: device_list(:)
+  end type
+
+  end module user_neg_info
+    
+!\end{verbatim}
