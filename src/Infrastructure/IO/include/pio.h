da0d994a2dc08a497d2c5735b53a08571c58e181
Walter Spector <walter.spector@hpe.com>
2017-04-17 13:30:27 -0700

    Add new ESMF cpp binding for float/real attributes.

--- src/Infrastructure/IO/include/pio.h
+++ src/Infrastructure/IO/include/pio.h
@@ -356,6 +356,9 @@ int pio_cpp_inquire_dimension(pio_file_desc_t ncid, int dimid,
 int pio_cpp_put_att_string (pio_file_desc_t file, pio_var_desc_t varDesc,
                               const char *keyword, const char *value);
 
+int pio_cpp_put_att_floats (pio_file_desc_t file, pio_var_desc_t varDesc,
+                              const char *keyword, const char *values, int nvalues);
+
 ///////////////////////////////////////////
 //
 //  darray read/write interface functions
