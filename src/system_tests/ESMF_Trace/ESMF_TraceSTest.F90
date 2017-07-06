c6125fc8dc9363e102601fa763403b9fe55a2bb3
Walter Spector <walter.spector@hpe.com>
2017-07-06 11:13:41 -0700

    Ensure a valid unit number on non-PET0s in TraceSTest.

--- src/system_tests/ESMF_Trace/ESMF_TraceSTest.F90
+++ src/system_tests/ESMF_Trace/ESMF_TraceSTest.F90
@@ -354,12 +354,12 @@ program ESMF_TraceSTest
        ESMF_CONTEXT, rcToReturn=rc)) &
        call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
   
-  if (localPet == 0) then
-     call ESMF_UtilIOUnitGet(funit, rc=localrc)
-     if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
-          ESMF_CONTEXT, rcToReturn=rc)) &
-          call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
-     
+  call ESMF_UtilIOUnitGet(funit, rc=localrc)
+  if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
+       ESMF_CONTEXT, rcToReturn=rc)) &
+       call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+
+  if (localPet == 0) then 
      ! verify metadata file exists
      ioerr = 0
      open (unit=funit, file="traceout/metadata", status="old", &
@@ -397,7 +397,7 @@ program ESMF_TraceSTest
   open (unit=funit, file=trim(filename), status="old", &
        action="read", iostat=ioerr)
   if (ioerr /= 0) then
-     write (failMsg, *) "Failed to open trace file."
+     write (failMsg, *) "Failed to open trace file: ", trim (filename), ", ioerr =", ioerr
      call ESMF_STest((ioerr /= 0), testname, failMsg, result, ESMF_SRCLINE)
      goto 20
   endif
