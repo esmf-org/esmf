28cf23ba3721d346a71ca6c8554452d681d9330c
Walter Spector <walter.spector@hpe.com>
2017-09-06 15:39:02 -0700

    New unit test and temporary debug tracing in ESMF_ClockUTest.

--- src/Infrastructure/TimeMgr/tests/ESMF_ClockUTest.F90
+++ src/Infrastructure/TimeMgr/tests/ESMF_ClockUTest.F90
@@ -811,7 +811,17 @@
                                stopTime=stopTime, name="Clock 1", rc=rc)
       call ESMF_Test((rc.eq.ESMF_RC_OBJ_INIT), &
                       name, failMsg, result, ESMF_SRCLINE)
+
+      ! ----------------------------------------------------------------------------
+
+      !EX_UTest
+      ! Destroy clock with uninitialized Start Time
+      write(failMsg, *) " Did not return ESMF_RC_OBJ_NOT_CREATED"
+      write(name, *) "Destroy uninitialized clock test"
+call ESMF_LogSet (trace=.true.)
       call ESMF_ClockDestroy(clock, rc=rc)
+      call ESMF_Test(rc == ESMF_RC_OBJ_NOT_CREATED, &
+                      name, failMsg, result, ESMF_SRCLINE)
 
       ! ----------------------------------------------------------------------------
 
@@ -824,6 +834,7 @@
                                    calendar=gregorianCalendar, rc=rc)
       call ESMF_Test((rc.eq.ESMF_SUCCESS), &
                       name, failMsg, result, ESMF_SRCLINE)
+call ESMF_LogSet (trace=.false.)
 
       ! ----------------------------------------------------------------------------
  
