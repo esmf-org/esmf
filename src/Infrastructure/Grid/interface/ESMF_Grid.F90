f3b61360e6226c3f8e9ac45b13a17c9f047e2673
Peggy Li <p.p.li@jpl.nasa.gov>
2017-10-17 11:03:50 -0600

    Fixed a bug regarding to calculate the count of block to be read by ESMF_GridSpecReadStagger()

--- src/Infrastructure/Grid/interface/ESMF_Grid.F90
+++ src/Infrastructure/Grid/interface/ESMF_Grid.F90
@@ -13723,7 +13723,7 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
                 staggerloc=staggerLocList(s), farrayPtr=lonPtr, rc=localrc)
             if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
                 ESMF_CONTEXT, rcToReturn=rc)) return
-            count=ubound(lonPtr)
+            count=ubound(lonPtr)-lbound(lonPtr)+1
             call ESMF_GridGetCoord(grid, coordDim=2, localDe=localDe, &
                 staggerloc=staggerLocList(s), farrayPtr=latPtr, rc=localrc)
             if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, &
@@ -13731,7 +13731,6 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
             !call ESMF_VMWtime(starttime, rc=localrc)
             ! Generate glocal edge coordinates and local center coordinates
             ! need to adjust the count???
-            ! Generate glocal edge coordinates and local center coordinates
             totallen = len_trim(mosaic%filenames(tile))+len_trim(mosaic%tileDirectory)
             tempname = trim(mosaic%tileDirectory)//trim(mosaic%filenames(tile))
             call ESMF_GridSpecReadStagger(trim(tempname),sizex, sizey, lonPtr, latPtr, &
