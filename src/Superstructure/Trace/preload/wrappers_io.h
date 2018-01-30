3bd0a0bf37a470df0aecd082794fa61709cc2c29
Rocky Dunlap <dunlap@ucar.edu>
2018-01-29 17:16:01 -0700

    Added missing header file

---
+++ src/Superstructure/Trace/preload/wrappers_io.h
@@ -0,0 +1,21 @@
+/**
+ *  wrappers_io.h
+ *
+ *  Wrappers for low level IO calls so we can trace them.
+ *  These are linked statically into the executable using
+ *  --wrap=SYMBOL flags to the linker.
+ *
+ */
+
+#ifndef WRAPPERS_IO_H
+#define WRAPPERS_IO_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern "C" {  
+  ssize_t __wrap_write(int fd, const void *buf, size_t nbytes);
+  ssize_t __wrap_read(int fd, void *buf, size_t nbyte);
+}
+
+#endif
