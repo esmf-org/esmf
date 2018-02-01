a1e5613099067dda47b90d4ae4be8013f48d1fc5
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-01-31 20:37:20 -0600

    Parse tokens in a run sequence
    Adding code to parse the tokens in the run sequence.
    
    * Components, component phases and connectors are now parsed
      into tokens and stored.
    * Util function to print information about a tokenized run
      sequence is added
    * Util function to get misc info from a tokenized run sequence
      is also added.

--- src/Superstructure/Mapper/src/ESMF_MapperRunSeqUtil.F90
+++ src/Superstructure/Mapper/src/ESMF_MapperRunSeqUtil.F90
@@ -60,11 +60,12 @@ module ESMF_MapperRunSeqUtilMod
                         ESMF_RUNSEQ_END_TOKEN=6,&
                         ESMF_OTHER_TOKEN=7
 
-  ! We currently only care about the first three tokens
-  integer, parameter :: ESMF_MAXNUMTOKENS = 3
-  integer, parameter :: ESMF_MAXTOKENSTR = 128
+  ! We currently only care about two tokens
+  integer, parameter :: ESMF_RUNSEQ_MAXTOKENS = 2
+  integer, parameter :: ESMF_RUNSEQ_TOK1=1, ESMF_RUNSEQ_TOK2=2
+  integer, parameter :: ESMF_RUNSEQ_MAXTOKENSTR = 128
   type ESMF_RunSeqTokenizedLine
-    character(len=ESMF_MAXTOKENSTR) :: tok(ESMF_MAXNUMTOKENS)
+    character(len=ESMF_RUNSEQ_MAXTOKENSTR) :: tok(ESMF_RUNSEQ_MAXTOKENS)
     integer :: tok_type
   end type
   type ESMF_RunSeqTokenizedCode
@@ -80,6 +81,7 @@ module ESMF_MapperRunSeqUtilMod
   type ESMF_RunSeqTimeLoopStack
     integer :: runSeqTimeLoopInfoIdx
   end type
+
 
 !------------------------------------------------------------------------------
 ! !PUBLIC TYPES:
@@ -102,6 +104,33 @@ module ESMF_MapperRunSeqUtilMod
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 contains
+
+! Trims spaces at the begin and end of a string
+function ESMF_FullStrTrim(str) result(ostr)
+  character(len=*), intent(in) :: str
+
+  character(len=ESMF_MAXSTR) :: ostr
+  character, parameter :: SPACE = ' '
+  integer :: slen, i
+
+  ostr = trim(str)
+  slen = len(trim(str))
+  if(slen > ESMF_MAXSTR) then
+    print *, "ERROR: Truncating string !"
+    slen = ESMF_MAXSTR
+  end if
+
+  do i=1,slen
+    if(str(i:i) /= SPACE) then
+      ostr = trim(str(i:))
+      exit
+    end if
+  end do
+  
+  !print *, """", trim(str), """"
+  !print *, """", trim(ostr), """"
+
+end function
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -135,41 +164,35 @@ contains
 !
 !EOP
   !-----------------------------------------------------------------------------    
-  ! Token types
-!  integer, parameter :: ESMF_TIMELOOP_TOKEN = 1,&
-!                        ESMF_COMP_PHASE_TOKEN=2,&
-!                        ESMF_CONN_TOKEN=3,&
-!                        ESMF_OTHER_TOKEN=4
-
-!  integer, parameter :: ESMF_MAXTOKENSTR = 128
-!  type ESMF_RunSeqTokenizedLine
-!    character(len=ESMF_MAXTOKENSTR), dimension(:), allocatable :: tok
-!    integer :: tok_type
-!  end type
-!  type ESMF_RunSeqTokenizedCode
-!    type(ESMF_RunSeqTokenizedLine), dimension(:), allocatable :: line
-!  end type
-
-!  type ESMF_RunSeqTimeLoopInfo
-!    integer :: timeLoopCount
-!    integer :: runSeqLoopLineNum
-!  end type
-!  type ESMF_RunSeqTimeLoopStack
-!    integer :: runSeqTimeLoopInfoIdx
-!  end type
-
     character(len=*), parameter :: COMMENT_TOKENSTR = "#"
+    character(len=*), parameter :: MISC_EOL_TOKENSTR = "#:"
+    character(len=*), parameter :: SPACE_TOKENSTR = " "
     character(len=*), parameter :: RUNSEQ_BEGIN_TOKENSTR = "runSeq::"
     character(len=*), parameter :: RUNSEQ_END_TOKENSTR = "::"
     character(len=*), parameter :: TIMELOOP_TOKENSTR = "@"
     character(len=*), parameter :: CONN_TOKENSTR = "->"
-    integer :: pos, i, nlines
+    integer :: pos, lpos, i, nlines
+    character(len=ESMF_RUNSEQ_MAXTOKENSTR) :: tmp_tok
     
+    if (present(rc)) rc = ESMF_SUCCESS
+
     nlines = size(runSeqCode)
     allocate(tokRunSeq%line(nlines))
     do i=1,nlines
+      tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) = ""
+      tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK2) = ""
+      ! A blank line
+      if(len(trim(runSeqCode(i))) == 0) then
+        tokRunSeq%line(i)%tok_type = ESMF_OTHER_TOKEN
+        cycle  
+      end if
+      ! Entire line is a comment
       pos = INDEX(runSeqCode(i), COMMENT_TOKENSTR)
-      if((pos /= 0) .and. (len(trim(runSeqCode(i)(:pos))) == 0)) then
+      if(pos == 1) then
+        tokRunSeq%line(i)%tok_type = ESMF_OTHER_TOKEN
+        cycle  
+      end if
+      if((pos /= 0) .and. (len(trim(runSeqCode(i)(:pos-1))) == 0)) then
         tokRunSeq%line(i)%tok_type = ESMF_OTHER_TOKEN
         cycle  
       end if
@@ -189,21 +212,210 @@ contains
           tokRunSeq%line(i)%tok_type = ESMF_TIMELOOP_END_TOKEN
         else
           tokRunSeq%line(i)%tok_type = ESMF_TIMELOOP_BEGIN_TOKEN
-          tokRunSeq%line(i)%tok(1) = trim(runSeqCode(i)(pos+1:))
+          tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) = trim(runSeqCode(i)(pos+1:))
         end if
       else
-        pos = INDEX(runSeqCode(i), CONN_TOKENSTR)
+        ! Ignore misc tokens (comments/remapMethods etc)
+        ! at the end of the line
+        lpos = SCAN(runSeqCode(i), MISC_EOL_TOKENSTR)
+        if(lpos == 1) then
+          ! We don't expect the tokens to be the first char in the line
+          ! Full line comments are handled above
+          print *, "ERROR: Parse error !"
+          if(present(rc)) then
+            rc = ESMF_FAILURE
+          end if
+          exit
+        else if(lpos == 0) then
+          lpos = len(trim(runSeqCode(i)))
+        else
+          ! Ignore the misc token
+          lpos = lpos - 1
+        end if
+        pos = INDEX(runSeqCode(i)(:lpos), CONN_TOKENSTR)
         if(pos /= 0) then
           tokRunSeq%line(i)%tok_type = ESMF_CONN_TOKEN
+          tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) =&
+            ESMF_FullStrTrim(runSeqCode(i)(:pos-1))
+          tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK2) =&
+            ESMF_FullStrTrim(runSeqCode(i)(pos+1:lpos))
         else
           tokRunSeq%line(i)%tok_type = ESMF_COMP_PHASE_TOKEN
+          tmp_tok = ESMF_FullStrTrim(runSeqCode(i)(:lpos))
+          print *, " tmp_tok = ", """", trim(tmp_tok), """"
+          pos = INDEX(tmp_tok, SPACE_TOKENSTR)
+          if(pos == 0)then
+            tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) =&
+              ESMF_FullStrTrim(tmp_tok)
+          else if(len(trim(tmp_tok(pos:))) == 0) then
+            tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) =&
+              ESMF_FullStrTrim(tmp_tok(:pos))
+          else
+            tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1) =&
+              ESMF_FullStrTrim(tmp_tok(:pos))
+            tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK2) =&
+              ESMF_FullStrTrim(tmp_tok(pos+1:lpos))
+          end if
         end if
       end if
     end do 
 
-    if (present(rc)) rc = ESMF_SUCCESS
   end subroutine
 !------------------------------------------------------------------------------
+
+#undef  ESMF_METHOD
+#define ESMF_METHOD "ESMF_MapperProcessRunSeq
+!BOP
+! !IROUTINE: ESMF_SeqCodeDbgInfo - Get debug info on the runsequence
+
+! !INTERFACE:
+subroutine GetSeqCodeDbgInfo(runSeqCode, tokRunSeq, curLine, dbgStr, rc)
+!
+!
+! !ARGUMENTS:
+    character(len=ESMF_MAXSTR), dimension(:), intent(in) :: runSeqCode
+    type(ESMF_RunSeqTokenizedCode), intent(in) :: tokRunSeq
+    integer, intent(in) :: curLine
+    character(len=ESMF_MAXSTR), intent(out) :: dbgStr
+    integer,             intent(out), optional :: rc
+
+! !DESCRIPTION:
+!   Gets debug information about the run sequence
+!
+! The arguments are:
+!   \begin{description}
+!   \item[{[runSeqCode]}]
+!     The run sequence (code)
+!   \item[{[tokRunSeq]}]
+!     Tokenized run sequence
+!   \item[{[curLine]}]
+!     Current line number in the run sequence
+!   \item[{[dbgStr]}]
+!     The debug information will be returned in this string
+!   \item[{[rc]}]
+!     Return code; equals {\tt ESMF\_SUCCESS} if there are no errors.
+!   \end{description}
+!
+!EOP
+
+  dbgStr = "Running : " // trim(runSeqCode(curLine))
+  if(tokRunSeq%line(curLine)%tok_type == ESMF_TIMELOOP_BEGIN_TOKEN) then
+    dbgStr = trim(dbgStr) // " : Timeloop (begin)" 
+  else if(tokRunSeq%line(curLine)%tok_type == ESMF_TIMELOOP_END_TOKEN) then
+    dbgStr = trim(dbgStr) // " : Timeloop (end)" 
+  else if(tokRunSeq%line(curLine)%tok_type == ESMF_COMP_PHASE_TOKEN) then
+    dbgStr = trim(dbgStr) // " : comp phase :" 
+    dbgStr = trim(dbgStr) // " comp = "
+    dbgStr = trim(dbgStr) // tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK1)
+    if(len(trim(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK2))) /= 0) then
+      dbgStr = trim(dbgStr) // " phase = "
+      dbgStr = trim(dbgStr) // trim(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK2))
+    end if
+  else if(tokRunSeq%line(curLine)%tok_type == ESMF_CONN_TOKEN) then
+    dbgStr = trim(dbgStr) // " : conn from : "
+    dbgStr = trim(dbgStr) // trim(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK1))
+    dbgStr = trim(dbgStr) // " - to - "
+    dbgStr = trim(dbgStr) // trim(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK2))
+  else if(tokRunSeq%line(curLine)%tok_type == ESMF_RUNSEQ_BEGIN_TOKEN) then
+    dbgStr = trim(dbgStr) // " : runseq (begin)" 
+  else if(tokRunSeq%line(curLine)%tok_type == ESMF_RUNSEQ_END_TOKEN) then
+    dbgStr = trim(dbgStr) // " : runseq (end)" 
+  else
+    dbgStr = trim(dbgStr) // " : misc" 
+  end if
+
+  if(present(rc)) then
+    rc = ESMF_SUCCESS
+  end if
+
+end subroutine
+
+subroutine ESMF_GetRunSeqInfo(runSeqCode, tokRunSeq, nTimeLoops, nExecLines, rc)
+  character(len=ESMF_MAXSTR), dimension(:), intent(in) :: runSeqCode
+  type(ESMF_RunSeqTokenizedCode), intent(in) :: tokRunSeq
+  integer, intent(out) :: nTimeLoops
+  integer, intent(out) :: nExecLines
+  integer, intent(out), optional :: rc
+
+  integer :: i, j, nlines
+  type LoopInfo
+    integer :: loopCount
+    real :: loopTime
+    integer :: nlinesInLoop
+  end type
+
+  type(LoopInfo), dimension(:), allocatable :: loopInfoStack
+  integer :: loopInfoStackTop
+
+  nTimeLoops = 0
+  nlines = size(tokRunSeq%line)
+  do i=1,nlines
+    if(tokRunSeq%line(i)%tok_type == ESMF_TIMELOOP_BEGIN_TOKEN) then
+      nTimeLoops = nTimeLoops + 1
+    end if
+  end do 
+
+  allocate(LoopInfoStack(nTimeLoops))
+
+  loopInfoStackTop = 0
+  do i=1,nlines
+    if(tokRunSeq%line(i)%tok_type == ESMF_TIMELOOP_BEGIN_TOKEN) then
+      loopInfoStackTop = loopInfoStackTop + 1
+      if(loopInfoStackTop == 1) then
+        read(tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1), *)&
+          loopInfoStack(loopInfoStackTop)%loopTime
+        loopInfoStack(loopInfoStackTop)%loopCount = 1
+        loopInfoStack(loopInfoStackTop)%nlinesInLoop = 0
+      else
+        read(tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1), *)&
+          loopInfoStack(loopInfoStackTop)%loopTime
+        loopInfoStack(loopInfoStackTop)%loopCount =&
+          INT(loopInfoStack(loopInfoStackTop-1)%loopTime/&
+              loopInfoStack(loopInfoStackTop)%loopTime)
+        loopInfoStack(loopInfoStackTop)%nlinesInLoop = 0
+      end if
+      cycle
+    else if(tokRunSeq%line(i)%tok_type == ESMF_TIMELOOP_END_TOKEN) then
+      if(loopInfoStackTop /= 1) then
+        do j=loopInfoStackTop-1,1,-1
+          if(loopInfoStack(j)%loopCount /= 0) then
+            loopInfoStack(loopInfoStackTop)%loopCount =&
+              loopInfoStack(loopInfoStackTop)%loopCount *&
+              loopInfoStack(j)%loopCount
+          end if
+        end do
+      end if
+      loopInfoStackTop = loopInfoStackTop - 1
+      cycle
+    else if(tokRunSeq%line(i)%tok_type == ESMF_COMP_PHASE_TOKEN) then
+      if(loopInfoStackTop == 0) then
+        print *, "ERROR: COMP PHASE outside timeloop !"
+        print *, "ERROR : ", tokRunSeq%line(i)%tok(ESMF_RUNSEQ_TOK1)
+      else
+        loopInfoStack(loopInfoStackTop)%nlinesInLoop =&
+          loopInfoStack(loopInfoStackTop)%nlinesInLoop + 1
+        !print *, "Counting : ", trim(runSeqCode(i)), loopInfoStack(loopInfoStackTop)%nlinesInLoop, loopInfoStack(loopInfoStackTop)%loopCount
+      end if
+    else if(tokRunSeq%line(i)%tok_type == ESMF_CONN_TOKEN) then
+      if(loopInfoStackTop == 0) then
+        print *, "ERROR: CONN outside timeloop !"
+      else
+        loopInfoStack(loopInfoStackTop)%nlinesInLoop =&
+          loopInfoStack(loopInfoStackTop)%nlinesInLoop + 1
+        !print *, "Counting : ", trim(runSeqCode(i)), loopInfoStack(loopInfoStackTop)%nlinesInLoop, loopInfoStack(loopInfoStackTop)%loopCount
+      end if
+    else
+      ! ignore these tokens
+    end if
+  end do 
+
+  nExecLines = 0
+  do i=1,nTimeLoops
+    nExecLines = nExecLines + loopInfoStack(i)%nlinesInLoop *&
+      loopInfoStack(i)%loopCount
+  end do
+
+end subroutine
 
 ! -------------------------- ESMF-public method -------------------------------
 #undef  ESMF_METHOD
@@ -238,9 +450,9 @@ contains
 !                        ESMF_CONN_TOKEN=3,&
 !                        ESMF_OTHER_TOKEN=4
 
-!  integer, parameter :: ESMF_MAXTOKENSTR = 128
+!  integer, parameter :: ESMF_RUNSEQ_MAXTOKENSTR = 128
 !  type ESMF_RunSeqTokenizedLine
-!    character(len=ESMF_MAXTOKENSTR), dimension(:), allocatable :: tok
+!    character(len=ESMF_RUNSEQ_MAXTOKENSTR), dimension(:), allocatable :: tok
 !    integer :: tok_type
 !  end type
 !  type ESMF_RunSeqTokenizedCode
@@ -255,25 +467,34 @@ contains
 !    integer :: runSeqTimeLoopInfoIdx
 !  end type
 
-    integer :: nTimeLoops, nlines, i
+    integer :: nTimeLoops, nlines, nExecLines, i
     integer :: curLine
     type(ESMF_RunSeqTimeLoopInfo), dimension(:), allocatable :: timeLoopInfoStack
     integer :: timeLoopInfoStackTop = 0
     type(ESMF_RunSeqTokenizedCode) :: tokRunSeq
+    character(len=ESMF_MAXSTR) :: dbgStr
+    integer :: localrc
     
 
-    call ESMF_MapperTokenizeRunSeq(runSeqCode, tokRunSeq, rc)
+    call ESMF_MapperTokenizeRunSeq(runSeqCode, tokRunSeq, rc=localrc)
     !if (ESMF_LogFoundError(localrc, &
     !  ESMF_ERR_PASSTHRU, &
-    !  ESMF_CONTEXT, rcToReturn=rc)) return
+    !  ESMF_CONTEXT, rcToReturn=localrc)) return
 
     nTimeLoops = 0
     nlines = size(tokRunSeq%line)
-    do i=1,nlines
-      if(tokRunSeq%line(i)%tok_type == ESMF_TIMELOOP_BEGIN_TOKEN) then
-        nTimeLoops = nTimeLoops + 1
-      end if
-    end do 
+    !do i=1,nlines
+    !  if(tokRunSeq%line(i)%tok_type == ESMF_TIMELOOP_BEGIN_TOKEN) then
+    !    nTimeLoops = nTimeLoops + 1
+    !  end if
+    !end do 
+    call ESMF_GetRunSeqInfo(runSeqCode, tokRunSeq, nTimeLoops, nExecLines, rc=localrc)
+    !if (ESMF_LogFoundError(localrc, &
+    !  ESMF_ERR_PASSTHRU, &
+    !  ESMF_CONTEXT, rcToReturn=localrc)) return
+
+    print *, "nTimeLoops = ", nTimeLoops, ", nExecLines=", nExecLines
+
     allocate(timeLoopInfoStack(nTimeLoops))
     curLine = 1
     do while(curLine <= nlines)
@@ -286,14 +507,14 @@ contains
         print *, "Timeloop tok = ", tokRunSeq%line(curLine)%tok(1)
         if(timeLoopInfoStackTop == 1) then
           ! First time loop
-          read(tokRunSeq%line(curLine)%tok(1), *)&
+          read(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK1), *)&
             timeLoopInfoStack(timeLoopInfoStackTop)%timeLoopTime
           timeLoopInfoStack(timeLoopInfoStackTop)%timeLoopCount = 1
           timeLoopInfoStack(timeLoopInfoStackTop)%runSeqLoopLineNum =&
             curLine 
         else
           ! We asssume that outer loops are multiples of inner loops
-          read(tokRunSeq%line(curLine)%tok(1), *)&
+          read(tokRunSeq%line(curLine)%tok(ESMF_RUNSEQ_TOK1), *)&
             timeLoopInfoStack(timeLoopInfoStackTop)%timeLoopTime
           timeLoopInfoStack(timeLoopInfoStackTop)%timeLoopCount =&
             INT(timeLoopInfoStack(timeLoopInfoStackTop-1)%timeLoopTime/&
@@ -318,7 +539,9 @@ contains
         end if
         cycle
       end if
-      print *, "Running : ", trim(runSeqCode(curLine)), " # ", curLine
+      call GetSeqCodeDbgInfo(runSeqCode, tokRunSeq, curLine, dbgStr, rc=localrc)
+      !print *, "Running : ", trim(runSeqCode(curLine)), " # ", curLine
+      print *, trim(dbgStr)
       curLine = curLine + 1
     end do 
 
