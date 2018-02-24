74660ea2c2e0daca9677a5c26f7db588ae1c837e
Gerhard Theurich <theurich@sourcespring.net>
2018-02-23 17:31:43 -0800

    Ensure correct shape on both sides of the array assignment, but do not use lhs bounds on rhs.

--- src/Infrastructure/Grid/interface/ESMF_Grid.F90
+++ src/Infrastructure/Grid/interface/ESMF_Grid.F90
@@ -13639,6 +13639,7 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
     integer                                    :: starti, startj, sizei, sizej
     integer                                    :: ind, rem, rem1, rem2
     integer                                    :: start(2), count(2)
+    integer                                    :: shapLon(2), shapLat(2)
     integer, allocatable                       :: minIndexPTile(:,:)
     integer, allocatable                       :: maxIndexPTile(:,:)
     integer, allocatable                       :: regDecomp(:,:)
@@ -14086,18 +14087,16 @@ type(ESMF_KeywordEnforcer), optional:: keywordEnforcer ! must use keywords below
              deallocate(lonPtrR4, latPtrR4)
            endif
            if (docorner) then
+             shapLon=shape(lonCornerPtr)  ! make sure lhs and rhs is same shape
+             shapLat=shape(latCornerPtr)! make sure lhs and rhs is same shape
              if (coordSysLocal .eq. ESMF_COORDSYS_SPH_DEG) then
-              lonCornerPtr(:,:) = &
-                lonCornerPtrR4(:ubound(lonCornerPtr,1),:ubound(lonCornerPtr,2))&
+              lonCornerPtr = lonCornerPtrR4(1:shapLon(1),1:shapLon(2)) &
                 * ESMF_COORDSYS_RAD2DEG
-              latCornerPtr(:,:) = &
-                latCornerPtrR4(:ubound(latCornerPtr,1),:ubound(latCornerPtr,2))&
+              latCornerPtr = latCornerPtrR4(1:shapLat(1),1:shapLat(2)) &
                 * ESMF_COORDSYS_RAD2DEG
              else
-              lonCornerPtr(:,:) = &
-                lonCornerPtrR4(:ubound(lonCornerPtr,1),:ubound(lonCornerPtr,2))
-              latCornerPtr(:,:) = &
-                latCornerPtrR4(:ubound(latCornerPtr,1),:ubound(latCornerPtr,2))
+              lonCornerPtr = lonCornerPtrR4(1:shapLon(1),1:shapLon(2))
+              latCornerPtr = latCornerPtrR4(1:shapLat(1),1:shapLat(2))
              endif
              deallocate(lonCornerPtrR4, latCornerPtrR4)
            endif  
