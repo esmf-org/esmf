b008f442f06d23e02743a6e6e55a9b6343b48ef0
Gerhard Theurich <theurich@sourcespring.net>
2018-04-13 16:22:53 -0700

    Pass the ESMF_BOPT setting through a CPP macro. Adjust perfromance threshold according to ESMF_BOPT setting.

--- src/Infrastructure/Array/tests/ESMF_ArrayRedistPerfUTest.F90
+++ src/Infrastructure/Array/tests/ESMF_ArrayRedistPerfUTest.F90
@@ -55,7 +55,7 @@ program ESMF_ArrayRedistPerfUTest
   real(ESMF_KIND_R8), pointer :: dstPtr(:,:), tstPtr(:,:)
   integer                     :: lrc, i, j
   logical                     :: mismatch
-  real(ESMF_KIND_R8)          :: t0, t1, t0_store, t1_store, dt
+  real(ESMF_KIND_R8)          :: t0, t1, t0_store, t1_store, dt, dtTest
 #endif
 
   ! cumulative result: count failures; no failures equals "all pass"
@@ -235,7 +235,12 @@ program ESMF_ArrayRedistPerfUTest
   dt = t1_store - t0_store
   write(msgString,*) "ArrayRedistStore() performance: ", dt, " seconds."
   call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
-  call ESMF_Test((dt<10.), name, failMsg, result, ESMF_SRCLINE)
+#ifdef ESMF_BOPT_g
+  dtTest = 10.  ! 10s is expected to pass in debug mode
+#else
+  dtTest = 1.   ! 1s is expected to pass in optimized mode
+#endif
+  call ESMF_Test((dt<dtTest), name, failMsg, result, ESMF_SRCLINE)
 
 !------------------------------------------------------------------------
   !EX_UTest_Multi_Proc_Only
@@ -244,7 +249,12 @@ program ESMF_ArrayRedistPerfUTest
   dt = t1 - t0
   write(msgString,*) "ArrayRedist() performance: ", dt, " seconds."
   call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
-  call ESMF_Test((dt<0.1), name, failMsg, result, ESMF_SRCLINE)
+#ifdef ESMF_BOPT_g
+  dtTest = 0.1    ! 0.1s is expected to pass in debug mode
+#else
+  dtTest = 0.01   ! 0.01s is expected to pass in optimized mode
+#endif
+  call ESMF_Test((dt<dtTest), name, failMsg, result, ESMF_SRCLINE)
 
 #endif
 
