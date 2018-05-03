4fc656cdf2bb4e220cd284787b4afb8bd390b7df
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-05-02 20:43:26 -0500

    Templates for the solver
    Adding the basic function templates for the solver
    
    Also adding a unit test that would test the solver (once
    implementation is complete)

--- src/Superstructure/Mapper/include/ESMCI_Solver.h
+++ src/Superstructure/Mapper/include/ESMCI_Solver.h
@@ -1,13 +1,67 @@
-#ifdef ESMCI_Solver_H
+#ifndef ESMCI_Solver_H
 #define ESMCI_Solver_H
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include "ESMCI_Poly.h"
+#include "ESMCI_PolyUV.h"
+#include "ESMCI_PolyTwoV.h"
+#include "ESMCI_PolyDer.h"
+#include "ESMCI_Mat.h"
 
 namespace ESMCI{
   namespace MapperUtil{
 
+    template<typename T>
     class SESolver{
       public:
         virtual ~SESolver() = default;
+        SESolver(const std::vector<T> &init_vals, const std::vector<TwoVIDPoly<T> > &funcs);
+        void set_init_vals(const std::vector<T> &init_vals);
+        void set_funcs(const std::vector<TwoVIDPoly<T> > &funcs);
+        void set_niters(int niters);
+        std::vector<T> minimize(void ) const;
+
+      private:
+        static const int DEFAULT_NITERS = 10;
+        int niters_;
+        std::vector<T> init_vals_;
+        std::vector<TwoVIDPoly<T> > funcs_;
     }; //class SESolver
+
+
+    template<typename T>
+    inline SESolver<T>::SESolver(const std::vector<T> &init_vals, const std::vector<TwoVIDPoly<T> > &funcs):niters_(DEFAULT_NITERS),init_vals_(init_vals),funcs_(funcs)
+    {}
+
+    template<typename T>
+    inline void SESolver<T>::set_init_vals(const std::vector<T> &init_vals)
+    {
+      init_vals_ = init_vals;
+    }
+
+    template<typename T>
+    inline void SESolver<T>::set_funcs(const std::vector<TwoVIDPoly<T> > &funcs)
+    {
+      funcs_ = funcs;
+    }
+
+    template<typename T>
+    inline void SESolver<T>::set_niters(int niters)
+    {
+      niters_ = niters;
+    }
+
+    template<typename T>
+    std::vector<T> SESolver<T>::minimize(void ) const
+    {
+      // Matrix<T> Xi(init_vals_);
+      // Matrix<T> Xj;
+
+      // Xj = Xi - inv(J) * F
+      return init_vals_;
+    }
 
   } // namespace MapperUtil
 } // namespace ESMCI
--- src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
+++ src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
 #include "ESMCI_Poly.h"
-#include "ESMCI_PolyDer.h"
+#include "ESMCI_PolyUV.h"
+#include "ESMCI_PolyTwoV.h"
 #include "ESMCI_Mat.h"
+#include "ESMCI_Solver.h"
 #include "ESMC_Test.h"
 
 int main(int argc, char *argv[])
@@ -14,9 +17,19 @@ int main(int argc, char *argv[])
 
   ESMC_TestStart(__FILE__, __LINE__, 0);
 
-  ESMCI::MapperUtil::Matrix<float> m1 = {{1, 2}, {5, 6}};
-  std::cout << m1 << std::endl;
+  std::vector<float> init_vals = {2.0, 4.0, 6.0};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1 = {1,0, 2.0, 3.0};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p2 = {4,0, 5.0, 6.0};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p3 = {7,0, 8.0, 9.0};
+  std::vector<ESMCI::MapperUtil::TwoVIDPoly<float> > funcs = {p1, p2, p3};
+  ESMCI::MapperUtil::SESolver<float> solver(init_vals, funcs);
+  std::vector<float> sol_vals = solver.minimize();
 
+  for(std::vector<float>::const_iterator citer = sol_vals.cbegin();
+      citer != sol_vals.cend(); ++citer){
+    std::cout << *citer << ", ";
+  }
+  std::cout << "\n";
 
   ESMC_TestEnd(__FILE__, __LINE__, 0);
 }
