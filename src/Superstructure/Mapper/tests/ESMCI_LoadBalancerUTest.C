6206482a6a6391d96f896576623260cc44a0ca70
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-05-09 13:57:18 -0500

    Adding a test for the LoadBalancer
    Adding a test for the load balancer class.
    * Data from runs on Mira for ATM/OCN/CICE components in E3SM are
      used to generate execution times for the components
    * The test invokes the load balancer in a loop to optimize the
      PET lists for 3 components running in parallel

--- src/Superstructure/Mapper/tests/ESMCI_LoadBalancerUTest.C
+++ src/Superstructure/Mapper/tests/ESMCI_LoadBalancerUTest.C
@@ -15,22 +15,45 @@ int main(int argc, char *argv[])
   const int ESMF_MAX_STRLEN = 128;
   char name[ESMF_MAX_STRLEN];
   char failMsg[ESMF_MAX_STRLEN];
+  const int MAX_ITER = 5;
 
   ESMC_TestStart(__FILE__, __LINE__, 0);
 
   int ncomps = 3;
-  std::vector<float> parallel_exec_times = {100.0, 200.0, 300.0};
-  std::vector<float> serial_exec_times = {3000.0, 5000.0, 9000.0};
-  std::vector<int> npets = {128, 256, 512};
-  ESMCI::MapperUtil::LoadBalancer<float> lb(ncomps, parallel_exec_times,
-    serial_exec_times, npets);
-  std::vector<int> opt_npets = lb.optimize();
+  ESMCI::MapperUtil::UVIDPoly<float> pcomp1, pcomp2, pcomp3;
+  std::vector<float> pcomp1_xvals = {3600.0, 7200.0, 14400.0, 28800.0};
+  std::vector<float> pcomp1_yvals = {1273.542, 819.700, 426.051, 290.470};
 
-  for(std::vector<int>::const_iterator citer = opt_npets.cbegin();
-      citer != opt_npets.cend(); ++citer){
-    std::cout << *citer << ", ";
+  std::vector<float> pcomp2_xvals = {2368, 4096, 8064, 21120};
+  std::vector<float> pcomp2_yvals = {959.630, 605.206, 324.372, 236.945};
+
+  std::vector<float> pcomp3_xvals = {1600, 4096, 8192, 9600};
+  std::vector<float> pcomp3_yvals = {756.506, 399.585, 270.775, 227.472};
+
+  rc = ESMCI::MapperUtil::PolyFit(ESMCI::MapperUtil::POLY_FIT_2D_LS_LAPACK, pcomp1_xvals, pcomp1_yvals, pcomp1);
+  assert(rc == 0);
+  rc = ESMCI::MapperUtil::PolyFit(ESMCI::MapperUtil::POLY_FIT_2D_LS_LAPACK, pcomp2_xvals, pcomp2_yvals, pcomp2);
+  assert(rc == 0);
+  rc = ESMCI::MapperUtil::PolyFit(ESMCI::MapperUtil::POLY_FIT_2D_LS_LAPACK, pcomp3_xvals, pcomp3_yvals, pcomp3);
+  assert(rc == 0);
+  
+  std::vector<int> npets = {128, 256, 512};
+  std::vector<float> parallel_exec_times = {pcomp1.eval(std::vector<float>(1,npets[0])), pcomp2.eval(std::vector<float>(1,npets[1])), pcomp3.eval(std::vector<float>(1,npets[2]))};
+  std::vector<float> serial_exec_times = {npets[0]*parallel_exec_times[0], npets[1] * parallel_exec_times[1], npets[2] * parallel_exec_times[2]};
+  ESMCI::MapperUtil::LoadBalancer<float> lb(ncomps, parallel_exec_times, serial_exec_times, npets);
+
+  for(int i=0; i<MAX_ITER; i++){
+    std::vector<int> opt_npets = lb.optimize();
+    parallel_exec_times = {pcomp1.eval(std::vector<float>(1,opt_npets[0])), pcomp2.eval(std::vector<float>(1,opt_npets[1])), pcomp3.eval(std::vector<float>(1,opt_npets[2]))};
+    serial_exec_times = {opt_npets[0]*parallel_exec_times[0], opt_npets[1] * parallel_exec_times[1], opt_npets[2] * parallel_exec_times[2]};
+    lb.set_lb_info(parallel_exec_times, serial_exec_times, opt_npets);
+
+    for(std::vector<int>::const_iterator citer = opt_npets.cbegin();
+        citer != opt_npets.cend(); ++citer){
+      std::cout << *citer << ", ";
+    }
+    std::cout << "\n";
   }
-  std::cout << "\n";
 
   ESMC_TestEnd(__FILE__, __LINE__, 0);
 }
