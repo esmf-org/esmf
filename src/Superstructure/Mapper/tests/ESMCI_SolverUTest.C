7fa1d9ea8bb179785711298f976d9fed317a7c9a
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-05-16 14:00:25 -0500

    Adding a solver test similar to usage by LB
    Modifying solver test to be similar to how it is used from
    the Load Balancer.

--- src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
+++ src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
@@ -14,22 +14,44 @@ int main(int argc, char *argv[])
   const int ESMF_MAX_STRLEN = 128;
   char name[ESMF_MAX_STRLEN];
   char failMsg[ESMF_MAX_STRLEN];
+  const int SOLVER_MAX_ITERS = 1;
 
   ESMC_TestStart(__FILE__, __LINE__, 0);
 
+  // Initial value of the variables
   std::vector<float> init_vals = {2.0, 4.0, 6.0};
+  // Functions in x, y and z used to create the user
+  // constraint functions
   std::vector<std::string> vnames = {"x", "y", "z"};
-  ESMCI::MapperUtil::TwoVIDPoly<float> p1 = {1.0, 2.0, 3.0};
-  std::vector<std::string> p1_vnames = {"x", "y"};
+  ESMCI::MapperUtil::UVIDPoly<float> p1 = {1.0, 2.0, 3.0};
+  std::vector<std::string> p1_vnames = {"x"};
   p1.set_vnames(p1_vnames);
-  ESMCI::MapperUtil::TwoVIDPoly<float> p2 = {4.0, 5.0, 6.0};
-  std::vector<std::string> p2_vnames = {"y", "z"};
+  ESMCI::MapperUtil::UVIDPoly<float> p2 = {4.0, 5.0, 6.0};
+  std::vector<std::string> p2_vnames = {"y"};
   p2.set_vnames(p2_vnames);
-  ESMCI::MapperUtil::TwoVIDPoly<float> p3 = {7.0, 8.0, 9.0};
-  std::vector<std::string> p3_vnames = {"x", "z"};
+  ESMCI::MapperUtil::UVIDPoly<float> p3 = {7.0, 8.0, 9.0};
+  std::vector<std::string> p3_vnames = {"z"};
   p3.set_vnames(p3_vnames);
-  std::vector<ESMCI::MapperUtil::TwoVIDPoly<float> > funcs = {p1, p2, p3};
+  // Create two variable polynomials from univariate polynomials
+  std::vector<std::string> p2v_xy_vnames = {"x", "y"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xy(p1, p2v_xy_vnames);
+  std::cout << p1_2v_xy << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p2_2v(p2, p2v_xy_vnames);
+  std::cout << p2_2v << "\n";
+  std::vector<std::string> p2v_xz_vnames = {"x", "z"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xz(p1, p2v_xz_vnames);
+  std::cout << p1_2v_xz << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p3_2v(p3, p2v_xz_vnames);
+  std::cout << p3_2v << "\n";
+  // User constraint functions
+  // Constraint functions are square of the difference between
+  // (pair-wise) the functions above
+  std::vector<ESMCI::MapperUtil::TwoVIDPoly<float> > funcs =
+    {(p1_2v_xy - p2_2v)* (p1_2v_xy - p2_2v), (p1_2v_xz - p3_2v) * (p1_2v_xz - p3_2v)};
+
+  // Create a solver instance and optimize init_vals
   ESMCI::MapperUtil::SESolver<float> solver(vnames, init_vals, funcs);
+  solver.set_niters(SOLVER_MAX_ITERS);
   std::vector<float> sol_vals = solver.minimize();
 
   for(std::vector<float>::const_iterator citer = sol_vals.cbegin();
