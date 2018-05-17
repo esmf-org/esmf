71496adc1cc50526a5c2fcc2c59833cecde2d3f6
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-05-17 07:31:42 -0500

    Adding two more unit tests for the solver
    Adding two more unit tests for the solver. These tests use
    two different polynomial fits (fit vs polyfit in Matlab) for
    the same data.

--- src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
+++ src/Superstructure/Mapper/tests/ESMCI_SolverUTest.C
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
   const int ESMF_MAX_STRLEN = 128;
   char name[ESMF_MAX_STRLEN];
   char failMsg[ESMF_MAX_STRLEN];
-  const int SOLVER_MAX_ITERS = 1;
+  const int SOLVER_MAX_ITERS = 10;
 
   ESMC_TestStart(__FILE__, __LINE__, 0);
 
@@ -56,6 +56,98 @@ int main(int argc, char *argv[])
 
   for(std::vector<float>::const_iterator citer = sol_vals.cbegin();
       citer != sol_vals.cend(); ++citer){
+    std::cout << *citer << ", ";
+  }
+  std::cout << "\n";
+
+  // Second test
+
+  // Initial value of the variables
+  std::vector<float> init_vals_test2 = {4096, 2048, 1024};
+  // Functions in x, y and z used to create the user
+  // constraint functions
+  std::vector<std::string> vnames_test2 = {"x", "y", "z"};
+  ESMCI::MapperUtil::UVIDPoly<float> p1_test2 = {350.7, -586.6, 439.4};
+  std::vector<std::string> p1_vnames_test2 = {"x"};
+  p1_test2.set_vnames(p1_vnames_test2);
+  ESMCI::MapperUtil::UVIDPoly<float> p2_test2 = {399.3, -569.9, 232.1};
+  std::vector<std::string> p2_vnames_test2 = {"y"};
+  p2_test2.set_vnames(p2_vnames_test2);
+  ESMCI::MapperUtil::UVIDPoly<float> p3_test2 = {161.3, -202.4, 292.6};
+  std::vector<std::string> p3_vnames_test2 = {"z"};
+  p3_test2.set_vnames(p3_vnames_test2);
+  // Create two variable polynomials from univariate polynomials
+  std::vector<std::string> p2v_xy_vnames_test2 = {"x", "y"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xy_test2(p1_test2, p2v_xy_vnames_test2);
+  std::cout << p1_2v_xy_test2 << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p2_2v_test2(p2_test2, p2v_xy_vnames_test2);
+  std::cout << p2_2v_test2 << "\n";
+  std::vector<std::string> p2v_xz_vnames_test2 = {"x", "z"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xz_test2(p1_test2, p2v_xz_vnames_test2);
+  std::cout << p1_2v_xz_test2 << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p3_2v_test2(p3_test2, p2v_xz_vnames_test2);
+  std::cout << p3_2v_test2 << "\n";
+  // User constraint functions
+  // Constraint functions are square of the difference between
+  // (pair-wise) the functions above
+  std::vector<ESMCI::MapperUtil::TwoVIDPoly<float> > funcs_test2 =
+    { (p1_2v_xy_test2 - p2_2v_test2)* (p1_2v_xy_test2 - p2_2v_test2),
+      (p1_2v_xz_test2 - p3_2v_test2) * (p1_2v_xz_test2 - p3_2v_test2)};
+
+  // Create a solver instance and optimize init_vals
+  ESMCI::MapperUtil::SESolver<float> solver_test2(vnames_test2,
+    init_vals_test2, funcs_test2);
+  solver_test2.set_niters(SOLVER_MAX_ITERS);
+  std::vector<float> sol_vals_test2 = solver_test2.minimize();
+
+  for(std::vector<float>::const_iterator citer = sol_vals_test2.cbegin();
+      citer != sol_vals_test2.cend(); ++citer){
+    std::cout << *citer << ", ";
+  }
+  std::cout << "\n";
+
+  // Third test
+
+  // Initial value of the variables
+  std::vector<float> init_vals_test3 = {4823, 1685, 657};
+  // Functions in x, y and z used to create the user
+  // constraint functions
+  std::vector<std::string> vnames_test3 = {"x", "y", "z"};
+  ESMCI::MapperUtil::UVIDPoly<float> p1_test3 = {2.82311e-06, -0.128872, 1664.65};
+  std::vector<std::string> p1_vnames_test3 = {"x"};
+  p1_test3.set_vnames(p1_vnames_test3);
+  ESMCI::MapperUtil::UVIDPoly<float> p2_test3 = {5.55039e-06, -0.166144, 1271.83};
+  std::vector<std::string> p2_vnames_test3 = {"y"};
+  p2_test3.set_vnames(p2_vnames_test3);
+  ESMCI::MapperUtil::UVIDPoly<float> p3_test3 = {1.1893e-05, -0.19465, 1025.47};
+  std::vector<std::string> p3_vnames_test3 = {"z"};
+  p3_test3.set_vnames(p3_vnames_test3);
+  // Create two variable polynomials from univariate polynomials
+  std::vector<std::string> p2v_xy_vnames_test3 = {"x", "y"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xy_test3(p1_test3, p2v_xy_vnames_test3);
+  std::cout << p1_2v_xy_test3 << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p2_2v_test3(p2_test3, p2v_xy_vnames_test3);
+  std::cout << p2_2v_test3 << "\n";
+  std::vector<std::string> p2v_xz_vnames_test3 = {"x", "z"};
+  ESMCI::MapperUtil::TwoVIDPoly<float> p1_2v_xz_test3(p1_test3, p2v_xz_vnames_test3);
+  std::cout << p1_2v_xz_test3 << "\n";
+  ESMCI::MapperUtil::TwoVIDPoly<float> p3_2v_test3(p3_test3, p2v_xz_vnames_test3);
+  std::cout << p3_2v_test3 << "\n";
+  // User constraint functions
+  // Constraint functions are square of the difference between
+  // (pair-wise) the functions above
+  std::vector<ESMCI::MapperUtil::TwoVIDPoly<float> > funcs_test3 =
+    { (p1_2v_xy_test3 - p2_2v_test3)* (p1_2v_xy_test3 - p2_2v_test3),
+      (p1_2v_xz_test3 - p3_2v_test3) * (p1_2v_xz_test3 - p3_2v_test3)};
+
+  // Create a solver instance and optimize init_vals
+  ESMCI::MapperUtil::SESolver<float> solver_test3(vnames_test3,
+    init_vals_test3, funcs_test3);
+  solver_test3.set_niters(SOLVER_MAX_ITERS);
+  std::vector<float> sol_vals_test3 = solver_test3.minimize();
+
+  for(std::vector<float>::const_iterator citer = sol_vals_test3.cbegin();
+      citer != sol_vals_test3.cend(); ++citer){
     std::cout << *citer << ", ";
   }
   std::cout << "\n";
