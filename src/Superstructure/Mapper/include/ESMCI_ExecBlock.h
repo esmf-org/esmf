73b84c929ee4b9ebe073598b43f6f928afda018a
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-08-03 12:17:27 -0500

    Find scaling func for an execution block
    Adding a util member function to the execution block class to
    find the associated scaling function.
    
    Also adding a unit test that tests the new function.

--- src/Superstructure/Mapper/include/ESMCI_ExecBlock.h
+++ src/Superstructure/Mapper/include/ESMCI_ExecBlock.h
@@ -10,6 +10,9 @@
 #include <cassert>
 #include <cmath>
 #include "ESMCI_CompInfo.h"
+#include "ESMCI_CompInfoUtils.h"
+#include "ESMCI_PolyUV.h"
+#include "ESMCI_PolyFit.h"
 #include "ESMC_Macros.h"
 
 namespace ESMCI{
@@ -32,12 +35,16 @@ namespace ESMCI{
         };
         PetCmpInfo cmp_pet_range(const CompInfo<T> &comp_info) const;
         PetCmpInfo cmp_pet_range(const ExecBlock<T> &exec_block) const;
+        bool get_scaling_function(UVIDPoly<T> &f);
         template<typename U>
         friend std::ostream &operator<<(std::ostream &ostr, const ExecBlock<U> &exec_block);
       private:
         std::vector<CompInfo<T> > comp_phases_;
         std::pair<int, int> pet_range_;
         std::pair<T, T> time_intvl_;
+        bool sfunc_is_valid_;
+        UVIDPoly<T> sfunc_;
+        int exec_id_;
     };
 
     template<typename T>
@@ -54,7 +61,7 @@ namespace ESMCI{
     }
 
     template<typename T>
-    inline ExecBlock<T>::ExecBlock()
+    inline ExecBlock<T>::ExecBlock():sfunc_is_valid_(false), exec_id_(0)
     {
     }
 
@@ -62,6 +69,18 @@ namespace ESMCI{
     inline ExecBlock<T>::ExecBlock(const std::vector<CompInfo<T> > &comp_phases):
             comp_phases_(comp_phases)
     {
+      for(typename std::vector<CompInfo<T> >::const_iterator citer =
+              comp_phases_.cbegin();
+            citer != comp_phases_.cend(); ++citer){
+        std::pair<int, int> comp_pet_range = citer->get_pet_range();
+        std::pair<T, T> comp_time_intvl = citer->get_time_interval();
+
+        pet_range_.first = std::min(pet_range_.first, comp_pet_range.first);
+        pet_range_.second = std::max(pet_range_.second, comp_pet_range.second);
+
+        time_intvl_.first = std::min(time_intvl_.first, comp_time_intvl.first);
+        time_intvl_.second = std::max(time_intvl_.second, comp_time_intvl.second);   
+      }
     }
 
     template<typename T>
@@ -167,7 +186,105 @@ namespace ESMCI{
         return PET_NIR;
       }
     }
-    
+
+    template<typename T>
+    bool ExecBlock<T>::get_scaling_function(UVIDPoly<T> &f)
+    {
+      if(sfunc_is_valid_){
+        f = sfunc_;
+      }
+      else{
+        CompInfoStore<T> *cinfo_store = CompInfoStore<T>::get_instance();
+        assert(cinfo_store);
+
+        std::vector<std::vector<std::pair<int, int> > > comp_past_pet_ranges;
+        std::vector<std::vector<std::pair<T, T> > > comp_past_time_intvls;
+        int npast_comp_times = 0;
+        /* Get the past wtime and npets for each component phase
+         * - each row is past info for component i
+         */
+        for(typename std::vector<CompInfo<T> >::const_iterator citer =
+              comp_phases_.cbegin();
+            citer != comp_phases_.cend(); ++citer){
+          std::vector<std::pair<int, int> > past_pet_ranges =
+            cinfo_store->get_past_pet_ranges(*citer);
+          comp_past_pet_ranges.push_back(past_pet_ranges);
+          comp_past_time_intvls.push_back(cinfo_store->get_past_time_intervals(*citer));
+          if(npast_comp_times == 0){
+            npast_comp_times = past_pet_ranges.size();
+          }
+          else{
+            npast_comp_times = std::min(npast_comp_times,
+                                static_cast<int>(past_pet_ranges.size()));
+          }
+        }
+
+        /* Reorder all past wtime and npets by execution
+         * - each row is past info for an execution 
+         */
+        std::vector<std::vector<std::pair<int, int> > > comp_past_pet_ranges_by_exec;
+        std::vector<std::vector<std::pair<T, T> > > comp_past_time_intvls_by_exec;
+        for(int i=0; i<npast_comp_times; i++){
+          std::vector<std::pair<int, int> > comp_past_pet_ranges_by_exec_row;
+          for(int j=0; j< static_cast<int>(comp_past_pet_ranges.size()); j++){
+            comp_past_pet_ranges_by_exec_row.push_back(comp_past_pet_ranges[j][i]);
+          }
+          comp_past_pet_ranges_by_exec.push_back(comp_past_pet_ranges_by_exec_row);
+          std::vector<std::pair<T, T> > comp_past_time_intvls_by_exec_row;
+          for(int j=0; j< static_cast<int>(comp_past_time_intvls.size()); j++){
+            comp_past_time_intvls_by_exec_row.push_back(comp_past_time_intvls[j][i]);
+          }
+          comp_past_time_intvls_by_exec.push_back(comp_past_time_intvls_by_exec_row);
+        }
+
+        std::vector<T> exec_block_past_npets;
+        std::vector<T> exec_block_past_wtimes;
+        /* FIXME: We are ignoring holes in PET ranges for the execution block */
+        for(std::vector<std::vector<std::pair<int, int> > >::const_iterator citer =
+                comp_past_pet_ranges_by_exec.cbegin();
+              citer != comp_past_pet_ranges_by_exec.cend(); ++citer){
+          int min_pet = 0;
+          int max_pet = 0;
+          for(std::vector<std::pair<int, int> >::const_iterator pet_citer =
+                  (*citer).cbegin();
+                pet_citer != (*citer).cend(); ++pet_citer){
+            min_pet = std::min(min_pet, pet_citer->first);
+            max_pet = std::max(max_pet, pet_citer->second);
+          }
+          exec_block_past_npets.push_back(max_pet - min_pet + 1);
+        }
+
+        for(typename std::vector<std::vector<std::pair<T, T> > >::const_iterator citer =
+                comp_past_time_intvls_by_exec.cbegin();
+              citer != comp_past_time_intvls_by_exec.cend(); ++citer){
+          T min_time = static_cast<T>(0);
+          T max_time = static_cast<T>(0);
+          for(typename std::vector<std::pair<T, T> >::const_iterator tciter =
+                  (*citer).cbegin();
+                tciter != (*citer).cend(); ++tciter){
+            min_time = std::min(min_time, tciter->first);
+            max_time = std::max(max_time, tciter->second);
+          }
+          assert(max_time > min_time);
+          exec_block_past_wtimes.push_back(max_time - min_time);
+        }
+
+        const int MIN_VALS_REQD_FOR_POLYFIT = 3;
+        const int MAX_DEG = 2;
+        if(npast_comp_times >= MIN_VALS_REQD_FOR_POLYFIT){
+          int ret = PolyFit(POLY_FIT_LS_LAPACK, MAX_DEG,
+                      exec_block_past_npets, exec_block_past_wtimes, sfunc_);
+          if(ret != ESMF_SUCCESS){
+            std::cerr << "Error: finding poly fit for exec block\n";
+          }
+          else{
+            sfunc_is_valid_ = true;
+          }
+        }
+      }
+
+      return sfunc_is_valid_;
+    }
 
   } // namespace MapperUtil
 } //namespace ESMCI
--- src/Superstructure/Mapper/tests/ESMCI_ExecBlockUTest.C
+++ src/Superstructure/Mapper/tests/ESMCI_ExecBlockUTest.C
@@ -33,6 +33,74 @@ int main(int argc, char *argv[])
 
   rc = ESMF_SUCCESS;
   try{
+    std::vector<std::pair<int, int> > pet_ranges = {
+                                              std::pair<int, int>(1, 10),
+                                              std::pair<int, int>(1, 20),
+                                              std::pair<int, int>(1, 25)
+                                            };
+    std::vector<std::pair<float, float> > time_intvls = {
+                                              std::pair<float, float>(0.0, 3.0),
+                                              std::pair<float, float>(3.1, 4.1),
+                                              std::pair<float, float>(4.2, 4.9)
+                                            };
+    std::vector<std::pair<int, int> >::const_iterator pet_iter = pet_ranges.cbegin();
+    std::vector<std::pair<float, float> >::const_iterator time_iter =time_intvls.cbegin();
+    ESMCI::MapperUtil::CompInfoStore<float> *comp_info_store =
+      ESMCI::MapperUtil::CompInfoStore<float>::get_instance();
+    for(; (pet_iter != pet_ranges.end()) && (time_iter != time_intvls.end());
+          ++pet_iter, ++time_iter){
+      std::pair<int, int> atm_pet_range(*pet_iter);
+      std::pair<float, float> atm_time_intvl(*time_iter);
+      int npets = pet_iter->second - pet_iter->first + 1;
+      std::pair<int, int> ocn_pet_range(pet_iter->second + 1, pet_iter->second + npets);
+      std::pair<float, float> ocn_time_intvl(*time_iter);
+      ESMCI::MapperUtil::CompInfo<float> comp1("ATM", "run", atm_pet_range, atm_time_intvl);
+      ESMCI::MapperUtil::CompInfo<float> comp2("OCN", "run", ocn_pet_range, ocn_time_intvl);
+
+      comp_info_store->add_comp_info(comp1);
+      comp_info_store->add_comp_info(comp2);
+
+    }
+    ESMCI::MapperUtil::UVIDPoly<float> sfunc;
+    ESMCI::MapperUtil::CompInfo<float> comp1("ATM", "run", pet_ranges[0], time_intvls[0]);
+    ESMCI::MapperUtil::CompInfo<float> comp2("OCN", "run", pet_ranges[0], time_intvls[0]);
+    bool has_scaling_func = comp_info_store->get_scaling_function(comp1, sfunc);
+    if(has_scaling_func){
+      std::cout << sfunc << "\n";
+    }
+    else{
+      rc = ESMF_FAILURE;
+    }
+    has_scaling_func = comp_info_store->get_scaling_function(comp2, sfunc);
+    if(has_scaling_func){
+      std::cout << sfunc << "\n";
+    }
+    else{
+      rc = ESMF_FAILURE;
+    }
+
+    ESMCI::MapperUtil::ExecBlock<float> eblock;
+    eblock.add_comp_phase(comp1);
+    eblock.add_comp_phase(comp2);
+
+    has_scaling_func = eblock.get_scaling_function(sfunc);
+    if(has_scaling_func){
+      std::cout << sfunc << "\n";
+    }
+    else{
+      rc = ESMF_FAILURE;
+    }
+  }
+  catch(...){
+    rc = ESMF_FAILURE;
+  }
+
+  strncpy(name, "Exec Block (two parallel comps) Scaling function test", ESMF_MAX_STRLEN);
+  strncpy(failMsg, "Exec Blocks (two parallel comps) Scaling function test failed", ESMF_MAX_STRLEN);
+  ESMC_Test((rc == ESMF_SUCCESS), name, failMsg, &result, __FILE__, __LINE__, 0);
+
+  rc = ESMF_SUCCESS;
+  try{
     std::vector<ESMCI::MapperUtil::CompInfo<float> > comps;
     std::pair<int, int> atm_pet_range(1, 2);
     std::pair<float, float> atm_time_intvl(0.0, 3.0);
