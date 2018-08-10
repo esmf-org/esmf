a0f429752ccd54f071bf713b67eff9ace512663c
Gerhard Theurich <theurich@sourcespring.net>
2018-08-10 15:54:41 +0000

    Turn memory profiling on.

--- src/Infrastructure/Array/src/ESMCI_Array.C
+++ src/Infrastructure/Array/src/ESMCI_Array.C
@@ -15,7 +15,7 @@
 
 #define ASMM_STORE_LOG_off
 #define ASMM_STORE_TIMING_off
-#define ASMM_STORE_MEMLOG_off
+#define ASMM_STORE_MEMLOG_on
 #define ASMM_STORE_TUNELOG_off
 #define ASMM_STORE_COMMMATRIX_on
 #define ASMM_STORE_DUMPSMM_off
