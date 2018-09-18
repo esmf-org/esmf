e4582a74168b2e04b8cdb798842543b2e44652f1
Jayesh Krishna <jayesh@mcs.anl.gov>
2018-09-18 12:24:48 -0500

    Adding polyfit for polys with double coeffs
    Adding polyfit support for polynomials with coefficients that
    are doubles.
    
    This commit also includes some code refactoring to avoid
    duplicating code and moving the util function inside the util
    namespace.

--- src/Superstructure/Mapper/include/ESMCI_PolyFit.h
+++ src/Superstructure/Mapper/include/ESMCI_PolyFit.h
@@ -18,67 +18,83 @@ namespace ESMCI{
       POLY_FIT_LS_LAPACK
     } PolyFitAlg;
 
-    /* Use LAPACK to minimize Least square errors and fit a polynomial (of maximum
-     * degree, max_deg) on a 2D user data set. The 2D user data is specified via
-     * xvals (x values for the user data) and yvals (y values for the user data),
-     * and the resulting fit polynomial coefficients are stored (returned via) in
-     * coeffs
-     */
-    inline int LAPACK_2D_Solver(int max_deg, const std::vector<float>& xvals, const std::vector<float> &yvals, std::vector<float> &coeffs)
-    {
-      if(xvals.size() == 0) return 0;
-      assert(xvals.size() == yvals.size());
-      // Using LAPACK to solve minimize || Ax - B ||
-      int num_obs = xvals.size();
-      int NUM_ROWS_IN_A = num_obs;
-      const int NUM_COLS_IN_A = max_deg + 1;
-      int NUM_ROWS_IN_B = num_obs;
-      const int NUM_COLS_IN_B = 1;
-
-      lapack_int info, m, n, lda, ldb, nrhs;
-      m = NUM_ROWS_IN_A;
-      n = NUM_COLS_IN_A;
-      lda = NUM_COLS_IN_A;
-      ldb = NUM_COLS_IN_B;
-      nrhs = NUM_COLS_IN_B;
-
-      float *A = (float *)calloc(NUM_ROWS_IN_A * NUM_COLS_IN_A, sizeof(float));
-      float *B = (float *)calloc(NUM_ROWS_IN_B * NUM_COLS_IN_B, sizeof(float));
-      assert(A && B);
-
-      /*
-       */ 
-      for(int i=0; i<num_obs; i++){
-        for(int j=0; j<NUM_COLS_IN_A; j++){
-          A[i * NUM_COLS_IN_A + j] = powf(xvals[i], j);
-        }
-        B[i] = yvals[i];
-      }
-
-      info = LAPACKE_sgels(LAPACK_ROW_MAJOR, 'N', m, n, nrhs, A, lda, B, ldb);
-      /* info == 0 is success */
-      if(info < 0){
-        std::cout << "LAPACKE_sgels failed, the " << -info
-          << "th arg in the input array, A[" << -info << "] ="
-          << A[-info] << " is invalid\n";
-        return ESMF_FAILURE;
-      }
-      else if(info > 0){
-        std::cout << "LAPACKE_sgels failed, the " << info
-          << "th diagonal element of the triangular factor of input array, A == 0, "
-          << "so A has no full rank and LES solution cannot be computed\n";
-        return ESMF_FAILURE;
-      }
-
-      coeffs.resize(max_deg + 1);
-      for(int i=0; i<max_deg+1; i++)
-      {
-        coeffs[i] = B[max_deg - i];
-      }
-      return ESMF_SUCCESS;
-    }
-
     namespace PolyFitUtil{
+
+      inline lapack_int LAPACKE_Gels(int matrix_layout, char trans,
+        lapack_int m, lapack_int n, lapack_int nrhs,
+        float *A, lapack_int lda, float *B, lapack_int ldb)
+      {
+        return LAPACKE_sgels(matrix_layout, trans, m, n, nrhs, A, lda, B, ldb);
+      }
+
+      inline lapack_int LAPACKE_Gels(int matrix_layout, char trans,
+        lapack_int m, lapack_int n, lapack_int nrhs,
+        double *A, lapack_int lda, double *B, lapack_int ldb)
+      {
+        return LAPACKE_dgels(matrix_layout, trans, m, n, nrhs, A, lda, B, ldb);
+      }
+
+      /* Use LAPACK to minimize Least square errors and fit a polynomial (of maximum
+       * degree, max_deg) on a 2D user data set. The 2D user data is specified via
+       * xvals (x values for the user data) and yvals (y values for the user data),
+       * and the resulting fit polynomial coefficients are stored (returned via) in
+       * coeffs
+       */
+      template<typename T>
+      inline int LAPACK_2D_Solver(int max_deg, const std::vector<T>& xvals, const std::vector<T> &yvals, std::vector<T> &coeffs)
+      {
+        if(xvals.size() == 0) return 0;
+        assert(xvals.size() == yvals.size());
+        // Using LAPACK to solve minimize || Ax - B ||
+        int num_obs = xvals.size();
+        int NUM_ROWS_IN_A = num_obs;
+        const int NUM_COLS_IN_A = max_deg + 1;
+        int NUM_ROWS_IN_B = num_obs;
+        const int NUM_COLS_IN_B = 1;
+
+        lapack_int info, m, n, lda, ldb, nrhs;
+        m = NUM_ROWS_IN_A;
+        n = NUM_COLS_IN_A;
+        lda = NUM_COLS_IN_A;
+        ldb = NUM_COLS_IN_B;
+        nrhs = NUM_COLS_IN_B;
+
+        T *A = (T *)calloc(NUM_ROWS_IN_A * NUM_COLS_IN_A, sizeof(T));
+        T *B = (T *)calloc(NUM_ROWS_IN_B * NUM_COLS_IN_B, sizeof(T));
+        assert(A && B);
+
+        /*
+         */ 
+        for(int i=0; i<num_obs; i++){
+          for(int j=0; j<NUM_COLS_IN_A; j++){
+            A[i * NUM_COLS_IN_A + j] = powf(xvals[i], j);
+          }
+          B[i] = yvals[i];
+        }
+
+        info = LAPACKE_Gels(LAPACK_ROW_MAJOR, 'N', m, n, nrhs, A, lda, B, ldb);
+        /* info == 0 is success */
+        if(info < 0){
+          std::cout << "LAPACKE_sgels failed, the " << -info
+            << "th arg in the input array, A[" << -info << "] ="
+            << A[-info] << " is invalid\n";
+          return ESMF_FAILURE;
+        }
+        else if(info > 0){
+          std::cout << "LAPACKE_sgels failed, the " << info
+            << "th diagonal element of the triangular factor of input array, A == 0, "
+            << "so A has no full rank and LES solution cannot be computed\n";
+          return ESMF_FAILURE;
+        }
+
+        coeffs.resize(max_deg + 1);
+        for(int i=0; i<max_deg+1; i++)
+        {
+          coeffs[i] = B[max_deg - i];
+        }
+        return ESMF_SUCCESS;
+      }
+
       template<typename VType>
       VType GetMean(const std::vector<VType> &vals)
       {
@@ -144,7 +160,7 @@ namespace ESMCI{
       std::vector<VType> xvals_centered_and_scaled = xvals;
       PolyCSInfo<VType> csinfo = PolyFitUtil::CenterAndScale(xvals_centered_and_scaled);
 
-      int ret = LAPACK_2D_Solver(max_deg, xvals_centered_and_scaled, yvals, coeffs);
+      int ret = PolyFitUtil::LAPACK_2D_Solver(max_deg, xvals_centered_and_scaled, yvals, coeffs);
       assert(ret == ESMF_SUCCESS);
 
       poly.set_coeffs(coeffs); 
