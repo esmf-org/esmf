38b7242ec1f9db32b52e73f92f8808b089d99156
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2018-10-04 09:44:36 -0600

    add Attribute JSON test directory

---
+++ src/Infrastructure/Attribute/test/ESMC_AttributeJSONUTest.C
@@ -0,0 +1,56 @@
+// $Id$
+//
+// Earth System Modeling Framework
+// Copyright 2002-2016, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+//
+//==============================================================================
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// ESMF header
+#include "ESMC.h"
+
+// ESMF Test header
+#include "ESMC_Test.h"
+
+//==============================================================================
+//BOP
+// !PROGRAM: ESMCI_AttributeJSONUTest - Internal Attribute JSON functionality
+//
+// !DESCRIPTION:
+//
+//EOP
+//-----------------------------------------------------------------------------
+
+int main(void){
+
+  char name[80];
+  char failMsg[80];
+  int result = 0;
+  int rc;
+
+  //----------------------------------------------------------------------------
+  ESMC_TestStart(__FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  //NEX_UTest
+  // Create an Attribute JSON
+  strcpy(name, "Attribute JSON Create");
+  strcpy(failMsg, "Did not return ESMF_SUCCESS");
+  ESMC_Test((rc==ESMF_SUCCESS), name, failMsg, &result, __FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  ESMC_TestEnd(__FILE__, __LINE__, 0);
+  //----------------------------------------------------------------------------
+
+  return 0;
+}
