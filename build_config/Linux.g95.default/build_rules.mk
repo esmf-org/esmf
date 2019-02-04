f9fb67059b5610da988502344091e15c79633568
Walter Spector <walter.spector@hpe.com>
2019-02-03 21:07:28 -0800

    Fix remaining mpich2 configurations to define ESMF_NO_MPI3.

--- build_config/Linux.g95.default/build_rules.mk
+++ build_config/Linux.g95.default/build_rules.mk
@@ -43,6 +43,8 @@ ESMF_F90DEFAULT         = mpif90
 ESMF_CXXDEFAULT         = mpicxx
 ESMF_MPIRUNDEFAULT      = mpirun $(ESMF_MPILAUNCHOPTIONS)
 ESMF_MPIMPMDRUNDEFAULT  = mpiexec $(ESMF_MPILAUNCHOPTIONS)
+ESMF_F90COMPILECPPFLAGS+= -DESMF_NO_MPI3
+ESMF_CXXCOMPILECPPFLAGS+= -DESMF_NO_MPI3
 else
 ifeq ($(ESMF_COMM),mpich3)
 # Mpich3 ---------------------------------------------------
