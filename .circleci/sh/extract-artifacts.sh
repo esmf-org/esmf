4728f90a4dad0b1bbd011dbf966d913503883c13
Ben Koziol <ben.koziol@noaa.gov>
2020-06-26 14:41:56 -0500

    WIP: Fixed run/exec to extract artifacts

--- .circleci/sh/extract-artifacts.sh
+++ .circleci/sh/extract-artifacts.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 
-docker run -dit -v /tmp:/dexc bash -c "cp /$ESMF_ARTIFACTS/doc-artifacts.zip /dexc" "${DOCKER_IMG}:${CIRCLE_BRANCH}"
+docker run -dit --name runner -v /tmp:/dexc "${DOCKER_IMG}:${CIRCLE_BRANCH}"
+docker exec -t runner bash -c "cp $ESMF_ARTIFACTS/doc-artifacts.zip /dexc"
+docker stop runner
+docker rm runner
