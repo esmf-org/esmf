deaaefe8610165447c74cebe5530fd701069614f
Ben Koziol <ben.koziol@noaa.gov>
2020-07-07 11:39:17 -0500

    WIP: Fix for cache reuse

--- .circleci/sh/osx/osx-conda-create-environment.sh
+++ .circleci/sh/osx/osx-conda-create-environment.sh
@@ -5,7 +5,7 @@ set -Eeuxo pipefail
 : "${CONDA_EXE?Need to set}"
 
 # Only install if the cache has not been restored
-if [ ! -d "${CONDA_INSTALLDIR}" ]; then
+if [ ! -d ~/miniconda ]; then
   curl -o  miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
   bash miniconda.sh -b -p "${CONDA_INSTALLDIR}"
   hash -r
