493670f1ed68fb784bf1a20b99a0a506c3ab4cc5
Ben Koziol <ben.koziol@noaa.gov>
2020-07-07 08:40:01 -0500

    WIP: Use curl instead of wget

--- .circleci/sh/osx/osx-conda-create-environment.sh
+++ .circleci/sh/osx/osx-conda-create-environment.sh
@@ -5,7 +5,7 @@ set -Eeuxo pipefail
 
 # Only install if the cache has not been restored
 if [ ! -d "/path/to/dir" ]; then
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+  curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
   bash miniconda.sh -b -p "${CONDA_INSTALLDIR}"
   source "${CONDA_INSTALLDIR}/etc/profile.d/conda.sh"
   hash -r
