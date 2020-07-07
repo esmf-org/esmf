0bf08266cb853b3d09a3c8455a75e3a35787a095
Ben Koziol <ben.koziol@noaa.gov>
2020-07-07 10:55:43 -0500

    WIP: Osx installation may actually work this time

--- .circleci/sh/osx/osx-conda-create-environment.sh
+++ .circleci/sh/osx/osx-conda-create-environment.sh
@@ -9,11 +9,10 @@ if [ ! -d "${CONDA_INSTALLDIR}" ]; then
   curl -o  miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
   bash miniconda.sh -b -p "${CONDA_INSTALLDIR}"
   hash -r
-  ${CONDA_EXE} config --set always_yes yes --set changeps1 no
-  ${CONDA_EXE} update -q conda
-#  ${CONDA_EXE} install -q conda-build
-  ${CONDA_EXE} create -n esmf -c conda-forge esmf esmpy
-  ${CONDA_EXE} remove -n esmf --force esmf esmpy
+  bash -c "${CONDA_EXE} config --set always_yes yes --set changeps1 no"
+  bash -c "${CONDA_EXE} update -q conda"
+  bash -c "${CONDA_EXE} create -n esmf -c conda-forge esmf esmpy"
+  bash -c "${CONDA_EXE} remove -n esmf --force esmf esmpy"
 else
   echo "Using CircleCI cache"
 fi
