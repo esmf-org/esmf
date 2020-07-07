e716785e09c3ac8cb5b56759d1e1c245f83553cf
Ben Koziol <ben.koziol@noaa.gov>
2020-07-07 15:19:55 -0500

    WIP: Get brew to link gcc

--- .circleci/sh/osx/osx-brew-install.sh
+++ .circleci/sh/osx/osx-brew-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
+brew link gcc || echo "INFO: brew gcc not installed"
 # Only install if the cache has not been restored
 if ! command -v gfortran; then
   brew install gcc
