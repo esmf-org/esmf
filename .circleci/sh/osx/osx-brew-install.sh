5a7d31ba41d60135b8d65beff2a40b8d100d390d
Ben Koziol <ben.koziol@noaa.gov>
2020-07-07 15:28:54 -0500

    WIP: Try gcc 9 series

--- .circleci/sh/osx/osx-brew-install.sh
+++ .circleci/sh/osx/osx-brew-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-brew link gcc || echo "INFO: brew gcc not installed"
+brew link gcc@9 || echo "INFO: brew gcc not installed"
 # Only install if the cache has not been restored
 if ! command -v gfortran; then
-  brew install gcc
+  brew install gcc@9
 else
   echo "Using CircleCI cache"
 fi
