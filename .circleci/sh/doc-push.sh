a99cb51a06a3d4189f61a095590974534d8ebf6b
Ben Koziol <ben.koziol@noaa.gov>
2020-07-08 13:24:01 -0500

    WIP: Publish docs initial test

---
+++ .circleci/sh/doc-push.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
+cd
+git clone git@github.com:esmf-org/esmf-org.github.io.git esmf
+cd esmf
+ssh -T git@github.com
+
+#cd ~/esmf/doc
+#cp -rf * ~/esmf-org.github.io/dev_docs/
+#
+#cd ~/esmf-org.github.io/
+#git pull
+#git add .
+#git commit -m " pushing trunk ref doc build by CircleCI on `date` "
+#git push origin master
+#
+#cd ~/esmf/src/addon/NUOPC/doc
+#
+#for i in  NUOPC_refdoc NUOPC_howtodoc
+#    do
+#        cp -rf $i ~/esmf-org.github.io/dev_docs/
+#
+#    done
+#
+#for i in  NUOPC_refdoc.pdf NUOPC_howtodoc.pdf
+#    do
+#        cp -rf $i ~/esmf-org.github.io/dev_docs/
+#    done
+#
+#cd ~/esmf-org.github.io/dev_docs
+#git pull
+#git add .
+#git commit -m " pushing nuopc doc build by CircleCI on `date` "
+#git push origin master
