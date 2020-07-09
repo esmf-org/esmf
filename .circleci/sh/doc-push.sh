2b809b3e2bc49ef67575179429b3e962f1bf5160
Ben Koziol <ben.koziol@noaa.gov>
2020-07-09 08:37:31 -0500

    WIP: Attempt at automated doc push

--- .circleci/sh/doc-push.sh
+++ .circleci/sh/doc-push.sh
@@ -1,35 +1,43 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-cd
-git clone git@github.com:esmf-org/esmf-org.github.io.git esmf-gio
-cd esmf-gio
-ssh -T git@github.com
+: "${CIRCLE_BRANCH?Need to set}"
 
-#cd ~/esmf/doc
-#cp -rf * ~/esmf-org.github.io/dev_docs/
-#
-#cd ~/esmf-org.github.io/
-#git pull
-#git add .
-#git commit -m " pushing trunk ref doc build by CircleCI on `date` "
-#git push origin master
-#
-#cd ~/esmf/src/addon/NUOPC/doc
-#
-#for i in  NUOPC_refdoc NUOPC_howtodoc
-#    do
-#        cp -rf $i ~/esmf-org.github.io/dev_docs/
-#
-#    done
-#
-#for i in  NUOPC_refdoc.pdf NUOPC_howtodoc.pdf
-#    do
-#        cp -rf $i ~/esmf-org.github.io/dev_docs/
-#    done
-#
-#cd ~/esmf-org.github.io/dev_docs
-#git pull
-#git add .
-#git commit -m " pushing nuopc doc build by CircleCI on `date` "
-#git push origin master
+DOC_ARTIFACTS="/tmp/artifacts"
+
+cd
+
+# Clone the docs repository
+git clone git@github.com:esmf-org/esmf-org.github.io.git
+
+# ESMF Docs -------------------------------------------------------------------
+
+cd ${DOC_ARTIFACTS}/doc-esmf
+cp -rf ./* ~/esmf-org.github.io/dev_docs/
+
+cd ~/esmf-org.github.io/
+git pull
+git add .
+git commit -m " pushing trunk ref doc build by CircleCI on `date` "
+git push origin "${CIRCLE_BRANCH}"
+
+# NUOPC Docs ------------------------------------------------------------------
+
+cd ${DOC_ARTIFACTS}/doc-nuopc
+
+for i in  NUOPC_refdoc NUOPC_howtodoc
+    do
+        cp -rf $i ~/esmf-org.github.io/dev_docs/
+
+    done
+
+for i in  NUOPC_refdoc.pdf NUOPC_howtodoc.pdf
+    do
+        cp -rf $i ~/esmf-org.github.io/dev_docs/
+    done
+
+cd ~/esmf-org.github.io/dev_docs
+git pull
+git add .
+git commit -m " pushing nuopc doc build by CircleCI on `date` "
+git push origin "${CIRCLE_BRANCH}"
