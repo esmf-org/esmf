1c695fd7408433a753723862ac1b4d46d8ddbc81
bekozi <ben.koziol@noaa.gov>
2020-07-10 13:19:11 -0500

    CircleCI osx support (#12)

--- .circleci/sh/osx/osx-brew-install.sh
+++ .circleci/sh/osx/osx-brew-install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-brew link gcc@9 || echo "INFO: brew gcc not installed"
+brew link gcc || echo "INFO: brew gcc not installed"
+
 # Only install if the cache has not been restored
 if ! command -v gfortran; then
-  brew install gcc@9
+  echo "INFO: install brew gcc"
+  brew install gcc
 else
-  echo "Using CircleCI cache"
+  echo "INFO: using CircleCI cache"
+fi
+
+# Confirm gfortran is installed
+if ! command -v gfortran; then
+  echo "ERROR: gfortran not installed or not linked"
+  exit 1
 fi
