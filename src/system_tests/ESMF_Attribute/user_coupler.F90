e5d8809d6c6ecf95bf760f2e3bae3ff6521803d4
Ben Koziol <ben.koziol@noaa.gov>
2020-07-26 14:36:53 -0500

    WIP: Before removing zapped proxies

--- src/system_tests/ESMF_Attribute/user_coupler.F90
+++ src/system_tests/ESMF_Attribute/user_coupler.F90
@@ -98,9 +98,9 @@ module user_coupler
     ! Need to reconcile import and export states
     call ESMF_CplCompGet(comp, vm=vm, rc=rc)
     if (rc/=ESMF_SUCCESS) return ! bail out
-    call ESMF_StateReconcile(importState, vm=vm, attreconflag=ESMF_ATTRECONCILE_ON, rc=rc)
+    call ESMF_StateReconcile(importState, vm=vm, rc=rc)
     if (rc/=ESMF_SUCCESS) return ! bail out
-    call ESMF_StateReconcile(exportState, vm=vm, attreconflag=ESMF_ATTRECONCILE_ON, rc=rc)
+    call ESMF_StateReconcile(exportState, vm=vm, rc=rc)
     if (rc/=ESMF_SUCCESS) return ! bail out
                                   
   end subroutine user_init
