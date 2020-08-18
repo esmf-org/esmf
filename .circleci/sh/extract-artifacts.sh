f31acfd41ad8499731d1edea15d65784bac0b6a9
Ben Koziol <ben.koziol@noaa.gov>
2020-08-18 09:54:39 -0600

    WIP: Optimized doc build by using caching

--- .circleci/sh/extract-artifacts.sh
+++ .circleci/sh/extract-artifacts.sh
@@ -3,7 +3,8 @@ set -Eeuxo pipefail
 
 : "${ESMF_ARTIFACTS?Need to set}"
 
-docker run -dit --name runner -v /tmp:/dexc "${DOCKER_IMG}:${CIRCLE_BRANCH}"
+mkdir -p /tmp/artifacts
+docker run -dit --name runner -v /tmp/artifacts:/dexc "${DOCKER_IMG}:${CIRCLE_BRANCH}"
 docker exec -t runner bash -c "cp ${ESMF_ARTIFACTS}/doc-artifacts.zip /dexc"
 docker stop runner
 docker rm runner
