c23f68f1430bfeef4bcae0323e310d23776ce3ee
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-08-20 13:11:30 -0700

    add large_file flag to ESMPy

--- src/Infrastructure/Field/tests/ESMC_FieldSMMFromFileUTest.C
+++ src/Infrastructure/Field/tests/ESMC_FieldSMMFromFileUTest.C
@@ -193,7 +193,7 @@ int main(void){
   rc = ESMC_FieldRegridStoreFile(srcfield, dstfield, 
                                  "data/weights_esmc_smmsff.nc", NULL, NULL,
                                  &routehandle, NULL, NULL, NULL, NULL, NULL,
-                                 NULL, NULL, &create_rh, NULL, NULL,
+                                 NULL, NULL, &create_rh, NULL, NULL, NULL,
                                  NULL, NULL, NULL, NULL, NULL);
 #if (defined ESMF_PIO && ( defined ESMF_NETCDF || defined ESMF_PNETCDF))
   ESMC_Test((rc==ESMF_SUCCESS), name, failMsg, &result, __FILE__, __LINE__, 0);
