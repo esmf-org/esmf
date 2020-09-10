91265122a5035e45262ddfab906cd7d397999f2d
Gerhard Theurich <theurich@sourcespring.net>
2020-09-10 11:22:51 -0700

    Filter out files during regression test summary that have extra characters past the expected file suffix.

--- scripts/test_scripts/unit_tests_bm_results.pl
+++ scripts/test_scripts/unit_tests_bm_results.pl
@@ -182,12 +182,7 @@ use File::Find
                         push @all_files, "$File::Find::name\n" ;
         }
         # Get all source unit tests files
-        @c_files=grep (/UTest.C/, @all_files);
-        @F90_files=grep (/UTest.F90/, @all_files);
-        foreach $file ( @c_files) {
-                push (@F90_files, $file);               
-        }
-	@st_ut_files = @F90_files;
+        @st_ut_files=grep (/UTest.F90$|UTest.C$/, @all_files);
         @ut_files = @st_ut_files;
         foreach ( @st_ut_files) {
                 s/\.\///; # Delete all the "./"
