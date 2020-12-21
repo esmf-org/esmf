8e668169645cc1fb3190a5f41310a8fe399a3eb5
Gerhard Theurich <theurich@sourcespring.net>
2020-12-21 15:01:51 -0800

    Squashed commit of the following:
    commit a64c894b537f296e8a4e0c82636db0f72685b724
    Author: Gerhard Theurich <theurich@sourcespring.net>
    Date:   Fri Nov 13 11:06:24 2020 -0800
    
        Rework the MethodTable implementation to be std::map based. This is
        expected to lower the overhead of method look-up for cases where many
        methods are registered.

--- src/Superstructure/Component/include/ESMCI_MethodTable.h
+++ src/Superstructure/Component/include/ESMCI_MethodTable.h
@@ -29,6 +29,7 @@
 //-----------------------------------------------------------------------------
 
 #include <string>
+#include <map>
 
 namespace ESMCI {
 
@@ -42,67 +43,35 @@ class MethodTable;
 
 class MethodElement{
   private:
-    const std::string label;
     void *pointer;
     std::string name;
     std::string shobj;
-    MethodElement *nextElement;
   public:
     // native C++ constructors/destructors
-    MethodElement(void):label(""){
+    MethodElement(void){
       pointer = NULL;
       name = std::string("");
       shobj = std::string("");
-      nextElement = NULL;
     }
-    MethodElement(std::string labelArg):label(labelArg){
-      pointer = NULL;
-      name = std::string("");
-      shobj = std::string("");
-      nextElement = NULL;
-    }
-    MethodElement(std::string labelArg, void *pointerArg):label(labelArg){
+    MethodElement(void *pointerArg){
       pointer = pointerArg;
       name = std::string("");
       shobj = std::string("");
-      nextElement = NULL;
     }
-    MethodElement(std::string labelArg, std::string nameArg,
-      std::string shobjArg):label(labelArg){
+    MethodElement(std::string nameArg, std::string shobjArg){
       pointer = NULL;
       name = nameArg;
       shobj = shobjArg;
-      nextElement = NULL;
-    }
-    ~MethodElement(void){
-      nextElement = NULL;
     }
     // other methods
-    int print(void)const;
-    int execute(void *object, int *userRc);
+    int execute(void *object, int *userRc)const;
     int resolve(void);
   friend class MethodTable;
 };
 
-
-class MethodTable{
-  private:
-    MethodElement *table;
+class MethodTable : std::map<std::string, MethodElement>{
   public:
-    // native C++ constructors/destructors
-    MethodTable(void){
-      table = NULL;
-    }
-    ~MethodTable(void){
-      while (table){
-        MethodElement *next = table->nextElement;   
-        delete table;
-        table = next;
-      }
-      table = NULL;
-    }
     // other methods
-    int print(void)const;
     int add(std::string labelArg, void *pointer);
     int add(std::string labelArg, std::string name, std::string sharedObj);
     int addreplace(std::string labelArg, void *pointer);
