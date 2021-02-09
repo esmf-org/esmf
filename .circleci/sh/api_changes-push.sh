55f7bbd5feab077aacc4b6b9c0b4a7c5c67eeb93
Himanshu Pillai <himanshupillai@gmail.com>
2021-02-09 11:34:37 -0700

    push Api change to platform_independent [ci skip]

--- .circleci/sh/api_changes-push.sh
+++ .circleci/sh/api_changes-push.sh
@@ -13,11 +13,11 @@ git clone --depth 1 git@github.com:esmf-org/esmf-test-artifacts.git
 
 # API changes -------------------------------------------------------------------
 
-cd esmf-test-artifacts/develop/
+cd esmf-test-artifacts/develop/platform_independent/
 mkdir -p api_change
 
 cd ${DOC_ARTIFACTS}
-cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/develop/api_change/
+cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/develop/platform_independent/api_change/
 
 
 cd ~/esmf-test-artifacts/
