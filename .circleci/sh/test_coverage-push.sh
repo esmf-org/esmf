2ac6bf278eddf2600c53837a38c3b01a13ff4d72
Himanshu Pillai <himanshupillai@gmail.com>
2021-02-24 15:47:47 -0700

    Release 8.1.0 Test Coverage [ci skip]

--- .circleci/sh/test_coverage-push.sh
+++ .circleci/sh/test_coverage-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-DOC_ARTIFACTS="/tmp/artifacts/log"
+DOC_ARTIFACTS="/tmp/artifacts/artifacts"
 
 cd 
 
@@ -14,16 +14,17 @@ git clone --depth 1 git@github.com:esmf-org/esmf-test-artifacts.git
 
 # Test coverage -------------------------------------------------------------------
 
-cd esmf-test-artifacts/develop/platform_independent/
-mkdir -p test_coverage
+cd esmf-test-artifacts/release/8.1.0/
+
+mkdir -p platform_independent/test_coverage
 
 cd ${DOC_ARTIFACTS}
-cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/develop/platform_independent/test_coverage/
+cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/release/8.1.0/platform_independent/test_coverage/
 
 
 cd ~/esmf-test-artifacts/
 git add .
-git commit -a -m " Test Coverage pushed in the artifacts `date` [ci skip] "
+git commit -a -m " Release 8.1.0 Test Coverage pushed in the artifacts `date` [ci skip] "
 
 # Push the changes ------------------------------------------------------------
 
