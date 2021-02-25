24e2eacd8df80be122355123c0b3d858797dbaf2
Himanshu Pillai <himanshupillai@gmail.com>
2021-02-25 14:34:51 -0700

    changes to api_push [ci skip]

--- .circleci/sh/api_changes-push.sh
+++ .circleci/sh/api_changes-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-DOC_ARTIFACTS="/tmp/artifacts/api"
+DOC_ARTIFACTS="/tmp/artifacts/artifacts"
 
 git config --global user.email "himanshu@ucar.edu"
 git config --global user.name "esmf-orgbot"
@@ -13,16 +13,16 @@ git clone --depth 1 git@github.com:esmf-org/esmf-test-artifacts.git
 
 # API changes -------------------------------------------------------------------
 
-cd esmf-test-artifacts/develop/platform_independent/
-mkdir -p api_change
+cd esmf-test-artifacts/
+mkdir -p ${CIRCLE_BRANCH}/platform_independent/api_change
 
 cd ${DOC_ARTIFACTS}
-cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/develop/platform_independent/api_change/
+cp -rf ${DOC_ARTIFACTS}/* ~/esmf-test-artifacts/${CIRCLE_BRANCH}/platform_independent/api_change/
 
 
 cd ~/esmf-test-artifacts/
 git add .
-git commit -a -m " API changes pushed in the artifacts `date` [ci skip] "
+git commit -a -m " `echo ${CIRCLE_BRANCH}` API changes pushed in the artifacts `date` [ci skip] "
 
 # Push the changes ------------------------------------------------------------
 
