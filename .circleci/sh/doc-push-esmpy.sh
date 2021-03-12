2de045ac216d5c77f40c50897d8097fff9090a8e
Himanshu Pillai <himanshupillai@gmail.com>
2021-03-12 10:25:56 -0700

    Update doc-push-esmpy.sh

--- .circleci/sh/doc-push-esmpy.sh
+++ .circleci/sh/doc-push-esmpy.sh
@@ -5,8 +5,11 @@ git config --global user.email "himanshu@ucar.edu"
 git config --global user.name "esmf-orgbot"
 
 cd
+
 git clone --depth 1 git@github.com:esmf-org/esmpy_doc.git
-cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/* ~/esmpy_doc/docs/develop/
+cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/html ~/esmpy_doc/docs/nightly/develop/
+cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/latex/ESMPy.pdf ~/esmpy_doc/docs/nightly/develop/
+
 cd ~/esmpy_doc/
 git add .
 git commit -m "ESMPy doc build by CircleCI"
