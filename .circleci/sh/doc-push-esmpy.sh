7d665d93dfad9dc4bec7c30f9b02f43038af912a
Himanshu Pillai <himanshupillai@gmail.com>
2021-03-12 11:47:29 -0700

    esmpy doc updated [ci skip]

--- .circleci/sh/doc-push-esmpy.sh
+++ .circleci/sh/doc-push-esmpy.sh
@@ -6,10 +6,14 @@ git config --global user.name "esmf-orgbot"
 
 cd
 git clone --depth 1 git@github.com:esmf-org/esmpy_doc.git
+
 cd esmpy_doc/
-mkdir -p docs/${CIRCLE_BRANCH}
+mkdir -p docs/nightly/${CIRCLE_BRANCH}
+
 cd ..
-cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/* ~/esmpy_doc/docs/${CIRCLE_BRANCH}/
+cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/html ~/esmpy_doc/docs/nightly/${CIRCLE_BRANCH}/
+cp -rf /tmp/artifacts/doc-esmpy/esmpy_doc/latex/ESMPy.pdf ~/esmpy_doc/docs/nightly/${CIRCLE_BRANCH}/
+
 cd ~/esmpy_doc/
 git add .
 git commit -m " `echo ${CIRCLE_BRANCH}` ESMPy doc build by CircleCI"
