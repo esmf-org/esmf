8205572189d37627bf9ce7b2c710500e3ebe3c33
Rocky Dunlap <rsdunlapiv@gmail.com>
2021-03-17 22:56:45 -0600

    Fix branch for api change push to esmf-test-artifacts

--- .circleci/sh/api_changes-push.sh
+++ .circleci/sh/api_changes-push.sh
@@ -27,4 +27,4 @@ git commit -a -m " `echo ${CIRCLE_BRANCH}` API changes pushed in the artifacts `
 # Push the changes ------------------------------------------------------------
 
 git remote prune origin
-git push origin master
+git push origin main
