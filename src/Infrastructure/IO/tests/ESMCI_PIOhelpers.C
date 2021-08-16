af68c8e89d376127c0394800e7c9dc579942067e
Jim Edwards <jedwards@ucar.edu>
2021-08-16 15:21:41 -0600

    updating pio tests

--- src/Infrastructure/IO/tests/ESMCI_PIOhelpers.C
+++ src/Infrastructure/IO/tests/ESMCI_PIOhelpers.C
@@ -32,42 +32,5 @@ using namespace std;
 
 using namespace ESMCI;
 
-#include <pio_kinds.h>
+#include <pio.h>
 
-extern "C" {
-
-void FTN_X(esmci_pio_size_check)(const char *strp,
-     const char *arg1, const char *arg2,
-     int *addr_diff,  // out: address delta between arg1 and arg2 in bytes
-     int *desc_len,   // out: sizeof the requested descriptor in bytes
-     int *rc, ESMCI_FortranStrLenArg slen) {
-
-  struct table_entry {
-    const char *name;
-    int len;};
-
-  table_entry table[] = {
-    { "iosystem_desc_t", PIO_SIZE_IOSYSTEM_DESC },
-    { "file_desc_t",     PIO_SIZE_FILE_DESC },
-    { "io_desc_t",       PIO_SIZE_IO_DESC },
-    { "var_desc_t",      PIO_SIZE_VAR_DESC}
-  };
-  const int table_size = sizeof (table) / sizeof (table_entry);
-
-  string str(strp, slen);
-  ptrdiff_t addr_diff_local = arg2 - arg1;
-  *addr_diff = addr_diff_local;
-  *desc_len = -1;
-
-  int i;
-  *rc = ESMF_FAILURE;
-  for (i=0; i<table_size; i++) {
-    if (str == table[i].name) {
-      *desc_len = table[i].len;
-      *rc = (addr_diff_local <= table[i].len) ? ESMF_SUCCESS : ESMF_FAILURE;
-      break;
-    }
-  }
-}
-
-}
