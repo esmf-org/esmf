8a25192e3b97882f99fe2ed44250242138150a93
Gerhard Theurich <theurich@sourcespring.net>
2021-08-24 07:49:47 -0700

    Improve example text.

--- src/Infrastructure/Route/examples/ESMF_RHandleVMEpochEx.F90
+++ src/Infrastructure/Route/examples/ESMF_RHandleVMEpochEx.F90
@@ -70,10 +70,11 @@ program ESMF_RHandleVMEpochEx
 ! Building on these primitives, asynchronous communications patterns can be
 ! implemented in user code. However, a more elegant option to achive
 ! asynchronous behavior between sending and receiving PETs of RouteHandle based
-! communications is provided by the VMEpoch feature.
+! communications is provided by the VMEpoch feature discussed here.
 !
 ! The VMEpoch is a low level message aggregation and buffering approach.
-! The VM level details are discussed under the ESMF\_VM section in \ref{VM:NBVMEpoch}.
+! The VM level details are discussed under the ESMF\_VM section in
+! \ref{VM:NBVMEpoch}.
 !
 ! There are several advantages of VMEpoch over the direct use of non-blocking
 ! RouteHandle based communication calls:
@@ -81,30 +82,28 @@ program ESMF_RHandleVMEpochEx
 ! \begin{itemize}
 !
 ! \item Multiple different RouteHandles can be aggregated within the same epoch.
-!  This allows e.g. to mix Redist(), Regrid(), and SMM() operations,
-!  aggregating all of the messages.
+!  This allows mixing of Redist(), Regrid(), and SMM() operations, and the
+!  aggregating of all messages.
 !
 ! \item The {\em same} RouteHandle can be used several times within the same
 !  epoch. This is not possible under the direct non-blocking execution.
 !
-! \item ArrayBundle and FieldBundle communications are support. This is not
+! \item ArrayBundle and FieldBundle communications are supported. This is not
 !  available for the direct non-blocking execution.
 !
 ! \end{itemize}
 !
 ! The {\tt ESMF\_VMEpoch} API consists of two interfaces:
-! {\tt ESMF\_VMEpochEnter()} and {\tt ESMF\_VMEpochExit()}. When entering an
-! epoch, the user specifies the type of epoch that is to be entered. Currently
-! only {\tt ESMF\_VMEPOCH\_BUFFER} is available. Inside an epoch,
+! {\tt ESMF\_VMEpochEnter()} and {\tt ESMF\_VMEpochExit()}. Inside an epoch,
 ! communication calls are aggregated. Data transfers on the 
-! {\tt src} side are not issued until the epoch is exited. On the {\tt dst} side
-! a single data transfer is received, and then divided over the individual
-! receive calls.
+! {\tt src} side are not issued until the epoch is exited. On the {\tt dst}
+! side, a single data transfer is received from any of the sending PETs, and
+! then divided over the individual receive calls.
 !
-! In the following code the {\tt srcArray} has DEs on PET 0 and 1, while
-! {\tt dstArray} has DEs on PET 2 and 3. Both Arrays are operating on the same
-! global index space. A Redist() RouteHandle {\tt rh} is created in the usual
-! manner.
+! In the following code example, the {\tt srcArray} has DEs on PET 0 and 1, 
+! while {\tt dstArray} has DEs on PET 2 and 3. Both Arrays are operating on
+! the same global index space. A Redist() RouteHandle {\tt rh} is created in
+! the usual manner.
 !EOE
   srcDL = ESMF_DELayoutCreate(petList=[0,1], rc=rc)
   if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
@@ -130,7 +129,8 @@ program ESMF_RHandleVMEpochEx
 ! VMEpoch is demonstrated.
 !
 ! First enter the VMEpoch using {\tt ESMF\_VMEpochEnter()}, specifying the kind
-! of epoch. Currently only {\tt ESMF\_VMEPOCH\_BUFFER} is available.
+! of epoch. Currently only a single VMEpoch kind is available:
+! {\tt ESMF\_VMEPOCH\_BUFFER}.
 !EOE
 !BOC
   call ESMF_VMEpochEnter(epoch=ESMF_VMEPOCH_BUFFER, rc=rc)
@@ -147,12 +147,13 @@ program ESMF_RHandleVMEpochEx
 ! Notice that ESMF implements a throttle on the VMEpoch as to limit the number
 ! of queued message between PETs. This is necessary to protect the receiving
 ! side in the {\tt EAGER} regime where MPI implementations send the
-! data to the receiving side, assuming it will be able to buffer. In cases where
-! the sending side runs ahead of the receiving side, this behavior can lead to
+! data from the sending to the receiving side, assuming the receiving side
+! will always be able to buffer. In cases where the sending side runs far
+! ahead of the receiving side, this strategy can lead to
 ! increasing memory pressure on the receiving side, ultimatily resulting in
-! out-of-memory conditions. The default throttle is set to $10$ outstanding
-! message between any two PETs. It can be adjusted by specifying the
-! {\tt throttle} argument to {\tt ESMF\_VMEpochEnter()}.
+! out-of-memory conditions. The default throttle in VMEpoch is set to $10$
+! outstanding message between any two PETs. It can be adjusted by specifying
+! the {\tt throttle} argument when calling {\tt ESMF\_VMEpochEnter()}.
 !
 ! Next the actual communication method, {\tt ESMF\_ArrayRedist()}, is called in
 ! the usual manner.
@@ -171,27 +172,35 @@ program ESMF_RHandleVMEpochEx
 !
 ! Waiting for the send buffers comes into play when a VMEpoch is entered and
 ! exited multiple times. The same send buffer is reused each time for the 
-! same src-dst-PET pairs (and grown automatically if needed). Each send buffer becomes
-! available once the MPI layer has indicated that the associated
-! {\tt MPI\_Isend()} has completed locally.
+! same src-dst-PET pairs (and grown automatically if needed). Each send buffer
+! becomes available once the MPI layer has indicated that the associated,
+! previous {\tt MPI\_Isend()} has completed locally.
 !
 ! Once the send buffer for a specific PET pair is available, the local data
-! movements defined by the {\tt rh} must complete before returning. For 
-! {\tt Regrid()} and {\tt SMM()} operations the {\tt srcTermProcessing} argument
-! specified during {\tt Store()} determines the amount of local data processing.
-! Once returned, it is safe to modify the {\tt srcArray} data on the local PET.
+! movements defined by the {\tt rh} and message aggregation must complete before
+! returning. For {\tt Regrid()} and {\tt SMM()} operations the
+! {\tt srcTermProcessing} argument specified during {\tt Store()} determines the
+! amount of local data processing. Once returned, it is safe to modify the
+! {\tt srcArray} data on the local PET.
 !
 ! {\bf DST side (PET 2 \& 3):}
 ! The receiving PETs block on the aggregated data from the src side for which
 ! the local PET has a dependency defined via the {\tt rh}. Once received, the
-! data is processed locally, and moved into the final location.
-! On return, it is safe to access the data in {\tt dstArray} on the local PET.
+! data is processed locally, and moved into the final location under the
+! {\tt dstArray}. On return, it is safe to access the data in {\tt dstArray}
+! on the local PET.
 !
-! Notice that several more RouteHandle based communication calls could be made inside
-! the same active VMEpoch. In fact, aggregating messages from multiple exchanges
-! is the typical use case of the VMEpoch approach. Additional communication calls
-! could either involve different RouteHandles, or even the same {\tt rh} for
-! different src/dst Array pairs.
+! Notice that any number of RouteHandle based communication calls can be made
+! inside the same active VMEpoch. In fact, aggregating messages from multiple
+! exchanges is the typical use case of the VMEpoch approach. Additional
+! communication calls cam either involve different RouteHandles, or even the
+! same {\tt rh} for different src/dst Array pairs.
+!
+! When using communication calls that allow the specification of the
+! {\tt termorderflag}, e.g. {\tt ESMF\_FieldRegrid()}, {\tt ESMF\_ArraySMM()},
+! etc. inside a VMEpoch, it is important to set it to either 
+! {\tt ESMF\_TERMORDER\_SRCPET} or {\tt ESMF\_TERMORDER\_SRCSEQ}. The default
+! value of {\tt ESMF\_TERMORDER\_FREE} is {\em not} compatible with VMEpoch.
 !
 ! Finally the active VMEpoch is exited by calling {\tt ESMF\_VMEpochExit()}.
 !EOE
