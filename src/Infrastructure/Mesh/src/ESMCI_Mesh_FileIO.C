2c0aaee587adcc86a10cf6b940cf339b561e4eb4
Robert Oehmke <oehmke@ucar.edu>
2021-11-01 15:31:05 -0600

    Add files for more scalable mesh create from file.

---
+++ src/Infrastructure/Mesh/src/ESMCI_Mesh_FileIO.C
@@ -0,0 +1,93 @@
+// $Id$
+//
+// Earth System Modeling Framework
+// Copyright 2002-2021, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+//
+//==============================================================================
+
+//==============================================================================
+//
+// This file contains the Fortran interface code to link F90 and C++.
+//
+//------------------------------------------------------------------------------
+// INCLUDES
+//------------------------------------------------------------------------------
+
+#include <string>
+#include <ostream>
+#include <iterator>
+
+#include "ESMCI_Macros.h"
+#include "ESMCI_F90Interface.h"
+#include "ESMCI_LogErr.h"
+#include "ESMCI_VM.h"
+#include "ESMCI_CoordSys.h"
+#include "ESMCI_Array.h"
+#include "ESMC_Util.h"
+
+#include "ESMCI_TraceMacros.h"  // for profiling
+
+#include "Mesh/include/ESMCI_Mesh.h"
+#include "Mesh/include/Legacy/ESMCI_MeshRead.h"
+#include "Mesh/include/Regridding/ESMCI_MeshRegrid.h" //only for the conservative flag in add_elements
+#include "Mesh/include/Legacy/ESMCI_MeshVTK.h"
+#include "Mesh/include/Legacy/ESMCI_ParEnv.h"
+#include "Mesh/include/Legacy/ESMCI_MeshUtils.h"
+#include "Mesh/include/Legacy/ESMCI_GlobalIds.h"
+#include "Mesh/include/ESMCI_MeshRedist.h"
+#include "Mesh/include/ESMCI_MeshDual.h"
+#include "Mesh/include/ESMCI_Mesh_Glue.h"
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+ static const char *const version = "$Id$";
+//-----------------------------------------------------------------------------
+
+
+using namespace ESMCI;
+
+
+void ESMCI_MeshCreateFromFile(char *filename, ESMC_FileFormat_Flag fileformat, 
+                              int convert_to_dual, int add_user_area, 
+                              Mesh **out_mesh, int *rc){
+#undef ESMC_METHOD
+#define ESMC_METHOD "ESMCI_MeshCreateFromFile()"
+
+  // Try-catch block around main part of method
+  try {
+    // local return code
+    int localrc;
+
+    // Only support ESMFMesh right now
+    if (fileformat != ESMC_FILEFORMAT_ESMFMESH) {
+      if(ESMC_LogDefault.MsgFoundError(ESMC_RC_ARG_VALUE,
+       " Only ESMFMesh format files supported right now.",
+                                 ESMC_CONTEXT, &localrc)) throw localrc;
+    }    
+
+
+
+
+
+  }catch(int localrc){
+    // catch standard ESMF return code
+    ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU, ESMC_CONTEXT, rc);
+    return;
+  } catch(...){
+    ESMC_LogDefault.MsgFoundError(ESMC_RC_INTNRL_BAD,
+          " Caught unknown exception", ESMC_CONTEXT, rc);
+    return;
+  }
+  
+  // We've gotten to bottom successfully, so return success
+  if(rc != NULL) *rc = ESMF_SUCCESS;
+}
+
+
+
+
