b8147dd9320f83ac86002777c293cfbf9ec7cfd3
Robert Oehmke <oehmke@ucar.edu>
2021-12-06 12:16:54 -0700

    Fix issue with missing argument.

--- src/Infrastructure/Mesh/src/ESMCI_Mesh_FileIO.C
+++ src/Infrastructure/Mesh/src/ESMCI_Mesh_FileIO.C
@@ -720,13 +720,12 @@ void ESMCI_mesh_create_from_file(char *filename,
 
 
     // Add elements
-    int regridConserve=1;
     ESMCI_meshaddelements(out_mesh,
                           &num_elems, elem_ids, numElementConn,
                           &elementMaskIA,
                           &areaPresent, elementArea,
                           &centerCoordsPresent, centerCoords, 
-                          &num_local_elem_conn, local_elem_conn, &regridConserve,
+                          &num_local_elem_conn, local_elem_conn, 
                           &coord_sys, &sdim, &localrc);
     if (ESMC_LogDefault.MsgFoundError(localrc, ESMCI_ERR_PASSTHRU, ESMC_CONTEXT,
                                       &localrc)) throw localrc;
