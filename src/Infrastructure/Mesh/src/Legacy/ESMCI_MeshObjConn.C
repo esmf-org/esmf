cfea0e5e8fb1c24a930d571f0480b2da4d64307b
Fei Liu <feiliu@narwhal07.navydsrc.hpc.mil>
2022-01-13 19:10:20 +0000

    Produce explicit template instantiation with LLVM backend as well

--- src/Infrastructure/Mesh/src/Legacy/ESMCI_MeshObjConn.C
+++ src/Infrastructure/Mesh/src/Legacy/ESMCI_MeshObjConn.C
@@ -703,7 +703,7 @@ template void face_info<>(MeshObj** node_begin,
                int *rotation, // out
                bool
                );
-#if __ICC==2021 && __INTEL_COMPILER_UPDATE>=3
+#if (__ICC==2021 && __INTEL_COMPILER_UPDATE>=3) || (__INTEL_LLVM_COMPILER >= 20210300)
 template void face_info<>(__gnu_cxx::__normal_iterator<ESMCI::MeshObj**, std::vector<ESMCI::MeshObj*, std::allocator<ESMCI::MeshObj*> > >, __gnu_cxx::__normal_iterator<ESMCI::MeshObj**, std::vector<ESMCI::MeshObj*, std::allocator<ESMCI::MeshObj*> > >, __gnu_cxx::__normal_iterator<ESMCI::MeshObj**, std::vector<ESMCI::MeshObj*, std::allocator<ESMCI::MeshObj*> > >, __gnu_cxx::__normal_iterator<ESMCI::MeshObj**, std::vector<ESMCI::MeshObj*, std::allocator<ESMCI::MeshObj*> > >, int*, int*, int*, bool);
 #endif
 
