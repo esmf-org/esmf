6ed324f6a22f5ec6df23bb3710431493a3067b8b
Gerhard Theurich <theurich@sourcespring.net>
2022-06-03 17:12:06 -0700

    Set versioning for 8.3.0 release.

--- src/Infrastructure/Util/src/ESMF_UtilTypes.F90
+++ src/Infrastructure/Util/src/ESMF_UtilTypes.F90
@@ -85,10 +85,10 @@
       integer, parameter :: ESMF_VERSION_MINOR        = 3
       integer, parameter :: ESMF_VERSION_REVISION     = 0
       integer, parameter :: ESMF_VERSION_PATCHLEVEL   = 0
-      logical, parameter :: ESMF_VERSION_PUBLIC       = .false.
-      logical, parameter :: ESMF_VERSION_BETASNAPSHOT = .true.
+      logical, parameter :: ESMF_VERSION_PUBLIC       = .true.
+      logical, parameter :: ESMF_VERSION_BETASNAPSHOT = .false.
 
-      character(*), parameter :: ESMF_VERSION_STRING  = "8.3.0 beta snapshot"
+      character(*), parameter :: ESMF_VERSION_STRING  = "8.3.0"
 
 #if defined (ESMF_NETCDF)
       logical, parameter :: ESMF_IO_NETCDF_PRESENT = .true.
