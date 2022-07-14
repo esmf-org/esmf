d798b950cabf5f5a6a25c3cc9b1a7392d9f81710
Robert Oehmke <oehmke@ucar.edu>
2022-07-14 15:30:19 -0600

    Fix XGrid creation to allow it to work with Meshes created with >4 sided elements.

--- src/Infrastructure/Mesh/include/Regridding/ESMCI_MeshRegrid.h
+++ src/Infrastructure/Mesh/include/Regridding/ESMCI_MeshRegrid.h
@@ -40,6 +40,11 @@ namespace ESMCI {
             int *unmappedaction,
             bool set_dst_status, WMat &dst_status, bool checkFlag);
 
+ void translate_split_src_elems_in_wts(Mesh *srcmesh, int num_entries,
+                                      int *iientries);
+ void translate_split_dst_elems_in_wts(Mesh *dstmesh, int num_entries,
+                                      int *iientries, double *factors);
+  
  int get_iwts(Mesh &mesh, MEField<> *iwts);
 
 } // namespace
