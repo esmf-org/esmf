cc821abd0720e46d857724b79a4bdeab70a4732e
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2022-09-08 14:11:29 -0700

    add file to test pytest functionality in question

---
+++ src/addon/ESMPy/src/esmpy/test/test_api/test_pytest.py
@@ -0,0 +1,28 @@
+"""
+pytest tests
+"""
+
+import pytest
+
+from esmpy.test.base import TestBase
+from esmpy.api.esmpymanager import pet_count
+
+class TestPyTest(TestBase):
+
+    @pytest.mark.xfail
+    @pytest.mark.skipif(pet_count()<3, reason="test must be run with more than 3 cores")
+    def test_pytest_2(self):
+
+        print ("Test2: I AM PET {}".format(self.mg.local_pet))
+        
+        if self.mg.local_pet == 2:
+            raise ValueError("Test failure on a single PET")
+
+    @pytest.mark.mpi(min_size=2)
+    def test_pytest_1(self):
+        from mpi4py import MPI
+
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+
+        print ("Test1: I AM PET {} and rank {}".format(local_pet(), rank))
