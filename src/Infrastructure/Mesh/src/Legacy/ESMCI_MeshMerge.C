0e3a9f832a4d82414020639ee0308c2c3f12a56d
Robert Oehmke <oehmke@ucar.edu>
2022-09-15 17:28:31 -0600

    Free search results that don't get deallocated when merging meshes during XGrid creation and running on 1 PET.

--- src/Infrastructure/Mesh/src/Legacy/ESMCI_MeshMerge.C
+++ src/Infrastructure/Mesh/src/Legacy/ESMCI_MeshMerge.C
@@ -212,16 +212,24 @@ void MeshMerge(Mesh &srcmesh, Mesh &dstmesh, Mesh **meshpp) {
   MPI_Allreduce(&nelem, &gnelem, 2, MPI_INT, MPI_SUM, Par::Comm());
 
   // if src and dst mesh are neigbors, just sew them together
-  if(gnelem[1] == 0) 
+  if(gnelem[1] == 0) {
     sew_meshes(srcmesh, dstmesh, meshmrg);
-  else
+  } else  {
     concat_meshes(srcmesh, dstmesh, meshmrg, *mesh_src, *mesh_dst, sres, &res_map);
+  }
 
   // clean up memory
-  if(interp) delete interp;
   interp_map_iter it = res_map.begin(), ie = res_map.end();
   for(; it != ie; ++it)
     delete it->second;
+
+  // Get rid of interp and search results
+  if(interp) {
+    delete interp; // This also gets rid of search results
+  } else {
+    // If no interp, then need to get rid of search results independantly
+    DestroySearchResult(sres);
+  }
 
 }
 
