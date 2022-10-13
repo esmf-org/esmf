0e03a729394e8749fe7a1ace7984a594ef72a280
Robert Oehmke <oehmke@ucar.edu>
2022-10-13 16:14:53 -0600

    Adding Pgon include file and start of implementation.

---
+++ src/Infrastructure/Mesh/include/ESMCI_Pgon.h
@@ -0,0 +1,61 @@
+// $Id$
+//
+// Earth System Modeling Framework
+// Copyright 2002-2022, University Corporation for Atmospheric Research,
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+// Laboratory, University of Michigan, National Centers for Environmental
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+//
+//==============================================================================
+
+//==============================================================================
+//
+// This file contains the Fortran interface code to link F90 and C++.
+//
+//------------------------------------------------------------------------------
+// INCLUDES
+//------------------------------------------------------------------------------
+
+#ifndef ESMCI_PGON_H
+#define ESMCI_PGON_H
+
+#include <vector>
+
+#include "ESMCI_Macros.h"
+#include "ESMCI_F90Interface.h"
+#include "ESMCI_LogErr.h"
+#include "ESMCI_VM.h"
+#include "ESMCI_CoordSys.h"
+#include "ESMCI_Array.h"
+#include "ESMCI_DistGrid.h"
+
+#include "Mesh/include/ESMCI_MathUtil.h"
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+// static const char *const version = "$Id$";
+//-----------------------------------------------------------------------------
+
+
+using namespace ESMCI;
+
+template <class GEOM>
+class Pgon {
+
+  int num_pnts;
+  double *pnt_coords;
+  
+  bool is_hole;
+
+public:
+  
+
+  
+};
+
+
+
+#endif // ESMCI_PGON_H
