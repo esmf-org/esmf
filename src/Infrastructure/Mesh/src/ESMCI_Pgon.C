f4c220ce70e942cc1a4a39f2654176c27cbb886f
Robert Oehmke <oehmke@ucar.edu>
2022-10-14 16:26:02 -0600

    Add source file for new polygon (Pgon) implementation.

---
+++ src/Infrastructure/Mesh/src/ESMCI_Pgon.C
@@ -0,0 +1,48 @@
+// $Id$
+//
+// Earth System Modeling Framework
+// Copyright 2002-2022, University Corporation for Atmospheric Research, 
+// Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+// Laboratory, University of Michigan, National Centers for Environmental 
+// Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+// NASA Goddard Space Flight Center.
+// Licensed under the University of Illinois-NCSA License.
+//
+//==============================================================================
+
+//==============================================================================
+//
+// This file contains the Fortran interface code to link F90 and C++.
+//
+//------------------------------------------------------------------------------
+// INCLUDES
+//------------------------------------------------------------------------------
+
+
+#include <limits>
+#include <string>
+#include <ostream>
+#include <iterator>
+#include <vector>
+
+#include "ESMCI_Macros.h"
+#include "ESMCI_F90Interface.h"
+#include "ESMCI_LogErr.h"
+#include "ESMCI_VM.h"
+#include "ESMCI_CoordSys.h"
+
+#include "VM/include/ESMC_VM.h"
+
+//-----------------------------------------------------------------------------
+ // leave the following line as-is; it will insert the cvs ident string
+ // into the object file for tracking purposes.
+// static const char *const version = "$Id$";
+//-----------------------------------------------------------------------------
+
+
+using namespace ESMCI;
+using namespace std;
+
+
+
+
