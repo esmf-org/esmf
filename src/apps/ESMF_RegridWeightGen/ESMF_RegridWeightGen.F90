7086ccf181908f564f0aed95b5a8cd29bd717eea
Bill Sacks <sacks@ucar.edu>
2023-02-16 15:31:12 -0700

    Add '(c)' following 'Copyright'
    On the ESMF core team call today, we decided to add this so that the
    copyright notices in each source file look the same as the main
    copyright notice in the LICENSE file.

--- src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
+++ src/apps/ESMF_RegridWeightGen/ESMF_RegridWeightGen.F90
@@ -2,7 +2,7 @@
 ! $Id$
 !
 ! Earth System Modeling Framework
-! Copyright 2002-2022, University Corporation for Atmospheric Research,
+! Copyright (c) 2002-2022, University Corporation for Atmospheric Research,
 ! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
 ! Laboratory, University of Michigan, National Centers for Environmental
 ! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
