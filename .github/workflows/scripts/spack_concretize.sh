85eb3d414b3f92ce7e6280d349fb9eb373a75093
uturuncoglu <turuncu@ucar.edu>
2023-02-27 16:14:36 -0700

    new test

--- .github/workflows/scripts/spack_concretize.sh
+++ .github/workflows/scripts/spack_concretize.sh
@@ -107,7 +107,7 @@ echo "    misc_cache: $install_dir/misc_cache" >> spack.yaml
 echo "    test_cache: $install_dir/test_cache" >> spack.yaml
 echo "    install_tree:" >> spack.yaml
 echo "      root: $install_dir/opt" >> spack.yaml
-echo "    install_missing_compilers: true"
+echo "    install_missing_compilers: true" >> spack.yaml
 cat spack.yaml
 echo "::endgroup::"
 
