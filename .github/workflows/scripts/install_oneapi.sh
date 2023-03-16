a6eae54c787a516e050f253f408073ff604df4ea
uturuncoglu <turuncu@ucar.edu>
2023-03-16 00:07:01 -0600

    test install

--- .github/workflows/scripts/install_oneapi.sh
+++ .github/workflows/scripts/install_oneapi.sh
@@ -11,7 +11,14 @@ done
 # print out arguments
 echo "Compiler: $comp"
 
+# exit if it is already installed
+if [ -d "/opt/intel/oneapi" ]; then
+  echo "/opt/intel/oneapi directory exists. Skip installing Intel oneAPI Compiler ..."
+  exit
+fi
+
 # download the key to system keyring
+echo "::group::Setup Intel oneAPI Repository"
 wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | \
   gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
 
@@ -20,9 +27,11 @@ echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] \
   https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 
 # update packages list and repository index
-sudo apt-get update
+sudo apt-get -qq update
+echo "::endgroup::"
 
 # get compiler version
+echo "::group::Check Specified Versiion"
 compiler_version=`echo "$comp" | awk -F\@ '{print $2}'`
 echo "Compiler Version: $compiler_version"
 
@@ -46,7 +55,16 @@ else
     exit 1
   fi
 fi
+echo "::endgroup::"
 
 # install
-sudo apt-get install intel-basekit-$compiler_version
-sudo apt-get install intel-hpckit-$compiler_version
+echo "::group::Install Intel oneAPI Base and HPC Kits"
+sudo apt-get install -qq intel-basekit-$compiler_version
+sudo apt-get install -qq intel-hpckit-$compiler_version
+echo "::endgroup::"
+
+# list content of the installation folders (for debugging)
+echo "::group::List Files in Installation Folder"
+ls -al /opt/intel/oneapi/compiler/
+ls -al /opt/intel/oneapi/mpi/
+echo "::endgroup::"
