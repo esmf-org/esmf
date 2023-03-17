070a65afdf7029cd3fb0deea3f2d52c9418c0514
Dan Rosen <daniel.rosen@noaa.gov>
2023-03-17 10:56:57 -0600

    Fix case sensitivity in ESMX_Data
    * clean up esmxGen.py

--- src/addon/ESMX/Driver/esmxGen.py
+++ src/addon/ESMX/Driver/esmxGen.py
@@ -33,59 +33,77 @@ def create_compList(_dict, odir):
           if type(v1) is not dict:
             v1 = {'build_type': 'auto'}
           f.write('# - auto-generated section for component: {}\n'.format(k1))
+
+          # determine component settings
           build_type = v1.get('build_type', 'auto')
           source_dir = v1.get('source_dir', k1)
-          if not source_dir.startswith('$'):
-            source_dir = os.path.abspath(source_dir)
+          if source_dir:
+            source_dir = source_dir.strip()
+            if not source_dir.startswith('$'):
+              source_dir = os.path.abspath(source_dir)
           cmake_config = v1.get('cmake_config', k1+'.cmake')
-          if (cmake_config == os.path.basename(cmake_config)):
-            install_prefix = v1.get('install_prefix', 'install')
-          else:
-            install_prefix = v1.get('install_prefix', os.path.dirname(cmake_config))
-          cmake_config = os.path.basename(cmake_config)
-          install_prefix = os.path.abspath(install_prefix)
+          install_prefix = v1.get('install_prefix', '')
+          if install_prefix:
+            install_prefix = install_prefix.strip()
+            if not install_prefix.startswith('$'):
+              install_prefix = os.path.abspath(install_prefix)
           config_dir = v1.get('config_dir', 'cmake')
-          if (os.path.isabs(config_dir)):
-            config_dir = os.path.basename(config_dir)
           library_dir = v1.get('library_dir', 'lib')
-          if (os.path.isabs(library_dir)):
-            library_dir = os.path.basename(library_dir)
           include_dir = v1.get('include_dir', 'include')
-          if (os.path.isabs(include_dir)):
-            include_dir = os.path.basename(include_dir)
           fort_module = v1.get('fort_module', (k1+'.mod').lower())
           libraries = v1.get('libraries', k1)
-          build_script = v1.get('build_script', "compile")
-          build_args = v1.get('build_args', None)
-          link_libraries = v1.get('link_libraries', "")
-          link_paths = v1.get('link_paths', "")
-          git_repository = v1.get('git_repository', "")
-          git_tag = v1.get('git_tag', "")
-          git_dir = v1.get('git_dir', source_dir)
-          if not git_dir.startswith('$'):
-            git_dir = os.path.abspath(git_dir)
-          test_dir = v1.get('test_dir', "")
+          build_script = v1.get('build_script', 'build.sh')
+          build_args = v1.get('build_args', '')
+          link_libraries = v1.get('link_libraries', '')
+          link_paths = v1.get('link_paths', '')
+          git_repository = v1.get('git_repository', '')
+          git_tag = v1.get('git_tag', '')
+          git_dir = v1.get('git_dir', '')
+          if git_dir:
+            git_dir = git_dir.strip()
+            if not git_dir.startswith('$'):
+              git_dir = os.path.abspath(git_dir)
+          test_dir = v1.get('test_dir', '')
           if test_dir:
+            test_dir = test_dir.strip()
             if not test_dir.startswith('$'):
               test_dir = os.path.abspath(test_dir)
-          f.write('set({}-BUILD_TYPE     {})\n'.format(k1, build_type))
-          f.write('set({}-SOURCE_DIR     {})\n'.format(k1, source_dir))
-          f.write('set({}-CMAKE_CONFIG   {})\n'.format(k1, cmake_config))
-          f.write('set({}-INSTALL_PREFIX {})\n'.format(k1, install_prefix))
-          f.write('set({}-LIBRARY_DIR    {})\n'.format(k1, library_dir))
-          f.write('set({}-CONFIG_DIR     {})\n'.format(k1, config_dir))
-          f.write('set({}-INCLUDE_DIR    {})\n'.format(k1, include_dir))
-          f.write('set({}-FORT_MODULE    {})\n'.format(k1, fort_module))
-          f.write('set({}-LIBRARIES      {})\n'.format(k1, libraries))
-          f.write('set({}-BUILD_SCRIPT   {})\n'.format(k1, build_script))
+
+          # write component settings to file
+          if (build_type):
+            f.write('set({}-BUILD_TYPE     {})\n'.format(k1, build_type))
+          if (source_dir):
+            f.write('set({}-SOURCE_DIR     {})\n'.format(k1, source_dir))
+          if (cmake_config):
+            f.write('set({}-CMAKE_CONFIG   {})\n'.format(k1, cmake_config))
+          if (install_prefix):
+            f.write('set({}-INSTALL_PREFIX {})\n'.format(k1, install_prefix))
+          if (library_dir):
+            f.write('set({}-LIBRARY_DIR    {})\n'.format(k1, library_dir))
+          if (config_dir):
+            f.write('set({}-CONFIG_DIR     {})\n'.format(k1, config_dir))
+          if (include_dir):
+            f.write('set({}-INCLUDE_DIR    {})\n'.format(k1, include_dir))
+          if (fort_module):
+            f.write('set({}-FORT_MODULE    {})\n'.format(k1, fort_module))
+          if (libraries):
+            f.write('set({}-LIBRARIES      {})\n'.format(k1, libraries))
+          if (build_script):
+            f.write('set({}-BUILD_SCRIPT   {})\n'.format(k1, build_script))
           if (build_args):
             f.write('set({}-BUILD_ARGS     {})\n'.format(k1, build_args))
-          f.write('set({}-GIT_REPOSITORY {})\n'.format(k1, git_repository))
-          f.write('set({}-GIT_TAG        {})\n'.format(k1, git_tag))
-          f.write('set({}-GIT_DIR        {})\n'.format(k1, git_dir))
-          f.write('set({}-LINK_LIBRARIES {})\n'.format(k1, link_libraries))
-          f.write('set({}-LINK_PATHS     {})\n'.format(k1, link_paths))
-          f.write('set({}-TEST_DIR       {})\n'.format(k1, test_dir))
+          if (git_repository):
+            f.write('set({}-GIT_REPOSITORY {})\n'.format(k1, git_repository))
+          if (git_tag):
+            f.write('set({}-GIT_TAG        {})\n'.format(k1, git_tag))
+          if (git_dir):
+            f.write('set({}-GIT_DIR        {})\n'.format(k1, git_dir))
+          if (link_libraries):
+            f.write('set({}-LINK_LIBRARIES {})\n'.format(k1, link_libraries))
+          if (link_paths):
+            f.write('set({}-LINK_PATHS     {})\n'.format(k1, link_paths))
+          if (test_dir):
+            f.write('set({}-TEST_DIR       {})\n'.format(k1, test_dir))
 
 def create_compUse(_dict, odir):
     # open file
@@ -140,6 +158,8 @@ def main(argv):
     if ('ESMX_Data' not in dict_drv['components'] and
         'esmx_data' not in disable_esmx_comps):
       dict_drv['components'].update({'ESMX_Data':{}})
+      dict_drv['components'].get('ESMX_Data').update(
+        {'build_type':'cmake'})
       dict_drv['components'].get('ESMX_Data').update(
         {'source_dir':'${ESMF_ESMXDIR}/Comps/ESMX_Data'})
       dict_drv['components'].get('ESMX_Data').update(
