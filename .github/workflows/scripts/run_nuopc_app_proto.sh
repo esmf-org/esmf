01008d28e75fc2ce85a67fbff3138d37d792baa9
uturuncoglu <turuncu@ucar.edu>
2023-05-12 14:10:59 -0600

    try to backtrace

--- .github/workflows/scripts/run_nuopc_app_proto.sh
+++ .github/workflows/scripts/run_nuopc_app_proto.sh
@@ -86,22 +86,22 @@ sleep 120
 # attach gdb to processes
 
 echo "#!/bin/bash" > trace_cmd.sh
-echo "pid_lst=`ps -ef | grep $2 | awk '{print $2}'`" >> trace_cmd.sh
-echo "for i in $pid_lst" >> trace_cmd.sh
+echo "pid_lst=\`ps -ef | grep \$2 | awk '{print \$2}'\`" >> trace_cmd.sh
+echo "for i in \$pid_lst" >> trace_cmd.sh
 echo "do" >> trace_cmd.sh
-echo "  prefix=\"${3}_${i}\"" >> trace_cmd.sh
-echo "  CONFFILE=\"$1/bt-${prefix}.conf\"" >> trace_cmd.sh
-echo "  echo \"set pagination off\" >\"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"set logging file $1/bt-${prefix}.txt\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"set logging overwrite on\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"set logging redirect on\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"set logging on\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"attach $i\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"bt\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"detach\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"set logging off\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  echo \"quit\" >> \"$CONFFILE\"" >> trace_cmd.sh
-echo "  gdb --batch -x \"$CONFFILE\" 2>/dev/null" >> trace_cmd.sh
+echo "  prefix=\"\${3}_\${i}\"" >> trace_cmd.sh
+echo "  CONFFILE=\"$1/bt-\${prefix}.conf\"" >> trace_cmd.sh
+echo "  echo \"set pagination off\" >\"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"set logging file $1/bt-\${prefix}.txt\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"set logging overwrite on\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"set logging redirect on\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"set logging on\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"attach $i\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"bt\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"detach\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"set logging off\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  echo \"quit\" >> \"\$CONFFILE\"" >> trace_cmd.sh
+echo "  gdb --batch -x \"\$CONFFILE\" 2>/dev/null" >> trace_cmd.sh
 echo "done" >> trace_cmd.sh
 
 chmod 755 trace_cmd.sh
