a9c7518c884b8aff7820aba6a2c8a309a9d598af
uturuncoglu <turuncu@ucar.edu>
2023-05-13 20:24:18 -0600

    clean code

--- .github/workflows/scripts/run_nuopc_app_proto.sh
+++ .github/workflows/scripts/run_nuopc_app_proto.sh
@@ -46,7 +46,7 @@ if [[ "$comp" == *"oneapi"* ]]; then
 else
   export PATH=$spack_install_dir/view/bin:$PATH
   # set it for OpenMPI to fix not enough slots issue
-  export TOOLRUN="--oversubscribe"
+  export TOOLRUN="--oversubscribe --mca fcoll dynamic_gen2"
   MPI_FC=mpif90
 fi
 which mpirun
