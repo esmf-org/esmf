b67af8a4ba5f3c89efc90c6788e714da8093c76a
Daniel Rosen <drosen@ucar.edu>
2024-08-28 13:26:56 -0600

    Fix esmx module paths and package linking
    * fix bug in variable name for ESMX_LINK_MODULE_PATHS
    * add capability to find packages before expanding variables
    * add capability to provide a full path to link library
    * fix documentation for link_packages

--- src/addon/ESMX/Driver/esmx_app_config.py
+++ src/addon/ESMX/Driver/esmx_app_config.py
@@ -3,13 +3,31 @@ import sys
 import argparse
 from esmx_tools import *
 
+def create_modConf(appCfg: ESMXAppCfg, odir):
+    options = [ESMXOpt('link_module_paths', None, dir),
+               ESMXOpt('link_packages', None, str)]
+    # open file
+    with open(os.path.join(odir, 'modConf.txt'), 'w') as f:
+        for opt in options:
+            val = appCfg.get(opt.option, opt.default)
+            if (val):
+                val = str(val)
+                val = val.replace(" ", ";")
+                val = val.replace(",", ";")
+                if (opt.ctype == dir):
+                    dirs = list(val.split(";"))
+                    for i in range(len(dirs)):
+                        dirs[i] = dirs[i].strip()
+                        if not dirs[i].startswith('$'):
+                            dirs[i] = os.path.abspath(dirs[i])
+                    val = ';'.join(dirs)
+                f.write('set(ESMX_{} {})\n'.format(opt.upper(), val))
+
 def create_appConf(appCfg: ESMXAppCfg, odir):
     options = [ESMXOpt('exe_name', None, str),
                ESMXOpt('disable_comps', None, str),
-               ESMXOpt('link_module_paths', None, dir),
                ESMXOpt('link_paths', None, dir),
                ESMXOpt('link_libraries', None, str),
-               ESMXOpt('link_packages', None, str),
                ESMXOpt('build_args', None, str),
                ESMXOpt('build_jobs', None, str),
                ESMXOpt('build_verbose', None, str),
@@ -58,6 +76,9 @@ def main(argv):
 
     # read app configuration yaml file
     appCfg = ESMXAppCfg(ifile)
+
+    # create modConf.txt for CMake
+    create_modConf(appCfg, odir)
 
     # create appConf.txt for CMake
     create_appConf(appCfg, odir)
