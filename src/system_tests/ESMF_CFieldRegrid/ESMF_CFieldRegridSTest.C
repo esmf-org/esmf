b71e5dae81c925cf67eba8399109b1ddd1e27e2a
Robert Oehmke <oehmke@ucar.edu>
2024-09-03 15:00:14 -0600

    Tweak new system test a bit.

--- src/system_tests/ESMF_CFieldRegrid/ESMF_CFieldRegridSTest.C
+++ src/system_tests/ESMF_CFieldRegrid/ESMF_CFieldRegridSTest.C
@@ -8,10 +8,11 @@
 // //DESCRIPTION:
 // System test CFieldRegrid
 // This system test is a basic check of the C-API when used for regridding.
-// It creates two Grids, two Field on the Grids, and then does regridding
-// between them. After the regridding, it does a simple check that the
-// destination values resulting from the regridding seem reasonable.
-// This test also checks the ESMC_Initialize() and ESMC_Finalize() calls.
+// It creates two Grids, two Field on the two Grids, and then does bilinear
+// regridding between the Fields. After the regridding, it does a simple check
+// that the destination values resulting from the regridding seem reasonable.
+// This test also checks the ESMC_Initialize(), ESMC_Finalize(), and
+// ESMC_FinalizeWithFlag() calls.
 //-------------------------------------------------------------------------
 //\begin{verbatim}
 
@@ -27,10 +28,9 @@
 
 int main(void) {
 #define ESMC_METHOD "CFieldRegridSTest"  
-#define MAXSTRLEN 80
-  char name[MAXSTRLEN];
-  char failMsg[MAXSTRLEN];
-  char msg[MAXSTRLEN];
+  char name[ESMF_MAXSTR];
+  char failMsg[ESMF_MAXSTR];
+  char msg[ESMF_MAXSTR];
   int result=0;
   int localrc, finalrc;
   int localPet, petCount;
@@ -78,7 +78,7 @@ int main(void) {
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
   
   
-  // Get source coordinates
+  // Get source coordinate memory
   int srcExLBnd[2], srcExUBnd[2];
   double *srcLon=(double *)ESMC_GridGetCoord(srcGrid, 1, ESMC_STAGGERLOC_CENTER, NULL,
                                           srcExLBnd, srcExUBnd, &localrc);
@@ -89,7 +89,7 @@ int main(void) {
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
   
-  // Get Field data
+  // Get Field data memory
   double *srcData=(double *)ESMC_FieldGetPtr(srcField, 0, &localrc);
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
@@ -133,7 +133,7 @@ int main(void) {
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
   
-  // Get destination coords
+  // Get destination coordinate memory
   int dstExLBnd[2], dstExUBnd[2];
   double *dstLon=(double *)ESMC_GridGetCoord(dstGrid, 1, ESMC_STAGGERLOC_CENTER, NULL,
                                           dstExLBnd, dstExUBnd, &localrc);
@@ -144,7 +144,7 @@ int main(void) {
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
   
-  // Get Field data
+  // Get destination Field data memory
   double *dstData=(double *)ESMC_FieldGetPtr(dstField, 0, &localrc);
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
@@ -184,6 +184,7 @@ int main(void) {
   localrc = ESMC_FieldRegridRelease(&routehandle);
   if (localrc != ESMF_SUCCESS) ESMC_FinalizeWithFlag(ESMC_END_ABORT);
 
+  
   // Check output
   bool correct=true;
   for (int p=0,i1=dstExLBnd[1]; i1<=dstExUBnd[1]; i1++) {
@@ -218,15 +219,15 @@ int main(void) {
 
   // Write standard system test output.
   // (On Fortran side this is written by ESMF_STest, but that doesn't exist in C.) 
-  snprintf(msg,MAXSTRLEN,"NUMBER_OF_PROCESSORS %d\n",petCount);
+  snprintf(msg,ESMF_MAXSTR,"NUMBER_OF_PROCESSORS %d\n",petCount);
   ESMC_LogWrite(msg, ESMC_LOGMSG_INFO);
 
   // Check that test results were correct
   ESMC_Test(correct, name, failMsg, &result, __FILE__, __LINE__, 0);
   
   // NOTE: 
-  // There's kind of a chicken and egg problem here where when you run finalize
-  // it gets rid of the log output, but then you can't report if finalize
+  // There's kind of a chicken and egg problem in testing ESMF finalize where when you run
+  // finalize it gets rid of the log file output, but then you can't report if finalize
   // succeeded. However, at least it gets run here and outputs an error to
   // standard out so we can see a failure with manual testing.
   // Also, I'm testing both interfaces here because we don't have
