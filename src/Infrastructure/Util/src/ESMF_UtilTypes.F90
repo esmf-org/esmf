c5a1114b171c552caac4213492079b83003a6882
Gerhard Theurich <theurich@sourcespring.net>
2025-01-08 11:25:52 -0800

    Fix version string.

--- src/Infrastructure/Util/src/ESMF_UtilTypes.F90
+++ src/Infrastructure/Util/src/ESMF_UtilTypes.F90
@@ -88,7 +88,7 @@
       logical, parameter :: ESMF_VERSION_PUBLIC       = .true.
       logical, parameter :: ESMF_VERSION_BETASNAPSHOT = .false.
 
-      character(*), parameter :: ESMF_VERSION_STRING  = "8.8.0
+      character(*), parameter :: ESMF_VERSION_STRING  = "8.8.0"
 
 #if defined (ESMF_NETCDF)
       logical, parameter :: ESMF_IO_NETCDF_PRESENT = .true.
