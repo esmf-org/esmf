5dcb04222d7fb718d966e8df2283befec7cae0f3
Benjamin Auer <benjamin.m.auer@nasa.gov>
2025-03-24 14:56:23 -0400

    update tests, still work in progress

--- src/Infrastructure/Field/tests/ESMC_FieldGridRegridCsrvUTest.c
+++ src/Infrastructure/Field/tests/ESMC_FieldGridRegridCsrvUTest.c
@@ -499,12 +499,12 @@ int main(void){
 #ifdef masking
   rc = ESMC_FieldRegridStore(srcfield, dstfield, &i_maskValues, NULL, &routehandle, 
                              &regridmethod, NULL, NULL, NULL, NULL, 
-                             NULL, NULL, NULL, NULL, &unmappedaction, NULL, NULL, NULL,
+                             NULL, NULL, NULL, NULL, &unmappedaction, NULL, NULL, NULL, NULL,
                              NULL, &srcFracField, &dstFracField);
 #else
   rc = ESMC_FieldRegridStore(srcfield, dstfield, NULL, NULL, &routehandle, 
                              &regridmethod, NULL, NULL, NULL, NULL, NULL, NULL,
-                             NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+                             NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
                              &srcFracField, &dstFracField);
 #endif
   ESMC_Test((rc==ESMF_SUCCESS), name, failMsg, &result, __FILE__, __LINE__, 0);
@@ -514,7 +514,7 @@ int main(void){
   //EX_UTest
   strcpy(name, "Execute ESMC_FieldRegrid()");
   strcpy(failMsg, "Did not return ESMF_SUCCESS");
-  rc = ESMC_FieldRegrid(srcfield, dstfield, routehandle, NULL);
+  rc = ESMC_FieldRegrid(srcfield, dstfield, routehandle, NULL, NULL);
   ESMC_Test((rc==ESMF_SUCCESS), name, failMsg, &result, __FILE__, __LINE__, 0);
   //----------------------------------------------------------------------------
 
