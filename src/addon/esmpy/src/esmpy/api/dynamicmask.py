37b4935b4377d109249e0a2239a0c44f2d357f23
Benjamin Auer <benjamin.m.auer@nasa.gov>
2025-04-09 16:43:01 -0400

    add dynamic mask class

---
+++ src/addon/esmpy/src/esmpy/api/dynamicmask.py
@@ -0,0 +1,29 @@
+# $Id$
+
+"""
+The DynamicMask API
+"""
+from esmpy.api import constants
+from esmpy.api.esmpymanager import *
+
+class DynamicMask(object):
+
+    @initialize
+    def __init__(self, mask_type=None, handle_all_elements=None, src_mask_value=None, dst_mask_value=None):
+     
+        self._struct = ESMP_DynamicMaskPredefinedSetR8R8R8(masktype=mask_type, 
+                            handleAllElements=handle_all_elements, 
+                            srcMaskValue=src_mask_value,
+                            dstMaskValue=dst_mask_value)
+        import atexit; atexit.register(self.__del__)
+        self._finalized = False
+
+    @property
+    def struct(self):
+        return self._struct
+
+    def __del__(self):
+        self.destroy()
+
+    def destroy(self):
+        self._finalized = True
